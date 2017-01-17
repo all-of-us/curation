509619c1dceb5e9d11f61ec44e9f66339531e34b
Mark Velez <mav7014@nyp.org>
2017-01-17 15:16:17 -0500

    Move resources to `docs/_data` so it can be shared by reporter and documentation

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@ import inspect
 import os
 
 base_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-resource_path = os.path.join(base_path, 'resources')
+resource_path = os.path.join(base_path, 'docs', '_data')
 example_path = os.path.join(base_path, 'examples')
 cdm_csv_path = os.path.join(resource_path, 'cdm.csv')
 hpo_csv_path = os.path.join(resource_path, 'hpo.csv')
