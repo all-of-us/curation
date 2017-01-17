c5b19e9d23d384f6dbe82f0b6f05a6363e5e8f52
Mark Velez <mav7014@nyp.org>
2017-01-17 15:54:39 -0500

    Only hpo.csv is shared by reporter and docs (unresolved build failures after moving all resources to _data)

--- resources.py
+++ resources.py
@@ -2,8 +2,9 @@ import inspect
 import os
 
 base_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+data_path = os.path.join(base_path, 'docs', '_data')
 resource_path = os.path.join(base_path, 'resources')
 example_path = os.path.join(base_path, 'examples')
 cdm_csv_path = os.path.join(resource_path, 'cdm.csv')
-hpo_csv_path = os.path.join(resource_path, 'hpo.csv')
+hpo_csv_path = os.path.join(data_path, 'hpo.csv')
 pmi_tables_csv_path = os.path.join(resource_path, 'pmi_tables.csv')
