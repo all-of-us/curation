9984082c0a640cd017107844ca8a1696f05de514
Mark Velez <mav7014@nyp.org>
2017-08-17 17:24:47 -0400

    Format tasks files and remove dead code.

--- report/tasks.py
+++ report/tasks.py
@@ -1,137 +1,27 @@
 #!/usr/bin/env python
 
-# author: Aahlad
-# date: Aug 16, 2017
-
 """
     tasks script for cron jobs
 """
 
-# [START imports]
 from __future__ import absolute_import
 
-# util imports 
-import os
-import uuid
-import sys
+# util imports
 import json
-import logging
+import os
 import unicodedata
-
-# app requirements
-from flask import Flask, url_for, render_template
-from flask_flatpages import FlatPages
-import jinja2
 
 # gcloud imports
 import cloudstorage
-from cloudstorage import cloudstorage_api
-from google.appengine.api import app_identity
-
+# app requirements
+from flask import Flask
+from flask_flatpages import FlatPages
+import jinja2
 import api_util
 
-# [END imports]
-# setting the environment variable and extra imports
-
-PROJECTID = 'bamboo-creek-172917'
 _DRC_SHARED_BUCKET = 'aou-drc-shared'
-gcs_path_prepend = os.path.dirname(os.path.realpath(__file__))
-
-if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-    # print "setting environment"
-    dev_flag = False
-    gcs_path_prepend = "gs://"
-
-# [START bucket info]
-def bucket_info():
-    bucket_name = os.environ.get(
-        'BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
-
-    logging.info('Using bucket name: \n\n'.format(bucket_name))
-
-    bucket = '/' + bucket_name
-    filename = bucket + '/tester.csv'
-
-    self.list_bucket_directory_mode(bucket)
-    sefl.response.write()
-
-# [END bucket info]
-
-# [START write]
-def create_file(self, filename):
-    """Create a file."""
-
-    self.response.write('Creating file {}\n'.format(filename))
-
-    # The retry_params specified in the open call will override the default
-    # retry params for this particular file handle.
-    write_retry_params = cloudstorage.RetryParams(backoff_factor=1.1)
-    with cloudstorage.open(
-        filename, 'w', content_type='text/plain', options={
-            'x-goog-meta-foo': 'foo', 'x-goog-meta-bar': 'bar'},
-            retry_params=write_retry_params) as cloudstorage_file:
-                cloudstorage_file.write('abcde\n')
-                cloudstorage_file.write(('f'*1024+',')*4 + '\n')
-    self.tmp_filenames_to_clean_up.append(filename)
-
-# [END write]
-
-# [START read]
-def read_file(self, filename):
-    self.response.write(
-        'Abbreviated file content (first line and last 1K):\n')
-
-    with cloudstorage.open(filename) as cloudstorage_file:
-        self.response.write(cloudstorage_file.readline())
-        cloudstorage_file.seek(-1024, os.SEEK_END)
-        self.response.write(cloudstorage_file.read())
-# [END read]
-
-def stat_file(self, filename):
-    self.response.write('File stat:\n')
-    stat = cloudstorage.stat(filename)
-    self.response.write(repr(stat))
-
-def create_files_for_list_bucket(self, bucket):
-    self.response.write('Creating more files for listbucket...\n')
-    filenames = [bucket + n for n in [
-        '/foo1', '/foo2', '/bar', '/bar/1', '/bar/2', '/boo/']]
-    for f in filenames:
-        self.create_file(f)
-
-def list_bucket_directory_mode(self, bucket):
-    self.response.write('Listbucket directory mode result:\n')
-    for stat in cloudstorage.listbucket(bucket + '/', delimiter='/'):
-        self.response.write(stat)
-        self.response.write('\n')
-        if stat.is_dir:
-            for subdir_file in cloudstorage.listbucket(
-                    stat.filename, delimiter='/'):
-                self.response.write('  {}'.format(subdir_file))
-                self.response.write('\n')
-
-# [START delete_files]
-def delete_files(self):
-    self.response.write('Deleting files...\n')
-    for filename in self.tmp_filenames_to_clean_up:
-        self.response.write('Deleting file {}\n'.format(filename))
-        try:
-            cloudstorage.delete(filename)
-        except cloudstorage.NotFoundError:
-            pass
-# [END delete_files]
-
-
-
-
-"""
-START HERE
-
-"""
-
-
 PREFIX = '/tasks/'
-SITE_ROOT =  os.path.dirname(os.path.abspath(__file__))
+SITE_ROOT = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -140,30 +30,26 @@ FLATPAGES_ROOT = SITE_ROOT + '/pages/'
 
 LOG_FILE = SITE_ROOT + '/log.json'
 
-app = Flask(__name__, template_folder= SITE_ROOT + '/templates')
+app = Flask(__name__, template_folder=SITE_ROOT + '/templates')
 app.config.from_object(__name__)
 pages = FlatPages(app)
-j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(SITE_ROOT + '/templates/'),  trim_blocks=True)
-
+j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(SITE_ROOT + '/templates/'), trim_blocks=True)
 
 
 # @app.route(PREFIX + '<string:path>.html')
-def _page(path): 
+def _page(path):
     data = None
-    page = pages.get_or_404(path) 
+    page = pages.get_or_404(path)
     html_to_use = 'page.html'
 
-    if page.meta.get('usehtml',None ) is not None:
+    if page.meta.get('usehtml', None) is not None:
         data = json.load(open(LOG_FILE))
         html_to_use = 'report.html'
-        
-    # this is pure html content. can be exported.
-    
-    html= j2_env.get_template(html_to_use).render(page = page, pages=pages)
-    # with app.app_context():
-    #    context = {'page':page,'pages':pages}
-    #    html = render_template(html_to_use, **context)
+
+    # this is pure html content. can be exported
+    html = j2_env.get_template(html_to_use).render(page=page, pages=pages, logs=data)
     return html
+
 
 def _create_file(filename, content):
     """
@@ -176,16 +62,14 @@ def _create_file(filename, content):
     # The retry_params specified in the open call will override the default
     # retry params for this particular file handle.
     write_retry_params = cloudstorage.RetryParams(backoff_factor=1.1)
-    with cloudstorage.open( filename, 'w', 
-            content_type='text/html', 
-            options={ 'x-goog-meta-foo': 'foo', 'x-goog-meta-bar': 'bar'},
-            retry_params=write_retry_params) as cloudstorage_file:
-        # cloudstorage_file.write('<!doctype html>')
+    with cloudstorage.open(filename, 'w',
+                           content_type='text/html',
+                           retry_params=write_retry_params) as cloudstorage_file:
         cloudstorage_file.write(content)
 
 
 @api_util.auth_required_cron
-def _generate_site(bucket = _DRC_SHARED_BUCKET):
+def _generate_site(bucket=_DRC_SHARED_BUCKET):
     """
     Construct html pages for each report, data_model, file_transfer and index.
 
@@ -193,15 +77,16 @@ def _generate_site(bucket = _DRC_SHARED_BUCKET):
     :return: returns 'okay' if succesful. logs critical error otherwise.
     """
 
-    for endpoint in ['report','data_model','index', 'file_transfer_procedures']:
+    for endpoint in ['report', 'data_model', 'index', 'file_transfer_procedures']:
         # generate the page
-        html = _page(endpoint) 
-        html = unicodedata.normalize('NFKD', html).encode('ascii','ignore')
+        html = _page(endpoint)
+        html = unicodedata.normalize('NFKD', html).encode('ascii', 'ignore')
 
         # write it to the drc shared bucket
-        _create_file('/{}/{}.html'.format(bucket, endpoint), html) 
+        _create_file('/{}/{}.html'.format(bucket, endpoint), html)
 
     return 'okay'
+
 
 app.add_url_rule(
     PREFIX + 'sitegen',
--- report/test/unit_test/tasks_test.py
+++ report/test/unit_test/tasks_test.py
@@ -2,14 +2,15 @@ import unittest
 from google.appengine.ext import testbed
 import mock
 
-from cloudstorage import cloudstorage_api  # stubbed by testbed
-from report.tasks import _generate_site
+import cloudstorage  # stubbed by testbed
+import tasks
 
 _FAKE_DRC_SHARE_BUCKET = 'fake-drc'
 
+
 class TasksTest(unittest.TestCase):
     def setUp(self):
-        super(ReportTest, self).setUp()
+        super(TasksTest, self).setUp()
         self.testbed = testbed.Testbed()
         self.testbed.activate()
         self.testbed.init_app_identity_stub()
@@ -20,26 +21,22 @@ class TasksTest(unittest.TestCase):
 
     @mock.patch('report.api_util.check_cron')
     def test_report_check_cron(self, mock_check_cron):
-        output = _generate_site()
+        output = tasks._generate_site()
         self.assertEquals(mock_check_cron.call_count, 1)
 
     def test_site_generation(self):
         # generating site
-        self.assertEquals(_generate_site(_FAKE_DRC_SHARE_BUCKET),'okay')
+        self.assertEquals(tasks._generate_site(_FAKE_DRC_SHARE_BUCKET), 'okay')
 
         # verify that page worked
         bucket = _FAKE_DRC_SHARE_BUCKET
         file_count = 0
         for stat in cloudstorage.listbucket(bucket + '/', delimiter='/'):
-            assert(not stat.is_dir)
-            assert(stat.filename in \
-                    [name + '.html' for name in ['report','data_model','file_transfer','index']])
+            assert (not stat.is_dir)
+            assert (stat.filename in
+                    [name + '.html' for name in ['report', 'data_model', 'file_transfer_procedures', 'index']])
             file_count += 1
-        self.assertEquals(file_count,4)
-
-    def _write_cloud_csv(self, file_name, contents_str):
-        with cloudstorage_api.open('/%s/%s' % (_FAKE_BUCKET, file_name), mode='w') as cloud_file:
-            cloud_file.write(contents_str.encode('utf-8'))
+        self.assertEquals(file_count, 4)
 
     def tearDown(self):
         self.testbed.deactivate()
