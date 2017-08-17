4d696b6915f8ab83cc09619c553b864c952eca84
Mark Velez <mav7014@nyp.org>
2017-08-17 17:48:27 -0400

    Supply request context in TasksTest and fix site generation test

--- report/test/unit_test/tasks_test.py
+++ report/test/unit_test/tasks_test.py
@@ -19,24 +19,28 @@ class TasksTest(unittest.TestCase):
         self.testbed.init_blobstore_stub()
         self.testbed.init_datastore_v3_stub()
 
-    @mock.patch('report.api_util.check_cron')
+    @mock.patch('api_util.check_cron')
     def test_report_check_cron(self, mock_check_cron):
-        output = tasks._generate_site()
-        self.assertEquals(mock_check_cron.call_count, 1)
+        with tasks.app.test_request_context():
+            tasks._generate_site()
+            self.assertEquals(mock_check_cron.call_count, 1)
 
-    def test_site_generation(self):
-        # generating site
-        self.assertEquals(tasks._generate_site(_FAKE_DRC_SHARE_BUCKET), 'okay')
+    @mock.patch('api_util.check_cron')
+    def test_site_generation(self, mock_check_cron):
+        with tasks.app.test_request_context():
+            result = tasks._generate_site(_FAKE_DRC_SHARE_BUCKET)
+            self.assertEquals(result, 'okay')
 
-        # verify that page worked
-        bucket = _FAKE_DRC_SHARE_BUCKET
-        file_count = 0
-        for stat in cloudstorage.listbucket(bucket + '/', delimiter='/'):
-            assert (not stat.is_dir)
-            assert (stat.filename in
-                    [name + '.html' for name in ['report', 'data_model', 'file_transfer_procedures', 'index']])
-            file_count += 1
-        self.assertEquals(file_count, 4)
+            # verify that page worked
+            bucket = '/' + _FAKE_DRC_SHARE_BUCKET
+            file_count = 0
+            for stat in cloudstorage.listbucket(bucket, delimiter='/'):
+                assert (not stat.is_dir)
+                filename = stat.filename.split('/')[-1]
+                assert (filename in
+                        [name + '.html' for name in ['report', 'data_model', 'file_transfer_procedures', 'index']])
+                file_count += 1
+            self.assertEquals(file_count, 4)
 
     def tearDown(self):
         self.testbed.deactivate()
