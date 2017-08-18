35d9839b16d004cbff1c91415300acd0c14a800f
aahladmanas <apm470@nyu.edu>
2017-08-18 16:03:19 -0400

    variable population for jinja. it works.

--- data_steward/spec/pages/index.md
+++ data_steward/spec/pages/index.md
@@ -1,4 +1,6 @@
 title: Home
+foo: foo
+bar: lolstillfoo
 
 # Introduction
 
--- data_steward/test/unit_test/spec_test.py
+++ data_steward/test/unit_test/spec_test.py
@@ -4,9 +4,15 @@ import cloudstorage  # stubbed by testbed
 import mock
 from google.appengine.ext import testbed
 
+import jinja2
+import unicodedata
 from spec import main
 
 _FAKE_DRC_SHARE_BUCKET = 'fake-drc'
+
+# move to some utils function
+def unicode_to_str(string):
+    return unicodedata.normalize('NFKD', string).encode('ascii','ignore')
 
 
 class SpecTest(unittest.TestCase):
@@ -42,6 +48,22 @@ class SpecTest(unittest.TestCase):
                         [name + '.html' for name in ['report', 'data_model', 'file_transfer_procedures', 'index']])
                 file_count += 1
             self.assertEquals(file_count, 4)
+    
+    @mock.patch('api_util.check_cron')
+    def test_variable_population(self, mock_check_cron):
+
+        pages = main.pages
+        page =  pages.get('index')
+
+        self.assertEquals(page.meta['title'], 'Home')
+        self.assertEquals(page.meta['foo'], 'foo')
+        self.assertEquals(page.meta['bar'], 'lolstillfoo')
+        assert(page.meta.get('usehtml',None) is None)
+
+        for page in pages:
+            template = "{{page.path}},{{page.title}}"
+            out = jinja2.Template(template).render(page=page)
+            self.assertEquals(unicode_to_str(out), "{},{}".format(page.path, page.meta['title']))
 
     def tearDown(self):
         self.testbed.deactivate()
