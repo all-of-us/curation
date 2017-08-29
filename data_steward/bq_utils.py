6c265166054b5538474bf7928050236ab88cae8d
aahladmanas <apm470@nyu.edu>
2017-08-29 15:25:24 -0400

    bad bq table name test now passes.

--- data_steward/bq_utils.py
+++ data_steward/bq_utils.py
@@ -36,7 +36,8 @@ def load_table_from_bucket(hpo_id, cdm_table_name):
     :param cdm_table_name: name of the CDM table
     :return: an object describing the associated bigquery job
     """
-    assert (cdm_table_name in common.CDM_TABLES)
+    if cdm_table_name not in common.CDM_TABLES:
+        raise ValueError
     app_id = app_identity.get_application_id()
     dataset_id = get_dataset_id()
     bq_service = build('bigquery', 'v2')
