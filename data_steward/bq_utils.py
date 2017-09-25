50826ae100b81e7f6e924ab570c7ff224103a914
aahladmanas <apm470@nyu.edu>
2017-09-25 17:19:46 -0400

    change recommneds as in PR#14
    the following changes have been made
     - query_table now has docstring
     - cleaned up copy job body

--- data_steward/bq_utils.py
+++ data_steward/bq_utils.py
@@ -15,6 +15,7 @@ import logging
 BQ_LOAD_DELAY_SECONDS = 5
 BQ_QUERY_DELAY_SECONDS = 5
 
+
 def get_dataset_id():
     return os.environ.get('BIGQUERY_DATASET_ID')
 
@@ -42,14 +43,16 @@ def load_table_from_bucket(hpo_id, cdm_table_name):
     :return: an object describing the associated bigquery job
     """
     if cdm_table_name not in common.CDM_TABLES:
-        raise ValueError('{} is not a valid table to load'.format(cdm_table_name))
+        raise ValueError('{} is not a valid table to load'.format(
+            cdm_table_name))
 
     app_id = app_identity.get_application_id()
     dataset_id = get_dataset_id()
     bq_service = build('bigquery', 'v2')
 
     bucket = gcs_utils.get_hpo_bucket(hpo_id)
-    fields_filename = os.path.join(resources.fields_path, cdm_table_name + '.json')
+    fields_filename = os.path.join(resources.fields_path,
+                                   cdm_table_name + '.json')
     gcs_object_path = 'gs://%s/%s.csv' % (bucket, cdm_table_name)
     table_id = get_table_id(hpo_id, cdm_table_name)
 
@@ -71,7 +74,8 @@ def load_table_from_bucket(hpo_id, cdm_table_name):
                     }
             }
     }
-    insert_result = bq_service.jobs().insert(projectId=app_id, body=job_body).execute()
+    insert_result = bq_service.jobs().insert(projectId=app_id,
+                                             body=job_body).execute()
     return insert_result
 
 
@@ -79,14 +83,17 @@ def delete_table(table_id):
     """
     Delete bigquery table by id
 
-    Note: This will throw `HttpError` if the table doesn't exist. Use `table_exists` prior if necessary.
+    Note: This will throw `HttpError` if the table doesn't exist.
+    Use `table_exists` prior if necessary.
     :param table_id: id of the table
     :return:
     """
     app_id = app_identity.get_application_id()
     dataset_id = get_dataset_id()
     bq_service = create_service()
-    return bq_service.tables().delete(projectId=app_id, datasetId=dataset_id, tableId=table_id).execute()
+    return bq_service.tables().delete(projectId=app_id,
+                                      datasetId=dataset_id,
+                                      tableId=table_id).execute()
 
 
 def table_exists(table_id):
@@ -112,7 +119,7 @@ def table_exists(table_id):
 
 def get_job_details(job_id):
     """Get job resource corresponding to job_id
-    :param job_id: id of the job to get (i.e. `jobReference.jobId` in response body of insert request)
+    :param job_id: id of the job to get (i.e. `jobReference.jobId`)
     :returns: the job resource (for details see https://goo.gl/bUE49Z)
     """
     bq_service = create_service()
@@ -120,7 +127,10 @@ def get_job_details(job_id):
     return bq_service.jobs().get(projectId=app_id, jobId=job_id).execute()
 
 
-def merge_tables(source_table_name_list, source_dataset_name, destination_table_name, destination_dataset_name):
+def merge_tables(source_dataset_id,
+                 source_table_id_list,
+                 destination_dataset_id,
+                 destination_table_id):
     """Takes a list of table names and runs a copy job
 
     :source_table_name_list: list of tables to merge
@@ -131,18 +141,19 @@ def merge_tables(source_table_name_list, source_dataset_name, destination_table_
 
     """
     app_id = app_identity.get_application_id()
-    job_body = { 
+    source_tables = [{
+        "projectId": app_id,
+        "datasetId": source_dataset_id,
+        "tableId": table_name
+    } for table_name in source_table_id_list]
+    job_body = {
         'configuration': {
-            "copy": { # "sourceTable": { "projectId": string, "datasetId": string, "tableId": string },
-                "sourceTables": [{
-                                        "projectId": app_id,
-                                        "datasetId": source_dataset_name,
-                                        "tableId": table_name
-                        } for table_name in source_table_name_list], 
+            "copy": {
+                "sourceTables": source_tables,
                 "destinationTable": {
                           "projectId": app_id,
-                          "datasetId": destination_dataset_name,
-                          "tableId": destination_table_name
+                          "datasetId": destination_dataset_id,
+                          "tableId": destination_table_id
                         },
                 "writeDisposition": "WRITE_TRUNCATE",
             }
@@ -150,42 +161,52 @@ def merge_tables(source_table_name_list, source_dataset_name, destination_table_
     }
 
     bq_service = create_service()
-    insert_result = bq_service.jobs().insert(projectId=app_id, body=job_body).execute()
+    insert_result = bq_service.jobs().insert(projectId=app_id,
+                                             body=job_body).execute()
     job_id = insert_result['jobReference']['jobId']
 
     time.sleep(BQ_LOAD_DELAY_SECONDS)
 
     job_status = get_job_details(job_id)['status']
 
-    if job_status['state'] == 'DONE': 
+    if job_status['state'] == 'DONE':
         if 'errorResult' in job_status:
             error_messages = ['{}'.format(item['message'])
-                             for item in job_status['errors']]
+                              for item in job_status['errors']]
             logging.info(' || '.join(error_messages))
-            return False,' || '.join(error_messages)
+            return False, ' || '.join(error_messages)
     else:
-        logging.info("Wait timeout exceeded before load job with id '%s' was done" % job_id)
-        return False,"Job timeout"
-    return True,""
+        logging.info("Wait timeout exceeded before load job with id '%s' was \
+                     done" % job_id)
+        return False, "Job timeout"
+    return True, ""
 
 
-def query_table(query_string, datasetId, tableId):
+def query_table(query_string):
+    """run a query job
+    :param query_string:    query command in SQL language.
+                            should contaon fully qualified ; dataset.table
+    :returns: query result (for details see
+                            https://https://goo.gl/xrXidw)
+    """
+    bq_service = create_service()
 
     app_id = app_identity.get_application_id()
 
-    job_body = { 
-        'configuration': { 
-            "query": { 
-                "query": query_string, 
+    job_body = {
+        'configuration': {
+            "query": {
+                "query": query_string,
             }
         }
     }
 
     bq_service = create_service()
-    insert_result = bq_service.jobs().insert(projectId=app_id, body=job_body).execute()
+    insert_result = bq_service.jobs().insert(projectId=app_id,
+                                             body=job_body).execute()
     job_id = insert_result['jobReference']['jobId']
-    
     time.sleep(BQ_QUERY_DELAY_SECONDS)
 
-    query_result = bq_service.jobs().getQueryResults(projectId=app_id, jobId=job_id).execute()
+    query_result = bq_service.jobs().getQueryResults(projectId=app_id,
+                                                     jobId=job_id).execute()
     return query_result
