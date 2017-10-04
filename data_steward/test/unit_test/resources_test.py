a0b09279ca2cce2fdfc39411696aa6c8f4b03ed9
Mark Velez <mav7014@nyp.org>
2017-10-04 19:34:26 -0400

    Fix test on cdm csv

--- data_steward/test/unit_test/resources_test.py
+++ data_steward/test/unit_test/resources_test.py
@@ -10,11 +10,15 @@ class ResourcesTest(unittest.TestCase):
         cdm_data_rows = resources.cdm_csv()
         expected_keys = {'table_name', 'column_name', 'is_nullable', 'data_type', 'description'}
         expected_table_names = {'person', 'visit_occurrence', 'condition_occurrence', 'procedure_occurrence',
-                                'drug_exposure', 'measurement', 'death', 'location'}
+                                'drug_exposure', 'measurement'}
+        actual_table_names = set()
         for row in cdm_data_rows:
             keys = set(row.keys())
             self.assertSetEqual(expected_keys, keys)
-            self.assertIn(row['table_name'], expected_table_names)
+            actual_table_names.add(row['table_name'])
+
+        for expected_table_name in expected_table_names:
+            self.assertIn(expected_table_name, actual_table_names)
 
     def tearDown(self):
         super(ResourcesTest, self).tearDown()
