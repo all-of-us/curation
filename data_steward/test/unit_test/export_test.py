2e76a11e30506486c83bb8f1c4a12d351fd1035f
Mark Velez <mav7014@nyp.org>
2017-10-05 16:29:25 -0400

    Add example achilles and bq payloads for testing export

--- data_steward/test/unit_test/export_test.py
+++ data_steward/test/unit_test/export_test.py
@@ -4,13 +4,16 @@ import os
 
 from google.appengine.ext import testbed
 
-import common
 import gcs_utils
+import resources
 from validation import export
 from validation import achilles
 from test_util import FAKE_HPO_ID
 import test_util
+import time
 import bq_utils
+
+BQ_TIMEOUT_SECONDS = 5
 
 
 class ExportTest(unittest.TestCase):
@@ -38,16 +41,37 @@ class ExportTest(unittest.TestCase):
         return gcs_utils.upload_object(bucket, name, fp)
 
     def _populate_achilles(self):
-        for cdm_table in common.CDM_TABLES:
-            cdm_file_name = os.path.join(test_util.FIVE_PERSONS_PATH, cdm_table + '.csv')
-            if os.path.exists(cdm_file_name):
-                self._write_cloud_file(self.hpo_bucket, cdm_file_name)
-            else:
-                self._write_cloud_str(self.hpo_bucket, cdm_table + '.csv', 'dummy\n')
-            bq_utils.load_table_from_bucket(FAKE_HPO_ID, cdm_table)
-        achilles.create_tables(FAKE_HPO_ID, True)
-        achilles.load_analyses(FAKE_HPO_ID)
-        achilles.run_analyses(hpo_id=FAKE_HPO_ID)
+        from google.appengine.api import app_identity
+
+        app_id = app_identity.get_application_id()
+        for table_name in achilles.ACHILLES_TABLES:
+            schema_file_name = table_name + '.json'
+            schema_path = os.path.join(resources.fields_path, schema_file_name)
+            test_file_name = table_name + '.csv'
+            test_file_path = os.path.join(test_util.TEST_DATA_PATH, table_name + '.csv')
+            self._write_cloud_file(self.hpo_bucket, test_file_path)
+            gcs_path = 'gs://' + self.hpo_bucket + '/' + test_file_name
+            dataset_id = bq_utils.get_dataset_id()
+            table_id = bq_utils.get_table_id(FAKE_HPO_ID, table_name)
+            bq_utils.load_csv(schema_path, gcs_path, app_id, dataset_id, table_id)
+        time.sleep(BQ_TIMEOUT_SECONDS)
+
+    def _export_from_path(p, hpo_id):
+        """Utility to create response test payloads"""
+        for f in export.list_files_only(p):
+            abs_path = os.path.join(p, f)
+            with open(abs_path, 'r') as fp:
+                sql = fp.read()
+                sql = export.render(sql, hpo_id, results_schema=bq_utils.get_dataset_id(), vocab_schema='synpuf_100')
+                query_result = bq_utils.query(sql)
+                with open(f + '.json', 'w') as fp:
+                    data = dict()
+                    if 'rows' in query_result:
+                        data['rows'] = query_result['rows']
+                    if 'schema' in query_result:
+                        data['schema'] = query_result['schema']
+                    import json
+                    json.dump(data, fp, sort_keys=True, indent=4, separators=(',', ': '))
 
     def test_export_from_path(self):
         self._populate_achilles()
