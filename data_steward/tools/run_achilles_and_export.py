60169686e729bf56a09c100471cf91cbc24a12de
aahladmanas <apm470@nyu.edu>
2017-11-09 13:03:24 -0500

    Clean run_achilles_and_export to reuse code
     * Clean up imports
     * Reformat

--- data_steward/tools/run_achilles_and_export.py
+++ data_steward/tools/run_achilles_and_export.py
@@ -4,65 +4,21 @@ run achilles, achilles_heel, and export on a particular dataset as defined by th
     -- hpo_id is an arugment wherein '{hpo_id}_' will be prepended when writing/reading tables.
     -- cdm tables that dont exist will be created as empty tables.
 """
-import sys
-sys.path.append('./lib/')
-sys.path.append('/usr/lib/google-cloud-sdk/platform/google_appengine/')
-
 import argparse
-import json
-import os
-import logging
-
 import common
-from validation import export
-import gcs_utils
-import StringIO
-
-
 import bq_utils
-from validation import achilles
-from validation import achilles_heel
-from google.appengine.ext import testbed
-
-
-def _run_export(hpo_id):
-    results = []
-    logging.info('running export for hpo_id %s' % hpo_id)
-    # TODO : add check for required tables
-    hpo_bucket = gcs_utils.get_hpo_bucket(hpo_id)
-    for export_name in common.ALL_REPORTS:
-        sql_path = os.path.join(export.EXPORT_PATH, export_name)
-        result = export.export_from_path(sql_path, hpo_id)
-        content = json.dumps(result)
-        fp = StringIO.StringIO(content)
-        result = gcs_utils.upload_object(hpo_bucket, export_name + '.json', fp)
-        results.append(result)
-    return results
-
-
-def _run_achilles(hpo_id):
-    """checks for full results and run achilles/heel
-
-    :hpo_id: hpo on which to run achilles
-    :returns:
-    """
-    logging.info('running achilles for hpo_id %s' % hpo_id)
-    achilles.create_tables(hpo_id, True)
-    achilles.load_analyses(hpo_id)
-    achilles.run_analyses(hpo_id=hpo_id)
-    logging.info('running achilles_heel for hpo_id %s' % hpo_id)
-    achilles_heel.create_tables(hpo_id, True)
-    achilles_heel.run_heel(hpo_id=hpo_id)
+from validation.main import run_export as _run_export
+from validation.main import run_achilles as _run_achilles
 
 
 def main(args):
     hpo_id = args.hpo_id
     for table_name in common.CDM_TABLES:
-        table_id = hpo_id+'_'+table_name
+        table_id = hpo_id + '_' + table_name
         if bq_utils.table_exists(table_id):
-            print table_id,' exists'
+            print table_id, ' exists'
         else:
-            print table_id,' being created'
+            print table_id, ' being created'
             bq_utils.create_standard_table(table_name, table_id, False)
 
     _run_achilles(hpo_id)
