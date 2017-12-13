3a82827877921f88195251f561bf626fddee5e0b
aahladmanas <apm470@nyu.edu>
2017-12-12 19:47:13 -0500

    first commit for environment config

---
+++ data_steward/config/config_setup.py
@@ -0,0 +1,24 @@
+"""
+flash config file and check requirements for each environment
+
+ * takes config name or custom config file and uses that to populate the required variables for our scripts
+
+"""
+import argparse
+import json
+import os
+import time
+
+import bq_utils
+from resources import fields_path
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--config',
+                        default='dev',
+                        help='config file type')
+    parser.add_argument('--config_file',
+                        default=None,
+                        help='custom config file')
+    main(parser.parse_args())
