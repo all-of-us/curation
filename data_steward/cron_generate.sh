1db903c3edcf1a12675ed40a223334f14bbb00f6
aahladmanas <apm470@nyu.edu>
2017-12-20 13:09:53 -0500

    mofidy resources.hpo_csv() to reflect all hpos
    - hpo_csv is now the latest
    - export_env and cron_generate reflect changes to hpo for stable deployment

---
+++ data_steward/cron_generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+hpos=( $(cut -d ',' -f1 ./spec/_data/hpo.csv) )
+hpos=("${hpos[@]:1}")
+env=$1
+
+echo "- description: validate all hpos"
+echo "  url: /data_steward/v1/ValidateHpoFiles/all"
+echo "  schedule: every 240 hours" 
+echo ""
+
+
+echo "- description: website generation"
+echo "  url: /tasks/sitegen"
+echo "  schedule: every 60 minutes"
+echo ""
+for i in "${hpos[@]}"
+do
+  temp="${i%\"}"
+  temp="${temp#\"}"
+  echo "- description: validate hpo $temp"
+  echo "  url: /data_steward/v1/ValidateHpoFiles/$temp"
+  echo "  schedule: every 24 hours" 
+  echo ""
+done
--- data_steward/export_env_variables.sh
+++ data_steward/export_env_variables.sh
@@ -5,9 +5,19 @@ env=$1
 appid="aou-res-curation-$env"
 export APPLICATION_ID="aou-res-curation-$env"
 export BIGQUERY_DATASET_ID="${env}_fake"
+export BUCKET_NAME_FAKE="$APPLICATION_ID-fake"
 for i in "${hpos[@]}"
 do
   temp="${i%\"}"
   temp="${temp#\"}"
-  echo "export BUCKET_NAME_${temp^^}="$APPLICATION_ID-$temp""
+  echo "  BUCKET_NAME_${temp^^}=\"$APPLICATION_ID-$temp\""
+  export BUCKET_NAME_${temp^^}="$APPLICATION_ID-$temp"
+done
+
+
+for i in "${hpos[@]}"
+do
+  temp="${i%\"}"
+  temp="${temp#\"}"
+  echo "  BUCKET_NAME_${temp^^}:\"$APPLICATION_ID-$temp\""
 done
