9a2a414019009b2c8351de92a5c120c00c8d4a2a
Mark Velez <mav7014@nyp.org>
2017-12-27 20:10:57 -0500

    Add output column `intervals` to query for person birth_year_histogram
     * In Achilles this is unnecessarily computed in R (see https://github.com/OHDSI/Achilles/blob/master/R/exportToJson.R#L1335)

--- data_steward/resources/export/person/birth_year_histogram.sql
+++ data_steward/resources/export/person/birth_year_histogram.sql
@@ -1,5 +1,6 @@
-select min(cast(ar1.stratum_1 as int64)) as min_value,
-  max(cast(ar1.stratum_1 as int64)) as max_value,
-	1 as interval_size
+select min(cast(ar1.stratum_1 as int64)) as min,
+  max(cast(ar1.stratum_1 as int64)) as max,
+	1 as interval_size,
+  max(cast(ar1.stratum_1 as int64)) - min(cast(ar1.stratum_1 as int64)) as intervals
 from @results_database_schema.achilles_results ar1
 where ar1.analysis_id = 3
