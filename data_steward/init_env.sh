6eeac0848c82dcd2f8f1cebe6effa0ad27187a19
Mark Velez <mav7014@nyp.org>
2018-04-20 20:46:14 -0400

    Update setup scripts
     * Test buckets are regional
     * Add another test bucket
     * Use $(..) instead of legacy `..`
     * Use ${variable//search/replace} instead of sed

--- data_steward/init_env.sh
+++ data_steward/init_env.sh
@@ -9,7 +9,7 @@ fi
 
 # Require username in GH_USERNAME or CIRCLE_USERNAME
 export APPLICATION_ID="aou-res-curation-$CONFIG"
-export USERNAME=${GH_USERNAME:-${CIRCLE_USERNAME:-}}
+export USERNAME="${GH_USERNAME:-${CIRCLE_USERNAME:-}}"
 
 if [ -z "${USERNAME}" ]
 then
@@ -19,19 +19,20 @@ fi
 
 # Dataset IDs must be alphanumeric (plus underscores) and <= 1024 characters
 # See https://cloud.google.com/bigquery/docs/reference/rest/v2/datasets#datasetReference.datasetId
-PROJECT_PREFIX=`echo ${APPLICATION_ID} | sed 's/-/_/g'`
-USERNAME_PREFIX=`echo ${USERNAME} | sed 's/-/_/g'`
+PROJECT_PREFIX="${APPLICATION_ID//-/_}"
+USERNAME_PREFIX="${USERNAME//-/_}"
 
 # GCS buckets are globally unique, so we prefix with project id and username
-export DRC_BUCKET_NAME=${PROJECT_PREFIX}_${USERNAME_PREFIX}_drc
-export BUCKET_NAME_FAKE=${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_fake
-export BUCKET_NAME_NYC=${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_nyc
-export BUCKET_NAME_PITT=${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_pitt
+export DRC_BUCKET_NAME="${PROJECT_PREFIX}_${USERNAME_PREFIX}_drc"
+export BUCKET_NAME_FAKE="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_fake"
+export BUCKET_NAME_NYC="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_nyc"
+export BUCKET_NAME_PITT="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_pitt"
+export BUCKET_NAME_CHS="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_chs"
 
 # Datasets can be scoped by project so we prefix with username
 # Note: Dataset IDs must be alphanumeric (plus underscores) and <= 1024 characters
 # See https://cloud.google.com/bigquery/docs/reference/rest/v2/datasets#datasetReference.datasetId
-export BIGQUERY_DATASET_ID=`echo ${USERNAME_PREFIX}_ehr | sed 's/-/_/g'`
+export BIGQUERY_DATASET_ID="${USERNAME_PREFIX}_ehr"
 
 # .circlerc is sourced before each test and deploy command
 # See https://www.compose.com/articles/experience-with-circleci/#dontcommitcredentials
@@ -43,5 +44,6 @@ then
   echo "export BUCKET_NAME_FAKE=${BUCKET_NAME_FAKE}" >> $HOME/.circlerc
   echo "export BUCKET_NAME_NYC=${BUCKET_NAME_NYC}" >> $HOME/.circlerc
   echo "export BUCKET_NAME_PITT=${BUCKET_NAME_PITT}" >> $HOME/.circlerc
+  echo "export BUCKET_NAME_CHS=${BUCKET_NAME_CHS}" >> $HOME/.circlerc
   echo "export BIGQUERY_DATASET_ID=${BIGQUERY_DATASET_ID}" >> $HOME/.circlerc
 fi
