d730d7c752fc8b3efa88b6e5d2b10162f39f7f68
Mark Velez <mav7014@nyp.org>
2018-04-26 17:50:16 -0400

    Include branch name in naming convention for test resources
    Allow one user to run two builds (from different branches) in parallel

--- data_steward/init_env.sh
+++ data_steward/init_env.sh
@@ -21,19 +21,26 @@ fi
 # See https://cloud.google.com/bigquery/docs/reference/rest/v2/datasets#datasetReference.datasetId
 PROJECT_PREFIX="${APPLICATION_ID//-/_}"
 USERNAME_PREFIX="${USERNAME//-/_}"
+# Determine branch name either by CIRCLE_BRANCH var or using git
+CURRENT_BRANCH="${CIRCLE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
+# Replace dashes, slashes with underscore
+CURRENT_BRANCH="${CURRENT_BRANCH//-/_}"
+CURRENT_BRANCH="${CURRENT_BRANCH//\//_}"
+BUCKET_PREFIX="${PROJECT_PREFIX}_${CURRENT_BRANCH}_${USERNAME_PREFIX}"
 
 # GCS buckets are globally unique, so we prefix with project id and username
-export DRC_BUCKET_NAME="${PROJECT_PREFIX}_${USERNAME_PREFIX}_drc"
-export BUCKET_NAME_FAKE="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_fake"
-export BUCKET_NAME_NYC="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_nyc"
-export BUCKET_NAME_PITT="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_pitt"
-export BUCKET_NAME_CHS="${PROJECT_PREFIX}_${USERNAME_PREFIX}_hpo_chs"
-export BUCKET_NAME_UNIONED_EHR="${PROJECT_PREFIX}_${USERNAME_PREFIX}_drc"
+export DRC_BUCKET_NAME="${BUCKET_PREFIX}_drc"
+export BUCKET_NAME_FAKE="${BUCKET_PREFIX}_hpo_fake"
+export BUCKET_NAME_NYC="${BUCKET_PREFIX}_hpo_nyc"
+export BUCKET_NAME_PITT="${BUCKET_PREFIX}_hpo_pitt"
+export BUCKET_NAME_CHS="${BUCKET_PREFIX}_hpo_chs"
+export BUCKET_NAME_UNIONED_EHR="${BUCKET_PREFIX}_drc"
 
 # Datasets can be scoped by project so we prefix with username
 # Note: Dataset IDs must be alphanumeric (plus underscores) and <= 1024 characters
 # See https://cloud.google.com/bigquery/docs/reference/rest/v2/datasets#datasetReference.datasetId
-export BIGQUERY_DATASET_ID="${USERNAME_PREFIX}_ehr"
+DATASET_PREFIX="${CURRENT_BRANCH}_${USERNAME_PREFIX}"
+export BIGQUERY_DATASET_ID="${DATASET_PREFIX}_ehr"
 
 # .circlerc is sourced before each test and deploy command
 # See https://www.compose.com/articles/experience-with-circleci/#dontcommitcredentials
@@ -47,5 +54,5 @@ then
   echo "export BUCKET_NAME_PITT=${BUCKET_NAME_PITT}" >> $HOME/.circlerc
   echo "export BUCKET_NAME_CHS=${BUCKET_NAME_CHS}" >> $HOME/.circlerc
   echo "export BIGQUERY_DATASET_ID=${BIGQUERY_DATASET_ID}" >> $HOME/.circlerc
-  echo "export BUCKET_NAME_UNIONED_EHR=${PROJECT_PREFIX}_${USERNAME_PREFIX}_drc" >> $HOME/.circlerc
+  echo "export BUCKET_NAME_UNIONED_EHR=${BUCKET_NAME_UNIONED_EHR}" >> $HOME/.circlerc
 fi
