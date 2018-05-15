c27642fdd65331d5d02398c6cfb35a80a394baa4
Mark Velez <mav7014@nyp.org>
2018-05-14 22:13:45 -0400

    DC-99 Set source folder in run_achilles_and_export

--- data_steward/tools/run_achilles_and_export.py
+++ data_steward/tools/run_achilles_and_export.py
@@ -17,6 +17,7 @@ from validation.main import run_achilles as _run_achilles
 
 def main(args):
     hpo_id = args.hpo_id
+    folder = args.folder
     for table_name in common.CDM_TABLES:
         table_id = hpo_id + '_' + table_name
         if bq_utils.table_exists(table_id):
@@ -26,13 +27,17 @@ def main(args):
             bq_utils.create_standard_table(table_name, table_id, False)
 
     _run_achilles(hpo_id)
-    _run_export(hpo_id)
+    _run_export(hpo_id, folder + '/')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--folder',
+                        required=True,
+                        help='Folder in the HPO bucket to save curation report')
     parser.add_argument('--hpo_id',
                         default='fake',
                         help='Identifier for the HPO. Output tables will be prepended with {hpo_id}_.')
+
     args = parser.parse_args()
     main(args)
