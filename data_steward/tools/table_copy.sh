6b7e708b8fa69863c23a9805f3c69c0c8ee495b3
Karthik <kn2174@cumc.columbia.edu>
2018-09-07 19:16:16 -0400

    Created script to run curation pipeline. This required an update to the table copy script to make it more generic.

--- data_steward/tools/table_copy.sh
+++ data_steward/tools/table_copy.sh
@@ -11,10 +11,12 @@
 # ## Copy `dataset1.tableA` to `dataset1.prefix_tableA`
 # > tools/table_copy.sh --source_dataset dataset1 --target_prefix prefix_ --target_dataset dataset1
 
-USAGE="tools/table_copy.sh --source_dataset <SOURCE_DATASET> --source_prefix <SOURCE_PREFIX> --target_dataset <TARGET_DATASET> [--target_prefix <TARGET_PREFIX:''>]"
+USAGE="tools/table_copy.sh [--source_app_id <SOURCE_APPLICATION_ID>] [--target_app_id <TARGET_APPLICATION_ID>] --source_dataset <SOURCE_DATASET> [--source_prefix <SOURCE_PREFIX:''>] --target_dataset <TARGET_DATASET> [--target_prefix <TARGET_PREFIX:''>]"
 
 while true; do
   case "$1" in
+    --source_app_id) SOURCE_APPLICATION_ID=$2; shift 2;;
+    --target_app_id) TARGET_APPLICATION_ID=$2; shift 2;;
     --source_dataset) SOURCE_DATASET=$2; shift 2;;
     --target_dataset) TARGET_DATASET=$2; shift 2;;
     --source_prefix) SOURCE_PREFIX=$2; shift 2;;
@@ -31,21 +33,30 @@ then
   TARGET_PREFIX=""
 fi
 
+if [ -z "${SOURCE_PREFIX}" ]
+then
+  SOURCE_PREFIX=""
+fi
+
 if [ -z "${SOURCE_DATASET}" ] || [ -z "${TARGET_DATASET}" ]
 then
   echo "Usage: $USAGE"
   exit 1
 fi
 
+SOURCE_DATASET=[[ "${SOURCE_APPLICATION_ID}" ]] && "${SOURCE_APPLICATION_ID}:${SOURCE_DATASET}" || "${SOURCE_DATASET}"
+TARGET_DATASET=[[ "${TARGET_APPLICATION_ID}" ]] && "${TARGET_APPLICATION_ID}:${TARGET_DATASET}" || "${TARGET_DATASET}"
+
 # Copy the tables
-# Tables beginning with underscore "_" are skipped
+#( [[ "${SOURCE_PREFIX}" ]] && grep ${SOURCE_PREFIX} || cat )) essentially is and if/else statement
+# if SOURCE_PREFIX is not empty then grep for SOURCE_PREFIX else CAT the input
 for t in $(bq ls -n 2000 ${SOURCE_DATASET} |
            grep TABLE |
            awk '{print $1}' |
-           grep -v ^\_ |
+#           grep -v ^\_ |  #Tables beginning with underscore "_" are skipped
            ( [[ "${SOURCE_PREFIX}" ]] && grep ${SOURCE_PREFIX} || cat ))
 do
-  TARGET_TABLE=${t//${SOURCE_PREFIX}/}
+  TARGET_TABLE=${t//${SOURCE_PREFIX}/} #replace all occurrences, use ${parameter//pattern/replacement_string}
   CP_CMD="bq cp -f ${SOURCE_DATASET}.${t} ${TARGET_DATASET}.${TARGET_PREFIX}${TARGET_TABLE}"
   echo $(${CP_CMD})
 done
