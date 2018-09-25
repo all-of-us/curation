0cf1a1a8d7bfdf072e642998829bffbe5cf15dda
Krishna Kalluri <krishnasaikalluri@gmail.com>
2018-09-25 17:02:05 -0400

    Sets Sockets request deadline to 300 seconds

--- data_steward/appengine_config.py
+++ data_steward/appengine_config.py
@@ -1,5 +1,10 @@
 from google.appengine.ext import vendor
 
+import socket
+DEFAULT_FETCH_DEADLINE = 300
 # Add any libraries installed in the "lib" folder.
 # vendor.add('lib2')
 vendor.add('lib')
+
+# makes the http request deadline to 60 sec
+socket.setdefaulttimeout(DEFAULT_FETCH_DEADLINE)
