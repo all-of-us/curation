a572498fac7ba82ccc7e4ffb731aa60c1e4973b3
ksdkalluri <developerksdk@gmail.com>
2019-02-11 19:28:14 -0500

    refactor code

--- data_steward/tools/common_heel_errors.py
+++ data_steward/tools/common_heel_errors.py
@@ -13,12 +13,21 @@ import argparse
 hpo_id_list = [item['hpo_id'] for item in resources.hpo_csv()]
 HEEL_ERRORS_JSON = 'heel_errors.json'
 HEEL_ERRORS_CSV = 'heel_errors.csv'
-heel_error_query ='''select '{hpo_id}' as hpo_name,
+heel_error_query ='''select '{hpo_id}' as dataset_name,
             analysis_id, 
             achilles_heel_warning as heel_error,
             rule_id,
             record_count
             FROM `{app_id}.{dataset_id}.{hpo_id}_achilles_heel_results`
+            WHERE achilles_heel_warning like 'ERROR:%'
+            order by record_count desc limit 10'''
+
+heel_error_query2 = '''select '{dataset_id}' as dataset_name,
+            analysis_id, 
+            achilles_heel_warning as heel_error,
+            rule_id,
+            record_count
+            FROM `{app_id}.{dataset_id}.achilles_heel_results`
             WHERE achilles_heel_warning like 'ERROR:%'
             order by record_count desc limit 10'''
 
@@ -38,51 +47,57 @@ def parse_json_csv():
         output.writerow(row.values())
 
 
-def most_common_heel_errors(app_id=None, dataset_id=None, hpo_ids=None):
+def most_common_heel_errors(app_id, dataset_id, hpo_ids):
     """
     :param app_id: Application Id
     :param dataset_id: Dataset Id
     :param hpo_ids: list of Hpo_ids
     :return: None
     """
+    print(hpo_ids)
     heel_errors = list()
     if not os.path.exists(HEEL_ERRORS_JSON) and not os.path.exists(HEEL_ERRORS_CSV):
-        for hpo_id in hpo_ids:
-            if bq_utils.table_exists(table_id='{hpo_id}_achilles_heel_results'.format(hpo_id=hpo_id), dataset_id=dataset_id):
-                query = heel_error_query.format(app_id=app_id, dataset_id=dataset_id, hpo_id=hpo_id)
+        if len(hpo_ids) == 1:
+            for hpo_id in hpo_ids:
+                query = heel_error_query2.format(hpo_id=hpo_id, app_id=app_id, dataset_id=dataset_id)
                 query_job = bq_utils.query(query)
                 result = bq_utils.response2rows(query_job)
                 heel_errors.extend(result)
+        else:
+            for hpo_id in hpo_ids:
+                if bq_utils.table_exists(table_id='{hpo_id}_achilles_heel_results'.
+                                         format(hpo_id=hpo_id), dataset_id=dataset_id):
+                    query = heel_error_query.format(app_id=app_id, dataset_id=dataset_id, hpo_id=hpo_id)
+                    query_job = bq_utils.query(query)
+                    result = bq_utils.response2rows(query_job)
+                    heel_errors.extend(result)
     with open(HEEL_ERRORS_JSON, 'w') as fp:
         json.dump(heel_errors, fp, sort_keys=True, indent=4)
     parse_json_csv()
 
 
-def run_heel_errors_report(name=None, app_id=None, dataset_id=None):
+def main(report_for, dataset_id, app_id=None):
     if app_id is None:
         app_id = app_identity.get_application_id()
     if dataset_id is None:
         dataset_id = bq_utils.get_dataset_id()
-    if name is "hpo_ids":
-        most_common_heel_errors(hpo_ids=hpo_id_list)
+    if report_for == 'hpo':
+        print(report_for)
+        most_common_heel_errors(hpo_ids=hpo_id_list, app_id=app_id, dataset_id=dataset_id)
     else:
-        dataset_list = list()
-        dataset_list.append(name)
+        print(report_for)
+        dataset_list = [report_for]
         most_common_heel_errors(hpo_ids=dataset_list, app_id=app_id, dataset_id=dataset_id)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--name',
-                        default="hpo_ids",
+    parser.add_argument('report_for',
                         help='Name of the dataset on which the heel_results should be obtained.("hpo_ids" for '
                              'all the hpos. Dataset name for any other dataset.')
-    parser.add_argument('--app_id',
-                        default=app_identity.get_application_id(),
-                        help='Identifier for the application.')
-    parser.add_argument('--dataset_id',
-                        default=bq_utils.get_dataset_id(),
-                        help='Identifier for the dataset.')
+    parser.add_argument('dataset_id',
+                        help='Name of the dataset')
 
     args = parser.parse_args()
-    run_heel_errors_report(args)
+    if args.report_for:
+        main(args.report_for, args.dataset_id)
