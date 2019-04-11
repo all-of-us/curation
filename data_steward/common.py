45e4ae716262927bdf46a277ed17a80c709de77c
Nishanth Pavinkurve <npp267@nyu.edu>
2019-04-11 14:49:30 -0400

    Add participant_match.json to specify schema, include it as pii file

--- data_steward/common.py
+++ data_steward/common.py
@@ -6,7 +6,7 @@ CDM_FILES = map(lambda t: t + '.csv', CDM_TABLES)
 ACHILLES_INDEX_FILES = resources.achilles_index_files()
 ALL_ACHILLES_INDEX_FILES = [name.split(resources.resource_path + os.sep)[1].strip() for name in ACHILLES_INDEX_FILES]
 DATASOURCES_JSON = os.path.join(resources.achilles_index_path, 'data/datasources.json')
-PII_TABLES = ['pii_name', 'pii_email', 'pii_phone_number', 'pii_address', 'pii_mrn']
+PII_TABLES = ['pii_name', 'pii_email', 'pii_phone_number', 'pii_address', 'pii_mrn', 'participant_match']
 AOU_REQUIRED = ['care_site', 'condition_occurrence', 'death', 'device_exposure', 'drug_exposure',
                 'fact_relationship', 'location', 'measurement', 'note', 'observation', 'person',
                 'procedure_occurrence', 'provider', 'specimen', 'visit_occurrence']
@@ -25,7 +25,7 @@ IGNORE_LIST = [PROCESSED_TXT, RESULTS_HTML] + ALL_ACHILLES_INDEX_FILES
 VOCABULARY_TABLES = ['concept', 'concept_ancestor', 'concept_class', 'concept_relationship', 'concept_synonym',
                      'domain', 'drug_strength', 'relationship', 'vocabulary']
 REQUIRED_TABLES = ['person']
-REQUIRED_FILES = [table + '.csv' for table in REQUIRED_TABLES]
+REQUIRED_FILES = map(lambda t: t+'.csv', REQUIRED_TABLES)
 ACHILLES_EXPORT_PREFIX_STRING = "curation_report/data/"
 IGNORE_STRING_LIST = [ACHILLES_EXPORT_PREFIX_STRING]
 ACHILLES_EXPORT_DATASOURCES_JSON = ACHILLES_EXPORT_PREFIX_STRING + 'datasources.json'
