d075cae2a32a307405112f74699ec1cb65ff0345
Lauren Biggers <lauren.r.biggers@vumc.org>
2019-05-03 12:19:09 -0500

    DC-356 develop id-match report
    This moves the constants for each module into their own module
    instead of a single package module.  This also updates the
    circleci config file to help us identify whether a build is
    associated with a commit or pull request.

--- data_steward/validation/participants/normalizers.py
+++ data_steward/validation/participants/normalizers.py
@@ -9,7 +9,7 @@ import logging
 # Third party imports
 
 # Project imports
-import constants.validation.participants.identity_match as consts
+import constants.validation.participants.normalizers as consts
 
 LOGGER = logging.getLogger(__name__)
 
