ceda84704e176a660c07254b5ef7d78591a06b01
Lauren Biggers <lauren.r.biggers@vumc.org>
2019-05-24 14:12:43 -0500

    DC-407  develoment enhancements
    This adds the ability to log to the console or to a file in /tmp.
    Adds the ability to parse the command line if run as an independent module.
    Adds structure to name the queries and store a list of query names in the
    constants files.  This will make modifications easier in the future.

--- data_steward/constants/cleaners/combined_deid.py
+++ data_steward/constants/cleaners/combined_deid.py
@@ -1,3 +1,7 @@
+# Create queries here and name them
+GET_PERSON_IDS = 'SELECT person_id FROM `{project}.{dataset}.person`'
+
+# Ensure query order is correct in this list
 SQL_QUERIES = [
-    'SELECT person_id FROM `{project}.{dataset}.person`',
+    GET_PERSON_IDS
 ]
