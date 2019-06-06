60452218ab73762f67065f05e951b967821f0417
Lauren <47253368+lrwb-aou@users.noreply.github.com>
2019-06-06 12:18:22 -0500

    [DC-407] Initial cleaner package (#155)
    * Creates the initial clean rules engine. It is expected to evolve over time. Each type of dataset can be cleaned by calling a single clean method, or the entire series of datasets can be cleaned by calling a clean all method. Queries should be added to corresponding constants files. The queries
    will execute in list order. Rules may log to the console or to a file in /tmp. Rules may parse the command line if run as an independent module.
    
    * Removes cache warning message caused by bq_utils module

---
+++ data_steward/constants/cleaners/ehr.py
@@ -0,0 +1,7 @@
+# Create queries here and name them
+GET_PERSON_IDS = 'SELECT * FROM `{project}.{dataset}.vocabulary`'
+
+# Ensure query order is correct in this list
+SQL_QUERIES = [
+    GET_PERSON_IDS
+]
---
+++ data_steward/constants/cleaners/unioned.py
@@ -0,0 +1,7 @@
+# Create queries here and name them
+GET_PERSON_IDS = 'SELECT person_id FROM `{project}.{dataset}.person`'
+
+# Ensure query order is correct in this list
+SQL_QUERIES = [
+    GET_PERSON_IDS
+]
