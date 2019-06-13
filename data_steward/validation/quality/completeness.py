97cf4e36b26cfb724292f2064d5e4cfa5e63470f
Mark Velez <mav7014@nyp.org>
2019-06-12 21:45:30 -0400

    Updates completeness (pipeline integration with output still needed)
     * Extracts query constants
     * Improves arg parsing
     * Adds README

--- data_steward/validation/quality/completeness.py
+++ data_steward/validation/quality/completeness.py
@@ -1,117 +1,99 @@
 import bq_utils
 import resources
 
+COLUMNS_QUERY_FMT = """
+SELECT table_name, 
+ column_name, 
+ t.row_count as table_row_count 
+FROM {dataset_id}.INFORMATION_SCHEMA.COLUMNS c
+ JOIN {dataset_id}.__TABLES__ t on c.table_name = t.table_id
+  WHERE t. table_id NOT LIKE '\\\\_%' 
+  AND c.IS_HIDDEN = 'NO'
+ ORDER BY table_name, c.ORDINAL_POSITION
+"""
+
+COMPLETENESS_QUERY_FMT = """
+SELECT *,
+ CASE 
+  WHEN table_row_count=0 THEN NULL 
+  ELSE 1 - (null_count + concept_zero_count)/(table_row_count)
+ END as percent_populated 
+FROM (
+ SELECT '{table_name}' AS table_name, 
+  {table_row_count} AS table_row_count,
+  '{column_name}' AS column_name,
+  {table_row_count} - count({column_name}) as null_count,
+  {concept_zero_expr} AS concept_zero_count
+ FROM {dataset_id}.{table_name}
+) AS counts
+"""
+
 
 def get_hpo_ids():
     return [hpo_item['hpo_id'] for hpo_item in resources.hpo_csv()]
 
 
-def get_table_columns(dataset_id):
+def get_columns(dataset_id):
     """
-    Retrieves summary of all domain tables in a specified dataset associated with a given hpo_id
+    Retrieves summary of all domain tables in a specified dataset
 
     :param dataset_id: identifies the dataset
     :param hpo_id: identifies the HPO
     :return: list of dict with keys table_name, column_name, row_count
     """
-    query = """SELECT table_name, column_name, t.row_count as table_row_count 
-               FROM {dataset_id}.INFORMATION_SCHEMA.COLUMNS c
-               JOIN {dataset_id}.__TABLES__ t on c.table_name=t.table_id
-               WHERE 
-               table_id NOT like '_mapping%' AND 
-                (
-                  table_id like '%person' OR
-                  table_id like '%visit_occurrence' OR
-                  table_id like '%condition_occurrence' OR
-                  table_id like '%procedure_occurrence' OR
-                  table_id like '%drug_exposure' OR
-                  table_id like '%measurement' OR
-                  table_id like '%observation' OR
-                  table_id like '%device_exposure' OR
-                  table_id like '%death' OR
-                  table_id like '%provider' OR
-                  table_id like '%specimen' OR
-                  table_id like '%location' OR
-                  table_id like '%care_site' OR
-                  table_id like '%note'
-                  )""".format(dataset_id=dataset_id)
+    query = COLUMNS_QUERY_FMT.format(dataset_id=dataset_id)
     query_response = bq_utils.query(query)
     rows = bq_utils.response2rows(query_response)
-    return rows
+    results = filter_domain_columns(rows)
+    return results
 
 
-def create_hpo_completeness_query(dataset_id, table_columns):
-    query_with_concept_id = """
-      SELECT current_datetime() as report_run_time, 
-        x.*, 
-        CASE WHEN total_rows=0 THEN 0 ELSE (num_nonnulls_zeros)/(total_rows) END as percent_field_populated 
-      FROM (
-        SELECT '{table_name}' as table_name,
-               '{column_name}' as column_name,
-               {table_row_count} as total_rows,
-               sum(case when {column_name}=0 then 0 else 1 end) as num_nonnulls_zeros,
-               ({table_row_count} - count({column_name})) as non_populated_rows
-        FROM {dataset_id}.{table_name} 
-      ) as x 
-    """
-    query_without_concept_id = """
-    SELECT current_datetime() as report_run_time, 
-      x.*,
-      CASE WHEN total_rows=0 THEN 0 ELSE (num_nonnulls_zeros)/(total_rows) END as percent_field_populated
-      FROM (
-        SELECT '{table_name}' as table_name, 
-          '{column_name}' as column_name,
-          {table_row_count} as total_rows,
-          count({column_name}) as num_nonnulls_zeros,
-          ({table_row_count} - count({column_name})) as non_populated_rows
-        FROM {dataset_id}.{table_name} 
-      ) as x 
-    """
-    queries = []
-    for row in table_columns:
-        if row['column_name'] == '_PARTITIONTIME':
-            continue
-        query_format = query_with_concept_id if row['column_name'].endswith('concept_id') else query_without_concept_id
-        x = query_format.format(dataset_id=dataset_id, **row)
-        queries.append(x)
-
-    return " union all ".join(queries)
+def create_completeness_query(dataset_id, columns):
+    subqueries = []
+    for column in columns:
+        concept_zero_expr = "0"
+        if column['column_name'].endswith('concept_id'):
+            concept_zero_expr = "SUM(CASE WHEN {column_name}=0 THEN 1 ELSE 0 END)".format(**column)
+        subquery = COMPLETENESS_QUERY_FMT.format(dataset_id=dataset_id, concept_zero_expr=concept_zero_expr, **column)
+        subqueries.append(subquery)
+    return '\nUNION ALL\n'.join(subqueries)
 
 
-def run_hpo(project_id, dataset_id, hpo_id):
+def filter_domain_columns(columns):
+    result = []
+    for column in columns:
+        for cdm_table in resources.CDM_TABLES:
+            if column['table_name'].endswith(cdm_table):
+                result.append(column)
+                break
+    return result
+
+
+def filter_hpo_columns(columns, hpo_id):
+    for column in columns:
+        if column['table_name'].startswith(hpo_id):
+            yield column
+
+
+def column_completeness(dataset_id, columns):
+    query = create_completeness_query(dataset_id, columns)
+    query_response = bq_utils.query(query)
+    results = bq_utils.response2rows(query_response)
+    return results
+
+
+def hpo_completeness(dataset_id, hpo_id):
     """
     Generate completeness metrics for OMOP dataset of specified HPO
 
-    :param project_id:
     :param dataset_id:
     :param hpo_id:
     :return: list of dict with table_name, column_name, total_rows, num_nonnulls_zeros, non_populated_rows
     """
-    all_table_columns = get_table_columns(dataset_id)
-    hpo_table_columns = []
-    for table_column in all_table_columns:
-        if table_column['table_name'].startswith(hpo_id):
-            hpo_table_columns.extend(table_column)
-    query = create_hpo_completeness_query(dataset_id, hpo_table_columns)
-    query_response = bq_utils.query(query)
-    rows = bq_utils.response2rows(query_response)
-    return rows
-
-
-def run_all(project_id, dataset_id):
-    """
-    Generate completeness metrics for all HPO omops in a bigquery dataset
-
-    :param project_id: identifies the project
-    :param dataset_id: identifies the bigquery dataset
-    :return: A dict with hpo_id => completeness rows
-    """
-    result = dict()
-    hpo_ids = get_hpo_ids()
-    for hpo_id in hpo_ids:
-        rows = run_hpo(project_id, dataset_id, hpo_id)
-        result[hpo_id] = rows
-    return result
+    columns = get_columns(dataset_id)
+    hpo_columns = filter_hpo_columns(columns, hpo_id)
+    results = column_completeness(dataset_id, hpo_columns)
+    return results
 
 
 if __name__ == '__main__':
@@ -119,21 +101,30 @@ if __name__ == '__main__':
     import json
     import os
 
-    parser = argparse.ArgumentParser(description=run_all.__doc__)
+    def get_creds(creds_path):
+        with open(creds_path, 'rb') as creds_fp:
+            return json.load(creds_fp)
+
+    def run_with_args(credentials, dataset_id, hpo_id):
+        creds = get_creds(credentials)
+        project_id = creds.get('project_id')
+        os.environ['APPLICATION_ID'] = project_id
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials
+        os.environ['BIGQUERY_DATASET_ID'] = dataset_id
+
+        hpo_ids = [hpo_id] if hpo_id else get_hpo_ids()
+        columns = get_columns(dataset_id)
+
+        results = dict()
+        for hpo_id in hpo_ids:
+            hpo_columns = filter_hpo_columns(columns, hpo_id)
+            hpo_results = column_completeness(dataset_id, hpo_columns)
+            results[hpo_id] = hpo_results
+        return results
+
+    parser = argparse.ArgumentParser(description=hpo_completeness.__doc__)
     parser.add_argument('--credentials', help='Path to credentials file')
     parser.add_argument('--dataset_id', help='Identifies the dataset containing the OMOP tables to report on')
     parser.add_argument('hpo_id', nargs='?', help='Identifies an HPO site to report on; all sites by default')
-    args = parser.parse_args()
-
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = args.credentials
-    os.environ['BIGQUERY_DATASET_ID'] = args.dataset_id
-
-    with open(args.credentials, 'rb') as creds_fp:
-        creds = json.load(creds_fp)
-        PROJECT_ID = creds.get('project_id')
-        os.environ['APPLICATION_ID'] = PROJECT_ID
-        if args.hpo_id:
-            RESULT = run_hpo(PROJECT_ID, args.dataset_id, args.hpo_id)
-        else:
-            RESULT = run_all(PROJECT_ID, args.dataset_id)
-        print RESULT
+    ARGS = parser.parse_args()
+    print run_with_args(ARGS.credentials, ARGS.dataset_id, ARGS.hpo_id)
