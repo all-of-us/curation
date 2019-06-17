122b20fda60abe7d1c2363a4815f9b892c12f4fb
Krishna Kalluri <krishnasaikalluri@gmail.com>
2019-06-17 16:43:53 -0400

    [DC-411] Remove duplicate id's in domain tables at ehr-union stage. (#160)
    * Removes Duplicates in id columns from the source tables while running UNION_EHR.
    * Modifies table_hpo_subquery_test to validate new changes in sub query
    * Refactored constants, moved from common to constants/validation/ehr_union
    
    Added condition to remove duplicates from mapping table in unioned dataset
    
    Added condition to remove duplicates from mapping table in combined dataset
    
    Removes Legacy test case (which is not in use)

---
+++ data_steward/constants/validation/ehr_union.py
@@ -0,0 +1,32 @@
+import common
+# This File consists of all the constants and sql queries from validation/ehr_union
+VISIT_OCCURRENCE = 'visit_occurrence'
+VISIT_OCCURRENCE_ID = 'visit_occurrence_id'
+CARE_SITE = 'care_site'
+CARE_SITE_ID = 'care_site_id'
+PERSON = 'person'
+PERSON_ID = 'person_id'
+LOCATION = 'location'
+LOCATION_ID = 'location_id'
+FACT_RELATIONSHIP = 'fact_relationship'
+
+CONCEPT_CONSTANT_FACTOR = 1000000000000
+
+# Starting factor to create ID space for person to observation mapped record
+EHR_PERSON_TO_OBS_CONSTANT = 2 * common.ID_CONSTANT_FACTOR
+
+
+# Starting factor to create ID spaces for each HPO (without RDR collisions)
+EHR_ID_MULTIPLIER_START = 3
+
+
+# person to observation (pto) constants
+GENDER_CONCEPT_ID = 4135376
+RACE_CONCEPT_ID = 4013886
+DOB_CONCEPT_ID = 4083587
+ETHNICITY_CONCEPT_ID = 4271761
+
+GENDER_CONSTANT_FACTOR = 1 * CONCEPT_CONSTANT_FACTOR
+RACE_CONSTANT_FACTOR = 2 * CONCEPT_CONSTANT_FACTOR
+DOB_CONSTANT_FACTOR = 3 * CONCEPT_CONSTANT_FACTOR
+ETHNICITY_CONSTANT_FACTOR = 4 * CONCEPT_CONSTANT_FACTOR
