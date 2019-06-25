e6fa5cc6e0663774e93a25ac0d210681fc2b53af
Lauren Biggers <lauren.r.biggers@vumc.org>
2019-06-25 13:17:10 -0500

    DC-391 change updates and deletes to select statements

--- data_steward/cleaners/query_generators/person_id_validator.py
+++ data_steward/cleaners/query_generators/person_id_validator.py
@@ -7,6 +7,8 @@ Run the person_id validation clean rule.
     Keep PPI records.
 """
 
+import resources
+
 MAPPED_VALIDATION_TABLES = [
     'visit_occurrence',
     'condition_occurrence',
@@ -24,31 +26,51 @@ UNMAPPED_VALIDATION_TABLES = [
 ]
 
 # The below query translates to:
-#DELETE `{project}.{dataset}.{table}` entry
-#WHERE entry.person_id IN (non-ehr-consenting person_ids)
-#AND entry.{table}_id IN (ehr mapping table results)
-NOT_CONSENTING_PERSON_IDS = """
-DELETE `{project}.{dataset}.{table}` entry
-WHERE entry.person_id IN (
-        SELECT person_id
-        FROM (
-            SELECT person_id, value_source_concept_id, observation_datetime,
-            ROW_NUMBER() OVER(
-                PARTITION BY person_id ORDER BY observation_datetime DESC,
-                value_source_concept_id ASC) AS rn
-            FROM `{project}.{dataset}.observation`
-            WHERE observation_source_value = 'EHRConsentPII_ConsentPermission')
-        WHERE rn=1 AND value_source_concept_id != 1586100)
-AND entry.{table}_id IN (
-    SELECT {table}_id
-    FROM `{project}.{mapping_dataset}._mapping_{table}`
-    WHERE src_dataset_id like '%ehr%')
-"""
+# Find all consented participants.
+# Find all mappings like ehr
+# Join the table with consented participants and join again with mappings
+NOT_CONSENTING_PERSON_IDS = (
+'WITH consented AS ( '
+        'SELECT person_id '
+        'FROM ( '
+            'SELECT person_id, value_source_concept_id, observation_datetime, '
+            'ROW_NUMBER() OVER( '
+                'PARTITION BY person_id ORDER BY observation_datetime DESC, '
+                'value_source_concept_id ASC) AS rn '
+            'FROM `{project}.{dataset}.observation` '
+            'WHERE observation_source_value = \'EHRConsentPII_ConsentPermission\')'
+        'WHERE rn=1 AND value_source_concept_id = 1586100),'
+'unconsented AS ( '
+        'SELECT person_id '
+        'FROM ( '
+            'SELECT person_id, value_source_concept_id, observation_datetime, '
+            'ROW_NUMBER() OVER( '
+                'PARTITION BY person_id ORDER BY observation_datetime DESC, '
+                'value_source_concept_id ASC) AS rn '
+            'FROM `{project}.{dataset}.observation` '
+            'WHERE observation_source_value = \'EHRConsentPII_ConsentPermission\')'
+        'WHERE rn=1 AND value_source_concept_id != 1586100),'
+'ppi_mappings AS ( '
+    'SELECT {table}_id '
+    'FROM `{project}.{dataset}._mapping_{table}` '
+    'WHERE src_dataset_id not like \'%ehr%\') '
+# get all consented rows
+'SELECT {fields} FROM `{project}.{dataset}.{table}` AS entry '
+'RIGHT JOIN consented AS cons '
+'ON entry.person_id = cons.person_id '
+'UNION ALL '
+# get all unconsented non-ehr rows
+'SELECT {fields} FROM `{project}.{dataset}.{table}` AS entry '
+'RIGHT JOIN unconsented as cons on entry.person_id = cons.person_id '
+'JOIN ppi_mappings AS maps ON maps.{table}_id = entry.{table}_id '
+)
 
-DELETE_ORPHANED_PERSON_IDS = """
-DELETE `{project}.{dataset}.{table}` entry
-WHERE entry.person_id NOT IN (SELECT person_id FROM `{project}.{dataset}.person`)
-"""
+# drop rows of person_ids not in the person table
+DELETE_ORPHANED_PERSON_IDS = (
+    'SELECT {fields} FROM `{project}.{dataset}.{table}` AS entry '
+    'RIGHT JOIN `{project}.{dataset}.person` AS person '
+    'ON entry.person_id = person.person_id'
+)
 
 def get_person_id_validation_queries(project=None, dataset=None):
     """
@@ -71,20 +93,42 @@ def get_person_id_validation_queries(project=None, dataset=None):
 
     # generate queries to remove EHR records of non-ehr consented persons
     for table in MAPPED_VALIDATION_TABLES:
+        field_names = ['entry.' + field['name'] for field in resources.fields_for(table)]
+        fields = ', '.join(field_names)
         consent_query = NOT_CONSENTING_PERSON_IDS.format(
-            project=project, dataset=dataset, table=table, mapping_dataset=mapping_ds
+            project=project,
+            dataset=dataset, table=table,
+            mapping_dataset=mapping_ds
+            fields=fields,
         )
-        query_list.append(consent_query)
+
+        query_dict = {
+            'query': consent_query,
+            'destination_table_id': table,
+            'destination_dataset_id': dataset,
+            'write_disposition': 'WRITE_TRUNCATE',
+        }
+        query_list.append(query_dict)
 
     all_tables = MAPPED_VALIDATION_TABLES
     all_tables.extend(UNMAPPED_VALIDATION_TABLES)
 
     # generate queries to remove person_ids of people not in the person table
     for table in all_tables:
+        field_names = [field['name'] for field in resources.fields_for(table)]
+        fields = ', '.join(field_names)
+
         delete_query = DELETE_ORPHANED_PERSON_IDS.format(
-            project=project, dataset=dataset, table=table
+            project=project, dataset=dataset, table=table, fields=fields
         )
-        query_list.append(delete_query)
+
+        query_dict = {
+            'query': delete_query,
+            'destination_table_id': table,
+            'destination_dataset_id': dataset,
+            'write_disposition': 'WRITE_TRUNCATE',
+        }
+        query_list.append(query_dict)
 
     return query_list
 
