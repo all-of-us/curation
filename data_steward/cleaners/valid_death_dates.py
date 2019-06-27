aae7cbf9f75bd428a147617286f9dcc406505936
Nishanth Pavinkurve <npp267@nyu.edu>
2019-06-27 18:06:06 -0400

    Add cleaning rule to remove deaths beyond program period

---
+++ data_steward/cleaners/valid_death_dates.py
@@ -0,0 +1,48 @@
+"""
+A death date is considered "valid" if it is after the program start date and before the current date.
+Allowing for more flexibility, we choose Jan 1, 2017 as the program start date.
+"""
+
+# Project imports
+import constants.bq_utils as bq_consts
+import constants.cleaners.clean_cdr as cdr_consts
+
+death = 'death'
+program_start_date = '2017-01-01'
+current_date = 'CURRENT_DATE()'
+
+KEEP_VALID_DEATH_TABLE_ROWS = (
+    "SELECT * "
+    "FROM `{project_id}.{dataset_id}.{table}` "
+    "WHERE {table}_date > '{program_start_date}' AND {table}_date < {current_date} "
+)
+
+
+def get_valid_death_date_queries(project_id, dataset_id):
+    """
+    This function gets the queries required to keep table records
+    associated with a person whose death date is "valid" described above
+
+    :param project_id: Project name
+    :param dataset_id: Name of the dataset where a rule should be applied
+    :return a list of queries.
+    """
+    queries = []
+    query = dict()
+    query[cdr_consts.QUERY] = KEEP_VALID_DEATH_TABLE_ROWS.format(project_id=project_id,
+                                                                 dataset_id=dataset_id,
+                                                                 table=death,
+                                                                 program_start_date=program_start_date,
+                                                                 current_date=current_date)
+    query[cdr_consts.DESTINATION_TABLE] = death
+    query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
+    query[cdr_consts.DESTINATION_DATASET] = dataset_id
+    queries.append(query)
+    return queries
+
+
+if __name__ == '__main__':
+    import args_parser as parser
+    if parser.args.dataset_id:
+        query_list = get_valid_death_date_queries(parser.args.project_id, parser.args.dataset_id)
+        parser.clean_engine.clean_dataset(parser.args.project_id, parser.args.dataset_id, query_list)
