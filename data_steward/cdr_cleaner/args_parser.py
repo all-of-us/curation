12235d2ece21cc6b439bc44257bae2d378d1afdf
Chao Pang <ChaoPang229@gmail.com>
2019-08-12 11:25:23 -0400

    [DC-402] Ensure alignment of the domain_id in the concept table for each concept_id (#193)
    * added 3 functions to read the csv files for domain alignment
    * expanded arg_parser to accept additional arguments from outside of the package
    * created a new package domain_mapping.py that contains helper functions for the domain alignment task
    * created a new package field_mapping.py for generating pairwise field mappings between any of the two domain tables
    * created a new package domain_alignment.py for aligning the domain_id of the concept_id with the corresponding domain table
    * added field_mapping_regenerated path
    * added get_ehr_rdr_snapshot

--- data_steward/cdr_cleaner/args_parser.py
+++ data_steward/cdr_cleaner/args_parser.py
@@ -1,7 +1,18 @@
 import argparse
 
+SHORT_ARGUMENT = 'argument'
+LONG_ARGUMENT = 'long_argument'
+ACTION = 'action'
+DEST = 'dest'
+HELP = 'help'
+REQUIRED = 'required'
+
 
 def parse_args():
+    return parse_args()
+
+
+def default_parse_args(additional_arguments=None):
     parser = argparse.ArgumentParser(description='Parse project_id and dataset_id',
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-p', '--project_id',
@@ -14,4 +25,13 @@ def parse_args():
                         required=True)
     parser.add_argument('-s', '--console_log', dest='console_log', action='store_true',
                         help='Send logs to console')
+
+    if additional_arguments is not None:
+        for argument in additional_arguments:
+            parser.add_argument(argument[SHORT_ARGUMENT],
+                                argument[LONG_ARGUMENT],
+                                dest=argument[DEST],
+                                action=argument[ACTION],
+                                help=argument[HELP])
+
     return parser.parse_args()
