81dd78f8ba085233577c8cf3cb1e16c7a37bbede
Noah Engel <ne2310@cumc.columbia.edu>
2019-08-30 16:48:34 -0400

    Adding delta_metrics_over_time.py and detect_problem_sites.py. Renamed email_generator file. Improved style for metrics_over_time.

---
+++ data_steward/analytics/tools/detect_problem_sites.py
@@ -0,0 +1,289 @@
+"""
+Developed with Python 2.7
+
+Goals
+-----
+This program should be used to detect sites with problematic
+data quality trends. The DRC (Columbia University Medical Center)
+should intervene with these sites and help them address their
+problems reaching the goals of AoU.
+
+Sites that require interventions are those that:
+    a. have not had ANY improvements in data quality during a
+       specified length of time
+    b. have not reached the thresholds communicated to them by
+       CUMC
+
+
+ASSUMPTIONS
+-----------
+This program relies on the output from two other programs that
+can also be found in the curation GitHub:
+    - metrics_over_time.py
+    - delta_metrics_over_time.py
+
+metrics_over_time.py will allow us to see if the sites are
+reaching their data quality metrics.
+
+delta_metrics_over_time.py will allow us to more easily see
+the 'change' in data quality for a particular site from
+report-to-report.
+
+This program runs on the assumptions that the reports will
+generate the same tabs for the same metric type generated.
+This is validated in delta_metrics_over_time.py.
+
+IMPORTANT NOTES
+---------------
+This program should be:
+    a. versatile
+        the benchmarks that sites should reach can be modified
+        new benchmarks can be added if needed
+    b. standardized
+        all sites should be held to the same AoU standards
+
+FIXME
+-----
+1. Determine whether or not the threshold for 'maximum' values
+(e.g. duplicates) should also apply to the 'total' row that
+is at the bottom of the sheet
+    Currently excluded
+
+2. Determine whether we should 'tag' a site for improvement
+in a particular area if even ONE of the tables is stagnant
+    Currently the site is only flagged for a particular
+    metric if NONE of the tables see improvement over
+    an extended period of time
+"""
+
+import pandas as pd
+import xlrd
+
+
+def get_metric_files_and_tabs():
+    """
+    Function should be used to organize the files that will
+        be used to detect sites that require intervention.
+        This function also organizes the tabs of those files
+        so we have a comprehensive list of the HPO sites.
+
+    :return:
+    all_files (dict):
+        metric (e.g. concept): list
+            the list that is the value has both of the file
+            names that are required to run the analysis
+
+    metric_goals(dict):
+        metric (e.g. concept): value
+            the value is the 'goal' of the metric (e.g. a value
+            of 0 means the 'ideal' is to ultimately have 0 of
+            that instance)
+
+    want_above_metric (dict):
+        metric (e.g. concept): bool
+            the bool indicates if the metric is a 'minimum'
+            that the sight should strive towards. False indicates
+            that the number in metric_goals is a 'maximum.'
+    """
+    metric_goals = {
+        'concept': 70,  # percent
+        'duplicates': 1500,  # total count
+        'end_before_begin': 15  # percent
+    }
+
+    want_above_metric = {
+        'concept': True,
+        'duplicates': False,
+        'end_before_begin': False
+    }
+
+    all_files = {}
+
+    for metric in sorted(metric_goals.keys()):
+        metric_files = []
+
+        analytics_data = metric + '_hpo_sheets_data_analytics.xlsx'
+        analytics_change = metric + '_weekly_changes_hpo_sheets.xlsx'
+
+        metric_files.append(analytics_data)
+        metric_files.append(analytics_change)
+
+        all_files[metric] = metric_files
+
+    return all_files, metric_goals, want_above_metric
+
+
+def find_hpos_not_meeting_benchmarks(hpo_sites,
+                                     files, metric_goals,
+                                     want_above_metric):
+    """
+    Function is used to identify the HPOs that are not
+        meeting at least one of the metrics established
+        by the HPO. This is based on the most recent
+        analytics report.
+
+    :param
+    hpo_site_names (list): list of all the HPO sites whose
+    metrics need to be investigated
+
+    files (dict):
+        metric (e.g. concept): list
+            the list that has both of the file names
+            required to run the analysis
+
+    metric_goals(dict):
+        metric (e.g. concept): value
+            the value is the 'goal' of the metric that the site
+            should aim to be above or below (depending on
+            the value in want_above_metric)
+
+    want_above_metric (dict):
+        metric (e.g. concept): bool
+            the bool indicates if the metric is a 'minimum'
+            that the sight should strive towards. False indicates
+            that the number in metric_goals is a 'maximum.'
+
+    :return
+    problem_hpos (list): contains the HPOs that are
+        not meeting the thresholds (either maximum or minimum)
+        established by the DRC
+    """
+    problem_hpos = {}
+
+    for hpo in hpo_sites:
+        problem_metrics_for_hpo = []
+        for metric in sorted(metric_goals.keys()):
+            file_to_use = files[metric][0]
+
+            threshold = metric_goals[metric]
+            threshold_is_min = want_above_metric[metric]
+
+            df = pd.read_excel(file_to_use, sheet_name=hpo)
+            last_col = df.iloc[:, -1]
+            last_row_idx = len(last_col) - 1
+
+            for row_num, value in enumerate(last_col):
+                if metric not in problem_metrics_for_hpo:
+                    if threshold_is_min and (value < threshold):
+                        problem_metrics_for_hpo.append(metric)
+
+                    # do not want to log if the 'total' row
+                    elif not threshold_is_min and (value > threshold) \
+                            and (row_num != last_row_idx):
+                        problem_metrics_for_hpo.append(metric)
+
+        if problem_metrics_for_hpo:
+            problem_hpos[hpo] = problem_metrics_for_hpo
+
+    return problem_hpos
+
+
+def find_stagnant_hpos(problem_hpos, files, num_reports,
+                       want_above_metric):
+    """
+    Function is used to find HPOs who fail to meet benchmarks
+        established by the DRC and have not made and progress
+        towards improving their data quality for a set number
+        of time.
+
+    :param
+    problem_hpos (list): contains the HPOs that are
+        not meeting the thresholds (either maximum or minimum)
+        established by the DRC
+
+    files (dict):
+        metric (e.g. concept): list
+            the list has both of the file names that are
+            required to run the analysis
+
+    num_reports (int): the number of final columns (number of
+        reports in the data analysis scripts) to look through
+        to ensure full indexing
+
+    want_above_metric (dict):
+        metric (e.g. concept): bool
+            the bool indicates if the metric is a 'minimum'
+            that the sight should strive towards. False indicates
+            that the number in metric_goals is a 'maximum.'
+
+    :return:
+    problem_hpos_with_stagnation (dict):
+        hpo: list
+            the inner list are the metrics that were deemed
+            problematic based on the most recent analytics
+            report AND have not had any improvement in the
+            past x reports (x = num_reports).
+    """
+    problem_hpos_with_stagnation = {}
+
+    for hpo, failing_metrics in problem_hpos.items():
+        metrics_with_no_progress = []
+
+        for metric in failing_metrics:
+            file_to_use = files[metric][1]
+            threshold_is_min = want_above_metric[metric]
+
+            df = pd.read_excel(file_to_use, sheet_name=hpo)
+            saw_improvement = False
+
+            num_cols = len(df.columns)
+            col_idxs = list(range(0, num_cols))
+            earliest_col = num_cols - num_reports
+            cols_to_investigate = col_idxs[num_cols-1: earliest_col - 1: -1]
+
+            for report_col in cols_to_investigate:
+                col_name = df.columns[report_col]
+                col = df[col_name]
+
+                for table_value in col:  # detecting positive changes in DQ
+                    if threshold_is_min and (table_value > 0):
+                        saw_improvement = True
+                    elif not threshold_is_min and (table_value < 0):
+                        saw_improvement = True
+
+            if not saw_improvement:  # no positive changes - log
+                metrics_with_no_progress.append(metric)
+
+        if metrics_with_no_progress:  # there was at least one prob metric
+            problem_hpos_with_stagnation[hpo] = metrics_with_no_progress
+
+    return problem_hpos_with_stagnation
+
+
+file_names, metrics_thresholds, metric_direction = get_metric_files_and_tabs()
+
+# get a metric whose analytics report only has all of the HPOs and an 'aggregate'
+# info tab. allows you to take the names of all of the tabs except for the last tab
+metric_with_only_agg_info = 'end_before_begin'
+sheet_with_hpos = file_names[metric_with_only_agg_info][0]
+
+all_tabs = xlrd.open_workbook(sheet_with_hpos,
+                              on_demand=True).sheet_names()
+
+hpo_site_names = all_tabs[:-1]  # take off aggregate_info
+
+problem_hpos_and_metric_failures = \
+    find_hpos_not_meeting_benchmarks(
+        hpo_site_names, file_names, metrics_thresholds, metric_direction)
+
+num_reports_before_stagnant = 3  # reports generally run on weekly basis
+
+stagnant_problem_hpos = find_stagnant_hpos(
+    problem_hpos_and_metric_failures, file_names, num_reports_before_stagnant,
+    metric_direction)
+
+text_file = open("output.txt", "w")
+
+for site, problem_metrics in stagnant_problem_hpos.items():
+    problems = ""
+    for num, metric_type in enumerate(problem_metrics):
+        problems += "{}. {}\n".format(num+1, metric_type)
+
+    end_message = "{} has issues that have not been improved upon " \
+                  "in {} reports in the following:\n".format(
+                    site, num_reports_before_stagnant) + problems \
+                  + "\n"
+
+    text_file.write(end_message)
+
+text_file.close()
--- data_steward/analytics/tools/email_generator.py
+++ data_steward/analytics/tools/email_generator.py
@@ -14,14 +14,14 @@ import os
 from contact_list import recipient_dict
 
 cwd = os.getcwd()
-excel_file_name = cwd + "\prod_drc_dataset_08232019.xlsx"  # change for each email date
+excel_file_name = cwd + "\\august_26_2019.xlsx"  # change for each email date
 
 duplicates = pd.read_excel(excel_file_name,
                            sheet_name="duplicates")
 end_before_begin = pd.read_excel(excel_file_name,
                                  sheet_name="end_before_begin")
-data_after_death = pd.read_excel(excel_file_name,
-                                 sheet_name="data_after_death")
+concept = pd.read_excel(excel_file_name,
+                        sheet_name="concept")
 
 prompt = "Please input the site ID of the HPO site that " \
          "you would like to use for an auto-generated e-mail. " \
@@ -63,7 +63,7 @@ def determine_row(sheet, site_hpo_id):
     return row_num
 
 
-def get_info(sheet, row_num, percentage):
+def get_info(sheet, row_num, percentage, succ_rate):
     """
     Function is used to create a dictionary that contains
     the number of flawed records for a particular site.
@@ -75,6 +75,9 @@ def get_info(sheet, row_num, percentage):
     percentage (boolean): used to determine whether or not the
                           number is a 'flawed' record count versus
                           the percentage of 'acceptable' records
+    succ_rate (boolean): used to determine if the sheet should only
+                         be looking for the columns of the analytics
+                         report that end with "success_rate"
     :return:
     err_dictionary (dictionary): key:value pairs represent the
                                  column and and number that represents
@@ -84,25 +87,38 @@ def get_info(sheet, row_num, percentage):
     err_dictionary = {}
 
     for col_label, number in data_info.iteritems():
-        if col_label == 'Unnamed: 0':  # first column with row indexes
-            pass
+        if succ_rate:
+            if len(col_label) > 12:
+                if col_label[-12:] == 'success_rate':
+                    try:
+                        number = float(number)
+                        if number < 100:  # concept rate is below 100%
+                            err_dictionary[col_label] = round(100 - number, 1)
+                    except TypeError:  # failed to convert to float
+                        pass
+                    except ValueError:  # 'no info' case
+                        pass
         else:
-            try:
-                float(number)
-                if percentage and number < 100:  # proportion w/ errors
-                    err_dictionary[col_label] = round(100 - number, 1)
-                elif not percentage and number > 0:
-                    err_dictionary[col_label] = int(number)
-            except TypeError:  # failed to convert to float
+            if col_label == 'Unnamed: 0':  # first column with row indexes
                 pass
-            except ValueError:  # cases of 'no info'
-                pass
+            else:
+                try:
+                    number = float(number)
+                    if percentage and number < 100:  # proportion w/ errors
+                        err_dictionary[col_label] = round(100 - number, 1)
+                    elif not percentage and number > 0:
+                        err_dictionary[col_label] = int(number)
+                except TypeError:  # failed to convert to float
+                    pass
+                except ValueError:  # cases of 'no info'
+                    pass
 
     return err_dictionary
 
 
 row_idx_dups = determine_row(duplicates, hpo_id)
-duplicate_err_dict = get_info(duplicates, row_idx_dups, percentage=False)
+duplicate_err_dict = get_info(duplicates, row_idx_dups, percentage=False,
+                              succ_rate=False)
 
 # let's get the full name of the HPO. should be same across all sheets
 hpo_full_name = 9999  # should be reset
@@ -117,18 +133,18 @@ if hpo_full_name == 9999:
 
 row_idx_end_before_begin = determine_row(end_before_begin, hpo_id)
 end_before_begin_dict = get_info(end_before_begin,
-                                 row_idx_end_before_begin, percentage=True)
+                                 row_idx_end_before_begin, percentage=True,
+                                 succ_rate=False)
 
-row_idx_data_after_death = determine_row(data_after_death, hpo_id)
-data_after_death_dict = get_info(data_after_death,
-                                 row_idx_data_after_death, percentage=True)
+row_idx_concept = determine_row(concept, hpo_id)
+concept_dict = get_info(concept, row_idx_concept, percentage=True, succ_rate=True)
 
 # RECAP: dictionaries storing the data quality errors for that particular site
 
 # want to know the number of sheets w/ errors for numbering purposes
 num_prob_sheets = 0
 for sheet_info in [duplicate_err_dict,
-                   end_before_begin_dict, data_after_death_dict]:
+                   end_before_begin_dict, concept_dict]:
     if sheet_info:
         num_prob_sheets += 1
 
@@ -190,7 +206,8 @@ if num_prob_sheets == 0:
 else:
     starting_number = 1  # value for the 'problem number'
 
-    print("We found the following error(s) with your submission: \n")
+    print("We found the following error(s) with your most recent "
+          "submission: \n")
 
     if duplicate_err_dict:  # there are problems with the error dict
         duplicates_err = str(starting_number) + \
@@ -210,36 +227,29 @@ else:
         starting_number += 1
         print(end_before_err + "\n")
 
-    if data_after_death_dict:
-        data_after_death_err = str(starting_number) + \
-                               ". There are data points in the that " \
-                               "are more than 30 days after the death " \
-                               "date for deceased patients in your " \
-                               "submission. This affects the following " \
-                               "table(s):"
-        data_after_death_err = print_error_info(data_after_death_dict,
-                                                data_after_death_err,
-                                                percent=True)
-        data_after_death_err = data_after_death_err + " "\
-            "This could be due to death dates that " \
-            "are prior to the start of " \
-            "the All of Us Research Program."
-        print(data_after_death_err + "\n")
+    if concept_dict:
+        concept_err = str(starting_number) + \
+                               ". The concept success rates for some of " \
+                               "the tables is below 100%. This affects the " \
+                               "following table(s):"
+        concept_err = print_error_info(concept_dict,
+                                       concept_err,
+                                       percent=True)
+        print(concept_err + "\n")
 
-    due_date = "July 28th, 2019"
-
-    github_link = 'https://github.com/all-of-us/curation/' \
+github_link = 'https://github.com/all-of-us/curation/' \
                   'tree/develop/data_steward/analytics'
 
-    sign_off = "Please fix the issue(s) from this e-mail and " \
-               "resubmit all tables by {}. \n\n" \
-               "You may also see the queries used to generate " \
-               "these analytics at the following site: {}. " \
-               "Do not hesitate to reach out to the " \
-               "curation team if you have any " \
-               "questions. ".format(due_date, github_link)
+sign_off = "Please aim to fix the issue(s) (if applicable) " \
+           "from this emails and continue to routinely upload " \
+           "your files so we can continue to track your " \
+           "progress. You may also see the queries used " \
+           "to generate these analytics at the following site: {}. " \
+           "Do not hesitate to reach out to the " \
+           "curation team if you have any " \
+           "questions. ".format(github_link)
 
-    print(sign_off)
+print(sign_off)
 
 # now let's print the contacts
 print("\nContact the following individual(s):")
