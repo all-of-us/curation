9fd74bc455b30480cf86c930039f3e93cef8cff4
Mark Velez <mav7014@nyp.org>
2019-09-03 12:04:30 -0400

    Add sync param to table_copy.sh which is true by default

--- data_steward/tools/table_copy.sh
+++ data_steward/tools/table_copy.sh
@@ -11,8 +11,16 @@
 # ## Copy `dataset1.tableA` to `dataset1.prefix_tableA`
 # > tools/table_copy.sh --source_dataset dataset1 --target_prefix prefix_ --target_dataset dataset1
 
-USAGE="tools/table_copy.sh [--source_app_id <SOURCE_APPLICATION_ID>] [--target_app_id <TARGET_APPLICATION_ID>] --source_dataset <SOURCE_DATASET> [--source_prefix <SOURCE_PREFIX:''>] --target_dataset <TARGET_DATASET> [--target_prefix <TARGET_PREFIX:''>]"
+USAGE="tools/table_copy.sh
+[--source_app_id <SOURCE_APPLICATION_ID>]
+[--target_app_id <TARGET_APPLICATION_ID>]
+--source_dataset <SOURCE_DATASET>
+[--source_prefix <SOURCE_PREFIX:''>]
+--target_dataset <TARGET_DATASET>
+[--target_prefix <TARGET_PREFIX:''>]
+[--sync <true|false:true>]"
 
+SYNC='true'
 while true; do
   case "$1" in
     --source_app_id) SOURCE_APPLICATION_ID=$2; shift 2;;
@@ -21,6 +29,7 @@ while true; do
     --target_dataset) TARGET_DATASET=$2; shift 2;;
     --source_prefix) SOURCE_PREFIX=$2; shift 2;;
     --target_prefix) TARGET_PREFIX=$2; shift 2;;
+    --sync) SYNC=$2 shift 2;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -57,6 +66,6 @@ for t in $(bq ls -n 2000 ${SOURCE_DATASET} |
            ( [[ "${SOURCE_PREFIX}" ]] && grep ${SOURCE_PREFIX} || cat ))
 do
   TARGET_TABLE=${t//${SOURCE_PREFIX}/} #replace all occurrences, use ${parameter//pattern/replacement_string}
-  CP_CMD="bq cp --sync=false -f ${SOURCE_DATASET}.${t} ${TARGET_DATASET}.${TARGET_PREFIX}${TARGET_TABLE}"
+  CP_CMD="bq cp --sync=${SYNC} -f ${SOURCE_DATASET}.${t} ${TARGET_DATASET}.${TARGET_PREFIX}${TARGET_TABLE}"
   echo $(${CP_CMD})
 done
