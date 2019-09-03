2b772a7e32e20bd06eaeb370639712be3179f2d5
Chao Pang <cp3016@cumc.columbia.edu>
2019-09-03 13:39:26 -0400

    refactored admin_api.py

--- data_steward/admin/admin_api.py
+++ data_steward/admin/admin_api.py
@@ -35,18 +35,24 @@ created_at={create_at}
 
 PREFIX = '/admin/v1/'
 REMOVE_EXPIRED_KEYS_RULE = PREFIX + 'RemoveExpiredServiceAccountKeys'
-LIST_EXPIRING_KEYS_RULE = PREFIX + 'ListExpiringServiceAccountKeys'
 
 app = Flask(__name__)
 
 
 def assemble_email_body(keys, delete_action):
-    email_body = BODY_HEADER_EXPIRED_KEY_TEMPLATE if delete_action else BODY_HEADER_EXPIRING_KEY_TEMPLATE
-
-    for _key in keys:
-        email_body += BODY_TEMPLATE.format(service_account_email=_key['service_account_email'],
-                                           key_name=_key['key_name'],
-                                           created_at=_key['created_at'])
+    """
+    This assemble an email body using the keys
+    :param keys:
+    :param delete_action:
+    :return: the email body
+    """
+    email_body = ''
+    if len(keys) != 0:
+        email_body = BODY_HEADER_EXPIRED_KEY_TEMPLATE if delete_action else BODY_HEADER_EXPIRING_KEY_TEMPLATE
+        for _key in keys:
+            email_body += BODY_TEMPLATE.format(service_account_email=_key['service_account_email'],
+                                               key_name=_key['key_name'],
+                                               created_at=_key['created_at'])
     return email_body
 
 
@@ -54,21 +60,31 @@ def assemble_email_body(keys, delete_action):
 def remove_expired_keys():
     project_id = app_identity.get_application_id()
     logging.info('Started removal of expired service account keys for %s' % project_id)
-    # suppressing so tests do not disrupt
+
     expired_keys = key_rotation.delete_expired_keys(project_id)
     logging.info('Completed removal of expired service account keys for %s' % project_id)
 
+    logging.info('Started listing expiring service account keys for %s' % project_id)
+    expiring_keys = key_rotation.get_expiring_keys(project_id)
+    logging.info('Completed listing expiring service account keys for %s' % project_id)
+
     if NOTIFICATION_ADDRESS is not None:
-        try:
-            mail.send_mail(sender=SENDER_ADDRESS,
-                           to=NOTIFICATION_ADDRESS,
-                           subject=SUBJECT,
-                           body=assemble_email_body(expired_keys, True))
-        except (
-                HttpError):
-            LOGGER.exception(
-                "Failed to send to ${notification_address}"
-            )
+
+        if len(expired_keys) != 0 and len(expired_keys) != 0:
+
+            email_body = assemble_email_body(expired_keys, True)
+            email_body += assemble_email_body(expiring_keys, False)
+
+            try:
+                mail.send_mail(sender=SENDER_ADDRESS,
+                               to=NOTIFICATION_ADDRESS,
+                               subject=SUBJECT,
+                               body=email_body)
+            except (
+                    HttpError):
+                LOGGER.exception(
+                    "Failed to send to ${notification_address}"
+                )
     else:
         LOGGER.exception(
             "The notification address is None"
@@ -77,40 +93,7 @@ def remove_expired_keys():
     return 'remove-expired-keys-complete'
 
 
-@api_util.auth_required_cron
-def list_expiring_keys():
-
-    project_id = app_identity.get_application_id()
-    expired_keys = key_rotation.get_expiring_keys(project_id)
-
-    if NOTIFICATION_ADDRESS is not None:
-        try:
-            mail.send_mail(sender=SENDER_ADDRESS,
-                           to=NOTIFICATION_ADDRESS,
-                           subject=SUBJECT,
-                           body=assemble_email_body(expired_keys, False))
-        except (
-                HttpError):
-            LOGGER.exception(
-                "Failed to send to ${notification_address}"
-            )
-    else:
-        LOGGER.exception(
-            "The notification address is None"
-        )
-
-    return 'list-expiring-keys-complete'
-
-
 app.add_url_rule(
     REMOVE_EXPIRED_KEYS_RULE,
     view_func=remove_expired_keys,
     methods=['GET'])
-
-app.add_url_rule(
-    LIST_EXPIRING_KEYS_RULE,
-    view_func=list_expiring_keys,
-    methods=['GET'])
-
-if __name__ == '__main__':
-    remove_expired_keys()
