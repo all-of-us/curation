7fc1fa2c0cc079822dd916ce3c1ce01dd8391364
Krishna Kalluri <krishnasaikalluri@gmail.com>
2019-09-10 14:37:29 -0400

    Removes deleting duplicate ids part from this script.

--- data_steward/cdr_cleaner/manual_cleaning_rules/AC68-Exclude_fake_ghost_withdrawn_test_duplicate_participants.py
+++ data_steward/cdr_cleaner/manual_cleaning_rules/AC68-Exclude_fake_ghost_withdrawn_test_duplicate_participants.py
@@ -1,5 +1,5 @@
 """
-Removes  duplicate, fake, ghost, withdrawn, and test participants from observation table the CDR
+Removes fake, ghost, withdrawn, and test participants from observation table the CDR
 
 TODO Get the pids of duplicate participants(AC-69)
      Get the pids of fake, ghost and withdrawn participants(AC-68) from the team drive before running this script.
@@ -55,40 +55,17 @@ def get_delete_ghost_fake_participants_query(project_id, dataset_id, pids):
     return query
 
 
-def get_delete_duplicate_participants_query(project_id, dataset_id, pids):
-    """
-    gets the query for deleting the ghost, fake and withdrawn participants from the CDR.
-    :param project_id: identifies the project containing the dataset
-    :param dataset_id: identifies the dataset containing the OMOP data
-    :param pids: pids of duplicate participants extracted from a external file
-    :return:
-    """
-    query = dict()
-    query[cdr_consts.QUERY] = DELETE_OBSERVATION_RECORDS.format(project_id=project_id,
-                                                                dataset_id=dataset_id,
-                                                                records=pids,
-                                                                )
-    query[cdr_consts.DESTINATION_TABLE] = OBSERVATION
-    query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
-    query[cdr_consts.DESTINATION_DATASET] = dataset_id
-
-    return query
-
-
-def main(project_id, dataset_id, fake_ghost_ids, duplicate_participants_ids):
+def main(project_id, dataset_id, fake_ghost_ids):
     """
     takes in the file path and gets the person ids. gets queries to delete the records with the pids collected
     from the external file.
     :param project_id: identifies the project containing the dataset
     :param dataset_id: identifies the dataset containing the OMOP data
     :param fake_ghost_ids: path to csv file containing pids of ghost, fake, test and withdrawn participants
-    :param duplicate_participants_ids: path to csv file containing pids of duplicate participants
-    :return: a list of query dict to remove the duplicate, fake, ghost, test participants
+    :return: a list of query dict to remove the fake, ghost, test participants
     """
     queries = []
     fake_ghost_ids = extract_pids_from_csv(fake_ghost_ids)
-    duplicate_participants_ids = extract_pids_from_csv(duplicate_participants_ids)
-    queries.append(get_delete_duplicate_participants_query(project_id, dataset_id, duplicate_participants_ids))
     queries.append(get_delete_ghost_fake_participants_query(project_id, dataset_id, fake_ghost_ids))
     return queries
 
@@ -100,20 +77,14 @@ def parse_args():
     """
     import cdr_cleaner.args_parser as parser
 
-    additional_argument_1 = {parser.SHORT_ARGUMENT: '-ghost_ids',
+    additional_argument_1 = {parser.SHORT_ARGUMENT: '-file_path',
                              parser.LONG_ARGUMENT: '--fake_ghost_ids',
                              parser.ACTION: 'store',
                              parser.DEST: 'fake_ghost_ids',
                              parser.HELP: 'path to csv file containing fake, ghost and withdrawn participants',
                              parser.REQUIRED: True}
-    additional_argument_2 = {parser.SHORT_ARGUMENT: '-dup_ids',
-                             parser.LONG_ARGUMENT: '--duplicate_participants_ids',
-                             parser.ACTION: 'store',
-                             parser.DEST: 'duplicate_participants_ids',
-                             parser.HELP: 'path to csv file containing duplicate participants',
-                             parser.REQUIRED: True}
 
-    args = parser.default_parse_args([additional_argument_1, additional_argument_2])
+    args = parser.default_parse_args([additional_argument_1])
     return args
 
 
@@ -123,5 +94,5 @@ if __name__ == '__main__':
     ARGS = parse_args()
 
     clean_engine.add_console_logging(ARGS.console_log)
-    query_list = main(ARGS.project_id, ARGS.dataset_id, ARGS.fake_ghost_ids, ARGS.duplicate_participants_ids)
+    query_list = main(ARGS.project_id, ARGS.dataset_id, ARGS.fake_ghost_ids)
     clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id, query_list)
