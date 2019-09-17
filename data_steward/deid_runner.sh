3acadf4227314efa682a6b655a910f480fd5bebb
Mark Velez <mav7014@nyp.org>
2019-09-17 13:01:27 -0400

    Update deid_runner.sh
     * Upgrade pyasn1* in base requirements.txt
     * Separate virtualenv for deid
     * Use absolute paths
     * Read APPLICATION_ID from key_file
     * Derive site packages path
     * TODO: must resolve ${version}

--- data_steward/deid_runner.sh
+++ data_steward/deid_runner.sh
@@ -6,7 +6,6 @@
 USAGE="
 Usage: deid_runner.sh
   --key_file <path to key file>
-  --app_id <application id>
   --cdr_id <combined_dataset name>
   --vocab_dataset <vocabulary dataset name>
 "
@@ -15,10 +14,6 @@ while true; do
   case "$1" in
   --cdr_id)
     cdr_id=$2
-    shift 2
-    ;;
-  --app_id)
-    app_id=$2
     shift 2
     ;;
   --key_file)
@@ -37,58 +32,64 @@ while true; do
   esac
 done
 
-if [[ -z "${key_file}" ]] || [[ -z "${cdr_id}" ]] || [[ -z "${app_id}" ]] || [[ -z "${cdr_id}" ]]; then
+if [[ -z "${key_file}" ]] || [[ -z "${cdr_id}" ]] || [[ -z "${cdr_id}" ]]; then
   echo "Specify the key file location and input dataset name application id and vocab dataset name. $USAGE"
   exit 1
 fi
 
-current_dir=$(pwd)
-
-echo "app_id --> ${app_id}"
 echo "key_file --> ${key_file}"
 echo "cdr_id --> ${cdr_id}"
 echo "vocab_dataset --> ${vocab_dataset}"
-echo "currend Dir --> ${current_dir}"
 
+APP_ID=$(cat ${key_file} | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["project_id"]);')
 export GOOGLE_APPLICATION_CREDENTIALS="${key_file}"
-export APPLICATION_ID="${app_id}"
+export APPLICATION_ID="${APP_ID}"
 
 #set application environment (ie dev, test, prod)
 gcloud auth activate-service-account --key-file="${key_file}"
-gcloud config set project "${app_id}"
+gcloud config set project "${APP_ID}"
 
 cdr_deid="${cdr_id}_deid"
 
 #------Create de-id virtual environment----------
 set -e
-# create a new environment in directory deid_env
 
+# create a new environment in directory deid_env
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DEID_DIR="${DIR}/deid"
 GCLOUD_PATH=$(which gcloud)
 CLOUDSDK_ROOT_DIR=${GCLOUD_PATH%/bin/gcloud}
 GAE_SDK_ROOT="${CLOUDSDK_ROOT_DIR}/platform/google_appengine"
+GAE_SDK_APPENGINE="${GAE_SDK_ROOT}/google/appengine"
+GAE_SDK_NET="${GAE_SDK_ROOT}/google/net"
+VENV_DIR="${DIR}/deid_venv"
 
-# activate it
-source ../venv/bin/activate
+virtualenv --python=$(which python) ${VENV_DIR}
+
+source ${VENV_DIR}/bin/activate
+
 # install the requirements in the virtualenv
-pip install -r requirements.txt -t ../lib/
-pip install -r deid/requirements.txt
+pip install -r "${DIR}/requirements.txt"
+pip install -r "${DEID_DIR}/requirements.txt"
 
-cp -R "${GAE_SDK_ROOT}"/google/appengine ../venv/lib/python2.7/site-packages/google/
-cp -R "${GAE_SDK_ROOT}"/google/net ../venv/lib/python2.7/site-packages/google/
+VENV_LIB_GOOGLE="$(python -c "import google as _; print(_.__path__[-1])")"
+
+cp -R ${GAE_SDK_APPENGINE} ${VENV_LIB_GOOGLE}
+cp -R ${GAE_SDK_NET} ${VENV_LIB_GOOGLE}
 
 export BIGQUERY_DATASET_ID="${cdr_deid}"
+export PYTHONPATH="${PYTHONPATH}:${DEID_DIR}:${DIR}"
 
 # create empty de-id dataset
-bq mk --dataset --description "${version} deid ${cdr}" ${app_id}:${cdr_deid}
+bq mk --dataset --description "${version} deid ${cdr}" ${APP_ID}:${cdr_deid}
 
-#Create the clinical tables for unioned EHR data set
-PYTHONPATH=$PYTHONPATH:./:./lib python cdm.py ${cdr_deid}
+# create the clinical tables
+python "${DIR}/cdm.py" ${cdr_deid}
 
-#Copy OMOP vocabulary to CDR EHR data set
-PYTHONPATH=$PYTHONPATH:./:./lib python cdm.py --component vocabulary ${cdr_deid}
-tools/table_copy.sh --source_app_id ${app_id} --target_app_id ${app_id} --source_dataset ${vocab_dataset} --target_dataset ${cdr_deid}
+# copy OMOP vocabulary
+python "${DIR}/cdm.py" --component vocabulary ${cdr_deid}
+${DIR}/tools/table_copy.sh --source_app_id ${APP_ID} --target_app_id ${APP_ID} --source_dataset ${vocab_dataset} --target_dataset ${cdr_deid}
 
-PYTHONPATH=$PYTHONPATH:./:./lib python run_deid.py --idataset "${cdr_id}" -p "${key_file}" -a submit --interactive |& tee -a deid_output.txt
+python "${DIR}/run_deid.py" --idataset "${cdr_id}" -p "${key_file}" -a submit --interactive |& tee -a deid_output.txt
 
-# deavtivate the virtual environment
 deactivate
