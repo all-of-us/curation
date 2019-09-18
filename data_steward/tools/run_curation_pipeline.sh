eea37fc3f3729948788dcada0ce5daf23e913bf6
Krishna Kalluri <krishnasaikalluri@gmail.com>
2019-09-18 13:54:25 -0400

    fixed deid_runner parameters in run_curation_pipeline [ci skip]

--- data_steward/tools/run_curation_pipeline.sh
+++ data_steward/tools/run_curation_pipeline.sh
@@ -110,7 +110,7 @@ export BUCKET_NAME_NYC="test-bucket"
 echo "-------------------------->Run de identification on the identified CDR"
 cdr_deid="${cdr}_deid"
 echo "cdr_deid --> ${cdr_deid}"
-./deid_runner.sh --key_file ${key_file} --app_id ${app_id} --cdr_id ${cdr}
+./deid_runner.sh --key_file ${key_file} --cdr_id ${cdr} --vocab_dataset ${vocab_dataset}
 
 cd tools || exit
 
