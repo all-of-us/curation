4ff5b1f7a83aae0190f5946b17168426326c1248
Mark Velez <mav7014@nyp.org>
2019-09-18 18:34:29 -0400

    Update test runner and set_path for GAE py3

--- data_steward/tools/set_path.sh
+++ data_steward/tools/set_path.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -e
 
-# Sets environment variables used to run Python with the AppEngine SDK.
 # Note: On Windows cygpath executable must be in current PATH in order to normalize paths
-
-GCLOUD_PATH=$(which gcloud)
-CLOUDSDK_ROOT_DIR=${GCLOUD_PATH%/bin/gcloud}
-APPENGINE_HOME="${CLOUDSDK_ROOT_DIR}/platform/appengine-java-sdk"
-GAE_SDK_ROOT="${CLOUDSDK_ROOT_DIR}/platform/google_appengine"
 
 # Determine separator to use for directories and PYTHONPATH
 SEP=':'
@@ -25,29 +19,9 @@ norm_path () {
   fi
 }
 
-# The next line enables Python libraries for Google Cloud SDK
-GAEPATH=$(norm_path ${GAE_SDK_ROOT})
+DATA_STEWARD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+export DATA_STEWARD_DIR="$(norm_path ${DATA_STEWARD_DIR})"
 
-# * OPTIONAL STEP *
-# If you wish to import all Python modules, you may iterate in the directory
-# tree and import each module.
-#
-# * WARNING *
-# Some modules have two or more versions available (Ex. django), so the loop
-# will import always its latest version.
-for module in ${GAE_SDK_ROOT}/lib/*; do
-  if [ -r ${module} ];
-  then
-    expr=$(norm_path ${module})
-    GAEPATH="${expr}${SEP}${GAEPATH}"
-  fi
-done
-unset module
-
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-export BASE_DIR="$(norm_path ${BASE_DIR})"
-LIB_DIR="${BASE_DIR}${PATHSEP}lib"
-ROOT_REPO_DIR="$( cd "${BASE_DIR}" && cd .. && pwd )"
 # libs should appear first in PYTHONPATH so we can override versions from
 # the GAE SDK. (Specifically, we need oauth2client >= 4.0.0 and GAE uses 1.x.)
-export PYTHONPATH=${PYTHONPATH}${SEP}${BASE_DIR}${SEP}${LIB_DIR}${SEP}${GAEPATH}
+export PYTHONPATH=${PYTHONPATH}${SEP}${DATA_STEWARD_DIR}
