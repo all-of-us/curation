6c17dcc66181c427920fb7f9915f233bfefa2f7d
Mark Velez <mav7014@nyp.org>
2019-09-18 14:33:35 -0400

    Apply fix_imports_six

--- data_steward/tools/consolidated_reports/curation_report/server.py
+++ data_steward/tools/consolidated_reports/curation_report/server.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
-import SimpleHTTPServer
-import SocketServer
+import six.moves.SimpleHTTPServer
+import six.moves.socketserver
 
 
 PORT = 8000
 
 
-class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class Handler(six.moves.SimpleHTTPServer.SimpleHTTPRequestHandler):
     pass
 
 
 Handler.extensions_map['.json'] = 'application/json'
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = six.moves.socketserver.TCPServer(("", PORT), Handler)
 
 
 print("serving at port {port}".format(port=PORT))
