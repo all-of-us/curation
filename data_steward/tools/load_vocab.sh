ddd6f956c99c84f3528ab6982ede0c0b3e564144
Krishna Kalluri <krishnasaikalluri@gmail.com>
2019-09-23 14:02:41 -0400

    add replace flag in load_vocab.sh reformat load_vocab.sh [ci skip]

--- data_steward/tools/load_vocab.sh
+++ data_steward/tools/load_vocab.sh
@@ -13,22 +13,39 @@ tools/load_vocab.sh --app_id app_id --in_dir in_dir [--gcs_path gcs_path --datas
  --gcs_path gcs_path full GCS path to save transformed files
  --dataset  dataset  name of BigQuery dataset to create and load vocabulary
 "
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-BASE_DIR="$( cd ${SCRIPT_PATH} && cd .. && pwd )"
+SCRIPT_PATH="$(
+  cd "$(dirname "$0")"
+  pwd -P
+)"
+BASE_DIR="$(cd ${SCRIPT_PATH} && cd .. && pwd)"
 OMOP_VOCABULARY_CONCEPT_ID="44819096"
 while true; do
   case "$1" in
-    --app_id) APP_ID=$2; shift 2;;
-    --in_dir) IN_DIR=$2; shift 2;;
-    --dataset) DATASET=$2; shift 2;;
-    --gcs_path) GCS_PATH=$2; shift 2;;
-    -- ) shift; break ;;
-    * ) break ;;
+  --app_id)
+    APP_ID=$2
+    shift 2
+    ;;
+  --in_dir)
+    IN_DIR=$2
+    shift 2
+    ;;
+  --dataset)
+    DATASET=$2
+    shift 2
+    ;;
+  --gcs_path)
+    GCS_PATH=$2
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *) break ;;
   esac
 done
 
-if [[ -z "${APP_ID}" ]] || [[ -z "${IN_DIR}" ]]
-then
+if [[ -z "${APP_ID}" ]] || [[ -z "${IN_DIR}" ]]; then
   echo "Usage: $USAGE"
   exit 1
 fi
@@ -60,8 +77,7 @@ python ${BASE_DIR}/vocabulary.py add_aou_general --in_dir ${TEMP_DIR} --out_dir 
 rm -rf ${TEMP_DIR}
 
 # Upload to bucket
-if [[ -z "${GCS_PATH}" ]]
-then
+if [[ -z "${GCS_PATH}" ]]; then
   echo "GCS path not specified, skipping GCS upload and BigQuery load"
   exit 0
 fi
@@ -69,19 +85,17 @@ echo "Uploading ${IN_DIR}/* to ${GCS_PATH}..."
 gsutil -m cp ${IN_DIR}/* ${GCS_PATH}
 
 # Load in BigQuery dataset
-if [[ -z "${DATASET}" ]]
-then
+if [[ -z "${DATASET}" ]]; then
   echo "Dataset not specified, skipping BigQuery load"
   exit 0
 fi
 echo "Creating and loading dataset ${DATASET}..."
 bq mk --project_id ${APP_ID} --dataset_id ${DATASET} --description "Vocabulary ${OMOP_VOCABULARY_VERSION} loaded from ${GCS_PATH}"
-for file in $(gsutil ls ${GCS_PATH})
-do
- filename=$(basename ${file})
- # bash3 friendly lowercase
- filename_lower=$(echo ${filename} | tr '[:upper:]' '[:lower:]')
- table_name="${filename_lower%.*}"
- echo "Loading ${DATASET}.${table_name}..."
- bq load --project_id ${APP_ID} --source_format CSV --quote "" --field_delimiter "\t" --max_bad_records 500 --skip_leading_rows 1 ${DATASET}.${table_name} ${file} ${BASE_DIR}/resources/fields/${table_name}.json
+for file in $(gsutil ls ${GCS_PATH}); do
+  filename=$(basename ${file})
+  # bash3 friendly lowercase
+  filename_lower=$(echo ${filename} | tr '[:upper:]' '[:lower:]')
+  table_name="${filename_lower%.*}"
+  echo "Loading ${DATASET}.${table_name}..."
+  bq load --replace=true --project_id ${APP_ID} --source_format CSV --quote "" --field_delimiter "\t" --max_bad_records 500 --skip_leading_rows 1 ${DATASET}.${table_name} ${file} ${BASE_DIR}/resources/fields/${table_name}.json
 done
