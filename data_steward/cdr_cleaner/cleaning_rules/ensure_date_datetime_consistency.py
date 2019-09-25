f54ad841c0ca19a9ee50faaa93d167078451dc7c
Nishanth Pavinkurve <npp267@nyu.edu>
2019-09-25 17:29:36 -0400

    Add cleaning rule to fix datetime issues

--- data_steward/cdr_cleaner/cleaning_rules/ensure_date_datetime_consistency.py
+++ data_steward/cdr_cleaner/cleaning_rules/ensure_date_datetime_consistency.py
@@ -3,21 +3,52 @@ import constants.bq_utils as bq_consts
 import constants.cdr_cleaner.clean_cdr as cdr_consts
 import common
 
-table_dates = {'condition_start_date': 'condition_start_datetime',
-               'condition_end_date': 'condition_end_datetime',
-               'drug_exposure_start_date': 'drug_exposure_start_datetime',
-               'drug_exposure_end_date': 'drug_exposure_end_datetime',
-               'device_exposure_start_date': 'device_exposure_start_datetime',
-               'device_exposure_end_date': 'device_exposure_end_datetime',
-               'measurement_date': 'measurement_datetime',
-               'observation_date': 'observation_datetime',
-               'procedure_date': 'procedure_datetime',
-               'death_date': 'death_datetime',
-               'specimen_date': 'specimen_datetime',
-               'observation_period_start_date': 'observation_period_start_datetime',
-               'observation_period_end_date': 'observation_period_end_datetime',
-               'visit_start_date': 'visit_start_datetime',
-               'visit_end_date': 'visit_end_datetime'}
+TABLE_DATES = {
+    common.CONDITION_OCCURRENCE: {'condition_start_datetime': 'condition_start_date',
+                                  'condition_end_datetime': 'condition_end_date'},
+    common.DRUG_EXPOSURE: {'drug_exposure_start_datetime': 'drug_exposure_start_date',
+                           'drug_exposure_end_datetime': 'drug_exposure_end_date'},
+    common.DEVICE_EXPOSURE: {'device_exposure_start_datetime': 'device_exposure_start_date',
+                             'device_exposure_end_datetime': 'device_exposure_end_date'},
+    common.MEASUREMENT: {'measurement_datetime': 'measurement_date'},
+    common.OBSERVATION: {'observation_datetime': 'observation_date'},
+    common.PROCEDURE_OCCURRENCE: {'procedure_datetime': 'procedure_date'},
+    common.DEATH: {'death_datetime': 'death_date'},
+    common.SPECIMEN: {'specimen_datetime': 'specimen_date'},
+    common.OBSERVATION_PERIOD: {'observation_period_start_datetime': 'observation_period_start_date',
+                                'observation_period_end_datetime': 'observation_period_end_date'},
+    common.VISIT_OCCURRENCE: {'visit_start_datetime': 'visit_start_date',
+                              'visit_end_datetime': 'visit_end_date'}
+}
+
+FIX_DATETIME_QUERY = """
+SELECT {cols}
+FROM `{project_id}.{dataset_id}.{table_id}`
+"""
+
+
+def get_cols(table_id):
+    table_fields = field_mapping.get_domain_fields(table_id)
+    col_exprs = []
+    for field in table_fields:
+        if field in TABLE_DATES[table_id]:
+            if field_mapping.is_field_required(table_id, field):
+                col_expr = (' CASE'
+                            ' WHEN EXTRACT(DATE FROM {field}) = {date_field}'
+                            ' THEN {field}'
+                            ' ELSE CAST(DATETIME({date_field}, EXTRACT(TIME FROM {field})) AS TIMESTAMP)'
+                            ' END AS {field}').format(field=field, date_field=TABLE_DATES[table_id][field])
+            else:
+                col_expr = (' CASE'
+                            ' WHEN EXTRACT(DATE FROM {field}) = {date_field}'
+                            ' THEN {field}'
+                            ' ELSE NULL'
+                            ' END AS {field}').format(field=field, date_field=TABLE_DATES[table_id][field])
+        else:
+            col_expr = field
+        col_exprs.append(col_expr)
+    cols = ', '.join(col_exprs)
+    return cols
 
 
 def get_remove_records_with_wrong_datetime_queries(project_id, dataset_id):
@@ -29,7 +60,16 @@ def get_remove_records_with_wrong_datetime_queries(project_id, dataset_id):
     :return: a list of query dicts for ensuring the dates and datetimes are consistent
     """
     queries = []
-
+    for table in TABLE_DATES:
+        query = dict()
+        query[cdr_consts.QUERY] = FIX_DATETIME_QUERY.format(project_id=project_id,
+                                                            dataset_id=dataset_id,
+                                                            table_id=table,
+                                                            cols=get_cols(table))
+        query[cdr_consts.DESTINATION_TABLE] = table
+        query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
+        query[cdr_consts.DESTINATION_DATASET] = dataset_id
+        queries.append(query)
     return queries
 
 
@@ -40,6 +80,5 @@ if __name__ == '__main__':
     ARGS = parser.parse_args()
 
     clean_engine.add_console_logging(ARGS.console_log)
-    query_list = get_remove_records_with_wrong_datetime_queries(ARGS.project_id,
-                                                                ARGS.dataset_id)
+    query_list = get_remove_records_with_wrong_datetime_queries(ARGS.project_id, ARGS.dataset_id)
     clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id, query_list)
