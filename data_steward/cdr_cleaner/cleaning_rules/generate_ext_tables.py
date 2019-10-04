97bf3a96ef2d48a6b2502dec14a8b205c78d8708
Nishanth Pavinkurve <npp267@nyu.edu>
2019-10-04 18:17:18 -0400

    Added cleaning rule for hpo mappings

--- data_steward/cdr_cleaner/cleaning_rules/generate_ext_tables.py
+++ data_steward/cdr_cleaner/cleaning_rules/generate_ext_tables.py
@@ -1,9 +1,9 @@
 import random
+
 import bq_utils
-
-
-# create mapping table
 import resources
+import constants.bq_utils as bq_consts
+import constants.cdr_cleaner.clean_cdr as cdr_consts
 
 EXT_FIELD_TEMPLATE = [
     {
@@ -36,6 +36,21 @@ SITE_MAPPING_FIELDS = [
 EXT_TABLE_SUFFIX = '_ext'
 MAPPING_PREFIX = '_mapping_'
 SITE_TABLE_ID = 'site_mappings'
+EHR_SITE_PREFIX = 'EHR site '
+RDR = 'rdr'
+PPI_PM = 'PPI/PM'
+
+INSERT_SITE_MAPPINGS_QUERY = """
+INSERT INTO `{project_id}.{dataset_id}.{table_id}` (hpo_id, src_id)
+VALUES{values}
+"""
+
+REPLACE_SRC_QUERY = """
+SELECT m.{cdm_table_id}_id, s.src_id
+FROM `{project_id}.{dataset_id}.{mapping_table_id}` m
+JOIN `{project_id}.{dataset_id}.{site_mappings_table_id}` s
+ON m.src_hpo_id = s.hpo_id
+"""
 
 
 def get_table_fields(table):
@@ -53,13 +68,14 @@ def get_table_fields(table):
     return table_fields
 
 
-def get_mapping_table_ids(mapping_dataset_id):
+def get_mapping_table_ids(project_id, mapping_dataset_id):
     """
     returns all the mapping table ids found in the dataset
+    :param project_id: project_id containing the dataset
     :param mapping_dataset_id: dataset_id containing mapping tables
     :return: returns mapping table ids
     """
-    table_objs = bq_utils.list_tables(mapping_dataset_id)
+    table_objs = bq_utils.list_tables(mapping_dataset_id, project_id=project_id)
     mapping_table_ids = []
     for table_obj in table_objs:
         table_id = bq_utils.get_table_id_from_obj(table_obj)
@@ -75,11 +91,38 @@ def generate_site_mappings():
     """
     hpo_list = resources.hpo_csv()
     rand_list = random.sample(range(100, 999), len(hpo_list))
-    mapping_list = dict()
+    mapping_dict = dict()
     for i in range(len(hpo_list)):
-        mapping_list[hpo_list[i]["hpo_id"]] = rand_list[i]
-    return mapping_list
-    # bq_utils.create_table(SITE_TABLE_ID, SITE_MAPPING_FIELDS, dataset_id=dataset_id)
+        mapping_dict[hpo_list[i]["hpo_id"]] = rand_list[i]
+    return mapping_dict
+
+
+def get_hpo_and_rdr_mappings():
+    """
+    generates list of lists containing the hpo_id and the identifier
+    :param
+    :return:
+    """
+    site_mapping_dict = generate_site_mappings()
+    mappings_list = []
+    for hpo_id in site_mapping_dict:
+        mappings_list.append([hpo_id, EHR_SITE_PREFIX + str(site_mapping_dict[hpo_id])])
+    mappings_list.append([RDR, PPI_PM])
+    return mappings_list
+
+
+def convert_to_bq_string(mapping_list):
+    """
+    Converts list of lists to bq INSERT friendly string
+    :param mapping_list: list of lists where the inner lists have two items
+    :return: bq INSERT formatted string
+    """
+    bq_insert_list = []
+    for hpo_rdr_item in mapping_list:
+        bq_insert_list.append("(\"{hpo_rdr_id}\", \"{src_id}\")".format(hpo_rdr_id=hpo_rdr_item[0],
+                                                                        src_id=hpo_rdr_item[1]))
+    bq_insert_string = ', '.join(bq_insert_list)
+    return bq_insert_string
 
 
 def get_cdm_table_from_mapping(mapping_table_id):
@@ -89,6 +132,25 @@ def get_cdm_table_from_mapping(mapping_table_id):
     :return: cdm table id
     """
     return mapping_table_id[len(MAPPING_PREFIX):]
+
+
+def create_and_populate_source_mapping_table(project_id, dataset_id):
+    """
+    creates the site mapping table and inserts the site mappings
+    :param project_id: project_id containing the dataset
+    :param dataset_id: dataset to create the mapping table in
+    :return: number of rows inserted in string from
+    """
+    mapping_list = get_hpo_and_rdr_mappings()
+    site_mapping_insert_string = convert_to_bq_string(mapping_list)
+    result = bq_utils.create_table(SITE_TABLE_ID, SITE_MAPPING_FIELDS, drop_existing=True, dataset_id=dataset_id)
+    site_mappings_insert_query = INSERT_SITE_MAPPINGS_QUERY.format(dataset_id=dataset_id,
+                                                                   project_id=project_id,
+                                                                   table_id=SITE_TABLE_ID,
+                                                                   values=site_mapping_insert_string)
+    result = bq_utils.query(site_mappings_insert_query)
+    rows_affected = result['numDmlAffectedRows']
+    return rows_affected
 
 
 def get_generate_ext_table_queries(project_id, dataset_id, mapping_dataset_id=None):
@@ -104,13 +166,24 @@ def get_generate_ext_table_queries(project_id, dataset_id, mapping_dataset_id=No
     if mapping_dataset_id is None:
         mapping_dataset_id = dataset_id
 
-    mapping_table_ids = get_mapping_table_ids(mapping_dataset_id)
+    mapping_table_ids = get_mapping_table_ids(project_id, mapping_dataset_id)
+    create_and_populate_source_mapping_table(project_id, dataset_id)
 
     for mapping_table_id in mapping_table_ids:
         cdm_table_id = get_cdm_table_from_mapping(mapping_table_id)
         ext_table_id = cdm_table_id + EXT_TABLE_SUFFIX
         ext_table_fields = get_table_fields(cdm_table_id)
-        bq_utils.create_table(ext_table_id, ext_table_fields, dataset_id=dataset_id)
+        bq_utils.create_table(ext_table_id, ext_table_fields, drop_existing=True, dataset_id=dataset_id)
+        query = dict()
+        query[cdr_consts.QUERY] = REPLACE_SRC_QUERY.format(project_id=project_id,
+                                                           dataset_id=dataset_id,
+                                                           mapping_table_id=mapping_table_id,
+                                                           site_mappings_table_id=SITE_TABLE_ID,
+                                                           cdm_table_id=cdm_table_id)
+        query[cdr_consts.DESTINATION_TABLE] = ext_table_id
+        query[cdr_consts.DESTINATION_DATASET] = dataset_id
+        query[cdr_consts.DISPOSITION] = bq_consts.WRITE_EMPTY
+        queries.append(query)
 
     return queries
 
--- data_steward/test/unit_test/data_steward/cdr_cleaner/cleaning_rules/generate_ext_tables_test.py
+++ data_steward/test/unit_test/data_steward/cdr_cleaner/cleaning_rules/generate_ext_tables_test.py
@@ -3,9 +3,12 @@ import unittest
 
 import mock
 
+import bq_utils
 import cdr_cleaner.cleaning_rules.generate_ext_tables as gen_ext
 import common
 import resources
+import constants.bq_utils as bq_consts
+import constants.cdr_cleaner.clean_cdr as cdr_consts
 
 
 class GenerateExtTablesTest(unittest.TestCase):
@@ -17,8 +20,9 @@ class GenerateExtTablesTest(unittest.TestCase):
         print('**************************************************************')
 
     def setUp(self):
-        self.project_id = 'test_project_id'
+        self.project_id = 'project_id'
         self.dataset_id = 'dataset_id'
+        self.bq_dataset_id = bq_utils.get_unioned_dataset_id()
         self.obs_fields = [{
                                 "type": "integer",
                                 "name": "observation_id",
@@ -30,6 +34,58 @@ class GenerateExtTablesTest(unittest.TestCase):
                                 "mode": "nullable",
                                 "description": "The provenance of the data associated with the observation_id."
                             }]
+        self.hpo_list = resources.hpo_csv()
+        self.mapping_tables = [gen_ext.MAPPING_PREFIX + cdm_table
+                               for cdm_table in common.AOU_REQUIRED
+                               if cdm_table not in [common.PERSON, common.DEATH, common.FACT_RELATIONSHIP]]
+        self.bq_string = ("(va, EHR site 236), "
+                          "(trans_am_baylor, EHR site 864), "
+                          "(cpmc_ucsd, EHR site 545), "
+                          "(cpmc_ucsf, EHR site 790), "
+                          "(jhchc, EHR site 285), "
+                          "(cpmc_usc, EHR site 974), "
+                          "(chci, EHR site 226), "
+                          "(uamc_uofa, EHR site 476), "
+                          "(cpmc_ceders, EHR site 110), "
+                          "(saou_uab_hunt, EHR site 125), "
+                          "(ipmc_northshore, EHR site 542), "
+                          "(saou_usahs, EHR site 798), "
+                          "(aouw_mcw, EHR site 559), "
+                          "(nyc_cu, EHR site 712), "
+                          "(seec_miami, EHR site 436), "
+                          "(aouw_mcri, EHR site 400), "
+                          "(tach_hfhs, EHR site 240), "
+                          "(uamc_banner, EHR site 891), "
+                          "(chs, EHR site 191), "
+                          "(seec_morehouse, EHR site 398), "
+                          "(nyc_cornell, EHR site 482), "
+                          "(saou_ummc, EHR site 766), "
+                          "(saou_uab, EHR site 608), "
+                          "(syhc, EHR site 836), "
+                          "(ipmc_rush, EHR site 299), "
+                          "(saou_cgmhs, EHR site 133), "
+                          "(hrhc, EHR site 680), "
+                          "(nec_bmc, EHR site 149), "
+                          "(ipmc_uchicago, EHR site 220), "
+                          "(seec_ufl, EHR site 425), "
+                          "(ecchc, EHR site 322), "
+                          "(aouw_uwh, EHR site 252), "
+                          "(nyc_hh, EHR site 134), "
+                          "(ipmc_nu, EHR site 575), "
+                          "(ghs, EHR site 607), "
+                          "(pitt_temple, EHR site 550), "
+                          "(pitt, EHR site 938), "
+                          "(saou_tul, EHR site 505), "
+                          "(trans_am_essentia, EHR site 293), "
+                          "(cpmc_uci, EHR site 724), "
+                          "(trans_am_spectrum, EHR site 639), "
+                          "(ipmc_uic, EHR site 885), "
+                          "(saou_umc, EHR site 912), "
+                          "(seec_emory, EHR site 442), "
+                          "(nec_phs, EHR site 982), "
+                          "(cpmc_ucd, EHR site 173), "
+                          "(saou_lsu, EHR site 709), "
+                          "(rdr, PPI/PM)")
 
     def test_get_obs_fields(self):
         table = common.OBSERVATION
@@ -44,7 +100,59 @@ class GenerateExtTablesTest(unittest.TestCase):
         actual = gen_ext.get_cdm_table_from_mapping(mapping_observation)
         self.assertItemsEqual(expected, actual)
 
-    def test_site_mapping_list(self):
-        hpo_list = resources.hpo_csv()
-        mapping_list = gen_ext.generate_site_mappings()
-        self.assertEqual(len(mapping_list), len(hpo_list))
+    def test_site_mapping_dict(self):
+        mapping_dict = gen_ext.generate_site_mappings()
+        self.assertEqual(len(mapping_dict), len(self.hpo_list))
+        for hpo_id in mapping_dict:
+            self.assertGreaterEqual(mapping_dict[hpo_id], 100)
+            self.assertLessEqual(mapping_dict[hpo_id], 999)
+
+    def test_get_hpo_and_rdr_mappings(self):
+        expected = [hpo_dict["hpo_id"] for hpo_dict in self.hpo_list] + [gen_ext.RDR]
+        hpo_rdr_mapping_list = gen_ext.get_hpo_and_rdr_mappings()
+        self.assertEqual(len(hpo_rdr_mapping_list), len(expected))
+        for hpo_rdr_id in expected:
+            self.assertIn(hpo_rdr_id, [hpo_item[0] for hpo_item in hpo_rdr_mapping_list])
+        for hpo_item in hpo_rdr_mapping_list:
+            self.assertIn(hpo_item[0], expected)
+            if hpo_item[0] == gen_ext.RDR:
+                self.assertEquals(hpo_item[1], gen_ext.PPI_PM)
+
+    def test_convert_to_bq_string(self):
+        hpo_rdr_mapping_list = gen_ext.get_hpo_and_rdr_mappings()
+        expected = self.bq_string
+        actual = gen_ext.convert_to_bq_string(hpo_rdr_mapping_list)
+        self.assertEquals(len(actual), len(expected))
+
+    def test_create_populate_source_mapping_table(self):
+        mapping_list = gen_ext.get_hpo_and_rdr_mappings()
+        expected = str(len(mapping_list))
+        num_rows_affected = gen_ext.create_and_populate_source_mapping_table(self.bq_dataset_id)
+        self.assertEquals(expected, num_rows_affected)
+
+    @mock.patch('bq_utils.create_table')
+    @mock.patch('cdr_cleaner.cleaning_rules.generate_ext_tables.create_and_populate_source_mapping_table')
+    @mock.patch('cdr_cleaner.cleaning_rules.generate_ext_tables.get_mapping_table_ids')
+    def test_generate_ext_table_queries(self, mock_mapping_tables,
+                                        mock_create_and_populate_mapping_table,
+                                        mock_create_table):
+        mock_mapping_tables.return_value = self.mapping_tables
+        expected = []
+        for cdm_table in common.AOU_REQUIRED:
+            if cdm_table not in [common.PERSON, common.DEATH, common.FACT_RELATIONSHIP]:
+                query = dict()
+                query[cdr_consts.QUERY] = gen_ext.REPLACE_SRC_QUERY.format(project_id=self.project_id,
+                                                                           dataset_id=self.dataset_id,
+                                                                           mapping_table_id=gen_ext.MAPPING_PREFIX
+                                                                                            +cdm_table,
+                                                                           site_mappings_table_id=gen_ext.SITE_TABLE_ID,
+                                                                           cdm_table_id=cdm_table)
+                query[cdr_consts.DESTINATION_TABLE] = cdm_table + gen_ext.EXT_TABLE_SUFFIX
+                query[cdr_consts.DESTINATION_DATASET] = self.dataset_id
+                query[cdr_consts.DISPOSITION] = bq_consts.WRITE_EMPTY
+                expected.append(query)
+        actual = gen_ext.get_generate_ext_table_queries(self.project_id, self.dataset_id)
+        self.assertItemsEqual(expected, actual)
+
+    def TearDown(self):
+        bq_utils.delete_table(gen_ext.SITE_TABLE_ID, self.bq_dataset_id)
