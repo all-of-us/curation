244dbede80873dff9560305b67d399932d32920e
Mark Velez <mav7014@nyp.org>
2019-10-10 13:48:47 -0400

    Include deid and combined dists in deid race notebook
     * Move race dupe to seperate notebook
     * [ci skip]

--- data_steward/analytics/cdr_ops/deid_race.py
+++ data_steward/analytics/cdr_ops/deid_race.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 pd.set_option('display.max_colwidth', -1)
 RDR = DEFAULT_DATASETS.latest.rdr
+DEID = DEFAULT_DATASETS.latest.deid
+VOCAB = DEFAULT_DATASETS.latest.vocabulary
 # -
 
 # For the race question (`observation_concept_id=1586140`) a generalization rule is applied such that responses with multiple selected races are replaced by a response containing `2000000008` "More than one population".
@@ -26,33 +28,36 @@ def display_dataframe(df):
     display(html)
 
 
+# # Counts for all race combo responses
+
 MULTIRACIAL_DIST_QUERY = """
 WITH race_combo AS
-(SELECT person_id, 
-  questionnaire_response_id, 
-  STRING_AGG(REPLACE(value_source_value, 'WhatRaceEthnicity_', ''), ' ' ORDER BY value_source_value) selected_races
- FROM {RDR}.observation
+(SELECT o.person_id, 
+  o.questionnaire_response_id, 
+  STRING_AGG(REPLACE(c.concept_code, 'WhatRaceEthnicity_', ''), ' ' ORDER BY value_source_value) selected_races
+ FROM {DATASET}.observation o
+ JOIN {VOCAB}.concept c ON o.value_source_concept_id = c.concept_id  
  WHERE observation_source_concept_id = 1586140
  GROUP BY person_id, questionnaire_response_id)
  
 SELECT 
   selected_races, 
+  (LENGTH(selected_races) - LENGTH(REPLACE(selected_races, ' ', '')) + 1) AS selected_count,
   COUNT(DISTINCT person_id) row_count
 FROM race_combo 
 GROUP BY selected_races
-ORDER BY selected_races
+ORDER BY selected_count, selected_races
 """
-multi_race_count_df = bq.query(MULTIRACIAL_DIST_QUERY.format(RDR=RDR))
 
+# ## In latest rdr dataset
+
+q = MULTIRACIAL_DIST_QUERY.format(DATASET=RDR, VOCAB=VOCAB)
+multi_race_count_df = bq.query(q)
 display_dataframe(multi_race_count_df)
 
-DUPLICATE_GEN_RACE_QUERY = """
-SELECT o.questionnaire_response_id, 
-  o.person_id, 
-  o.value_source_concept_id, 
-  COUNT(1) row_count
- FROM {DEID}.observation o
-WHERE observation_source_concept_id = 1586140
-GROUP BY o.questionnaire_response_id, o.person_id, o.value_source_concept_id
-HAVING row_count > 1
-"""
+# ## Deid dataset
+# Generalization during the privacy methodology should limit the populations represented in the deidentified dataset to those who selected 1 or 2 races only. Where 2 races are selected, Hispanic must be one of them.
+
+q = MULTIRACIAL_DIST_QUERY.format(DATASET=DEID, VOCAB=VOCAB)
+multi_race_count_df = bq.query(q)
+display_dataframe(multi_race_count_df)
