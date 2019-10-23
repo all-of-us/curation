a0f9ad31e98ea4e4d86316db17e01579690c05cf
Krishna Kalluri <krishnasaikalluri@gmail.com>
2019-10-23 16:51:26 -0400

    Move clean_ppi_numeric_values to cdr_clean package. [ci skip]

--- data_steward/tools/fix_rdr_data.py
+++ data_steward/tools/fix_rdr_data.py
@@ -5,105 +5,6 @@ from google.appengine.api.app_identity import app_identity
 
 import bq_utils
 
-CLEAN_PPI_NUMERIC_FIELDS = """
-UPDATE
-  {project}.{dataset}.observation u1
-SET
-  u1.value_as_number = NULL,
-  u1.value_as_concept_id = 2000000010
-FROM
-  (
-  SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585889 AND (value_as_number < 0 OR value_as_number > 20)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585890 AND (value_as_number < 0 OR value_as_number > 20)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585795 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585802 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585864 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585870 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1585873 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1586159 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  {project}.{dataset}.observation
-WHERE
-  observation_concept_id = 1586162 AND (value_as_number < 0 OR value_as_number > 99) ) a
-WHERE
-  u1.observation_id = a.observation_id
-"""
-
 REMOVE_ADDITIONAL_RESPONSES_OTHER_THAN_NOT = """
 DELETE
 FROM
@@ -120,28 +21,6 @@ WHERE
   AND (observation_concept_id = 1586140
     AND value_source_concept_id != 1586148)
 """
-
-
-
-
-def clean_ppi_numeric_fields_using_parameters(project_id, dataset_id):
-    """
-    Applies value range restrictions given to the value_as_number field across the entire dataset.
-    For values which do not meet range criteria, sets value_as_number to NULL and set value_as_concept_id and
-    observation_type_concept_id to a new AoU custom concept (2000000010)
-
-    :param project_id: Name of the project
-    :param dataset_id: Name of the dataset where the queries should be run
-    :return:
-    """
-    if project_id is None:
-        project_id = app_identity.get_application_id()
-
-    q = CLEAN_PPI_NUMERIC_FIELDS.format(project=project_id,
-                                        dataset=dataset_id
-                                        )
-    logging.debug('Query to clean numeric free text values is {q}'.format(q=q))
-    bq_utils.query(q=q)
 
 
 def remove_additional_responses(project_id, dataset_id):
@@ -170,9 +49,6 @@ def main(project_id, dataset_id):
     :param dataset_id: Name of the dataset where the queries should be run
     :return:
     """
-
-    logging.info('Applying clean ppi numeric value ranges fix')
-    clean_ppi_numeric_fields_using_parameters(project_id, dataset_id)
 
     logging.info('Applying Removing additional responses for sex/gender fix')
     remove_additional_responses(project_id, dataset_id)
