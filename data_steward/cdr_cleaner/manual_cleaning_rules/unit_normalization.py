0e2ef36631a07c2eeda5a544dbd8e9b420412856
Chao Pang <ChaoPang229@gmail.com>
2019-10-24 17:13:51 -0400

    made the unit_mapping fields a global field

--- data_steward/cdr_cleaner/manual_cleaning_rules/unit_normalization.py
+++ data_steward/cdr_cleaner/manual_cleaning_rules/unit_normalization.py
@@ -7,9 +7,31 @@ import resources
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 
 LOGGER = logging.getLogger(__name__)
+UNIT_MAPPING_TABLE = 'unit_mapping'
 UNIT_MAPPING_FILE = 'unit_mapping'
 MEASUREMENT = 'measurement'
-UNIT_MAPPING_TABLE = 'unit_mapping'
+UNIT_MAPPING_FIELDS = [
+    {
+        "type": "integer",
+        "name": "measurement_concept_id",
+        "mode": "nullable"
+    },
+    {
+        "type": "integer",
+        "name": "unit_concept_id",
+        "mode": "nullable"
+    },
+    {
+        "type": "integer",
+        "name": "set_unit_concept_id",
+        "mode": "nullable"
+    },
+    {
+        "type": "string",
+        "name": "transform_value_as_number",
+        "mode": "nullable"
+    }
+]
 
 INSERT_UNITS_QUERY = """
 INSERT INTO `{project_id}.{dataset_id}.{units_table_id}`
@@ -93,8 +115,7 @@ FROM
 LEFT JOIN
   `{project_id}.{dataset_id}.{unit_table_name}`
 USING
-  (measurement_concept_id,
-    unit_concept_id)
+  (measurement_concept_id, unit_concept_id)
 """
 
 
@@ -115,30 +136,14 @@ def get_mapping_list(unit_mappings_list):
     return formatted_mapping_list
 
 
-def create_unit_standards_table(project_id, dataset_id, table_name):
-    fields = [
-        {
-            "type": "integer",
-            "name": "measurement_concept_id",
-            "mode": "nullable"
-        },
-        {
-            "type": "integer",
-            "name": "unit_concept_id",
-            "mode": "nullable"
-        },
-        {
-            "type": "integer",
-            "name": "set_unit_concept_id",
-            "mode": "nullable"
-        },
-        {
-            "type": "string",
-            "name": "transform_value_as_number",
-            "mode": "nullable"
-        }
-    ]
-    bq_utils.create_table(table_id=table_name, fields=fields, drop_existing=True,
+def create_unit_mapping_table(project_id, dataset_id):
+    """
+    This function creates the unit_mapping table and populate it with the values from resources/unit_mapping.csv
+    :param project_id:
+    :param dataset_id:
+    :return:
+    """
+    bq_utils.create_table(table_id=UNIT_MAPPING_TABLE, fields=UNIT_MAPPING_FIELDS, drop_existing=True,
                           dataset_id=dataset_id)
 
     unit_mappings_csv = os.path.join(resources.resource_path, UNIT_MAPPING_FILE + ".csv")
@@ -146,10 +151,10 @@ def create_unit_standards_table(project_id, dataset_id, table_name):
     unit_mappings_populate_query = INSERT_UNITS_QUERY.format(
         dataset_id=dataset_id,
         project_id=project_id,
-        units_table_id=table_name,
+        units_table_id=UNIT_MAPPING_TABLE,
         mapping_list=get_mapping_list(unit_mappings_list))
     result = bq_utils.query(unit_mappings_populate_query)
-    LOGGER.info("Created %s.%s", dataset_id, table_name)
+    LOGGER.info("Created %s.%s", dataset_id, UNIT_MAPPING_TABLE)
     return result
 
 
@@ -180,6 +185,6 @@ if __name__ == '__main__':
     ARGS = parser.parse_args()
     clean_engine.add_console_logging(ARGS.console_log)
 
-    create_unit_standards_table(ARGS.project_id, ARGS.dataset_id, UNIT_MAPPING_TABLE)
+    create_unit_mapping_table(ARGS.project_id, ARGS.dataset_id)
     query_list = get_unit_normalization_query(ARGS.project_id, ARGS.dataset_id)
     clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id, query_list)
