e16e87778abf187a12add20da307ab29c7080d5a
Mark <mav9014@nyp.org>
2019-10-28 14:11:01 -0400

    Explicitly set project_id using env var in notebooks.bq

--- data_steward/notebooks/bq.py
+++ data_steward/notebooks/bq.py
@@ -1,4 +1,11 @@
+import os
+
+from google import datalab
 from google.datalab import bigquery
+
+# In Windows we found that datalab project_id resolution prioritizes an obscure config.json over env vars
+# APPLICATION_ID or PROJECT_ID. This explicitly sets it to env var PROJECT_ID.
+datalab.Context.default().set_project_id(project_id=os.getenv('PROJECT_ID'))
 
 
 # Wrapper so we can more easily swap to whatever client library we prefer in the future
