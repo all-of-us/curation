137b347db52d487d78187a661c52aadc7756bc3f
Mark Velez <mav7014@nyp.org>
2019-10-30 11:34:18 -0400

    Fixes for sandbox functionality (#296) [ci skip]
    * added create_sandbox_table_name in the sandbox module
    * Return dataset_id in create_sandbox_datasetseperate
    * Use raw string literal in sandbox table regex

--- data_steward/sandbox.py
+++ data_steward/sandbox.py
@@ -1,5 +1,6 @@
 from bq_utils import create_dataset
 from constants import bq_utils as bq_consts
+import re
 
 SANDBOX_SUFFIX = 'sandbox'
 
@@ -9,14 +10,16 @@ def create_sandbox_dataset(project_id, dataset_id):
     A helper function create a sandbox dataset if the sandbox dataset doesn't exist
     :param project_id: project_id
     :param dataset_id: any dataset_id
-    :return:
+    :return: the sandbox dataset_id
     """
     sandbox_dataset_id = get_sandbox_dataset_id(dataset_id)
     friendly_name = 'Sandbox for {dataset_id}'.format(dataset_id=dataset_id)
-    return create_dataset(project_id=project_id,
-                          dataset_id=sandbox_dataset_id,
-                          friendly_name=friendly_name,
-                          overwrite_existing=bq_consts.FALSE)
+    create_dataset(project_id=project_id,
+                   dataset_id=sandbox_dataset_id,
+                   friendly_name=friendly_name,
+                   overwrite_existing=bq_consts.FALSE)
+
+    return sandbox_dataset_id
 
 
 def get_sandbox_dataset_id(dataset_id):
@@ -26,3 +29,14 @@ def get_sandbox_dataset_id(dataset_id):
     :return:
     """
     return '{dataset_id}_{sandbox_suffix}'.format(dataset_id=dataset_id, sandbox_suffix=SANDBOX_SUFFIX)
+
+
+def get_sandbox_table_name(dataset_id, rule_name):
+    """
+    A helper function to create a table in the sandbox dataset
+
+    :param dataset_id: the dataset_id to which the rule is applied
+    :param rule_name: the name of the cleaning rule
+    :return: the concatenated table name
+    """
+    return '{dataset_id}_{rule_name}'.format(dataset_id=dataset_id, rule_name=re.sub(r'\W', '_', rule_name))
