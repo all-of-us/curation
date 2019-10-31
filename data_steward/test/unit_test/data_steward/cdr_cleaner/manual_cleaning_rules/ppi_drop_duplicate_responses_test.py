a941ad82e775569e0479128c21f7e088030632bd
Chao Pang <ChaoPang229@gmail.com>
2019-10-31 09:56:46 -0400

    [DC-532] Drop different responses (#276)
    * separate the delete query template into a select and delete query templates
    * integrate ppi_drop_duplicate_responses into the pipeline
    * update usages of sandbox.get_sandbox_table_name

--- data_steward/test/unit_test/data_steward/cdr_cleaner/manual_cleaning_rules/ppi_drop_duplicate_responses_test.py
+++ data_steward/test/unit_test/data_steward/cdr_cleaner/manual_cleaning_rules/ppi_drop_duplicate_responses_test.py
@@ -1,6 +1,8 @@
 import unittest
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 import cdr_cleaner.manual_cleaning_rules.ppi_drop_duplicate_responses as ppi_drop
+import constants.bq_utils as bq_consts
+import sandbox
 
 
 class PPIDropDuplicateResponsesTest(unittest.TestCase):
@@ -14,15 +16,24 @@ class PPIDropDuplicateResponsesTest(unittest.TestCase):
     def setUp(self):
         self.project_id = "project_id"
         self.dataset_id = "dataset_id"
+        self.sandbox_dataset_id = "sandbox_dataset_id"
 
     def test_get_remove_duplicate_queries(self):
-        query = dict()
-        query[cdr_consts.QUERY] = ppi_drop.REMOVE_DUPLICATE_TEMPLATE.format(project_id=self.project_id,
-                                                                            dataset_id=self.dataset_id)
-        query[cdr_consts.BATCH] = True
-        expected = [query]
+        select_query = dict()
+        select_query[cdr_consts.QUERY] = ppi_drop.get_select_statement(self.project_id, self.dataset_id)
+        select_query[cdr_consts.DESTINATION_TABLE] = sandbox.get_sandbox_table_name(self.dataset_id,
+                                                                                    ppi_drop.CLEANING_RULE_NAME)
+        select_query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
+        select_query[cdr_consts.DESTINATION_DATASET] = self.sandbox_dataset_id
+
+        delete_query = dict()
+        delete_query[cdr_consts.QUERY] = ppi_drop.get_delete_statement(project_id=self.project_id,
+                                                                       dataset_id=self.dataset_id)
+        delete_query[cdr_consts.BATCH] = True
+        expected = [select_query, delete_query]
 
         actual = ppi_drop.get_remove_duplicate_set_of_responses_to_same_questions_queries(self.project_id,
-                                                                                          self.dataset_id)
+                                                                                          self.dataset_id,
+                                                                                          self.sandbox_dataset_id)
 
         self.assertItemsEqual(expected, actual)
