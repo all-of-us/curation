c3b87741623b8bc9dc0b3975802a43dfab7d3ad2
CH Albach <calbach@google.com>
2019-12-17 17:31:22 -0800

    fix bash_envs

--- data_steward/init_env.sh
+++ data_steward/init_env.sh
@@ -55,19 +55,19 @@ export UNIONED_DATASET_ID="${DATASET_PREFIX}_unioned"
 # See https://www.compose.com/articles/experience-with-circleci/#dontcommitcredentials
 if [ -n "${CIRCLECI}" ]
 then
-  export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-credentials-key.json >> ${BASH_ENV}
-  export APPLICATION_ID=${APPLICATION_ID} >> ${BASH_ENV}
-  export GOOGLE_CLOUD_PROJECT=${GOOGLE_CLOUD_PROJECT} >> ${BASH_ENV}
-  export USERNAME=${USERNAME} >> ${BASH_ENV}
-  export DRC_BUCKET_NAME=${DRC_BUCKET_NAME} >> ${BASH_ENV}
-  export BUCKET_NAME_FAKE=${BUCKET_NAME_FAKE} >> ${BASH_ENV}
-  export BUCKET_NAME_NYC=${BUCKET_NAME_NYC} >> ${BASH_ENV}
-  export BUCKET_NAME_PITT=${BUCKET_NAME_PITT} >> ${BASH_ENV}
-  export BUCKET_NAME_CHS=${BUCKET_NAME_CHS} >> ${BASH_ENV}
-  export BIGQUERY_DATASET_ID=${BIGQUERY_DATASET_ID} >> ${BASH_ENV}
-  export RDR_DATASET_ID=${RDR_DATASET_ID} >> ${BASH_ENV}
-  export EHR_RDR_DATASET_ID=${EHR_RDR_DATASET_ID} >> ${BASH_ENV}
-  export UNIONED_DATASET_ID=${UNIONED_DATASET_ID} >> ${BASH_ENV}
-  export BUCKET_NAME_UNIONED_EHR=${BUCKET_NAME_UNIONED_EHR} >> ${BASH_ENV}
-  echo "export PATH=${PATH}:${CIRCLE_WORKING_DIRECTORY}/ci" >> $BASH_ENV >> ${BASH_ENV}
+  echo "export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-credentials-key.json" >> ${BASH_ENV}
+  echo "export APPLICATION_ID=${APPLICATION_ID}" >> ${BASH_ENV}
+  echo "export GOOGLE_CLOUD_PROJECT=${GOOGLE_CLOUD_PROJECT}" >> ${BASH_ENV}
+  echo "export USERNAME=${USERNAME}" >> ${BASH_ENV}
+  echo "export DRC_BUCKET_NAME=${DRC_BUCKET_NAME}" >> ${BASH_ENV}
+  echo "export BUCKET_NAME_FAKE=${BUCKET_NAME_FAKE}" >> ${BASH_ENV}
+  echo "export BUCKET_NAME_NYC=${BUCKET_NAME_NYC}" >> ${BASH_ENV}
+  echo "export BUCKET_NAME_PITT=${BUCKET_NAME_PITT}" >> ${BASH_ENV}
+  echo "export BUCKET_NAME_CHS=${BUCKET_NAME_CHS}" >> ${BASH_ENV}
+  echo "export BIGQUERY_DATASET_ID=${BIGQUERY_DATASET_ID}" >> ${BASH_ENV}
+  echo "export RDR_DATASET_ID=${RDR_DATASET_ID}" >> ${BASH_ENV}
+  echo "export EHR_RDR_DATASET_ID=${EHR_RDR_DATASET_ID}" >> ${BASH_ENV}
+  echo "export UNIONED_DATASET_ID=${UNIONED_DATASET_ID}" >> ${BASH_ENV}
+  echo "export BUCKET_NAME_UNIONED_EHR=${BUCKET_NAME_UNIONED_EHR}" >> ${BASH_ENV}
+  echo "export PATH=${PATH}:${CIRCLE_WORKING_DIRECTORY}/ci" >> $BASH_ENV
 fi
