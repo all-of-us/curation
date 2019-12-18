f3ec6fafa3bfd7ce02aae0ba7a94098e084bfe67
Lauren Biggers <lauren.r.biggers@vumc.org>
2019-12-18 15:40:06 -0600

    [DC-438] testing integration test changes
    * test to see if a PR created from a branch changes known environment variables

--- data_steward/tests/run_tests.sh
+++ data_steward/tests/run_tests.sh
@@ -8,7 +8,8 @@ subset="all"
 
 
 function usage() {
-  echo "Usage: run_tests.sh " \
+  echo "Usage: run_test.sh " \
+      "[-s all|unit|integration]" \
       "[-r <file name match glob, e.g. 'extraction_*'>]" >& 2
   exit 1
 }
@@ -37,14 +38,25 @@ then
    echo Executing tests that match glob ${substring}
 fi
 
+if [[ "$subset" == "all" ]];
+then
+  path="tests/"
+fi
+
+if [[ "$subset" == "unit" ]];
+then
+  path="tests/unit_tests/"
+fi
+
+if [[ "$subset" == "integration" ]];
+then
+  path="tests/integration_tests/"
+fi
+
 if [[ -z ${substring} ]]
 then
-  if [[ -z ${substring} ]]
-  then
-    cmd="tests/runner.py --test-path tests/unit_tests/ ${sdk_dir}"
-  else
-    cmd="tests/runner.py --test-path tests/unit_tests/ ${sdk_dir} --test-pattern $substring"
-  fi
-  (cd ${BASE_DIR}; python ${cmd})
+  cmd="tests/runner.py --test-path ${path} ${sdk_dir}"
+else
+  cmd="tests/runner.py --test-path ${path} ${sdk_dir} --test-pattern $substring"
 fi
 (cd ${BASE_DIR}; python ${cmd})
