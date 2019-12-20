64978b4ccb7c56d89c6b6f835d084fbbe9f36395
Krishna Kalluri <krishnasaikalluri@gmail.com>
2019-12-20 14:36:06 -0500

    Fix jupytext notebook load error. [ci skip]

--- data_steward/analytics/table_metrics/Table_Metrics_part_6.py
+++ data_steward/analytics/table_metrics/Table_Metrics_part_6.py
@@ -1,12 +1,11 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.3'
-#       jupytext_version: 1.0.5
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
