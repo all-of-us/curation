53a02492e65351618a6a84e790bbb38108248dfb
Noah Engel <ne2310@cumc.columbia.edu>
2019-12-20 13:13:32 -0500

    Most recent visualization notebooks

--- data_steward/analytics/tools/route_concept_population_visualizations.py
+++ data_steward/analytics/tools/route_concept_population_visualizations.py
@@ -5,20 +5,12 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-<<<<<<< HEAD
 #       format_version: '1.4'
 #       jupytext_version: 1.2.3
-||||||| merged common ancestors
-#       format_version: '1.4'
-#       jupytext_version: 1.2.4
-=======
-#       format_version: '1.5'
-#       jupytext_version: 1.3.0
->>>>>>> 743c86bca6683c2b89d5505b473df2c3a6246fb8
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 2
 #     language: python
-#     name: python3
+#     name: python2
 # ---
 
 # NOTES:
@@ -371,7 +363,6 @@ lgd = ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5,-0.1))
 
 img_name = name_of_interest + "_route_integration_line_graph.jpg"
 # plt.savefig(img_name, bbox_extraartist=(lgd,), bbox_inches='tight')
-<<<<<<< HEAD
 # -
 
 
@@ -381,7 +372,3 @@ img_name = name_of_interest + "_route_integration_line_graph.jpg"
 
 
 
-||||||| merged common ancestors
-# -
-=======
->>>>>>> 743c86bca6683c2b89d5505b473df2c3a6246fb8
