b6a40440b40bfcfa8f560cdb138d348d77236e96
Krishna Kalluri <krishnasaikalluri@gmail.com>
2020-01-28 11:06:56 -0500

    [DC-658] Fix maps_to_value_ppi_vocab_update query (#389)

--- data_steward/cdr_cleaner/cleaning_rules/maps_to_value_ppi_vocab_update.py
+++ data_steward/cdr_cleaner/cleaning_rules/maps_to_value_ppi_vocab_update.py
@@ -2,34 +2,41 @@ import bq_utils
 import constants.cdr_cleaner.clean_cdr as cdr_consts
 
 UPDATE_PPI_QUERY = """
-UPDATE 
-      `{project}.{dataset}.observation` a 
-    SET 
-      a.value_as_concept_id = b.concept_id 
-    FROM ( 
-      SELECT 
-        * 
-      FROM ( 
-        SELECT 
-          c2.concept_name, 
-          c2.concept_id, 
-          o.*, 
-          RANK() OVER (PARTITION BY o.observation_id, o.value_source_concept_id ORDER BY c2.concept_id ASC) AS rank 
-        FROM 
-          `{project}.{dataset}.observation` o 
-        JOIN 
-          `{project}.{ehr_dataset}.concept_relationship` cr 
-        ON 
-          o.value_source_concept_id = cr.concept_id_1 
-          AND cr.relationship_id = 'Maps to value' 
-        JOIN 
-          `{project}.{ehr_dataset}.concept` c2 
-        ON 
-          c2.concept_id = cr.concept_id_2 ) AS x 
-      WHERE 
-        rank=1 ) AS b 
-    WHERE 
-      a.observation_id = b.observation_id
+UPDATE
+  `{project}.{dataset}.observation` a
+SET
+  a.value_as_concept_id = b.concept_id
+FROM (
+  SELECT
+    *
+  FROM (
+    SELECT
+      c2.concept_name,
+      c2.concept_id,
+      o.*,
+      RANK() OVER (PARTITION BY o.observation_id, o.value_source_concept_id ORDER BY c2.concept_id ASC) AS rank
+    FROM
+      `{project}.{dataset}.observation` o
+    JOIN
+      `{project}.{dataset}.concept` c
+    ON
+      o.value_source_concept_id = c.concept_id
+    JOIN
+      `{project}.{dataset}.concept_relationship` cr
+    ON
+      c.concept_id = cr.concept_id_1
+      AND cr.relationship_id = 'Maps to value'
+    JOIN
+      `{project}.{dataset}.concept` c2
+    ON
+      c2.concept_id = cr.concept_id_2
+    WHERE
+      o.observation_concept_id = o.value_as_concept_id
+      AND o.observation_concept_id != 0 )
+  WHERE
+    rank=1 ) AS b
+WHERE
+  a.observation_id = b.observation_id
 """
 
 
@@ -44,12 +51,9 @@ def get_maps_to_value_ppi_vocab_update_queries(project_id, dataset_id):
     queries_list = []
 
     query = dict()
-    query[cdr_consts.QUERY] = UPDATE_PPI_QUERY.format(
-        dataset=dataset_id,
-        project=project_id,
-        ehr_dataset=bq_utils.get_dataset_id())
+    query[cdr_consts.QUERY] = UPDATE_PPI_QUERY.format(dataset=dataset_id,
+                                                      project=project_id)
     queries_list.append(query)
-
     return queries_list
 
 
