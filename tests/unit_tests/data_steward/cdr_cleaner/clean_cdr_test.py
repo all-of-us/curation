299abc902d4605c6715b91cd39a731951ae6357f
Chao Pang <ChaoPang229@gmail.com>
2020-02-07 23:22:29 -0500

    completed clean_cdr_test.py for testing the add_module_info_decorator function

--- tests/unit_tests/data_steward/cdr_cleaner/clean_cdr_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/clean_cdr_test.py
@@ -1,7 +1,7 @@
 import mock
 import unittest
-import inspect
 from cdr_cleaner import clean_cdr
+from constants.cdr_cleaner import clean_cdr as cdr_consts
 
 
 class CleanCDRTest(unittest.TestCase):
@@ -15,8 +15,57 @@ class CleanCDRTest(unittest.TestCase):
     def setUp(self):
         self.project_id = 'test project'
         self.dataset_id = 'test dataset'
+        self.sandbox_dataset_id = 'test sandbox'
+        self.dest_table = 'dest_table'
 
+        self.function_name = 'anonymous function'
+        self.model_name = 'test module'
+        self.line_no = 1
+
+        self.query_dict = {
+            cdr_consts.QUERY: 'query',
+            cdr_consts.DESTINATION_DATASET: self.dataset_id,
+            cdr_consts.DESTINATION_TABLE: self.dest_table
+        }
+
+        self.module_info_dict = {
+            cdr_consts.MODULE_NAME: self.model_name,
+            cdr_consts.FUNCTION_NAME: self.function_name,
+            cdr_consts.LINE_NO: self.line_no
+        }
+
+    @mock.patch('inspect.signature')
+    @mock.patch('inspect.getsourcelines')
     @mock.patch('inspect.getmodule')
-    def test_add_module_info_decorator(self, mock_get_module):
-        mock_function = mock.Mock(__name__='anonymous_function')
-        pass
+    def test_add_module_info_decorator(self, mock_get_module,
+                                       mock_getsourcelines, mock_signature):
+
+        mock_function = mock.Mock(__name__=self.function_name)
+        mock_function.return_value = [self.query_dict]
+        mock_get_module.return_value = mock.Mock(__name__=self.model_name)
+        mock_getsourcelines.return_value = (dict(), self.line_no)
+
+        mock_project_id_parameter = mock.Mock(name=clean_cdr.PROJECT_ID)
+        type(mock_project_id_parameter).name = mock.PropertyMock(
+            return_value=clean_cdr.PROJECT_ID)
+
+        mock_dataset_id_parameter = mock.Mock(name=clean_cdr.DATASET_ID)
+        type(mock_dataset_id_parameter).name = mock.PropertyMock(
+            return_value=clean_cdr.DATASET_ID)
+
+        type(mock_signature.return_value).parameters = mock.PropertyMock(
+            return_value={
+                clean_cdr.PROJECT_ID: mock_project_id_parameter,
+                clean_cdr.DATASET_ID: mock_dataset_id_parameter
+            })
+
+        actual_query_dict = clean_cdr.add_module_info_decorator(
+            mock_function, self.project_id, self.dataset_id,
+            self.sandbox_dataset_id)
+
+        expected_query_dict = [dict(**self.query_dict, **self.module_info_dict)]
+
+        self.assertListEqual(actual_query_dict, expected_query_dict)
+
+        mock_get_module.assert_called_once_with(mock_function)
+        mock_getsourcelines.assert_called_once_with(mock_function)
