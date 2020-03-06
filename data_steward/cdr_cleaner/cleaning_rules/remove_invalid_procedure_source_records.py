83851708e04b41c11c8d4e1a713dadf3ec40c68c
aimeemcgrenera <aimee.mcgrenera@vumc.org>
2020-03-06 12:22:41 -0600

    [DC-698] added sandboxing and fixed python string

--- data_steward/cdr_cleaner/cleaning_rules/remove_invalid_procedure_source_records.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_invalid_procedure_source_records.py
@@ -16,17 +16,22 @@ AND
 
 # Project imports
 import constants.cdr_cleaner.clean_cdr as cdr_consts
+from sandbox import get_sandbox_dataset_id
 
-REMOVE_INVALID_PROCEDURE_SOURCE_CONCEPT_IDS_QUERY = """
-DELETE
+INTERMEDIARY_TABLE_NAME = 'procedure_occurrence_dc583'
+
+INVALID_PROCEDURE_SOURCE_CONCEPT_IDS_QUERY = """
+CREATE OR REPLACE TABLE
+`{project}.{sandbox_dataset}.{intermediary_table}`
+SELECT *
 FROM
-  `{project}.{dataset}.procedure_occurrence`
+  `{project}.{dataset}.procedure_occurrence` p
 -- procedure_concept_id is not a standard concept in the procedure domain
 WHERE p.procedure_concept_id NOT IN (
   SELECT
     concept_id
   FROM
-    `{DATASET_ID}.concept`
+    `{project}.{dataset}.concept`
   WHERE
     domain_id = 'Procedure'
     AND TRIM(concept_class_id) IN ('Procedure', 'CPT4')
@@ -38,10 +43,18 @@ p.procedure_source_concept_id IN (
  SELECT
     concept_id
   FROM
-    `{DATASET_ID}.concept`
+    `{project}.{dataset}.concept`
   WHERE
     TRIM(concept_class_id) = 'CPT4 Modifier'
 )
+"""
+
+VALID_PROCEDURE_SOURCE_CONCEPT_IDS_QUERY = """
+SELECT * FROM
+`{project}.{dataset}.procedure_occurrence`
+WHERE
+procedure_occurrence_id
+IN (SELECT procedure_occurrence_id FROM `{project}.{sandbox_dataset}.{intermediary_table}`)
 """
 
 
@@ -57,11 +70,25 @@ def get_remove_invalid_procedure_source_queries(project_id, dataset_id):
     """
     queries_list = []
 
-    query = dict()
-    query[cdr_consts.
-          QUERY] = REMOVE_INVALID_PROCEDURE_SOURCE_CONCEPT_IDS_QUERY.format(
-              dataset=dataset_id, project=project_id)
-    queries_list.append(query)
+    # queries to sandbox
+    invalid_records = dict()
+    invalid_records[
+        cdr_consts.QUERY] = INVALID_PROCEDURE_SOURCE_CONCEPT_IDS_QUERY.format(
+            project=project_id,
+            dataset=dataset_id,
+            sandbox_dataset=get_sandbox_dataset_id(dataset_id),
+            intermediary_table=INTERMEDIARY_TABLE_NAME)
+    queries_list.append(invalid_records)
+
+    # queries to delete invalid procedure source records
+    valid_records = dict()
+    valid_records[
+        cdr_consts.QUERY] = VALID_PROCEDURE_SOURCE_CONCEPT_IDS_QUERY.format(
+            project=project_id,
+            dataset=dataset_id,
+            sandbox_dataset=get_sandbox_dataset_id(dataset_id),
+            intermediary_table=INTERMEDIARY_TABLE_NAME)
+    queries_list.append(valid_records)
 
     return queries_list
 
