0404bc0374d947f4345ae1ba0846dc331b3e179b
amcgrenera-vumc <56938070+amcgrenera-vumc@users.noreply.github.com>
2020-03-06 13:29:57 -0600

    [DC-685] cleaning rule and unit test to remove aian participants (#435)
    * [DC-685] cleaning rule and unit test to remove aian participants
    
    * [DC-685] added new cleaning rule to clean_cdr.py
    
    * [DC-685] reformatted for accepted yapf formatting
    
    * [DC-685] adjusted formatting and changed append to extend

---
+++ data_steward/cdr_cleaner/cleaning_rules/remove_aian_participants.py
@@ -0,0 +1,73 @@
+"""
+Background
+
+DC-685 - removing participants who identified as AI/AN in PPI.
+
+There is a requirement from the NIH that participants who identified as AI/AN in PPI cannot have ANY of their data
+included in released datasets for the Data Browser or Workbench/Cohort Builder. These participants have had their data
+retracted non-programmatically previously. However, since this is expected to be a long-term policy, this should be a
+cleaning rule applied to combined so this can be automated.
+We will remove ALL DATA in combined associated with PIDs who have rows with the following in the observation table:
+observation_source_concept_id = 1586140
+value_source_concept_id = 1586141
+"""
+
+# Project imports
+from cdr_cleaner.cleaning_rules import sandbox_and_remove_pids
+from notebooks import bq
+
+TICKET_NUMBER = 'DC685'
+
+PIDS_QUERY = """
+SELECT person_id
+FROM `{project}.{dataset}.observation`
+WHERE observation_source_concept_id IN (1586140) AND value_source_concept_id IN (1586141)
+"""
+
+
+def get_pids_list(project_id, dataset_id, pids_query):
+    """
+    takes a query based on the cleaning rule and returns a list of person_ids
+    :param project_id: bq name of project_id
+    :param dataset_id: bq name of dataset_id
+    :param pids_query: query that grabs all person_ids based on the cleaning rule
+    :return: list of person_ids
+    """
+
+    pid_list = bq.query(
+        pids_query.format(project=project_id,
+                          dataset=dataset_id))['person_id'].tolist()
+
+    return pid_list
+
+
+def get_queries(project_id, dataset_id):
+    """
+    return a list of queries to remove AIAN participant rows
+    :param project_id: Name of the project
+    :param dataset_id: Name of the dataset where the queries should be run
+    :return: A list of string queries that can be executed to delete AIAN participants and
+    all corresponding rows from the dataset with the associated PID.
+    """
+
+    queries_list = []
+
+    queries_list.extend(
+        sandbox_and_remove_pids.get_sandbox_queries(
+            project_id, dataset_id,
+            get_pids_list(project_id, dataset_id, PIDS_QUERY), TICKET_NUMBER))
+    queries_list.extend(
+        sandbox_and_remove_pids.get_remove_pids_queries(
+            project_id, dataset_id,
+            get_pids_list(project_id, dataset_id, PIDS_QUERY)))
+    return queries_list
+
+
+if __name__ == '__main__':
+    import cdr_cleaner.args_parser as parser
+    import cdr_cleaner.clean_cdr_engine as clean_engine
+
+    ARGS = parser.parse_args()
+    clean_engine.add_console_logging(ARGS.console_log)
+    query_list = get_queries(ARGS.project_id, ARGS.dataset_id)
+    clean_engine.clean_dataset(ARGS.project_id, query_list)
---
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/remove_aian_participants_test.py
@@ -0,0 +1,67 @@
+# Python imports
+import unittest
+
+# Third party imports
+import mock
+from mock import patch
+import pandas as pd
+
+# Project imports
+import cdr_cleaner.cleaning_rules.remove_aian_participants as remove_aian
+
+
+class RemoveAIANParticipantsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+    def setUp(self):
+        self.project_id = 'project_id'
+        self.dataset_id = 'dataset_id'
+        self.pids_list = [
+            324264993, 307753491, 335484227, 338965846, 354812933, 324983298,
+            366423185, 352721597, 352775367, 314281264, 319123185, 325306942,
+            324518105, 320577401, 339641873, 329210551, 364674103, 339564778,
+            309381334, 352068257, 353001073, 319604059, 336744297, 357830316,
+            352653514, 349988031, 349731310, 359249014, 361359486, 315083772,
+            358741126, 312045923, 313427389, 341366267, 305170199, 308597253,
+            348834424, 325536292, 360363123
+        ]
+        mock_bq_query_patcher = patch(
+            'cdr_cleaner.cleaning_rules.sandbox_and_remove_pids.bq.query')
+        self.mock_bq_query = mock_bq_query_patcher.start()
+        self.mock_bq_query.return_value = pd.DataFrame()
+        self.addCleanup(mock_bq_query_patcher.stop)
+
+    @mock.patch('cdr_cleaner.cleaning_rules.remove_aian_participants.bq.query')
+    def test_get_pids_list(self, mock_query):
+        mock_query.return_value = pd.DataFrame(self.pids_list,
+                                               columns=['person_id'])
+        result = remove_aian.get_pids_list(
+            self.project_id, self.dataset_id,
+            remove_aian.PIDS_QUERY.format(project=self.project_id,
+                                          dataset=self.dataset_id))
+        self.assertListEqual(self.pids_list, result)
+
+    @mock.patch(
+        'cdr_cleaner.cleaning_rules.sandbox_and_remove_pids.get_sandbox_queries'
+    )
+    @mock.patch(
+        'cdr_cleaner.cleaning_rules.remove_aian_participants.get_pids_list')
+    @mock.patch(
+        'cdr_cleaner.cleaning_rules.sandbox_and_remove_pids.get_remove_pids_queries'
+    )
+    def test_query_generation(self, mock_get_sandbox_queries,
+                              mock_get_pids_list, mock_get_remove_pids_queries):
+
+        mock_get_pids_list.return_value = self.pids_list
+        result = remove_aian.get_queries(self.project_id, self.dataset_id)
+
+        expected = list()
+        expected.extend(mock_get_remove_pids_queries.return_value)
+        expected.extend(mock_get_sandbox_queries.return_value)
+
+        self.assertEquals(result, expected)
