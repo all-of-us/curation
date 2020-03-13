effdb714c4fdb694ce7d1db947578af15402dcf3
Krishna Kalluri <krishnasaikalluri@gmail.com>
2020-03-13 15:51:21 -0400

    Add project_id to create client
    Signed-off-by: Krishna Kalluri <krishnasaikalluri@gmail.com>

--- data_steward/utils/bq.py
+++ data_steward/utils/bq.py
@@ -1,7 +1,17 @@
 from google.cloud import bigquery
 
 
-def query(q, use_cache=False):
-    client = bigquery.Client()
+def get_client(project_id=None):
+    """
+    Get a client for a specified project.
+    """
+    if project_id is None:
+        return bigquery.Client()
+    else:
+        return bigquery.Client(project=project_id)
+
+
+def query(q, project_id=None, use_cache=False):
+    client = get_client(project_id)
     query_job_config = bigquery.job.QueryJobConfig(use_query_cache=use_cache)
     return client.query(q, job_config=query_job_config).to_dataframe()
