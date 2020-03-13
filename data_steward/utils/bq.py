888e116d8a14cb7ad955b29e64815508205e8abf
Krishna Kalluri <krishnasaikalluri@gmail.com>
2020-03-13 15:37:08 -0400

    fix client placement
    Signed-off-by: Krishna Kalluri <krishnasaikalluri@gmail.com>

--- data_steward/utils/bq.py
+++ data_steward/utils/bq.py
@@ -1,8 +1,7 @@
 from google.cloud import bigquery
 
-client = bigquery.Client()
-
 
 def query(q, use_cache=False):
+    client = bigquery.Client()
     query_job_config = bigquery.job.QueryJobConfig(use_query_cache=use_cache)
     return client.query(q, job_config=query_job_config).to_dataframe()
