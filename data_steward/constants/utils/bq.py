b0226e3043cad7885d3dd7ccf0a51b2c2cbac29c
Chao Pang <ChaoPang229@gmail.com>
2020-03-18 16:07:09 -0400

    copied some constants from constants/bq_utils.py to constanst/utils/bq.py

--- data_steward/constants/utils/bq.py
+++ data_steward/constants/utils/bq.py
@@ -1,2 +1,10 @@
+BLANK = ''
+
+# Dataset Environment variable names
+MATCH_DATASET = 'VALIDATION_RESULTS_DATASET_ID'
+
 # Validation dataset prefix
 VALIDATION_PREFIX = 'validation'
+VALIDATION_DATASET_FORMAT = 'validation_{}'
+VALIDATION_DATASET_REGEX = 'validation_\d{8}'
+VALIDATION_DATE_FORMAT = '%Y%m%d'
