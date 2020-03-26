36bb05297dcd6e758c2fd89575e4f456106e7a50
Nishanth Pavinkurve <npp267@nyu.edu>
2020-03-25 23:33:13 -0400

    Major refactor

---
+++ data_steward/tools/participant_row_counts.py
@@ -0,0 +1,293 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# Python imports
+import argparse
+import logging
+
+# Third party imports
+from google.api_core.exceptions import BadRequest
+import pandas as pd
+from jinja2 import Template
+
+# Project imports
+from utils import bq
+import common
+from constants.tools import participant_row_counts as consts
+
+
+def get_table_information_for_dataset(project_id, dataset_id):
+    """
+    Get df of table_ids(first column) and table names that have a person_id column as the second column
+
+    :param project_id: identifies the project
+    :param dataset_id: identifies the dataset
+    :return df containing table column information
+    """
+    person_table_query = consts.TABLE_INFO_QUERY.format(project=project_id,
+                                                        dataset=dataset_id)
+    result_df = bq.query(person_table_query)
+    return result_df
+
+
+def get_combined_deid_query(project_id, dataset_id, pid_sql_expr, table_df):
+    """
+    Get query to determine all row counts and ehr row counts from combined, deid and release datasets
+
+    :param project_id: identifies the project
+    :param dataset_id: identifies the dataset
+    :param pid_sql_expr: string containing sql_expression identifying pids
+    :param table_df: dataframe from BQ INFORMATION_SCHEMA.COLUMNS
+    :return: query: 
+    """
+    tables = table_df.get(consts.TABLE_NAME).unique()
+    tables_with_pid = table_df[table_df.get(consts.COLUMN_NAME) ==
+                               'person_id'].get(consts.TABLE_NAME).unique()
+    mapping_ext_tables = [
+        table for table in tables
+        if common.MAPPING_PREFIX in table or common.EXT_SUFFIX in table
+    ]
+
+    cdm_tables_with_mapping = dict((get_cdm_table(table), table)
+                                   for table in mapping_ext_tables
+                                   if get_cdm_table(table) in tables_with_pid)
+
+    # Combined, DEID, Release
+    query_list = []
+    for table in tables_with_pid:
+        if table in cdm_tables_with_mapping:
+            table_id = table + '_id'
+            tmpl = Template(consts.CDM_MAPPING_TABLE_COUNT).render(
+                project=project_id,
+                dataset=dataset_id,
+                table=table,
+                table_id=table_id,
+                pids_expr=pid_sql_expr,
+                mapping_table=cdm_tables_with_mapping[table])
+        else:
+            ehr_count = 0
+            # death table does not have mapping and could have come from EHR or RDR, needs investigation
+            if table == common.DEATH:
+                ehr_count = "COUNT(*)"
+            tmpl = Template(consts.PID_TABLE_COUNT).render(
+                project=project_id,
+                dataset=dataset_id,
+                table=table,
+                pids_expr=pid_sql_expr,
+                ehr_count=ehr_count)
+        query_list.append(tmpl)
+    query = consts.UNION_ALL.join(query_list)
+    return query
+
+
+def get_dataset_query(project_id, dataset_id, pid_sql_expr, table_df):
+    """
+    Get query to determine all row counts and ehr row counts from unioned, rdr and nonconforming datasets
+    
+    :param project_id: identifies the project
+    :param dataset_id: identifies the dataset
+    :param pid_sql_expr: string containing sql_expression identifying pids
+    :param table_df: dataframe from BQ INFORMATION_SCHEMA.COLUMNS
+    :return: 
+    """
+    tables_with_pid = table_df[table_df.get(consts.COLUMN_NAME) ==
+                               'person_id'].get(consts.TABLE_NAME).unique()
+    # Unioned EHR
+    query_list = []
+    for table in tables_with_pid:
+        ehr_count = "COUNT(*)"
+        tmpl = Template(consts.PID_TABLE_COUNT).render(project=project_id,
+                                                       dataset=dataset_id,
+                                                       table=table,
+                                                       pids_expr=pid_sql_expr,
+                                                       ehr_count=ehr_count)
+        query_list.append(tmpl)
+    query = consts.UNION_ALL.join(query_list)
+    return query
+
+
+def get_ehr_query(project_id, dataset_id, pid_sql_expr, hpo_id, table_df):
+    """
+    Get query to determine all row counts and ehr row counts from ehr datasets
+
+    :param project_id: identifies the project
+    :param dataset_id: identifies the dataset
+    :param pid_sql_expr: string containing sql_expression identifying pids
+    :param hpo_id: identifies the hpo site that submitted the pids
+    :param table_df: dataframe from BQ INFORMATION_SCHEMA.COLUMNS
+    :return: 
+    """
+    tables_with_pid = table_df[table_df.get(consts.COLUMN_NAME) ==
+                               'person_id'].get(consts.TABLE_NAME).unique()
+    # EHR
+    query_list = []
+    hpo_tables_with_pid = [
+        table for table in tables_with_pid if hpo_id in table
+    ]
+    unioned_tables = [
+        table for table in tables_with_pid if common.UNIONED_EHR in table
+    ]
+    for table in hpo_tables_with_pid + unioned_tables:
+        ehr_count = "COUNT(*)"
+        tmpl = Template(consts.PID_TABLE_COUNT).render(project=project_id,
+                                                       dataset=dataset_id,
+                                                       table=table,
+                                                       pids_expr=pid_sql_expr,
+                                                       ehr_count=ehr_count)
+        query_list.append(tmpl)
+    query = consts.UNION_ALL.join(query_list)
+    return query
+
+
+def get_cdm_table(mapping_ext_table):
+    if common.MAPPING_PREFIX in mapping_ext_table:
+        return mapping_ext_table.replace(common.MAPPING_PREFIX, '')
+    return mapping_ext_table.replace(common.EXT_SUFFIX, '')
+
+
+def get_dataset_type(dataset_id):
+    if common.COMBINED in dataset_id and common.DEID not in dataset_id:
+        return common.COMBINED
+    if common.DEID in dataset_id:
+        return common.DEID
+    if common.UNIONED_EHR in dataset_id:
+        return common.UNIONED_EHR
+    if common.EHR in dataset_id and common.UNIONED_EHR not in dataset_id:
+        return common.EHR
+    return common.OTHER
+
+
+def get_pid_sql_expr(pid_source):
+    """
+    Converts a list of integer pids into a bq-compatible sql expression containing the pids as values
+    or a string of the form 'project.dataset.table' into a SELECT query that selects pids from the table
+
+    :param pid_source: can be a list of pids or string of the form 'project.dataset.table', where table contains pids
+    :return: bq-compatible string expression of pids or SELECT query that selects pids from table
+    """
+    if type(pid_source) == list:
+        return str(tuple(pid_source))
+    if pid_source.count('.') == 2:
+        return consts.PID_QUERY.format(pid_source=pid_source)
+    raise ValueError(
+        'Please specify pid_table parameters as "project.dataset.table"')
+
+
+def count_pid_rows_in_dataset(project_id, dataset_id, hpo_id, pid_source):
+    """
+    Returns df containing tables which have non-zero counts of participant rows for pids in pids_string
+
+    :param project_id: identifies the project
+    :param dataset_id: identifies the dataset
+    :param hpo_id: Identifies the hpo site that submitted the pids
+    :param pid_source: string containing query or list containing pids
+    :return: df with headers table_name, all_counts and ehr_counts
+    """
+    pid_sql_expr = get_pid_sql_expr(pid_source)
+    dataset_type = get_dataset_type(dataset_id)
+    counts_df = pd.DataFrame(
+        columns=[consts.TABLE_ID, consts.ALL_COUNT, consts.EHR_COUNT])
+    table_df = get_table_information_for_dataset(project_id, dataset_id)
+
+    if dataset_type == common.COMBINED or dataset_type == common.DEID or dataset_type == common.RELEASE:
+        query = get_combined_deid_query(project_id, dataset_id, pid_sql_expr,
+                                        table_df)
+    elif dataset_type == common.EHR:
+        query = get_ehr_query(project_id, dataset_id, pid_sql_expr, hpo_id,
+                              table_df)
+    else:
+        query = get_dataset_query(project_id, dataset_id, pid_sql_expr,
+                                  table_df)
+
+    if query:
+        counts_df = bq.query(query)
+        # ignore zero counts
+        counts_df = counts_df[counts_df[consts.ALL_COUNT] > 0]
+    return counts_df
+
+
+def log_total_rows(df, dataset_id):
+    """
+    Logs rows from counts dataframe
+
+    :param df: dataframe containing counts
+    :param dataset_id: Identifies the dataset under consideration
+    :return: 
+    """
+    rows = df.get_values()
+    if rows.size > 0:
+        for count_row in rows:
+            logging.info(
+                'DATASET_ID: {}\tTABLE_ID: {}\tALL_COUNT: {}\tEHR_COUNT: {}'.
+                format(dataset_id, *count_row))
+
+
+def count_pid_rows_in_project(project_id, hpo_id, pid_source):
+    """
+    Logs dataset_name, table_name, non_cdm_count and ehr_count to count rows pertaining to pids
+
+    :param project_id: identifies the project
+    :param hpo_id: Identifies the hpo site that submitted the pids
+    :param pid_source: string containing query or list containing pids
+    :return: dataframe containing 
+    """
+    datasets = bq.list_datasets(project_id)
+    for dataset in datasets:
+        dataset_id = dataset.dataset_id
+        try:
+            counts_df = count_pid_rows_in_dataset(project_id, dataset_id,
+                                                  hpo_id, pid_source)
+            log_total_rows(counts_df, dataset_id)
+        except BadRequest:
+            logging.exception(f'Dataset {dataset_id} could not be analyzed')
+
+
+def fetch_parser():
+    parser = argparse.ArgumentParser(
+        description='Estimates the prevalence of specified pids in the project',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-p',
+                        '--project_id',
+                        action='store',
+                        dest='project_id',
+                        help='Identifies the project to retract data from',
+                        required=True)
+    parser.add_argument('-o',
+                        '--hpo_id',
+                        action='store',
+                        dest='hpo_id',
+                        help='Identifies the site submitting the person_ids',
+                        required=True)
+    subparsers = parser.add_subparsers()
+
+    subparser_pid_list = subparsers.add_parser(
+        name='pid_list',
+        help='Specifies the source of pids in list of int form')
+    subparser_pid_list.add_argument(dest='pid_source',
+                                    nargs='+',
+                                    type=int,
+                                    help='person/research ids to consider')
+
+    subparser_pid_table = subparsers.add_parser(
+        name='pid_table', help='Specifies the source of pids in BQ table form')
+    subparser_pid_table.add_argument(
+        dest='pid_source', help='Specify table as "project.dataset.table"')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = fetch_parser()
+    args = parser.parse_args()
+
+    count_pid_rows_in_project(args.project_id, args.hpo_id, args.pid_source)
