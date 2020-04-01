abacd82bc5742f5bd0041b5fdf50aa2af8dcac4c
Lauren Biggers <lauren.r.biggers@vumc.org>
2020-04-01 17:16:36 -0500

    [DC-734] Create an Integration Test Cleaning Rule Example
    * initial integration test works
    * needs to be updated to use a template for easier testing

--- data_steward/utils/bq.py
+++ data_steward/utils/bq.py
@@ -1,30 +1,151 @@
+"""
+A utility to standardize use of the BigQuery python client library.
+"""
 # Python Imports
 import logging
 import os
+import time
 
 # Third-party imports
 from google.cloud import bigquery
+from google.api_core.exceptions import GoogleAPIError
 
 # Project Imports
-from app_identity import GOOGLE_CLOUD_PROJECT
+from app_identity import PROJECT_ID
 from constants.utils import bq as consts
+from resources import fields_for
+
+LOGGER = logging.getLogger(__name__)
 
 
 def get_client(project_id=None):
     """
     Get a client for a specified project.
+
+    :param project_id:  Name of the project to create a bigquery library client for
+
+    :return:  A bigquery Client object.  It is being nice for now, but will begin to
+        require users to provide the project_id.
     """
     if project_id is None:
-        logging.info(f'You should specify project_id for a reliable experience.'
-                     f'Defaulting to {os.environ.get(GOOGLE_CLOUD_PROJECT)}.')
+        LOGGER.info(f'You should specify project_id for a reliable experience.'
+                    f'Defaulting to {os.environ.get(PROJECT_ID)}.')
         return bigquery.Client()
     return bigquery.Client(project=project_id)
 
 
-def query(q, project_id=None, use_cache=False):
+def job_status_done(project_id, job_id):
+    """
+    Check if the job is complete without errors
+
+    Raises exceptions if they are found while checking the job state
+    
+    :param project_id: project containing the job status we want to check 
+    :param job_id: the job id
+    :return: a bool indicating whether the job is done
+    :raises:  any exceptions the job encounters
+    """
+    client = get_client(project_id)
+    job = client.get_job(job_id)
+    if job.exception():
+        raise (job.exception())
+    return job.done()
+
+
+def get_table_schema(table_name):
+    fields = fields_for(table_name)
+
+    schema = []
+    for column in fields:
+        name = column.get('name')
+        field_type = column.get('type')
+        mode = column.get('mode')
+        description = column.get('description')
+        column_def = bigquery.SchemaField(name, field_type, mode, description)
+
+        schema.append(column_def)
+
+    return schema
+
+
+def create_table(project_id=None, fq_table_names=None, exists_ok=False):
+
+    if not project_id:
+        raise RuntimeError("Must specify the project to create the tables in")
+
+    if not fq_table_names:
+        raise RuntimeError(
+            "Must specify a list of fully qualified table names to create")
+
+    client = get_client(project_id=project_id)
+
+    successes = []
+    failures = []
+    for table_name in fq_table_names:
+        schema = get_table_schema(table_name.split('.')[2])
+
+        try:
+            table = bigquery.Table(table_name, schema=schema)
+            table = client.create_table(table, exists_ok)
+        except (GoogleAPIError, OSError, AttributeError, TypeError, ValueError):
+            LOGGER.exception(f'Unable to create table {table_name}')
+            failures.append(table_name)
+        else:
+            successes.append(table)
+
+    if failures:
+        raise RuntimeError(f"Unable to create tables: {failures}")
+
+    return successes
+
+
+def wait_on_jobs(project_id, job_ids, retry_count=10, max_poll_interval=300):
+    """
+    Exponential backoff wait for jobs to complete
+
+    :param project_id: project containing the jobs we want to wait on
+    :param job_ids: a list of job ids to check for completion
+    :param retry_count:  the maximum number of retry attempts
+    :param max_poll_interval:  the maximum amount of time to wait on a job to finish
+    :return: list of jobs that failed to complete or empty list if all completed
+    """
+    if not isinstance(job_ids, list):
+        _job_ids = [job_ids]
+
+    _job_ids = job_ids
+    poll_interval = 1
+    for i in range(retry_count):
+        LOGGER.info('Waiting %s seconds for completion of job(s): %s' %
+                    (poll_interval, _job_ids))
+        time.sleep(poll_interval)
+        _job_ids = [
+            job_id for job_id in _job_ids
+            if not job_status_done(project_id, job_id)
+        ]
+
+        if not _job_ids:
+            return []
+
+        if poll_interval < max_poll_interval:
+            poll_interval = 2**i
+
+    LOGGER.info('Job(s) failed to complete: %s' % _job_ids)
+    return _job_ids
+
+
+def query(query, project_id=None, use_cache=False):
+    """
+    Run the specified query.
+
+    :param query: the query to execute
+    :param project_id:  the project identifier to run the query in.
+    :param use_cache:  allow using cached query objects.
+
+    :return:  The results of executing the query in the given project.
+    """
     client = get_client(project_id)
     query_job_config = bigquery.job.QueryJobConfig(use_query_cache=use_cache)
-    return client.query(q, job_config=query_job_config).to_dataframe()
+    return client.query(query, job_config=query_job_config)
 
 
 def list_datasets(project_id):
@@ -65,6 +186,77 @@ def get_dataset(project_id, dataset_id):
     return client.get_dataset(dataset_id)
 
 
+def create_dataset(project_id,
+                   dataset_id,
+                   description,
+                   labels={},
+                   exists_ok=False):
+    """
+    Create the dataset reference.
+
+    :param project_id:  string name of the project to search for a dataset
+    :param dataset_id:  string name of the dataset id to return a reference of
+
+    :return: a dataset reference object.
+
+    :raises: any GoogleAPIError that is not a 404 error.  
+        google.api_core.exceptions.Conflict if the dataset already exists
+    """
+    if description.isspace() or not description:
+        raise RuntimeError("you must provide a description.")
+
+    if not project_id:
+        raise RuntimeError(
+            "You must specify the project_id for the project containing the dataset"
+        )
+
+    if not dataset_id:
+        raise RuntimeError("You must provide a dataset_id")
+
+    client = get_client(project_id)
+
+    dataset_id = "{}.{}".format(client.project, dataset_id)
+
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
+
+    # TODO(developer): Specify the geographic location where the dataset should reside.
+    dataset.location = "US"
+
+    try:
+        dataset = client.create_dataset(dataset,
+                                        exists_ok)  # Make an API request.
+    except google.api_core.exceptions.Conflict as err:
+        logger.exception("Dataset {} already exists.  Returning that dataset",
+                         dataset_id)
+        return client.get_dataset(dataset_id)
+
+    return dataset
+
+
+def get_or_create_dataset(project_id, dataset_id):
+    """
+    Get the dataset reference if it exists.  Else create it.
+
+    :param project_id:  string name of the project to search for a dataset
+    :param dataset_id:  string name of the dataset id to return a reference of
+
+    :return: a dataset reference object.
+
+    :raises: any GoogleAPIError that is not a 404 error
+    """
+    client = get_client(project_id)
+
+    try:
+        return get_dataset(project_id, dataset_id)
+    except GoogleAPIError as err:
+        if err.code != 404:
+            raise err
+
+        desc = 'dataset automatically generated because it doesnt exist yet'
+        return create_dataset(project_id, dataset_id, desc)
+
+
 def delete_dataset(project_id,
                    dataset_id,
                    delete_contents=True,
@@ -84,7 +276,7 @@ def delete_dataset(project_id,
     client.delete_dataset(dataset_id,
                           delete_contents=delete_contents,
                           not_found_ok=not_found_ok)
-    logging.info(f'Deleted dataset {project_id}.{dataset_id}')
+    LOGGER.info(f'Deleted dataset {project_id}.{dataset_id}')
 
 
 def is_validation_dataset_id(dataset_id):
---
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/rdr_observation_source_concept_id_suppression_test.py
@@ -0,0 +1,191 @@
+"""
+Integration Test for the rdr_observation_source_concept_id_suppression module.
+
+Remove three irrelevant observation_source_concept_ids from the RDR dataset.
+
+Original Issue:  DC-734 implements integration tests for DC-529
+
+The intent is to remove PPI records from the observation table in the RDR
+export where observation_source_concept_id in (43530490, 43528818, 43530333).
+The records for removal should be archived in the dataset sandbox.  It should
+also ensure that records that have null values or do not match the specified
+ids are not removed.
+"""
+# Python imports
+import os
+import unittest
+
+# Third party imports
+from google.cloud import bigquery
+import google.api_core.exceptions as exc
+import google.cloud.exceptions as gc_exc
+from jinja2 import Template
+
+# Project imports
+import resources
+from app_identity import PROJECT_ID
+from constants.bq_utils import WRITE_TRUNCATE
+from constants.cdr_cleaner import clean_cdr as clean_consts
+from cdr_cleaner.cleaning_rules.rdr_observation_source_concept_id_suppression import ObservationSourceConceptIDRowSuppression
+from cdr_cleaner import clean_cdr_engine as engine
+from utils import bq
+
+INSERT_FAKE_PARTICIPANTS_TMPLS = [
+    Template("""
+INSERT INTO `{{fq_table_name}}` (observation_id, person_id, observation_concept_id, observation_date, observation_type_concept_id, observation_source_concept_id)
+VALUES
+  (801, 337361, 1585899, date('2016-05-01'), 45905771, {{age_prediabetes}}),
+  (802, 129884, 1585899, date('2016-05-01'), 45905771, {{meds_prediabetes}}),
+  (803, 337361, 1585899, date('2016-05-01'), 45905771, {{now_prediabetes}}),
+  (804, 129884, 1585899, date('2016-05-01'), 45905771, null),
+  (805, 337361, 1585899, date('2016-05-01'), 45905771, 45)
+""")
+]
+
+
+class ObservationSourceConceptIDRowSuppressionTest(unittest.TestCase):
+
+    fq_sandbox_table = ''
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+        print("class setup function\n")
+        # get the test dataset
+        cls.project_id = os.environ.get(PROJECT_ID)
+        if 'test' not in cls.project_id:
+            raise RuntimeError('This should only be run on a test environment')
+
+        cls.dataset_id = os.environ.get('RDR_DATASET_ID')
+        cls.dataset = bq.get_or_create_dataset(cls.project_id, cls.dataset_id)
+
+        cls.sandbox_id = cls.dataset_id + '_sandbox'
+        cls.sandbox_dataset = bq.get_or_create_dataset(cls.project_id,
+                                                       cls.sandbox_id)
+
+        # create empty table in test environment
+        cls.table_name = 'observation'
+        cls.client = bq.get_client(cls.project_id)
+
+        cls.fq_table_name = f'{cls.project_id}.{cls.dataset_id}.{cls.table_name}'
+        table_info = {
+            # list of fully qualified table names to create with schemas
+            'fq_table_names': [cls.fq_table_name],
+            'project_id': cls.project_id,
+            'exists_ok': True,
+        }
+        bq.create_table(**table_info)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Remove the test daaset table(s).
+        """
+        cls.client.delete_table(cls.fq_table_name)
+        cls.client.delete_table(cls.fq_sandbox_table)
+
+    def tearDown(self):
+        """
+        Ensure the data is dropped from the table(s).
+        """
+        drop_rows_job = self.client.query(
+            f"delete from {self.fq_table_name} where observation_id > -1")
+        job_status = bq.wait_on_jobs(self.project_id, [drop_rows_job.job_id])
+
+        self.assertFalse(
+            job_status,
+            f"The contents of {self.fq_table_name} could not be cleared")
+
+        drop_rows_job = self.client.query(
+            f"delete from {self.fq_sandbox_table} where observation_id > -1")
+        job_status = bq.wait_on_jobs(self.project_id, [drop_rows_job.job_id])
+
+        self.assertFalse(
+            job_status,
+            f"The contents of {self.fq_sandbox_table} could not be cleared")
+
+    def setUp(self):
+        """
+        Add data to the tables for the rule to run on.
+        """
+        self.age_prediabetes = 43530490
+        self.meds_prediabetes = 43528818
+        self.now_prediabetes = 43530333
+
+        # create the string(s) to load the data
+        for tmpl in INSERT_FAKE_PARTICIPANTS_TMPLS:
+            query = tmpl.render(fq_table_name=self.fq_table_name,
+                                age_prediabetes=self.age_prediabetes,
+                                meds_prediabetes=self.meds_prediabetes,
+                                now_prediabetes=self.now_prediabetes)
+            response = bq.query(query, self.project_id)
+            self.assertIsNotNone(response.result())
+            self.assertIsNone(response.exception())
+
+        self.query_class = ObservationSourceConceptIDRowSuppression(
+            self.project_id, self.dataset_id, self.sandbox_id)
+
+        table_name = self.query_class.get_sandbox_tablenames()[0]
+        self.fq_sandbox_table = f'{self.project_id}.{self.sandbox_id}.{table_name}'
+        ObservationSourceConceptIDRowSuppressionTest.fq_sandbox_table = self.fq_sandbox_table
+
+    def test_expected_cleaning_performance(self):
+        """
+        Test getting the query specifications.
+
+        This should test that the specifications for the query perform
+        as designed.  The rule should drop only what it is designed to
+        drop.  No more and no less.
+        """
+        # pre-conditions
+        # validate sandbox table doesn't exist yet
+        response = bq.query(f"select count(*) from `{self.fq_sandbox_table}`",
+                            self.project_id)
+        self.assertRaises(gc_exc.GoogleCloudError, response.result, timeout=15)
+
+        # validate only 5 records exist before starting
+        response = bq.query(f"select count(*) from `{self.fq_table_name}`",
+                            self.project_id)
+        # start the job and wait for it to complete
+        for index, item in enumerate(response.result()):
+            self.assertEqual(
+                1, index + 1,
+                "The pre-condition query did not return a single row")
+            self.assertEqual(5, item[0],
+                             "The test data did not load as expected")
+
+        # test
+        query_list = self.query_class.get_query_specs()
+
+        # run the queries
+        engine.clean_dataset(self.project_id, query_list)
+
+        # post conditions
+        # validate three records are dropped
+        response = bq.query(f"select * from `{self.fq_table_name}`",
+                            self.project_id)
+        result_list = list(response.result())
+        self.assertEqual(2, len(result_list))
+
+        rows_kept = []
+        for row in result_list:
+            rows_kept.append(row[0])
+
+        # assert the contents are equal regardless of the order
+        self.assertCountEqual([804, 805], rows_kept)
+
+        # validate three records are saved
+        response = bq.query(f"select * from `{self.fq_sandbox_table}`",
+                            self.project_id)
+        result_list = list(response.result())
+        self.assertEqual(3, len(result_list))
+
+        rows_kept = []
+        for row in result_list:
+            rows_kept.append(row[0])
+
+        # assert the contents are equal regardless of the order
+        self.assertCountEqual([801, 802, 803], rows_kept)
