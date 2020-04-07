597dc5fb6d7acc5edbc0413ad5efb8fd21583965
dcampbell-vumc <danyel.campbell@vumc.org>
2020-04-07 11:14:28 -0500

    circleci integration check debug

--- data_steward/ci/activate_creds.sh
+++ data_steward/ci/activate_creds.sh
@@ -4,9 +4,11 @@
 # below) which combine to decrypt the keys for a service account.
 # Does gcloud auth using the result.
 
+echo "1 ---------------------"
 echo -n $GCLOUD_CREDENTIALS | tail -c 3
-echo ""
+echo "2 ---------------------"
 echo -n $GCLOUD_CREDENTIALS_KEY | tail -c 3
+echo "3 ---------------------"
 
 echo $GCLOUD_CREDENTIALS | \
      openssl enc -d -aes-256-cbc -base64 -A -md md5 -k $GCLOUD_CREDENTIALS_KEY \
