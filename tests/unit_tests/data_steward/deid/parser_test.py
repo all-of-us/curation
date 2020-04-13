786d5fa63c6613941051274e5241ede7953bf216
dcampbell-vumc <danyel.campbell@vumc.org>
2020-04-13 15:31:02 -0500

    [DC-744] Update deid script
    Added happy and sad test paths for test_odataset_name_verification and test_parse_args

--- tests/unit_tests/data_steward/deid/parser_test.py
+++ tests/unit_tests/data_steward/deid/parser_test.py
@@ -1,10 +1,20 @@
+"""
+Unit Test for parser module
+
+Ensure that the output dataset name includes _deid
+
+Original Issue: DC-744
+"""
+
 # Python imports
 import unittest
-
+import os
+from argparse import ArgumentTypeError, ArgumentError
 
 # Project imports
-from deid.parser import odataset_name_verification, parse_args
-from tools import run_deid
+from deid.parser import odataset_name_verification, parse_args, Parse
+from resources import DEID_PATH
+
 
 class ParserTest(unittest.TestCase):
 
@@ -15,17 +25,49 @@ class ParserTest(unittest.TestCase):
         print('**************************************************************')
 
     def setUp(self):
-        self.input_dataset = "foo_input"
-        self.output_dataset = "food_output_deid"
-        self.tablename = "bar_table"
+        # input parameters expected by the class
+        self.output_dataset = 'foo_output_deid'
+        self.input_dataset = 'foo_input'
+        self.tablename = 'bar_table'
+        self.action = 'debug'
         self.log_path = 'deid-fake.log'
-        self.parameter_list = ['--rules',
-                               ]
+        self.private_key = 'fake/SA/file/path.json'
+        self.rules = os.path.join(DEID_PATH, 'config', 'ids', 'config.json')
+        self.pipeline = ['generalize', 'suppress', 'shift', 'compute']
+        self.interactive = 'BATCH'
 
+        self.correct_parameter_list = [
+            '--rules', self.rules, '--private_key', self.private_key, '--table', self.tablename,
+            '--action', self.action, '--idataset', self.input_dataset, '--odataset',
+            self.output_dataset, '--log', self.log_path, '--pipeline', self.pipeline,
+            '--interactive', self.interactive, '--interactive', self.interactive
+        ]
+        self.incorrect_parameter_list = [
+            '--rules', self.rules, '--private_key', self.private_key, '--table', self.tablename,
+            '--action', self.action, '--idataset', self.input_dataset, '--log', self.log_path
+        ]
 
-    def test_odataset_name_verification(self, output_dataset):
-        parse_args()
+    def test_parse_args(self):
+        # Tests if incorrect parameters are given
+        self.assertRaises(SystemExit, parse_args, self.incorrect_parameter_list)
 
+        # Tests if correct parameters are given
+        it = iter(self.correct_parameter_list)
+        correct_parameter_dict = dict(zip(it, it))
+        correct_parameter_dict = {k.strip('--'): v for (k, v) in correct_parameter_dict.items()}
 
+        results_dict = parse_args(self.correct_parameter_list)
+        if 'cluster' in results_dict:
+            del results_dict['cluster']
+            del results_dict['age-limit']
 
-    # def test_parse_args(self, parameter_list):
+        self.assertEqual(correct_parameter_dict, results_dict)
+
+    def test_odataset_name_verification(self):
+        # Tests if output dataser name does not contain _deid
+        self.assertRaises(ArgumentTypeError, odataset_name_verification, "foo_output")
+
+        # Tests if output dataset name does contain _deid
+        result = odataset_name_verification("foo_output_deid")
+
+        self.assertEqual(result, self.output_dataset)
