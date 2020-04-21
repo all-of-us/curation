f92e52adb933f08a42dd87cd6f510db892d908eb
Nishanth Pavinkurve <npp267@nyu.edu>
2020-04-21 13:53:24 -0400

    [DC-759] Require commit message titles to contain a jira issue number (#481)
    * [DC-759] Add blank files for hook
    * [DC-759] Add pre-push file and change to prepare-commit-msg
    * [DC-759] Propagate exit code in pre-push
    * [DC-759] Change JIRA to Jira in circle config
    * [DC-759] Explicitly define develop
    * [DC-759] Simplify rev-list for Circle
    * [DC-759] Add check for PR titles, ensure regex checks beginning
    * [DC-759] Restrict only to pull requests

---
+++ tools/validate_commit_message.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Validates that all commits have an associated [DC-###] or [EDQ-###] Jira
+# ticket tag in the beginning of the commit message, fails if any don't
+# For commits on develop, also ensure that the Jira title follows the Jira tag
+
+TICKET_REGEX="^\[(DC|EDQ)-[[:digit:]]+\][[:space:]]"
+ERROR_MSG="The commit message below does not start with the Jira ticket tag, please rename."
+
+for rev in $(git rev-list origin/develop...HEAD);
+  do
+  msg=$(git cat-file commit "${rev}" | sed '1,/^$/d')
+  if [[ ! $msg =~ $TICKET_REGEX ]];
+  then
+    echo "${ERROR_MSG}"
+    echo "${msg}"
+    exit 1
+  fi
+done
