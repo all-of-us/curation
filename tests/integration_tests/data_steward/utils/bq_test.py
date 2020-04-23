08c402bb20a65d4aebe196f1b4832d19d4bb2464
dcampbell-vumc <danyel.campbell@vumc.org>
2020-04-22 19:55:19 -0500

    [DC-757] Tag pipeline dataset
    (1) Added test_dataset_exists function to test if dataset_id or project_id
    parameters are supplied
    (2) Modified test_create_dataset so only one dataset needs to be created
    and tests

--- tests/integration_tests/data_steward/utils/bq_test.py
+++ tests/integration_tests/data_steward/utils/bq_test.py
@@ -15,7 +15,7 @@ labels, and description.
 import unittest
 
 # Project imports
-from utils.bq import create_dataset, delete_dataset, get_dataset
+from utils.bq import create_dataset, delete_dataset, get_dataset, dataset_exists
 
 
 class BqTest(unittest.TestCase):
@@ -29,40 +29,38 @@ class BqTest(unittest.TestCase):
     def setUp(self):
         # input parameters expected by the class
         self.project_id = 'aou-res-curation-test'
-        self.existing_dataset_id = 'bar_dataset'
-        self.new_dataset_id = 'baz_dataset'
+        self.dataset_id = "foo_dataset"
         self.description = 'fake description'
-        self.label = {'fake_label': 'label'}
+        self.label_or_tag = {'fake_label': 'label', 'fake_tag': ''}
 
-    def test_create_dataset(self):
-        # Tests failure of create_dataset due to dataset already existing
-        self.assertRaises(RuntimeError, create_dataset,
-                          self.existing_dataset_id, self.description,
-                          self.label, self.project_id)
-
+    def test_dataset_exists(self):
+        """Only tests whether a dataset_id and or project_id are supplied"""
         # Tests if project_id is given
-        self.assertRaises(TypeError, create_dataset, self.existing_dataset_id,
-                          self.description, self.label)
+        self.assertRaises(TypeError, dataset_exists, self.dataset_id)
 
         # Tests if dataset_id is given
-        self.assertRaises(TypeError, create_dataset, self.description,
-                          self.label, self.project_id)
+        self.assertRaises(TypeError, dataset_exists, self.project_id)
 
+    def test_create_dataset(self):
         # Tests if description is given
-        self.assertRaises(TypeError, create_dataset, self.project_id,
-                          self.existing_dataset_id, self.label)
+        self.assertRaises(TypeError, create_dataset, self.dataset_id,
+                          self.label_or_tag, self.project_id)
 
-        # Tests if label is given
-        self.assertRaises(TypeError, create_dataset, self.project_id,
-                          self.existing_dataset_id, self.description)
+        # Tests if no label or tag is given
+        self.assertRaises(TypeError, create_dataset, self.dataset_id,
+                          self.description, self.project_id)
 
-        # Tests creation of dataset "baz_dataset" is successful
-        create_dataset(self.new_dataset_id, self.description, self.label,
+        # Tests creation of dataset "foo_dataset"
+        create_dataset(self.dataset_id, self.description, self.label_or_tag,
                        self.project_id)
 
+        # Tests failure of create_dataset since the dataset "foo_dataset" already exists
+        self.assertRaises(RuntimeError, create_dataset, self.dataset_id,
+                          self.description, self.label_or_tag, self.project_id)
+
         # Post conditions
-        self.assertTrue(get_dataset(self.project_id, self.new_dataset_id))
+        self.assertTrue(get_dataset(self.project_id, self.dataset_id))
 
     def tearDown(self):
-        # Deletes dataset "baz_dataset"
-        delete_dataset(self.project_id, self.new_dataset_id)
+        # Deletes dataset "foo_dataset"
+        delete_dataset(self.project_id, self.dataset_id)
