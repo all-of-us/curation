091d1c2e93d9fa168d32f61bdb9d7747c7102410
dcampbell-vumc <danyel.campbell@vumc.org>
2020-04-27 13:49:24 -0500

    [DC-757] Tag pipeline datasets
    (1) Deleted test_dataset_exists and delete_dataset functions
    (2) Changed test_create_dataset to test_define_dataset to test the
    define_dataset functionality

--- tests/integration_tests/data_steward/utils/bq_test.py
+++ tests/integration_tests/data_steward/utils/bq_test.py
@@ -14,8 +14,11 @@ labels, and description.
 # Python imports
 import unittest
 
+# Third-party imports
+from google.cloud import bigquery
+
 # Project imports
-from utils.bq import create_dataset, delete_dataset, get_dataset, dataset_exists
+from utils.bq import define_dataset
 
 
 class BqTest(unittest.TestCase):
@@ -30,56 +33,29 @@ class BqTest(unittest.TestCase):
         # input parameters expected by the class
         self.project_id = 'aou-res-curation-test'
         self.dataset_id = "foo_dataset"
-        self.dataset_id_label_only = "bar_dataset"
-        self.dataset_id_tag_only = "baz_dataset"
         self.description = 'fake description'
-        self.label = {'fake_label': 'label'}
-        self.no_label = None
-        self.tag = {'fake_tag': ''}
-        self.no_tag = None
+        self.label_or_tag = {'fake_label': 'label', 'fake_tag': ''}
 
-    def test_dataset_exists(self):
-        """Only tests whether a dataset_id and or project_id are supplied"""
+    def test_define_dataset(self):
         # Tests if project_id is given
-        self.assertRaises(TypeError, dataset_exists, self.dataset_id)
+        self.assertRaises(TypeError, define_dataset, self.dataset_id,
+                          self.description, self.label_or_tag)
 
         # Tests if dataset_id is given
-        self.assertRaises(TypeError, dataset_exists, self.project_id)
+        self.assertRaises(TypeError, define_dataset, self.project_id,
+                          self.description, self.label_or_tag)
 
-    def test_create_dataset(self):
         # Tests if description is given
-        self.assertRaises(TypeError, create_dataset, self.dataset_id,
-                          self.label, self.tag, self.project_id)
+        self.assertRaises(TypeError, define_dataset, self.project_id,
+                          self.dataset_id, self.label_or_tag)
 
         # Tests if no label or tag is given
-        self.assertRaises(TypeError, create_dataset, self.dataset_id,
-                          self.description, self.project_id)
+        self.assertRaises(TypeError, define_dataset, self.project_id,
+                          self.dataset_id, self.description)
 
-        # Tests creation of dataset "foo_dataset" with both label and tag
-        create_dataset(self.dataset_id, self.description, self.label, self.tag,
-                       self.project_id)
-
-        # Tests creation of dataset "bar_dataset" with only label supplied
-        create_dataset(self.dataset_id_label_only, self.description, self.label,
-                       self.no_tag, self.project_id)
-
-        # Tests creation of dataset "baz_dataset" with only tag supplied
-        create_dataset(self.dataset_id_tag_only, self.description,
-                       self.no_label, self.tag, self.project_id)
-
-        # Tests failure of create_dataset since the dataset "foo_dataset" already exists
-        self.assertRaises(RuntimeError, create_dataset, self.dataset_id,
-                          self.description, self.label, self.tag,
-                          self.project_id)
+        # Pre-conditions
+        results = define_dataset(self.project_id, self.dataset_id,
+                                 self.description, self.label_or_tag)
 
         # Post conditions
-        self.assertTrue(get_dataset(self.project_id, self.dataset_id))
-        self.assertTrue(get_dataset(self.project_id,
-                                    self.dataset_id_label_only))
-        self.assertTrue(get_dataset(self.project_id, self.dataset_id_tag_only))
-
-    def tearDown(self):
-        # Deletes the above created datasets
-        delete_dataset(self.project_id, self.dataset_id)
-        delete_dataset(self.project_id, self.dataset_id_label_only)
-        delete_dataset(self.project_id, self.dataset_id_tag_only)
+        self.assertIsInstance(results, bigquery.Dataset)
