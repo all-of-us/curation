149f5e825660eb96520fa3bd6709631a20ba09db
dcampbell-vumc <danyel.campbell@vumc.org>
2020-04-29 14:34:32 -0500

    [DC-758] update dataset labels
    Modified bq_test.py to:
    (1) update integration test module description
    (2) add input parameters to the setUp function expected for the update_labels_and_tags bq.py function
    (3) created the test_update_labels_and_tags function to test the
    functionality of the update_labels_and_tags bq.py function

--- tests/integration_tests/data_steward/utils/bq_test.py
+++ tests/integration_tests/data_steward/utils/bq_test.py
@@ -1,14 +1,16 @@
 """
 Integration Test for the bq module
 
-Ensures the create_dataset function has the proper parameters passed to it
+Ensures the define_dataset function has the proper parameters passed to it
+Ensures the update_labels_and_tags function has the proper parameters passed to it and
+    sets, replaces, or updates labels and tags properly
 
-Original Issue: DC-757
+Original Issues: DC-757, DC-758
 
-The intent is to check whether the dataset exists or not. If the dataset does
-exist, a runtime error is raised stating as such. If the dataset does not
-exist, it needs to be created with the proper parameters: project_id, dataset_id,
-labels, and description.
+The intent is to check that the proper parameters (project_id, dataset_id,
+ label_or_tag, and or description) are passed to the define_dataset and
+ update_labels_and_tags functions. Also to make sure the labels and tags can be either set,
+ replaced, or updated with different labels or tags in the update_labels_and_tags function.
 """
 
 # Python imports
@@ -18,7 +20,7 @@ import unittest
 from google.cloud import bigquery
 
 # Project imports
-from utils.bq import define_dataset
+from utils.bq import define_dataset, update_labels_and_tags
 
 
 class BqTest(unittest.TestCase):
@@ -35,6 +37,11 @@ class BqTest(unittest.TestCase):
         self.dataset_id = "foo_dataset"
         self.description = 'fake description'
         self.label_or_tag = {'fake_label': 'label', 'fake_tag': ''}
+        self.overwritten_label_or_tag = {'new_fake_label': 'label', "new_fake_tag": ''}
+        self.updated_label_or_tag = {'fake_label': 'label', 'fake_tag': '',
+                                     'new_fake_label': 'label', "new_fake_tag": ''}
+        self.set_or_update_true = True
+        self.set_or_update_false = False
 
     def test_define_dataset(self):
         # Tests if project_id is given
@@ -60,3 +67,42 @@ class BqTest(unittest.TestCase):
         # Post conditions
         self.assertIsInstance(results, bigquery.Dataset)
         self.assertEqual(results.labels, self.label_or_tag)
+
+    def test_update_labels_and_tags(self):
+        # Tests if project_id is given
+        self.assertRaises(TypeError, update_labels_and_tags, self.dataset_id,
+                          self.label_or_tag)
+
+        # Tests if dataset_id is given
+        self.assertRaises(TypeError, update_labels_and_tags, self.project_id,
+                          self.label_or_tag)
+
+        # Tests if no label or tag is given
+        self.assertRaises(TypeError, update_labels_and_tags, self.project_id,
+                          self.dataset_id)
+
+        # Pre-conditions
+        results = define_dataset(self.project_id, self.dataset_id,
+                                 self.description, self.label_or_tag)
+
+        self.client = bigquery.Client()
+        self.dataset = self.client.create_dataset(results)
+
+        update_false_expected = update_labels_and_tags(self.project_id, self.dataset_id,
+                                                       self.overwritten_label_or_tag, self.set_or_update_false)
+
+        update_true_expected = update_labels_and_tags(self.project_id, self.dataset_id,
+                                                      self.updated_label_or_tag, self.set_or_update_true)
+
+        # Post conditions
+        self.assertIsInstance(results, bigquery.Dataset)
+        self.assertEqual(update_true_expected.labels, self.updated_label_or_tag)
+        self.assertEqual(update_false_expected.labels, self.overwritten_label_or_tag)
+
+    def tearDown(self):
+        # Deletes bar_dataset
+        self.client = bigquery.Client()
+        self.client.delete_dataset(f'{self.project_id}.{self.dataset_id}',
+                                   delete_contents=True,
+                                   not_found_ok=True)
+
