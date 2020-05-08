37244f11327151de09b211441a97f8a8a5090083
Mark Velez <mav7014@nyp.org>
2020-05-08 13:15:33 -0400

    Revert "Changes to ci/setup do not need to be pushed to repo"
    This reverts commit 4380f220

---
+++ data_steward/ci/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create buckets
+for b in ${DRC_BUCKET_NAME} ${BUCKET_NAME_FAKE} ${BUCKET_NAME_NYC} ${BUCKET_NAME_PITT} ${BUCKET_NAME_CHS} ${BUCKET_NAME_UNIONED_EHR}; do
+  $(git rev-parse --show-toplevel)/data_steward/ci/create_bucket.sh ${b}
+done
+
+# Delete datasets if existing
+bq rm -r -d -f ${APPLICATION_ID}:${RDR_DATASET_ID}
+bq rm -r -d -f ${APPLICATION_ID}:${COMBINED_DATASET_ID}
+bq rm -r -d -f ${APPLICATION_ID}:${BIGQUERY_DATASET_ID}
+bq rm -r -d -f ${APPLICATION_ID}:${UNIONED_DATASET_ID}
+
+# Create datasets
+bq mk --dataset --description "Test RDR dataset for ${USERNAME}" ${APPLICATION_ID}:${RDR_DATASET_ID}
+bq mk --dataset --description "Test COMBINED dataset for ${USERNAME}" ${APPLICATION_ID}:${COMBINED_DATASET_ID}
+bq mk --dataset --description "Test EHR dataset for ${USERNAME}" ${APPLICATION_ID}:${BIGQUERY_DATASET_ID}
+bq mk --dataset --description "Test EHR union dataset for ${USERNAME}" ${APPLICATION_ID}:${UNIONED_DATASET_ID}
+
+# Create vocabulary tables if they do not already exist
+VOCABULARY_DATASET="${APPLICATION_ID}:vocabulary20190423"
+DEST_PREFIX="${APPLICATION_ID}:${BIGQUERY_DATASET_ID}"
+for t in $(bq ls ${VOCABULARY_DATASET} | grep TABLE | awk '{print $1}')
+do
+  CLONE_CMD="bq cp --project_id=${APPLICATION_ID} -n ${VOCABULARY_DATASET}.${t} ${DEST_PREFIX}.${t}"
+  echo $(${CLONE_CMD})
+done
