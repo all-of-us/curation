0ba7613406f953e6285559fc7ca72dba7883cb31
dcampbell-vumc <danyel.campbell@vumc.org>
2020-05-08 11:12:41 -0500

    [DC-758] Update Dataset Labels
    (1) Removed test_labels_and_tags and delete_dataset functions
    (2) Modified module description
    (3) Removed unused import statements

--- tests/integration_tests/data_steward/utils/bq_test.py
+++ tests/integration_tests/data_steward/utils/bq_test.py
@@ -1,16 +1,12 @@
 """
 Integration Test for the bq module
 
-Ensures the define_dataset function has the proper parameters passed to it
-Ensures the update_labels_and_tags function has the proper parameters passed to it and
-    sets, replaces, or updates labels and tags properly
+Ensures the define_dataset function has the proper parameters passed to it.
 
-Original Issues: DC-757, DC-758
+Original Issue: DC-757
 
 The intent is to check that the proper parameters (project_id, dataset_id,
- label_or_tag, and or description) are passed to the define_dataset and
- update_labels_and_tags functions. Also to make sure the labels and tags can be either set,
- replaced, or updated with different labels or tags in the update_labels_and_tags function.
+ label_or_tag, and or description) are passed to the define_dataset function.
 """
 
 # Python imports
@@ -20,7 +16,7 @@ import unittest
 from google.cloud import bigquery
 
 # Project imports
-from utils.bq import define_dataset, update_labels_and_tags
+from utils.bq import define_dataset
 
 
 class BqTest(unittest.TestCase):
@@ -37,18 +33,6 @@ class BqTest(unittest.TestCase):
         self.dataset_id = "foo_dataset"
         self.description = 'fake description'
         self.label_or_tag = {'fake_label': 'label', 'fake_tag': ''}
-        self.overwritten_label_or_tag = {
-            'new_fake_label': 'label',
-            "new_fake_tag": ''
-        }
-        self.updated_label_or_tag = {
-            'fake_label': 'label',
-            'fake_tag': '',
-            'new_fake_label': 'label',
-            "new_fake_tag": ''
-        }
-        self.set_or_update_true = True
-        self.set_or_update_false = False
 
     def test_define_dataset(self):
         # Tests if project_id is given
@@ -74,45 +58,3 @@ class BqTest(unittest.TestCase):
         # Post conditions
         self.assertIsInstance(results, bigquery.Dataset)
         self.assertEqual(results.labels, self.label_or_tag)
-
-    def test_update_labels_and_tags(self):
-        # Tests if project_id is given
-        self.assertRaises(TypeError, update_labels_and_tags, self.dataset_id,
-                          self.label_or_tag)
-
-        # Tests if dataset_id is given
-        self.assertRaises(TypeError, update_labels_and_tags, self.project_id,
-                          self.label_or_tag)
-
-        # Tests if no label or tag is given
-        self.assertRaises(TypeError, update_labels_and_tags, self.project_id,
-                          self.dataset_id)
-
-        # Pre-conditions
-        results = define_dataset(self.project_id, self.dataset_id,
-                                 self.description, self.label_or_tag)
-
-        self.client = bigquery.Client()
-        self.dataset = self.client.create_dataset(results)
-
-        update_false_expected = update_labels_and_tags(
-            self.project_id, self.dataset_id, self.overwritten_label_or_tag,
-            self.set_or_update_false)
-
-        update_true_expected = update_labels_and_tags(self.project_id,
-                                                      self.dataset_id,
-                                                      self.updated_label_or_tag,
-                                                      self.set_or_update_true)
-
-        # Post conditions
-        self.assertIsInstance(results, bigquery.Dataset)
-        self.assertEqual(update_true_expected.labels, self.updated_label_or_tag)
-        self.assertEqual(update_false_expected.labels,
-                         self.overwritten_label_or_tag)
-
-    def tearDown(self):
-        # Deletes bar_dataset
-        self.client = bigquery.Client()
-        self.client.delete_dataset(f'{self.project_id}.{self.dataset_id}',
-                                   delete_contents=True,
-                                   not_found_ok=True)
