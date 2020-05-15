92c5b10a77c2109030adf856caac89ff63823f13
dcampbell-vumc <danyel.campbell@vumc.org>
2020-05-15 12:35:55 -0500

    [DC-637] Create Logging Setup Utility
    Created unit test pipeline_logging_test.py to test pipeline_logging
    module.

---
+++ tests/unit_tests/data_steward/utils/pipeline_logging_test.py
@@ -0,0 +1,16 @@
+"""
+Unit test for pipeline_logging module.
+
+Ensures logging handlers are set up correctly by making sure:
+    1. The right number of expected handlers exists
+    2. The StreamHandler exists depending on test input
+    3. The log files that are configured point to the right file location
+
+Original Issue = DC-637
+"""
+
+# Python imports
+import unittest
+
+# Project imports
+from utils import pipeline_logging
