0e4dd650c41490912eb7735a463d7c155b6d29c6
Krishna Kalluri <krishnasaikalluri@gmail.com>
2020-05-18 12:09:22 -0400

    [DC-866] Add unioned_ehr to whitelist, where it can be prepended to the Achilles tables.
    Signed-off-by: Krishna Kalluri <krishnasaikalluri@gmail.com>

--- data_steward/tools/run_achilles_and_export.py
+++ data_steward/tools/run_achilles_and_export.py
@@ -18,10 +18,9 @@ from validation.main import run_export as _run_export
 
 
 def main(args):
-    folder = args.folder
     dataset_id = get_dataset_id()
     target_bucket = args.bucket
-    folder_prefix = folder + '/'
+    folder_prefix = args.folder + '/'
     _run_achilles()
     _run_export(datasource_id=dataset_id,
                 folder_prefix=folder_prefix,
