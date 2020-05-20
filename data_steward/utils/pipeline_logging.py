edf374de9e5f6da0e34c21dc512ad1b94448a45f
dcampbell-vumc <danyel.campbell@vumc.org>
2020-05-20 15:32:09 -0500

    [DC-637] Create Logging Setup Utility
    (1) Added function description
    (2) Changed logic to determine whether to log to file or console and
    file
    (3) Modified locgic to determine if directory needs to be created or not

--- data_steward/utils/pipeline_logging.py
+++ data_steward/utils/pipeline_logging.py
@@ -15,12 +15,15 @@ from datetime import datetime
 
 
 def setup_logger(log_filepath_list, console_logging=False):
-    output_log_path = [
-        Path(filepath)
-        for filepath in log_filepath_list
-        if 'log' in os.path.basename(filepath)
-    ]
+    """
+    Sets up python logging to file and or console for use in other modules.
 
+    :param log_filepath_list:  desired string path and name of the log file
+    :param console_logging:  determines if log should be output to the console
+                             is false by default, if true, will log output to console
+    """
+
+    # iterates through log_filepath_list and sets path to provided path and default_file_name
     default_file_name = datetime.now().strftime('curation%Y%m%d_%H%M%S.log')
     default_output_log_path = [
         os.path.join(filepath, default_file_name)
@@ -28,12 +31,19 @@ def setup_logger(log_filepath_list, console_logging=False):
         if 'log' not in os.path.basename(filepath)
     ]
 
+    # iterates through log_filepath_list and sets path to log files
+    output_log_path = [
+        Path(filepath)
+        for filepath in log_filepath_list
+        if 'log' in os.path.basename(filepath)
+    ]
+
+    log_path = output_log_path + default_output_log_path
+
     # if any path in output_log_path or default_output_log_path doesn't exist, will be created
-    for output_log, default_output_log in [
-            output_log_path, default_output_log_path
-    ]:
+    for path in log_path:
         try:
-            os.makedirs(output_log, default_output_log)
+            os.makedirs(path)
         except OSError:
             # directory already exists
             pass
@@ -42,57 +52,33 @@ def setup_logger(log_filepath_list, console_logging=False):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
+    # formatters for both FileHandler and StreamHandler
     file_formatter = logging.Formatter(
         fmt='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
         datefmt='%(asctime)s')
     stream_formatter = logging.Formatter(
         '%(levelname)s - %(name)s - %(message)s')
 
-    # want output to go to file
-    if console_logging:
-        file_handler1 = logging.FileHandler(default_file_name)
-        file_handler1.setLevel(logging.INFO)
-        file_handler1.setFormatter(file_formatter)
-        logger.addHandler(file_handler1)
+    for filename in log_path:
+        if console_logging:
+            file_handler = logging.FileHandler(filename)
+            file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(file_formatter)
+            logger.addHandler(file_handler)
 
-        return logger
+            return logger
 
-    if console_logging is True:
-        file_handler1 = logging.FileHandler(default_file_name)
-        stream_handler = logging.StreamHandler()
+        if console_logging is True:
+            file_handler = logging.FileHandler(filename)
+            stream_handler = logging.StreamHandler()
 
-        file_handler1.setLevel(logging.INFO)
-        stream_handler.setLevel(logging.INFO)
+            file_handler.setLevel(logging.INFO)
+            stream_handler.setLevel(logging.INFO)
 
-        stream_handler.setFormatter(stream_formatter)
-        file_handler1.setFormatter(file_formatter)
+            file_handler.setFormatter(file_formatter)
+            stream_handler.setFormatter(stream_formatter)
 
-        logger.addHandler(stream_handler)
-        logger.addHandler(file_handler1)
+            logger.addHandler(file_handler)
+            logger.addHandler(stream_handler)
 
-        return logger
-
-    # # want output to go to console
-    # if console_logging is True:
-    #
-    #     file_formatter = logging.Formatter(
-    #         fmt='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-    #         datefmt='%(asctime)s')
-    #     stream_handler = logging.StreamHandler()
-    #     file_handler = logging.FileHandler(log_filepath_list)
-    #
-    #     stream_handler.setLevel(logging.INFO)
-    #     file_handler.setLevel(logging.INFO)
-    #
-    #     return logger
-
-    # # want output to go to file
-    # if console_logging:
-    #     # Sets default log location if non is specified
-    #     if not log_filepath_list:
-    #         log_filepath_list = os.path.join(log_dir, 'curation%Y%m%d_%H%M%S.log')
-    #     formatter =
-    #     file_handler = logging.FileHandler(log_filepath_list)
-    #     file_handler.setLevel(logging.INFO)
-    #     file_handler.setFormatter(formatter)
-    #     logger.addHandler(file_handler)
+            return logger
