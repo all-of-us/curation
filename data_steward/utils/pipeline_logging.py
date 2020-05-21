49cf3e8de7e7dcd4b0471885ddf6b9eb78789154
dcampbell-vumc <danyel.campbell@vumc.org>
2020-05-21 15:07:04 -0500

    [DC-637] Create Logging Setup Utility
    (1) modified file path generation if just filename is passed
    (2) modified file path generation if full filepath is passed
    (3) modified creation of directory if doesn't already exist
    (4) modified if filehandler or streamhandler is created depending on if
    console_logging is true or false

--- data_steward/utils/pipeline_logging.py
+++ data_steward/utils/pipeline_logging.py
@@ -18,35 +18,49 @@ def setup_logger(log_filepath_list, console_logging=False):
     """
     Sets up python logging to file and or console for use in other modules.
 
-    :param log_filepath_list:  desired string path and name of the log file
+    :param log_filepath_list:  desired string path and or name of the log file
+                               example: ['path/', 'faked.log', 'path/fake.log']
     :param console_logging:  determines if log should be output to the console
                              is false by default, if true, will log output to console
     """
 
-    # iterates through log_filepath_list and sets path to provided path and default_file_name
     default_file_name = datetime.now().strftime('curation%Y%m%d_%H%M%S.log')
+
+    # iterates through log_filepath_list and sets path to
+    # provided path and default_file_name
     default_output_log_path = [
         os.path.join(filepath, default_file_name)
         for filepath in log_filepath_list
         if 'log' not in os.path.basename(filepath)
     ]
 
+    # iterates through log_filepath_list and sets path to default path
+    # if just filename was item passed in log_filepath_list
+    default_path = 'logs/'
+    default_log_path = [
+        os.path.join(default_path, filename)
+        for filename in log_filepath_list
+        if not os.path.dirname(filename)
+    ]
+
     # iterates through log_filepath_list and sets path to log files
     output_log_path = [
         Path(filepath)
         for filepath in log_filepath_list
-        if 'log' in os.path.basename(filepath)
+        if os.path.dirname(filepath) and 'log' in os.path.basename(filepath)
     ]
 
-    log_path = output_log_path + default_output_log_path
+    # appends all generated filepaths to list of log_paths
+    log_path = default_output_log_path + default_log_path + output_log_path
 
-    # if any path in output_log_path or default_output_log_path doesn't exist, will be created
+    # if any path in log_path list doesn't exist, it will be created
     for path in log_path:
-        try:
-            os.makedirs(path)
-        except OSError:
-            # directory already exists
-            pass
+        if not os.path.isdir(path):
+            try:
+                os.makedirs(os.path.dirname(path))
+            except OSError:
+                # directory already exists
+                pass
 
     # gets new logger, will be created if doesn't exist
     logger = logging.getLogger(__name__)
@@ -59,16 +73,17 @@ def setup_logger(log_filepath_list, console_logging=False):
     stream_formatter = logging.Formatter(
         '%(levelname)s - %(name)s - %(message)s')
 
-    for filename in log_path:
-        if console_logging:
+    if console_logging is False:
+        for filename in log_path:
             file_handler = logging.FileHandler(filename)
             file_handler.setLevel(logging.INFO)
             file_handler.setFormatter(file_formatter)
             logger.addHandler(file_handler)
 
-            return logger
+        return logger
 
-        if console_logging is True:
+    if console_logging is True:
+        for filename in log_path:
             file_handler = logging.FileHandler(filename)
             stream_handler = logging.StreamHandler()
 
@@ -81,4 +96,4 @@ def setup_logger(log_filepath_list, console_logging=False):
             logger.addHandler(file_handler)
             logger.addHandler(stream_handler)
 
-            return logger
+        return logger
