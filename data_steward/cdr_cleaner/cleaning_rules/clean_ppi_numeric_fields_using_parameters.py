a8aebe14814fb08907b74b40b8e7a47279bddab8
dcampbell-vumc <danyel.campbell@vumc.org>
2020-06-06 17:57:16 -0500

    [DC-827] Update Numeric Free Text CR
    Modified CLEAN_PPI_NUMERIC_FIELDS_QUERY for faster query processing

--- data_steward/cdr_cleaner/cleaning_rules/clean_ppi_numeric_fields_using_parameters.py
+++ data_steward/cdr_cleaner/cleaning_rules/clean_ppi_numeric_fields_using_parameters.py
@@ -79,9 +79,9 @@ END AS
     value_as_number,
     value_as_string,
 CASE
-    WHEN observation_concept_id IN (1585889, 1585890) AND (value_as_number < 0 OR value_as_number > 20) THEN NULL
-    WHEN observation_concept_id IN (1585795, 1585802, 1585864, 1585870, 1585873, 1586159, 1586162) AND (value_as_number < 0 OR value_as_number > 99) THEN NULL
-    WHEN observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255) THEN NULL
+    WHEN observation_concept_id IN (1585889, 1585890) AND (value_as_number < 0 OR value_as_number > 20) THEN 2000000010
+    WHEN observation_concept_id IN (1585795, 1585802, 1585864, 1585870, 1585873, 1586159, 1586162) AND (value_as_number < 0 OR value_as_number > 99) THEN 2000000010
+    WHEN observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255) THEN 2000000010
   ELSE value_as_concept_id
 END AS
     value_as_concept_id,
