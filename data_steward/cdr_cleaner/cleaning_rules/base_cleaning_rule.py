e7dc084d30ada75cc3e3c5beaf78b139e99f8812
dcampbell-vumc <62044448+dcampbell-vumc@users.noreply.github.com>
2020-06-08 10:35:33 -0500

    [DC-827] Update Numeric Free Text CR to New Base Class (#524)
    * [DC-827] Update Numeric Free Text CR
    
    Created clean_ppi_numeric_fields_using_parameters_test.py. Unit test for
    clean_ppi_numeric_fields_using_parameters module.
    
    * [DC-827] Update Numeric Free Text CR
    
    Added rule to RDR export. Replaced old module
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Converted DML UPDATE and SET statements to SQL SELECT statements
    (2) Converted to inherit from base class
    
    * [DC-827] Update Numeric Free Text CR
    
    Integration test for clean_ppi_numeric_fields_using_parameters.py module
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Removed commented out code
    (2) Changed AND statements to OR statements in SANDBOX_QUERY
    
    * [DC-827] Update Numeric Free Text CR
    
    Changed self.value_as_number to None
    
    * [DC-827] Update Numeric Free Text CR
    
    Applied yapf
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Added setup_validation function
    (2) Added validate_rule function
    
    * [DC-827] Update Numeric Free Text CR
    
    Created clean_ppi_numeric_fields_using_parameters_test.py. Unit test for
    clean_ppi_numeric_fields_using_parameters module.
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Converted DML UPDATE and SET statements to SQL SELECT statements
    (2) Converted to inherit from base class
    
    * [DC-827] Update Numeric Free Text CR
    
    Integration test for clean_ppi_numeric_fields_using_parameters.py module
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Removed commented out code
    (2) Changed AND statements to OR statements in SANDBOX_QUERY
    
    * [DC-827] Update Numeric Free Text CR
    
    Changed self.value_as_number to None
    
    * [DC-827] Update Numeric Free Text CR
    
    Applied yapf
    
    * [DC-827] Update Numeric Free Text CR
    
    Added self.client = None to setUp() function
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Modified get_sandbox_tablenames to programmatically generate sandbox
    table names
    (2) Added 'observation' as the affected table
    (3) Changed intermediary_table in SANDBOX_QUERY.render code block
    
    * [DC-827] Update Numeric Free Text CR
    
    Modified CLEAN_PPI_NUMERIC_FIELDS_QUERY for faster query processing
    
    * [DC-827] Update Numeric Free Text CR
    
    Modified CLEAN_PPI_NUMERIC_FIELDS_QUERY for faster query processing
    
    * [DC-827] Update Numeric Free Text CR
    
    Modified intermediary_table
    
    * [DC-827] Update Numeric Free Text CR
    
    (1) Removed SAVE_TABLE_NAME from import statement
    (2) Modified intermediary_table
    (3) Modfied test_log_queries function
    
    * [DC-827] Update numeric Free Text Cr
    
    Changed issue_numbers as tables cannot be created if there is a dash in
    the name
    
    * [DC-827] Update Numeric Free Text CR
    
    Added setup_validation and validate_rule abstract methods
    
    * [DC-827] Update Numeric Free Text CR
    
    Added self._affected_tables = affected_tables to __init__ function

--- data_steward/cdr_cleaner/cleaning_rules/base_cleaning_rule.py
+++ data_steward/cdr_cleaner/cleaning_rules/base_cleaning_rule.py
@@ -169,6 +169,7 @@ class BaseCleaningRule(AbstractBaseCleaningRule):
         self._sandbox_dataset_id = sandbox_dataset_id
         self._issue_urls = issue_urls if issue_urls else []
         self._depends_on_classes = depends_on if depends_on else []
+        self._affected_tables = affected_tables
 
         super().__init__()
 
--- data_steward/cdr_cleaner/cleaning_rules/clean_ppi_numeric_fields_using_parameters.py
+++ data_steward/cdr_cleaner/cleaning_rules/clean_ppi_numeric_fields_using_parameters.py
@@ -1,123 +1,185 @@
-import constants.cdr_cleaner.clean_cdr as cdr_consts
+"""
+Apply value ranges to ensure that values are reasonable and to minimize the likelihood
+of sensitive information (like phone numbers) within the free text fields.
 
-CLEAN_PPI_NUMERIC_FIELDS = """
-UPDATE
-  `{project}.{dataset}.observation` u1
-SET
-  u1.value_as_number = NULL,
-  u1.value_as_concept_id = 2000000010
-FROM
-  (
-  SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585889 AND (value_as_number < 0 OR value_as_number > 20)
+Original Issues: DC-827, DC-502, DC-487
 
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585890 AND (value_as_number < 0 OR value_as_number > 20)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585795 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585802 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585864 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585870 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1585873 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1586159 AND (value_as_number < 0 OR value_as_number > 99)
-
-UNION ALL
-
-SELECT
-  *
-FROM
-  `{project}.{dataset}.observation`
-WHERE
-  observation_concept_id = 1586162 AND (value_as_number < 0 OR value_as_number > 99) ) a
-WHERE
-  u1.observation_id = a.observation_id
+The intent is to ensure that numeric free-text fields that are not manipulated by de-id
+have value range restrictions applied to the value_as_number field across the entire dataset.
 """
 
+# Python imports
+import logging
 
-def get_clean_ppi_num_fields_using_parameters_queries(project_id, dataset_id):
+# Third party imports
+from jinja2 import Environment
+
+# Project imports
+import constants.cdr_cleaner.clean_cdr as cdr_consts
+from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
+from constants.bq_utils import WRITE_TRUNCATE
+
+LOGGER = logging.getLogger(__name__)
+
+jinja_env = Environment(
+    # help protect against cross-site scripting vulnerabilities
+    autoescape=True,
+    # block tags on their own lines
+    # will not cause extra white space
+    trim_blocks=True,
+    lstrip_blocks=True,
+    # syntax highlighting should be better
+    # with these comment delimiters
+    comment_start_string='--',
+    comment_end_string=' --')
+
+# Query to create tables in sandbox with the rows that will be removed per cleaning rule
+SANDBOX_QUERY = jinja_env.from_string("""
+CREATE OR REPLACE TABLE
+    `{{project}}.{{sandbox_dataset}}.{{intermediary_table}}` AS (
+SELECT *
+FROM
+    `{{project}}.{{dataset}}.observation`
+WHERE
+    (observation_concept_id = 1585889 AND (value_as_number < 0 OR value_as_number > 20))
+OR
+    (observation_concept_id = 1585890 AND (value_as_number < 0 OR value_as_number > 20))
+OR
+    (observation_concept_id = 1585795 AND (value_as_number < 0 OR value_as_number > 99))
+OR
+    (observation_concept_id = 1585802 AND (value_as_number < 0 OR value_as_number > 99))
+OR
+    (observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255))
+OR
+    (observation_concept_id = 1585864 AND (value_as_number < 0 OR value_as_number > 99))
+OR
+    (observation_concept_id = 1585870 AND (value_as_number < 0 OR value_as_number > 99))
+OR 
+    (observation_concept_id = 1585873 AND (value_as_number < 0 OR value_as_number > 99))
+OR
+    (observation_concept_id = 1586159 AND (value_as_number < 0 OR value_as_number > 99))
+OR
+    (observation_concept_id = 1586162 AND (value_as_number < 0 OR value_as_number > 99)))
+""")
+
+CLEAN_PPI_NUMERIC_FIELDS_QUERY = jinja_env.from_string("""
+SELECT 
+    observation_id,
+    person_id,
+    observation_concept_id,
+    observation_date,
+    observation_datetime,
+    observation_type_concept_id,
+CASE
+    WHEN observation_concept_id IN (1585889, 1585890) AND (value_as_number < 0 OR value_as_number > 20) THEN NULL
+    WHEN observation_concept_id IN (1585795, 1585802, 1585864, 1585870, 1585873, 1586159, 1586162) AND (value_as_number < 0 OR value_as_number > 99) THEN NULL
+    WHEN observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255) THEN NULL
+  ELSE value_as_number
+END AS
+    value_as_number,
+    value_as_string,
+CASE
+    WHEN observation_concept_id IN (1585889, 1585890) AND (value_as_number < 0 OR value_as_number > 20) THEN 2000000010
+    WHEN observation_concept_id IN (1585795, 1585802, 1585864, 1585870, 1585873, 1586159, 1586162) AND (value_as_number < 0 OR value_as_number > 99) THEN 2000000010
+    WHEN observation_concept_id = 1585820 AND (value_as_number < 0 OR value_as_number > 255) THEN 2000000010
+  ELSE value_as_concept_id
+END AS
+    value_as_concept_id,
+    qualifier_concept_id,
+    unit_concept_id,
+    provider_id,
+    visit_occurrence_id,
+    observation_source_value,
+    observation_source_concept_id,
+    unit_source_value,
+    qualifier_source_value,
+    value_source_concept_id,
+    value_source_value,
+    questionnaire_response_id
+FROM
+    {{project}}.{{dataset}}.observation""")
+
+
+class CleanPPINumericFieldsUsingParameters(BaseCleaningRule):
     """
-    runs the query which updates the ppi numeric fields in observation table based on the 
-    upper and lower bounds specified.
-    :param project_id: Name of the project
-    :param dataset_id: Name of the dataset where the queries should be run
-    :return:
+    Apply value ranges to ensure that values are reasonable and to minimize the likelihood
+    of sensitive information (like phone numbers) within the free text fields.
     """
-    queries_list = []
 
-    query = dict()
-    query[cdr_consts.QUERY] = CLEAN_PPI_NUMERIC_FIELDS.format(
-        dataset=dataset_id,
-        project=project_id,
-    )
-    queries_list.append(query)
+    def __init__(self, project_id, dataset_id, sandbox_dataset_id):
+        """
+        Initialize the class with proper information.
 
-    return queries_list
+        Set the issue numbers, description and affected datasets. As other tickets may affect
+        this SQL, append them to the list of Jira Issues.
+        DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
+        """
+        desc = (
+            'Sets value_as_number to NULL and value_as_concept_id and observation_type_concept_id '
+            'to new AOU custom concept 2000000010')
+        super().__init__(issue_numbers=['DC827', 'DC502', 'DC487'],
+                         description=desc,
+                         affected_datasets=[cdr_consts.RDR],
+                         affected_tables=['observation'],
+                         project_id=project_id,
+                         dataset_id=dataset_id,
+                         sandbox_dataset_id=sandbox_dataset_id)
+
+    def get_query_specs(self):
+        """
+        Return a list of dictionary query specifications.
+
+        :return:  A list of dictionaries. Each dictionary contains a single query
+            and a specification for how to execute that query. The specifications
+            are optional but the query is required.
+        """
+        save_changed_rows = {
+            cdr_consts.QUERY:
+                SANDBOX_QUERY.render(
+                    project=self.get_project_id(),
+                    dataset=self.get_dataset_id(),
+                    sandbox_dataset=self.get_sandbox_dataset_id(),
+                    intermediary_table=self.get_sandbox_tablenames()[0]),
+        }
+
+        clean_ppi_numeric_fields_query = {
+            cdr_consts.QUERY:
+                CLEAN_PPI_NUMERIC_FIELDS_QUERY.render(
+                    project=self.get_project_id(),
+                    dataset=self.get_dataset_id()),
+            cdr_consts.DESTINATION_TABLE:
+                'observation',
+            cdr_consts.DESTINATION_DATASET:
+                self.get_dataset_id(),
+            cdr_consts.DISPOSITION:
+                WRITE_TRUNCATE
+        }
+
+        return [save_changed_rows, clean_ppi_numeric_fields_query]
+
+    def setup_rule(self, client):
+        """
+        Function to run any data upload options before executing a query.
+        """
+        pass
+
+    def setup_validation(self, client):
+        """
+        Run required steps for validation setup
+        """
+        pass
+
+    def validate_rule(self, client):
+        """
+        Validates the cleaning rule which deletes or updates the data from the tables
+        """
+        pass
+
+    def get_sandbox_tablenames(self):
+        sandbox_table_names = list()
+        sandbox_table_names.append(self._issue_numbers[0] +
+                                   self._affected_tables[0])
+        return sandbox_table_names
 
 
 if __name__ == '__main__':
@@ -125,7 +187,14 @@ if __name__ == '__main__':
     import cdr_cleaner.clean_cdr_engine as clean_engine
 
     ARGS = parser.parse_args()
+
     clean_engine.add_console_logging(ARGS.console_log)
-    query_list = get_clean_ppi_num_fields_using_parameters_queries(
-        ARGS.project_id, ARGS.dataset_id)
-    clean_engine.clean_dataset(ARGS.project_id, query_list)
+    rdr_cleaner = CleanPPINumericFieldsUsingParameters(ARGS.project_id,
+                                                       ARGS.dataset_id,
+                                                       ARGS.sandbox_dataset_id)
+    query_list = rdr_cleaner.get_query_specs()
+
+    if ARGS.list_queries:
+        rdr_cleaner.log_queries()
+    else:
+        clean_engine.clean_dataset(ARGS.project_id, query_list)
