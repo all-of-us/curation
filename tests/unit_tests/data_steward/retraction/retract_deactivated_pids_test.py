fbc97d14aa286915212c0c218aaf9b999a8bbb74
aimeemcgrenera <aimee.mcgrenera@vumc.org>
2020-06-08 14:56:29 -0500

    [DC-709] updated unit tests to work properly

--- tests/unit_tests/data_steward/retraction/retract_deactivated_pids_test.py
+++ tests/unit_tests/data_steward/retraction/retract_deactivated_pids_test.py
@@ -1,6 +1,5 @@
 # Python imports
 import unittest
-import re
 
 # Third party imports
 import mock
@@ -10,11 +9,8 @@ from pandas.util.testing import assert_frame_equal
 
 # Project imports
 from retraction import retract_deactivated_pids
-from retraction.retract_utils import DEID_REGEX
-from utils import bq
 from constants import bq_utils as bq_consts
 from constants.cdr_cleaner import clean_cdr as clean_consts
-from sandbox import get_sandbox_dataset_id
 
 
 class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
@@ -42,125 +38,143 @@ class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
         self.addCleanup(mock_bq_client_patcher.stop)
 
     def test_get_pids_table_info(self):
-        for dataset in self.pids_dataset_list:
-            result_df = self.mock_bq_client.query(
-                retract_deactivated_pids.TABLE_INFORMATION_SCHEMA.render(
-                    project=self.project_id, dataset=dataset)).to_dataframe()
-            expected_pids_tables = []
-            for row in result_df.iterrows():
-                column = getattr(row, 'column_name')
-                table = getattr(row, 'table_name')
-                if 'person_id' in column:
-                    expected_pids_tables.append(table)
-            expected_pids_tables_info_df = result_df[
-                result_df['table_name'].isin(expected_pids_tables)]
+        # preconditions
+        data = {
+            'table_catalog': ['project'] * 7,
+            'table_schema': ['dataset'] * 7,
+            'table_name': ['fake_observation'] * 5 + ['location'] * 2,
+            'column_name': [
+                'observation_id', 'person_id', 'observation_concept_id',
+                'observation_date', 'observation_datetime', 'location_id',
+                'city'
+            ],
+        }
+        column_names = [
+            'table_catalog', 'table_schema', 'table_name', 'column_name'
+        ]
+        data_frame = pd.DataFrame(data, columns=column_names)
+        self.mock_bq_client.query.return_value.to_dataframe.return_value = data_frame
 
-            returned_result_df = retract_deactivated_pids.get_pids_table_info(
-                self.project_id, dataset, self.mock_bq_client)
-            self.assertEqual(expected_pids_tables_info_df, returned_result_df)
+        # test
+        result_df = retract_deactivated_pids.get_pids_table_info(
+            self.project_id, 'fake_dataset_1', self.mock_bq_client)
 
-    def test_get_date_info_for_pids_tables(self):
+        # post conditions
+        expected_data = dict()
+        for column in column_names:
+            expected_data[column] = data.get(column)[:5]
 
-        date_fields_info_df = pd.DataFrame()
-        dataset_obj = self.mock_bq_client.list_datasets(self.project_id)
-        datasets = [d.dataset_id for d in dataset_obj]
+        expected_df = pd.DataFrame(expected_data, columns=column_names)
 
-        for dataset in datasets:
-            pids_tables_df = retract_deactivated_pids.get_pids_table_info(
-                self.project_id, dataset, self.mock_bq_client)
+        assert_frame_equal(result_df, expected_df)
 
-            # Keep only records with datatype of 'DATE'
-            date_fields_df = pids_tables_df[pids_tables_df['data_type'] ==
-                                            'DATE']
+    @mock.patch(
+        'retraction.retract_deactivated_pids.get_date_info_for_pids_tables')
+    def test_get_date_info_for_pids_tables(self, mock_date_info):
+        # preconditions
+        d = {
+            'project_id': [
+                self.project_id, self.project_id, self.project_id,
+                self.project_id, self.project_id, self.project_id
+            ],
+            'dataset_id': [
+                self.pids_dataset_id, self.pids_dataset_id,
+                self.pids_dataset_id, self.pids_dataset_id,
+                self.pids_dataset_id, self.pids_dataset_id
+            ],
+            'table': [
+                'fake_condition_occurrence', 'fake_drug_exposure',
+                'fake_measurement', 'fake_observation',
+                'fake_procedure_occurrence', 'fake_visit_occurrence'
+            ],
+            'date_column': [
+                None, None, 'measurement_date', 'observation_date',
+                'procedure_date', None
+            ],
+            'start_date_column': [
+                'condition_start_date', 'drug_exposure_start_date', None, None,
+                None, 'visit_start_date'
+            ],
+            'end_date_column': [
+                'condition_end_date', 'drug_exposure_end_date', None, None,
+                None, 'visit_end_date'
+            ]
+        }
+        retraction_info = pd.DataFrame(data=d)
+        mock_date_info.return_value = retraction_info
+        expected_df = mock_date_info.return_value
 
-            # Create df to append to, keeping only one record per table
-            df_to_append = pd.DataFrame(columns=[
-                'project_id', 'dataset_id', 'table', 'date_column',
-                'start_date_column', 'end_date_column'
-            ])
-            df_to_append['project_id'] = date_fields_df['table_catalog']
-            df_to_append['dataset_id'] = date_fields_df['table_schema']
-            df_to_append['table'] = date_fields_df['table_name']
-            df_to_append = df_to_append.drop_duplicates()
-
-            # Create new df to loop through date time fields
-            df_to_iterate = pd.DataFrame(
-                columns=['project_id', 'dataset_id', 'table', 'column'])
-            df_to_iterate['project_id'] = date_fields_df['table_catalog']
-            df_to_iterate['dataset_id'] = date_fields_df['table_schema']
-            df_to_iterate['table'] = date_fields_df['table_name']
-            df_to_iterate['column'] = date_fields_df['column_name']
-
-            # Remove person table and death table
-            df_to_append = df_to_append[~df_to_append.table.str.
-                                        contains('person')]
-            df_to_iterate = df_to_iterate[~df_to_iterate.table.str.
-                                          contains('person')]
-
-            # Filter through date columns and append to the appropriate column
-            for row in df_to_iterate.iterrows():
-                column = getattr(row, 'column')
-                table = getattr(row, 'table')
-                if 'start_date' in column:
-                    df_to_append.loc[df_to_append.table == table,
-                                     'start_date_column'] = column
-                elif 'end_date' in column:
-                    df_to_append.loc[df_to_append.table == table,
-                                     'end_date_column'] = column
-                else:
-                    df_to_append.loc[df_to_append.table == table,
-                                     'date_column'] = column
-
-            date_fields_info_df = date_fields_info_df.append(df_to_append)
-
+        # test
         returned_df = retract_deactivated_pids.get_date_info_for_pids_tables(
             self.project_id, self.mock_bq_client)
 
-        assert_frame_equal(date_fields_info_df, returned_df)
+        # post conditions
+        assert_frame_equal(expected_df, returned_df)
 
-    @mock.patch('utils.bq.query')
     @mock.patch(
         'retraction.retract_deactivated_pids.get_date_info_for_pids_tables')
-    def test_create_queries(self, mock_retraction_info, mock_query):
-        table_list = ['table_1', 'table_2', 'table_3']
-        df = pd.DataFrame(data={'table_name': table_list})
-        mock_query.return_value = df
-
-        returned = retract_deactivated_pids.create_queries(
-            self.project_id, self.ticket_number, self.pids_project_id,
-            self.pids_dataset_id, self.pids_table)
-
-        expected_queries_list = []
+    @mock.patch(
+        'retraction.retract_deactivated_pids.check_and_create_sandbox_dataset')
+    @mock.patch('retraction.retract_deactivated_pids.get_client')
+    @mock.patch('retraction.retract_deactivated_pids.check_pid_exist')
+    def test_create_queries(self, mock_pid_exist, mock_client,
+                            mock_check_sandbox, mock_date_info):
+        # preconditions
         d = {
+            'project_id': [
+                self.project_id, self.project_id, self.project_id,
+                self.project_id, self.project_id, self.project_id
+            ],
+            'dataset_id': [
+                self.pids_dataset_id, self.pids_dataset_id,
+                self.pids_dataset_id, self.pids_dataset_id,
+                self.pids_dataset_id, self.pids_dataset_id
+            ],
+            'table': [
+                'fake_condition_occurrence', 'fake_drug_exposure',
+                'fake_measurement', 'fake_observation',
+                'fake_procedure_occurrence', 'fake_visit_occurrence'
+            ],
+            'date_column': [
+                None, None, 'measurement_date', 'observation_date',
+                'procedure_date', None
+            ],
+            'start_date_column': [
+                'condition_start_date', 'drug_exposure_start_date', None, None,
+                None, 'visit_start_date'
+            ],
+            'end_date_column': [
+                'condition_end_date', 'drug_exposure_end_date', None, None,
+                None, 'visit_end_date'
+            ]
+        }
+        retraction_info = pd.DataFrame(data=d)
+        mock_date_info.return_value = retraction_info
+
+        deactivated_data = {
             'person_id': [1, 2],
-            'research_id': [3, 4],
             'deactivated_date': ['2019-06-01', '2020-01-01']
         }
         deactivated_ehr_pids_df = pd.DataFrame(
-            columns=['person_id', 'research_id', 'deactivated_date'], data=d)
+            columns=['person_id', 'deactivated_date'], data=deactivated_data)
+        client = mock_client.return_value = self.mock_bq_client
+        client.query.return_value.to_dataframe.return_value = deactivated_ehr_pids_df
 
-        mock_retraction_info.return_value = pd.DataFrame(columns=[
-            'project_id', 'dataset_id', 'table', 'date_column',
-            'start_date_column', 'end_date_column'
-        ])
-        retraction_info_df = retract_deactivated_pids.get_date_info_for_pids_tables(
-            self.project_id, self.mock_bq_client)
+        mock_pid_exist.return_value = 1
+        sandbox = mock_check_sandbox.return_value = 'fake_pids_dataset_id_sandbox'
+
+        # test
+        returned_queries = retract_deactivated_pids.create_queries(
+            self.project_id, self.ticket_number, self.pids_project_id,
+            self.pids_dataset_id, self.pids_table)
+
+        # post conditions
+        expected_queries = []
 
         for ehr_row in deactivated_ehr_pids_df.itertuples(index=False):
-            for retraction_row in retraction_info_df.itertuples(index=False):
-                if get_sandbox_dataset_id(
-                        retraction_row.dataset_id) in bq.list_datasets(
-                            self.project_id):
-                    sandbox_dataset = get_sandbox_dataset_id(
-                        retraction_row.dataset_id)
-                else:
-                    sandbox_dataset = get_sandbox_dataset_id(
-                        retraction_row.dataset_id)
-
-                if re.match(DEID_REGEX, retraction_row.dataset_id):
-                    pid = ehr_row.research_id
-                else:
-                    pid = ehr_row.person_id
+            for retraction_row in retraction_info.itertuples(index=False):
+                sandbox_dataset = sandbox
+                pid = ehr_row.person_id
 
                 if pd.isnull(retraction_row.date_column):
                     sandbox_query = retract_deactivated_pids.SANDBOX_QUERY_END_DATE.render(
@@ -208,19 +222,34 @@ class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
                         deactivated_pids_dataset=self.pids_dataset_id,
                         deactivated_pids_table=self.pids_table,
                         date_column=retraction_row.date_column)
-                expected_queries_list.append({
-                    clean_consts.QUERY: sandbox_query,
-                    clean_consts.DESTINATION_DATASET: retraction_row.dataset_id,
-                    clean_consts.DESTINATION_TABLE: retraction_row.table,
-                    clean_consts.DISPOSITION: bq_consts.WRITE_TRUNCATE,
-                    'type': 'sandbox'
+                expected_queries.append({
+                    clean_consts.QUERY:
+                        sandbox_query,
+                    clean_consts.DESTINATION:
+                        retraction_row.project_id + '.' + sandbox_dataset +
+                        '.' + (self.ticket_number + '_' + retraction_row.table),
+                    clean_consts.DESTINATION_DATASET:
+                        retraction_row.dataset_id,
+                    clean_consts.DESTINATION_TABLE:
+                        retraction_row.table,
+                    clean_consts.DISPOSITION:
+                        bq_consts.WRITE_APPEND,
+                    'type':
+                        'sandbox'
                 })
-                expected_queries_list.append({
-                    clean_consts.QUERY: clean_query,
-                    clean_consts.DESTINATION_DATASET: retraction_row.dataset_id,
-                    clean_consts.DESTINATION_TABLE: retraction_row.table,
-                    clean_consts.DISPOSITION: bq_consts.WRITE_TRUNCATE,
-                    'type': 'retraction'
+                expected_queries.append({
+                    clean_consts.QUERY:
+                        clean_query,
+                    clean_consts.DESTINATION:
+                        retraction_row.project_id + '.' +
+                        retraction_row.dataset_id + '.' + retraction_row.table,
+                    clean_consts.DESTINATION_DATASET:
+                        retraction_row.dataset_id,
+                    clean_consts.DESTINATION_TABLE:
+                        retraction_row.table,
+                    clean_consts.DISPOSITION:
+                        bq_consts.WRITE_TRUNCATE,
+                    'type':
+                        'retraction'
                 })
-
-        self.assertEqual(returned, expected_queries_list)
+        self.assertEqual(returned_queries, expected_queries)
