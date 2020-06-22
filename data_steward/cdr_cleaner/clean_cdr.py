61d4e58c51b73305836843bed29526e9ca33122d
Krishna Kalluri <krishnasaikalluri@gmail.com>
2020-06-22 13:42:20 -0400

    [DC-700] Add unit normalization in clean_cdr
    Signed-off-by: Krishna Kalluri <krishnasaikalluri@gmail.com>

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -4,21 +4,21 @@ A module to serve as the entry point to the cdr_cleaner package.
 It gathers the list of query strings to execute and sends them
 to the query engine.
 """
+import inspect
 # Python imports
 import logging
-import inspect
 
 # Third party imports
 import app_identity
-
 # Project imports
 import bq_utils
-import sandbox
 import cdr_cleaner.clean_cdr_engine as clean_engine
 import cdr_cleaner.cleaning_rules.backfill_pmi_skip_codes as back_fill_pmi_skip
 import cdr_cleaner.cleaning_rules.clean_years as clean_years
 import cdr_cleaner.cleaning_rules.domain_alignment as domain_alignment
 import cdr_cleaner.cleaning_rules.drop_duplicate_states as drop_duplicate_states
+import cdr_cleaner.cleaning_rules.drop_extreme_measurements as extreme_measurements
+import cdr_cleaner.cleaning_rules.drop_multiple_measurements as drop_mult_meas
 import cdr_cleaner.cleaning_rules.drop_participants_without_ppi_or_ehr as drop_participants_without_ppi_or_ehr
 import cdr_cleaner.cleaning_rules.drug_refills_days_supply as drug_refills_supply
 import cdr_cleaner.cleaning_rules.fill_free_text_source_value as fill_source_value
@@ -28,32 +28,33 @@ import cdr_cleaner.cleaning_rules.negative_ages as neg_ages
 import cdr_cleaner.cleaning_rules.no_data_30_days_after_death as no_data_30days_after_death
 import cdr_cleaner.cleaning_rules.null_invalid_foreign_keys as null_foreign_key
 import cdr_cleaner.cleaning_rules.populate_route_ids as populate_routes
+import cdr_cleaner.cleaning_rules.remove_aian_participants as remove_aian_participants
+import cdr_cleaner.cleaning_rules.remove_invalid_procedure_source_records as invalid_procedure_source
 import cdr_cleaner.cleaning_rules.remove_multiple_race_ethnicity_answers as remove_multiple_race_answers
+import cdr_cleaner.cleaning_rules.remove_non_matching_participant as validate_missing_participants
 import cdr_cleaner.cleaning_rules.remove_records_with_wrong_date as remove_records_with_wrong_date
 import cdr_cleaner.cleaning_rules.replace_standard_id_in_domain_tables as replace_standard_concept_ids
 import cdr_cleaner.cleaning_rules.repopulate_person_post_deid as repopulate_person
 import cdr_cleaner.cleaning_rules.round_ppi_values_to_nearest_integer as round_ppi_values
 import cdr_cleaner.cleaning_rules.temporal_consistency as bad_end_dates
-import cdr_cleaner.cleaning_rules.valid_death_dates as valid_death_dates
 import cdr_cleaner.cleaning_rules.update_family_history_qa_codes as update_family_history
-import cdr_cleaner.cleaning_rules.remove_invalid_procedure_source_records as invalid_procedure_source
+import cdr_cleaner.cleaning_rules.valid_death_dates as valid_death_dates
 import cdr_cleaner.manual_cleaning_rules.clean_smoking_ppi as smoking
-import cdr_cleaner.cleaning_rules.drop_multiple_measurements as drop_mult_meas
-import cdr_cleaner.cleaning_rules.drop_extreme_measurements as extreme_measurements
 import cdr_cleaner.manual_cleaning_rules.negative_ppi as negative_ppi
 import cdr_cleaner.manual_cleaning_rules.ppi_drop_duplicate_responses as ppi_drop_duplicates
 import cdr_cleaner.manual_cleaning_rules.remove_operational_pii_fields as operational_pii_fields
 import cdr_cleaner.manual_cleaning_rules.update_questiona_answers_not_mapped_to_omop as map_questions_answers_to_omop
-import cdr_cleaner.cleaning_rules.remove_aian_participants as remove_aian_participants
-import cdr_cleaner.cleaning_rules.remove_non_matching_participant as validate_missing_participants
+import sandbox
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 from cdr_cleaner.cleaning_rules.clean_mapping import CleanMappingExtTables
-from cdr_cleaner.cleaning_rules.ensure_date_datetime_consistency import EnsureDateDatetimeConsistency
-from cdr_cleaner.cleaning_rules.rdr_observation_source_concept_id_suppression import ObservationSourceConceptIDRowSuppression
-from cdr_cleaner.cleaning_rules.null_concept_ids_for_numeric_ppi import NullConceptIDForNumericPPI
 from cdr_cleaner.cleaning_rules.clean_ppi_numeric_fields_using_parameters import CleanPPINumericFieldsUsingParameters
-from constants.cdr_cleaner.clean_cdr import DataStage as stage
+from cdr_cleaner.cleaning_rules.ensure_date_datetime_consistency import EnsureDateDatetimeConsistency
+from cdr_cleaner.cleaning_rules.null_concept_ids_for_numeric_ppi import NullConceptIDForNumericPPI
+from cdr_cleaner.cleaning_rules.rdr_observation_source_concept_id_suppression import \
+    ObservationSourceConceptIDRowSuppression
+from cdr_cleaner.cleaning_rules.unit_normalization import UnitNormalization
 from constants.cdr_cleaner import clean_cdr as cdr_consts
+from constants.cdr_cleaner.clean_cdr import DataStage as stage
 
 LOGGER = logging.getLogger(__name__)
 
@@ -181,7 +182,8 @@ DEID_CLEAN_CLEANING_CLASSES = [
     # can't work with mocked strings.  should use base class
     # setup_query_execution function to load dependencies before query execution
     (
-        CleanMappingExtTables,)
+        UnitNormalization,),
+    (CleanMappingExtTables,)
 ]
 
 
