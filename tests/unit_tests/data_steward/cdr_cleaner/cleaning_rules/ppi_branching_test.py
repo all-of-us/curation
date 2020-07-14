8ec7cff72ff5856dac9a452e7d6a2a00665e325a
Mark Velez <mav7014@nyp.org>
2020-07-14 12:02:11 -0400

    Refactor ppi_branching unit tests for class impl

--- tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/ppi_branching_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/ppi_branching_test.py
@@ -1,9 +1,27 @@
 import unittest
 
-from google.cloud.bigquery import DatasetReference, TableReference
-
-from data_steward.cdr_cleaner.cleaning_rules import ppi_branching
+from data_steward.cdr_cleaner.cleaning_rules.ppi_branching import OBSERVATION_BACKUP_TABLE_ID
+from data_steward.cdr_cleaner.cleaning_rules.ppi_branching import PpiBranching, OBSERVATION
+from data_steward.cdr_cleaner.cleaning_rules.ppi_branching import PPI_BRANCHING_RULE_PATHS
 from utils.bq import get_table_schema
+
+from google.cloud import bigquery
+from pandas import DataFrame
+import mock
+
+
+def _get_csv_row_count() -> int:
+    """
+    Get total number of rows in rule csv files
+
+    :return: number of rows
+    """
+    csv_row_count = 0
+    for rule_path in PPI_BRANCHING_RULE_PATHS:
+        with open(rule_path, ) as rule_fp:
+            header, *lines = rule_fp.readlines()
+            csv_row_count += len(lines)
+    return csv_row_count
 
 
 class PpiBranchingTest(unittest.TestCase):
@@ -15,22 +33,35 @@ class PpiBranchingTest(unittest.TestCase):
         print('**************************************************************')
 
     def setUp(self) -> None:
-        dataset_ref = DatasetReference('fake_project', 'fake_dataset')
-        sandbox_dataset_ref = DatasetReference('fake_project', 'fake_sandbox')
-        self.src_table = TableReference(dataset_ref, 'src_table')
-        self.backup_table = TableReference(sandbox_dataset_ref, 'backup_table')
-        self.lookup_table = TableReference(sandbox_dataset_ref, 'lookup_table')
+        self.project_id = 'fake_project'
+        self.dataset_id = 'fake_dataset'
+        self.sandbox_dataset_id = 'fake_sandbox'
         self.observation_schema = get_table_schema('observation')
+        self.cleaning_rule = PpiBranching(self.project_id, self.dataset_id, self.sandbox_dataset_id)
+
+    def test_load_rules_lookup(self):
+        # dataframe has same number of rows as all input csv files (minus headers)
+        expected_row_count = _get_csv_row_count()
+
+        client = bigquery.Client(self.project_id)
+        client.load_table_from_dataframe = mock.MagicMock()
+        self.cleaning_rule.load_rules_lookup(client)
+        # unpacking first arg; call_args is collection of (args: Tuple, kwargs: dict)
+        dataframe_arg, *_ = client.load_table_from_dataframe.call_args[0]
+        self.assertIsInstance(dataframe_arg, DataFrame)
+        self.assertEqual(expected_row_count, len(dataframe_arg.index))
 
     def test_get_backup_rows_query(self):
-        result = ppi_branching.get_backup_rows_ddl(self.src_table,
-                                                   self.backup_table,
-                                                   self.lookup_table).strip()
-        self.assertTrue(result.startswith('CREATE OR REPLACE TABLE fake_sandbox.backup_table'))
+        # check that DDL table location is correct and contains all field descriptions
+        result = self.cleaning_rule.get_backup_rows_ddl().strip()
+        expected_sql = (f'CREATE OR REPLACE TABLE {self.sandbox_dataset_id}.'
+                        f'{OBSERVATION_BACKUP_TABLE_ID}')
+        self.assertTrue(result.startswith(expected_sql))
         self.assertTrue(all(field.description in result for field in self.observation_schema))
 
     def test_get_observation_replace_query(self):
-        result = ppi_branching.get_drop_rows_ddl(self.src_table,
-                                                 self.backup_table).strip()
-        self.assertTrue(result.startswith('CREATE OR REPLACE TABLE fake_dataset.src_table'))
+        # check that DDL table location is correct and contains all field descriptions
+        result = self.cleaning_rule.get_drop_rows_ddl().strip()
+        expected_sql = f'CREATE OR REPLACE TABLE {self.dataset_id}.{OBSERVATION}'
+        self.assertTrue(result.startswith(expected_sql))
         self.assertTrue(all(field.description in result for field in self.observation_schema))
