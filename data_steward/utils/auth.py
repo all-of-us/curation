405d824c9e43e240c1b1adfbc16826fdf269fca9
Nishanth Pavinkurve <npp267@nyu.edu>
2020-07-14 11:36:38 -0400

    [DC-964] Fix scope str

--- data_steward/utils/auth.py
+++ data_steward/utils/auth.py
@@ -17,7 +17,8 @@ def get_access_token(scopes):
     :param scopes: List of Google scopes as strings
     :return: access token as string
     """
-    url = f'{METADATA_URL}instance/service-accounts/{SERVICE_ACCOUNT}/token?scopes={scopes}'
+    scopes_str = ','.join(scopes)
+    url = f'{METADATA_URL}instance/service-accounts/{SERVICE_ACCOUNT}/token?scopes={scopes_str}'
     # Request an access token from the metadata server.
     r = requests.get(url, headers=METADATA_HEADERS)
     r.raise_for_status()
