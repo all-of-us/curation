f0849de8473cb15f638d4409aa8fc9e6cee9ef12
dcampbell-vumc <danyel.campbell@vumc.org>
2020-07-14 16:33:27 -0500

    [DC-971] Fetch Deactivated Partipants
    (1) Modified test_get_access_token (currently is passing)
    (2) Started test_get_deactivated_participants

--- tests/unit_tests/data_steward/utils/deactivated_participants_test.py
+++ tests/unit_tests/data_steward/utils/deactivated_participants_test.py
@@ -12,9 +12,10 @@ The intent of this module is to check that GCR access token is generated properl
 
 # Python imports
 import unittest
+from unittest.mock import Mock
 import requests
 import pandas
-import os
+import mock
 
 # Project imports
 from utils.deactivated_participants import get_access_token, get_deactivated_participants
@@ -23,7 +24,8 @@ from utils.deactivated_participants import get_access_token, get_deactivated_par
 from google.oauth2 import service_account
 from google.cloud import storage
 
-class BqTest(unittest.TestCase):
+
+class DeactivatedParticipantsTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -33,13 +35,18 @@ class BqTest(unittest.TestCase):
 
     def setUp(self):
         # Input parameters expected by the class
-        self.project_id = 'foo_dataset'
-        # self.project_id = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+        self.project_id = 'foo_project'
+        self.service_account_file = '/path/to/SA/key.json'
+        self.scopes = [
+            'https://www.fakewebsite.com', 'fake_email', 'fake_profile'
+        ]
+        self.access_token = 'ya29.12345'
 
-    def test_get_access_token(self):
-        results = get_access_token(self.project_id)
-        self.assertIn('ya', results)
+    @mock.patch('utils.deactivated_participants.get_access_token', return_value='ya29.12345')
+    def test_get_access_token(self, mocked_access_token):
+        self.assertEqual(mocked_access_token(), self.access_token)
 
+    @mock.patch('utils.deactivated_participants.get_access_token', return_value='ya29.12345')
     def test_get_deactivated_participants(self):
         results = get_deactivated_participants(self.project_id)
         print(results)
