394b1df34f16ac757826586b25ad91d727a60b03
dcampbell-vumc <danyel.campbell@vumc.org>
2020-07-21 14:04:26 -0500

    [DC-971] Fetch Deactivated Participants
    (1) Renamed file as it will be a utility to fetch more than only
    deactivated participants
    (2) Refactored to remove data fetch from deactivated_participants into a
    helper function get_participant_data
    (3) Other small changes

--- data_steward/utils/participant_summary_requests.py
+++ data_steward/utils/participant_summary_requests.py
@@ -1,26 +1,26 @@
 """
 Utility to fetch and store deactivated participants information.
 
-Original Issues: DC-797, DC-971 (sub-task), DC-972 (sub-task)
-
 The intent of this module is to call and store deactivated participants information by leveraging the RDR Participant
     Summary API. Deactivated participant information stored is `participantID`, `suspensionStatus`, and `suspensionTime`.
 """
 
 # Python imports
-import pandas
 import os
 
 # Third party imports
 import google.auth
 import google.auth.transport.requests
 import requests
+import pandas
 from google.oauth2 import service_account
 
 
-def get_access_token():
+def get_access_token(sa_key):
     """
     Obtains GCP Bearer token
+
+    :param sa_key: path that points to your cdr-ops SA key
 
     :return: returns the access_token
     """
@@ -29,8 +29,7 @@ def get_access_token():
         'https://www.googleapis.com/auth/cloud-platform', 'email', 'profile'
     ]
 
-    # GOOGLE_APPLICATION_CREDENTIALS needs to point to cdr-ops SA key
-    service_account_file = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    service_account_file = sa_key
 
     credentials = service_account.Credentials.from_service_account_file(
         filename=service_account_file, scopes=scopes)
@@ -43,15 +42,47 @@ def get_access_token():
     return access_token
 
 
-def get_deactivated_participants(project_id):
+def get_participant_data(url, headers):
     """
-    Gets all deactivated participants via API if suspensionStatus = 'NO_CONTACT'
+    Fetches participant data via ParticipantSummary API
 
-    :param project_id: The RDR project that contains the deactivated participants.
+    :param url: the /ParticipantSummary endpoint to fetch information about the participant
+    :param headers: the metadata associated with the API request and response
+
+    :return: list of data fetched from the ParticipantSummary API
+    """
+
+    done = False
+    participant_data = []
+
+    while not done:
+        resp = requests.get(url, headers=headers)
+        if not resp or resp.status_code != 200:
+            print(f'Error: API request failed because {resp}')
+        else:
+            r_json = resp.json()
+            participant_data += r_json.get('entry', {})
+            if 'link' in r_json:
+                link_obj = r_json.get('link')
+                url = link_obj[0].get('url')
+            else:
+                done = True
+
+    return participant_data
+
+
+def get_deactivated_participants(project_id, sa_key, columns):
+    """
+    Fetches all deactivated participants via API if suspensionStatus = 'NO_CONTACT'
+
+    :param project_id: THE RDR project that contains participant summary data
+    :param sa_key: path that points to your cdr-ops SA key
+    :param columns: columns to be pushed to a table in BigQuery
 
     :return: returns dataframe of deactivated participants
     """
-    token = get_access_token()
+
+    token = get_access_token(sa_key)
 
     headers = {
         'content-type': 'application/json',
@@ -63,28 +94,13 @@ def get_deactivated_participants(project_id):
     url = 'https://{0}.appspot.com/rdr/v1/ParticipantSummary?_sort=lastModified&suspensionStatus={1}'.format(
         project_id, 'NO_CONTACT')
 
-    not_done = True
-    data = []
+    participant_data = get_participant_data(url, headers)
 
-    while not_done:
-        resp = requests.get(url, headers=headers)
-        if not resp or resp.status_code != 200:
-            print(f'Error: API request failed because {resp}')
-        else:
-            r_json = resp.json()
-            data += r_json['entry']
-            if 'link' in r_json:
-                url = r_json['link'][0]['url']
-            else:
-                not_done = False
-
-    deactivated_participants_cols = [
-        'participantId', 'suspensionStatus', 'suspensionTime'
-    ]  # any other relevant columns we want to push to a table in BQ
+    deactivated_participants_cols = columns
 
     deactivated_participants = []
     # loop over participant summary records, insert participant data in same order as good_cols.
-    for entry in data:
+    for entry in participant_data:
         item = []
         for col in deactivated_participants_cols:
             for key, val in entry['resource'].items():
