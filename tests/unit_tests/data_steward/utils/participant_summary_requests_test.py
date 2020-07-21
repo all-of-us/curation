636d4aba96ca6e33d41611c144107e5f230b2aee
dcampbell-vumc <danyel.campbell@vumc.org>
2020-07-21 15:12:14 -0500

    [DC-971] Fetch Deactivated Participants
    (1) Modified all @mock.patch to point to renamed module
    (2) Modified test_get_deactivated_participants function to take the proper parameters for get_deactivated_participants
    (3) Changed class name

--- tests/unit_tests/data_steward/utils/participant_summary_requests_test.py
+++ tests/unit_tests/data_steward/utils/participant_summary_requests_test.py
@@ -12,15 +12,18 @@ The intent of this module is to check that GCR access token is generated properl
 
 # Python imports
 import unittest
-import pandas
 import mock
+
+# Third Party imports
+import pandas
 import pandas.testing
 
 # Project imports
-from utils.deactivated_participants import get_deactivated_participants
+from utils.participant_summary_requests import get_access_token, get_deactivated_participants, \
+    get_participant_data
 
 
-class DeactivatedParticipantsTest(unittest.TestCase):
+class ParticipantSummaryRequests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -31,7 +34,9 @@ class DeactivatedParticipantsTest(unittest.TestCase):
     def setUp(self):
         # Input parameters expected by the class
         self.project_id = 'foo_project'
+        self.fake_sa_key = '/path/to/sa/key.json'
         self.access_token = 'ya29.12345'
+
         self.columns = ['participantId', 'suspensionStatus', 'suspensionTime']
         self.deactivated_participants = [[
             'P111', 'NO_CONTACT', '2018-12-07T08:21:14'
@@ -59,15 +64,16 @@ class DeactivatedParticipantsTest(unittest.TestCase):
             }]
         }
 
-    @mock.patch('utils.deactivated_participants.get_access_token',
+    @mock.patch('utils.participant_summary_requests.get_access_token',
                 return_value='ya29.12345')
-    def test_get_access_token(self, mock_access_token):
-        self.assertEqual(mock_access_token(), self.access_token)
+    def test_get_access_token(self, mock_get_access_token):
 
-    @mock.patch('utils.deactivated_participants.get_access_token',
+        self.assertEqual(mock_get_access_token(), self.access_token)
+
+    @mock.patch('utils.participant_summary_requests.get_access_token',
                 return_value='ya29.12345')
-    @mock.patch('utils.deactivated_participants.get_deactivated_participants')
-    @mock.patch('utils.deactivated_participants.requests.get')
+    @mock.patch('utils.participant_summary_requests.get_deactivated_participants')
+    @mock.patch('utils.participant_summary_requests.requests.get')
     def test_get_deactivated_participants(self, mock_get,
                                           mock_get_deactivated_participants,
                                           mock_access_token):
@@ -75,7 +81,7 @@ class DeactivatedParticipantsTest(unittest.TestCase):
         mock_get.return_value.json.return_value = self.json_response_entry
 
         mock_get_deactivated_participants.return_value = self.fake_dataframe
-        response = get_deactivated_participants(self.project_id)
+        response = get_deactivated_participants(self.project_id, self.fake_sa_key, self.columns)
 
         pandas.testing.assert_frame_equal(
             response,
