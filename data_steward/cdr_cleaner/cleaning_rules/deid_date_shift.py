d55d9304cc1d4ba76e79fa0362c51afff4a25d45
Lauren Biggers <lauren.r.biggers@vumc.org>
2020-08-14 15:56:53 -0500

    [DC-1005] Date Shift Fitbit Tables
    * breaks the major date shifting into an abstracted date shift class
    * builds an extending fitbit class to provide tables to
    * adds dependency info for the fitbit date shifting rule.  this rule depends on the pid/rid mapping having occurred.  this value will need to be updated to the class name when the name is known.
    * adds a constant for the fitbit dataset

--- data_steward/cdr_cleaner/cleaning_rules/deid_date_shift.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid_date_shift.py
@@ -5,6 +5,7 @@ Original Issue:  DC-1005
 """
 # Python Imports
 import logging
+from abc import abstractmethod
 
 # Third party imports
 from jinja2 import Environment
@@ -14,119 +15,6 @@ from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 
 LOGGER = logging.getLogger(__name__)
-
-ISSUE_NUMBERS = ['DC-1005']
-
-TABLES_AND_SCHEMAS = {
-    'activity_summary': [{
-        "mode": "NULLABLE",
-        "name": "date",
-        "type": "DATE"
-    }, {
-        "mode": "NULLABLE",
-        "name": "activity_calories",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "calories_bmr",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "calories_out",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "elevation",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "fairly_active_minutes",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "floors",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "lightly_active_minutes",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "marginal_calories",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "sedentary_minutes",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "steps",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "very_active_minutes",
-        "type": "FLOAT"
-    }, {
-        "mode": "NULLABLE",
-        "name": "person_id",
-        "type": "INTEGER"
-    }],
-    'heart_rate_minute_level': [{
-        "mode": "NULLABLE",
-        "name": "datetime",
-        "type": "DATETIME"
-    }, {
-        "mode": "NULLABLE",
-        "name": "heart_rate_value",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "person_id",
-        "type": "INTEGER"
-    }],
-    'heart_rate_summary': [{
-        "mode": "NULLABLE",
-        "name": "person_id",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "date",
-        "type": "DATE"
-    }, {
-        "mode": "NULLABLE",
-        "name": "zone_name",
-        "type": "STRING"
-    }, {
-        "mode": "NULLABLE",
-        "name": "min_heart_rate",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "max_heart_rate",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "minute_in_zone",
-        "type": "INTEGER"
-    }, {
-        "mode": "NULLABLE",
-        "name": "calorie_count",
-        "type": "FLOAT"
-    }],
-    'steps_intraday': [{
-        "mode": "NULLABLE",
-        "name": "datetime",
-        "type": "DATETIME"
-    }, {
-        "mode": "NULLABLE",
-        "name": "steps",
-        "type": "NUMERIC"
-    }, {
-        "mode": "NULLABLE",
-        "name": "person_id",
-        "type": "INTEGER"
-    }]
-}
 
 jinja_env = Environment(
     # help protect against cross-site scripting vulnerabilities
@@ -140,42 +28,41 @@ jinja_env = Environment(
     comment_start_string='--',
     comment_end_string=' --')
 
-DATE_FIELD_SHIFT = jinja_env.from_string("""
-DATE_SUB( CAST({{field}} AS DATE), INTERVAL (
+SHIFT_EXP = jinja_env.from_string("""
+  {{field_type}}_SUB( CAST({{field}} AS {{field_type}}), INTERVAL (
     SELECT
       shift
     FROM
     -- could be _deid_map or pid_rid_mapping --
       `{{project}}.{{map_dataset}}.{{map_table}}` AS map
     WHERE
-      map.research_id = {{table}}.person_id) DAY) AS {{field}}
-""")
-
-DATETIME_FIELD_SHIFT = jinja_env.from_string("""
-  TIMESTAMP_SUB( CAST({{field}} AS TIMESTAMP), INTERVAL (
-    SELECT
-      shift
-    FROM
-    -- could be _deid_map or pid_rid_mapping --
-      `{{project}}.{{map_dataset}}.{{map_table}}` AS map
-    WHERE
-      map.research_id = {table}.person_id) DAY) AS {{field}}
+      map.research_id = remodel.person_id) DAY) AS {{field}}
 """)
 
 SELECT_STATEMENT = jinja_env.from_string("""
-CREATE OR REPLACE `{{project}}.{{dataset}}.{{table}}` AS (
+CREATE OR REPLACE TABLE `{{project}}.{{dataset}}.{{table}}` AS (
 SELECT 
 {{fields}}
-FROM `{{project}}.{{dataset}}.{{table}}`) 
+FROM `{{project}}.{{dataset}}.{{table}}` AS remodel) 
 """)
 
 
-class DateShiftFitbitFieldsRule(BaseCleaningRule):
+class DateShiftFieldsRule(BaseCleaningRule):
     """
     Suppress rows by values in the observation_source_concept_id field.
     """
 
-    def __init__(self, project_id, dataset_id, sandbox_dataset_id):
+    def __init__(self,
+                 project_id,
+                 dataset_id,
+                 sandbox_dataset_id,
+                 issue_numbers,
+                 description,
+                 affected_datasets,
+                 affected_tables,
+                 map_dataset,
+                 map_table,
+                 depends_on=[]):
         """
         Initialize the class.
 
@@ -185,13 +72,42 @@ class DateShiftFitbitFieldsRule(BaseCleaningRule):
         """
         desc = (f'Date shift date and timestamp fields by the date shift '
                 f'calculated in the static mapping table.')
-        super().__init__(issue_numbers=ISSUE_NUMBERS,
-                         description=desc,
-                         affected_datasets=[cdr_consts.RDR],
+
+        self._map_dataset_id = map_dataset
+        self._map_tablename = map_table
+
+        super().__init__(issue_numbers=issue_numbers,
+                         description=description,
+                         affected_datasets=affected_datasets,
                          project_id=project_id,
                          dataset_id=dataset_id,
                          sandbox_dataset_id=sandbox_dataset_id,
-                         affected_tables=["all"])
+                         affected_tables=affected_tables,
+                         depends_on=depends_on)
+
+    @property
+    def map_dataset_id(self):
+        """
+        Return dataset id where the mapping table is located.
+        """
+        return self._map_dataset_id
+
+    @property
+    def map_tablename(self):
+        """
+        Return table name of the mapping table.
+        """
+        return self._map_tablename
+
+    @abstractmethod
+    def get_tables_and_schemas(self):
+        """
+        Provide dictionary of table names and schemas.
+
+        :returns: a dictionary whose key, value patterns are in the
+            form of {"tablename": "json schema",}.
+        """
+        pass
 
     def get_query_specs(self):
         """
@@ -202,85 +118,32 @@ class DateShiftFitbitFieldsRule(BaseCleaningRule):
             The specifications are optional but the query is required.
         """
         date_shift_queries = []
-        for table, schema in TABLES_AND_SCHEMAS.items():
+        for table, schema in self.get_tables_and_schemas().items():
+            LOGGER.info(f"Building Date Shifting query for {self.dataset_id}."
+                        f"{table}")
             fields = []
             for field in schema:
                 field_type = field.get('type').lower()
                 field_name = field.get('name')
-                if field_type == 'date':
-                    shift_string = DATE_FIELD_SHIFT.render(
+                if field_type in ['date', 'datetime', 'timestamp']:
+                    shift_string = SHIFT_EXP.render(
                         project=self.project_id,
-                        map_dataset='pipeline_logging',
-                        map_table='pid_rid_mapping',
-                        field=field_name,
-                        table=table)
-                    fields.append(shift_string)
-                elif field_type == 'timestamp':
-                    shift_string = DATETIME_FIELD_SHIFT.render(
-                        project=self.project_id,
-                        map_dataset='pipeline_logging',
-                        map_table='pid_rid_mapping',
+                        map_dataset=self.map_dataset_id,
+                        map_table=self.map_tablename,
+                        field_type=field_type.upper(),
                         field=field_name,
                         table=table)
                     fields.append(shift_string)
                 else:
                     fields.append(field_name)
 
-            fields_string = ', '.join(fields)
+            fields_string = ',\n'.join(fields)
 
             query = SELECT_STATEMENT.render(project=self.project_id,
                                             dataset=self.dataset_id,
                                             table=table,
                                             fields=fields_string)
 
-            date_shift_queries.append(query)
+            date_shift_queries.append({'query': query})
 
         return date_shift_queries
-
-    def setup_rule(self, client):
-        """
-        Function to run any data upload options before executing a query.
-        """
-        pass
-
-    def setup_validation(self, client):
-        """
-        Run required steps for validation setup
-
-        This abstract method was added to the base class after this rule was authored.
-        This rule needs to implement logic to setup validation on cleaning rules that
-        will be updating or deleting the values.
-        Until done no issue exists for this yet.
-        """
-        raise NotImplementedError("Please fix me.")
-
-    def validate_rule(self):
-        """
-        Validates the cleaning rule which deletes or updates the data from the tables
-
-        This abstract method was added to the base class after this rule was authored.
-        This rule needs to implement logic to run validation on cleaning rules that will
-        be updating or deleting the values.
-        Until done no issue exists for this yet.
-        """
-        raise NotImplementedError("Please fix me.")
-
-    def get_sandbox_tablenames(self):
-        return []
-
-
-if __name__ == '__main__':
-    import cdr_cleaner.args_parser as parser
-    import cdr_cleaner.clean_cdr_engine as clean_engine
-
-    ARGS = parser.parse_args()
-    clean_engine.add_console_logging(ARGS.console_log)
-    date_shifter = DateShiftFitbitFieldsRule(ARGS.project_id, ARGS.dataset_id,
-                                             ARGS.sandbox_dataset_id)
-    query_list = date_shifter.get_query_specs()
-
-    print(query_list)
-    #if ARGS.list_queries:
-    #    rdr_cleaner.log_queries()
-    #else:
-    #clean_engine.clean_dataset(ARGS.project_id, query_list)
