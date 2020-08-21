51fd44b8513beaa63449ea07d1a994022e259f89
dcampbell-vumc <danyel.campbell@vumc.org>
2020-08-21 12:33:05 -0500

    [DC-1037] FitBit Max Age Fix
    Modified module to utilize `combined_dataset_id`

--- data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded.py
@@ -1,7 +1,7 @@
 """
 Remove all FitBit data for participants exceeding the maximum age of 89
 
-Original Issue: DC-1001
+Original Issue: DC-1001, DC-1037
 
 The intent is to ensure there is no data for participants over the age of 89 in
 Activity Summary, Heart Rate Minute Level, Heart Rate Summary, and Steps Intraday tables
@@ -12,12 +12,14 @@ by sandboxing the applicable records and then dropping them.
 import logging
 
 # Third Party Imports
+import google.cloud.bigquery as gbq
 from jinja2 import Environment
 
 # Project Imports
 import constants.cdr_cleaner.clean_cdr as cdr_consts
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 from constants.bq_utils import WRITE_TRUNCATE
+from common import FITBIT_TABLES
 
 LOGGER = logging.getLogger(__name__)
 
@@ -33,11 +35,6 @@ jinja_env = Environment(
     comment_start_string='--',
     comment_end_string=' --')
 
-TABLES = [
-    'activity_summary', 'heart_rate_minute_level', 'heart_rate_summary',
-    'steps_intraday'
-]
-
 # Save rows that will be dropped to a sandboxed dataset
 SAVE_ROWS_TO_BE_DROPPED_QUERY = jinja_env.from_string("""
 CREATE OR REPLACE TABLE `{{project}}.{{sandbox_dataset}}.{{sandbox_table}}` AS
@@ -47,7 +44,7 @@ WHERE person_id IN (
     FROM (
     SELECT DISTINCT person_id,
         EXTRACT(YEAR FROM CURRENT_DATE()) - year_of_birth AS age
-            FROM `{{project}}.{{dataset}}.person` ORDER BY 2)
+            FROM `{{combined_dataset.project}}.{{combined_dataset.dataset_id}}.{{combined_dataset.table_id}}` ORDER BY 2)
         WHERE age >= 89)
 """)
 
@@ -67,7 +64,8 @@ class RemoveFitbitDataIfMaxAgeExceeded(BaseCleaningRule):
     Steps Intraday FitBit tables.
     """
 
-    def __init__(self, project_id, dataset_id, sandbox_dataset_id):
+    def __init__(self, project_id, dataset_id, combined_dataset_id,
+                 sandbox_dataset_id):
         """
         Initialize the class with proper information.
 
@@ -77,13 +75,15 @@ class RemoveFitbitDataIfMaxAgeExceeded(BaseCleaningRule):
         """
         desc = (
             'Drops all FitBit data from participants whose max age exceeds 89')
-        super().__init__(issue_numbers=['DC1001'],
+        super().__init__(issue_numbers=['DC1001', 'DC1037'],
                          description=desc,
                          affected_datasets=[cdr_consts.FITBIT],
                          project_id=project_id,
                          dataset_id=dataset_id,
                          sandbox_dataset_id=sandbox_dataset_id,
-                         affected_tables=TABLES)
+                         affected_tables=FITBIT_TABLES)
+
+        self.person = gbq.TableReference.from_string(f'{project_id}.{combined_dataset_id}.person')
 
     def get_query_specs(self):
         """
@@ -96,7 +96,7 @@ class RemoveFitbitDataIfMaxAgeExceeded(BaseCleaningRule):
 
         sandbox_queries_list = []
         drop_queries_list = []
-        for i, table in enumerate(TABLES):
+        for i, table in enumerate(FITBIT_TABLES):
             sandbox_queries_list.append({
                 cdr_consts.QUERY:
                     SAVE_ROWS_TO_BE_DROPPED_QUERY.render(
@@ -104,10 +104,12 @@ class RemoveFitbitDataIfMaxAgeExceeded(BaseCleaningRule):
                         sandbox_dataset=self.sandbox_dataset_id,
                         sandbox_table=self.get_sandbox_tablenames()[i],
                         dataset=self.dataset_id,
-                        table=table)
+                        table=table,
+                        combined_dataset=self.person,
+                    )
             })
 
-        for i, table in enumerate(TABLES):
+        for i, table in enumerate(FITBIT_TABLES):
             drop_queries_list.append({
                 cdr_consts.QUERY:
                     DROP_MAX_AGE_EXCEEDED_ROWS_QUERY.render(
@@ -157,11 +159,21 @@ if __name__ == '__main__':
     import cdr_cleaner.args_parser as parser
     import cdr_cleaner.clean_cdr_engine as clean_engine
 
-    ARGS = parser.parse_args()
+    combined_dataset_arg = {
+        parser.SHORT_ARGUMENT: '-c',
+        parser.LONG_ARGUMENT: '--combined_dataset_id',
+        parser.ACTION: 'store',
+        parser.DEST: 'combined_dataset_id',
+        parser.HELP: 'Identifies the combined dataset',
+        parser.REQUIRED: True
+    }
+
+    ARGS = parser.default_parse_args([combined_dataset_arg])
 
     clean_engine.add_console_logging(ARGS.console_log)
 
     cleaner = RemoveFitbitDataIfMaxAgeExceeded(ARGS.project_id, ARGS.dataset_id,
+                                               ARGS.combined_dataset_id,
                                                ARGS.sandbox_dataset_id)
     query_list = cleaner.get_query_specs()
 
