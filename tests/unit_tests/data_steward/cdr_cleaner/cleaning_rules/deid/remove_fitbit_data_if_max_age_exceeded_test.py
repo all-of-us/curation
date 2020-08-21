acee1620630f16125ec863b150505126ada644c9
dcampbell-vumc <danyel.campbell@vumc.org>
2020-08-21 12:35:36 -0500

    [DC-1037] FitBit Max Age Fix
    Ensure that `combined_dataset_id` fix works properly in both unit and
    integration test for the remove_fitbit_data_if_max_age_exceeded.py module

--- tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded_test.py
@@ -1,7 +1,7 @@
 """
 Unit test for remove_fitbit_data_if_max_age_exceeded module
 
-Original Issue: DC-1001
+Original Issue: DC-1001, DC-1037
 
 Ensures any participant with FitBit data who is over the age of 89 is dropped from
 activity_summary, steps_intraday, heart_rate_summary, and heart_rate_minute_level
@@ -10,13 +10,15 @@ FitBit tables.
 
 # Python imports
 import unittest
+import mock
 
 # Project imports
 import constants.cdr_cleaner.clean_cdr as cdr_consts
 from cdr_cleaner.cleaning_rules.deid.remove_fitbit_data_if_max_age_exceeded import RemoveFitbitDataIfMaxAgeExceeded, \
-    TABLES, SAVE_ROWS_TO_BE_DROPPED_QUERY, DROP_MAX_AGE_EXCEEDED_ROWS_QUERY
+    SAVE_ROWS_TO_BE_DROPPED_QUERY, DROP_MAX_AGE_EXCEEDED_ROWS_QUERY
 from constants.cdr_cleaner import clean_cdr as clean_consts
 from constants.bq_utils import WRITE_TRUNCATE
+from common import FITBIT_TABLES
 
 
 class RemoveFitbitDataIfMaxAgeExceededTest(unittest.TestCase):
@@ -31,10 +33,11 @@ class RemoveFitbitDataIfMaxAgeExceededTest(unittest.TestCase):
         self.project_id = 'foo_project'
         self.dataset_id = 'foo_dataset'
         self.sandbox_id = 'foo_sandbox_dataset'
+        self.combined_dataset_id = 'combined_dataset'
         self.client = None
 
         self.query_class = RemoveFitbitDataIfMaxAgeExceeded(
-            self.project_id, self.dataset_id, self.sandbox_id)
+            self.project_id, self.dataset_id, self.combined_dataset_id, self.sandbox_id)
 
         self.assertEqual(self.query_class.project_id, self.project_id)
         self.assertEqual(self.query_class.dataset_id, self.dataset_id)
@@ -44,8 +47,14 @@ class RemoveFitbitDataIfMaxAgeExceededTest(unittest.TestCase):
         # Test
         self.query_class.setup_rule(self.client)
 
-    def test_get_query_specs(self):
+    @mock.patch('google.cloud.bigquery.table.TableReference.from_string')
+    def test_get_query_specs(self, mock_table_reference):
         # Pre conditions
+        mock_table_reference = mock.MagicMock()
+        type(mock_table_reference).project = mock.PropertyMock(return_value=self.project_id)
+        type(mock_table_reference).dataset_id = mock.PropertyMock(return_value=self.combined_dataset_id)
+        type(mock_table_reference).table_id = mock.PropertyMock(return_value='person')
+
         self.assertEqual(self.query_class.affected_datasets,
                          [clean_consts.FITBIT])
 
@@ -56,7 +65,7 @@ class RemoveFitbitDataIfMaxAgeExceededTest(unittest.TestCase):
         expected_sandbox_queries_list = []
         expected_drop_queries_list = []
 
-        for i, table in enumerate(TABLES):
+        for i, table in enumerate(FITBIT_TABLES):
             expected_sandbox_queries_list.append({
                 clean_consts.QUERY:
                     SAVE_ROWS_TO_BE_DROPPED_QUERY.render(
@@ -65,7 +74,9 @@ class RemoveFitbitDataIfMaxAgeExceededTest(unittest.TestCase):
                         sandbox_table=self.query_class.get_sandbox_tablenames()
                         [i],
                         dataset=self.dataset_id,
-                        table=table)
+                        table=table,
+                        combined_dataset=mock_table_reference
+                    )
             })
 
             expected_drop_queries_list.append({
