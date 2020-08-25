8fe6d7a14d2544c800019c53cf4ff4be1f2522fc
dcampbell-vumc <danyel.campbell@vumc.org>
2020-08-25 08:06:47 -0500

    [DC-686] Remove EHR Data
    Testing modules for remove_ehr_data_past_deactivation_date.py

---
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_past_deactivation_date_test.py
@@ -0,0 +1,269 @@
+"""
+Integration test for the remove_ehr_data_past_deactivation_date module
+
+Original Issue: DC-686
+
+The intent is to sandbox and drop records dated after the date of deactivation for participants
+who have deactivated from the Program.
+"""
+
+# Python imports
+import unittest
+import mock
+import os
+import logging
+
+# Third party imports
+import pandas
+
+# Project imports
+import app_identity
+import bq_utils
+import gcs_utils
+import utils.participant_summary_requests as psr
+import tests.integration_tests.data_steward.retraction.retract_deactivated_pids_test as rdpt
+from sandbox import get_sandbox_dataset_id
+from constants.cdr_cleaner import clean_cdr as clean_consts
+from utils import bq
+from tests import test_util
+
+
+class RemoveEhrDataPastDeactivationDateTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+    def setUp(self):
+        self.project_id = app_identity.get_application_id()
+        self.dataset_id = bq_utils.get_dataset_id()
+        self.sandbox_id = get_sandbox_dataset_id(self.dataset_id)
+        self.tablename = '_deactivated_participants'
+        self.ticket_number = 'DC12345'
+
+        self.deactivated_participants = [(1, 'NO_CONTACT', '2018-12-07T08:21:14'),
+                                         (2, 'NO_CONTACT', '2019-12-07T08:21:14'),
+                                         (3, 'NO_CONTACT', '2017-12-07T08:21:14')]
+        self.columns = ['participantId', 'suspensionStatus', 'suspensionTime']
+        self.deactivated_participants_data = {
+            'person_id': [1, 2],
+            'suspension_status': ['NO_CONTACT', 'NO_CONTACT'],
+            'suspension_time': ['2018-12-07T08:21:14', '2019-12-07T08:21:14']
+        }
+        self.deactivated_participants_df = pandas.DataFrame(
+            columns=self.columns, data=self.deactivated_participants_data)
+
+        self.json_response_entry = {
+            'entry': [{
+                'fullUrl':
+                    'https//foo_project.appspot.com/rdr/v1/Participant/1/Summary',
+                'resource': {
+                    'participantId': '1',
+                    'suspensionStatus': 'NO_CONTACT',
+                    'suspensionTime': '2018-12-07T08:21:14'
+                }
+            }, {
+                'fullUrl':
+                    'https//foo_project.appspot.com/rdr/v1/Participant/2/Summary',
+                'resource': {
+                    'participantId': '2',
+                    'suspensionStatus': 'NO_CONTACT',
+                    'suspensionTime': '2019-12-07T08:21:14'
+                }
+            }, {
+                'fullUrl':
+                    'https//foo_project.appspot.com/rdr/v1/Participant/3/Summary',
+                'resource': {
+                    'participantId': '3',
+                    'suspensionStatus': 'NO_CONTACT',
+                    'suspensionTime': '2017-12-07T08:21:14'
+                }
+            }]
+        }
+
+        self.client = bq.get_client(self.project_id)
+
+    @mock.patch('utils.participant_summary_requests.requests.get')
+    @mock.patch(
+        'retraction.retract_deactivated_pids.get_date_info_for_pids_tables')
+    def test_remove_ehr_data_past_deactivation_date(self, mock_retraction_info, mock_get):
+        # pre conditions for participant summary API module
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = self.json_response_entry
+
+        # Ensure deactivated participants table is created and or updated
+        psr.get_deactivated_participants(self.project_id, self.dataset_id,
+                                         self.tablename, self.columns)
+
+        # pre conditions for retraction module
+        d = {
+            'project_id': [
+                self.project_id, self.project_id, self.project_id,
+                self.project_id, self.project_id, self.project_id
+            ],
+            'dataset_id': [
+                self.dataset_id, self.dataset_id, self.dataset_id,
+                self.dataset_id, self.dataset_id, self.dataset_id
+            ],
+            'table': [
+                'fake_condition_occurrence', 'fake_drug_exposure',
+                'fake_measurement', 'fake_observation',
+                'fake_procedure_occurrence', 'fake_visit_occurrence'
+            ],
+            'date_column': [
+                None, None, 'measurement_date', 'observation_date',
+                'procedure_date', None
+            ],
+            'start_date_column': [
+                'condition_start_date', 'drug_exposure_start_date', None, None,
+                None, 'visit_start_date'
+            ],
+            'end_date_column': [
+                'condition_end_date', 'drug_exposure_end_date', None, None,
+                None, 'visit_end_date'
+            ]
+        }
+        retraction_info = pandas.DataFrame(data=d)
+        mock_retraction_info.return_value = retraction_info
+
+        job_ids = []
+        dropped_row_count_queries = []
+        kept_row_count_queries = []
+        hpo_table_list = []
+
+        # Load the cdm files into dataset
+        for cdm_file in test_util.NYC_FIVE_PERSONS_FILES:
+            cdm_file_name = os.path.basename(cdm_file)
+            cdm_table = cdm_file_name.split('.')[0]
+            hpo_table = bq_utils.get_table_id(self.project_id, cdm_table)
+            # Do not process if person table
+            if hpo_table == 'fake_person':
+                continue
+            hpo_table_list.append(hpo_table)
+            logging.info(
+                f'Preparing to load table {self.dataset_id}.{hpo_table}')
+            with open(cdm_file, 'rb') as f:
+                gcs_utils.upload_object(gcs_utils.get_hpo_bucket(self.project_id),
+                                        cdm_file_name, f)
+            result = bq_utils.load_cdm_csv(self.project_id,
+                                           cdm_table,
+                                           dataset_id=self.dataset_id)
+            logging.info(f'Loading table {self.dataset_id}.{hpo_table}')
+            job_id = result['jobReference']['jobId']
+            job_ids.append(job_id)
+
+        incomplete_jobs = bq_utils.wait_on_jobs(job_ids)
+        self.assertEqual(len(incomplete_jobs), 0,
+                         'NYC five person load job did not complete')
+        logging.info('All tables loaded successfully')
+
+        # Store query for checking number of rows to delete
+        for ehr in self.deactivated_participants:
+            pid = ehr[0]
+            for row in retraction_info.itertuples(index=False):
+                if row.date_column is None:
+                    dropped_query = rdpt.EXPECTED_DROPPED_ROWS_QUERY_END_DATE.format(
+                        dataset_id=self.dataset_id,
+                        table_id=row.table,
+                        pid_table_id=self.tablename,
+                        pid=pid,
+                        start_date_column=row.start_date_column,
+                        end_date_column=row.end_date_column)
+                    kept_query = rdpt.EXPECTED_KEPT_ROWS_QUERY_END_DATE.format(
+                        dataset_id=self.dataset_id,
+                        table_id=row.table,
+                        pid_table_id=self.tablename,
+                        pid=pid,
+                        start_date_column=row.start_date_column,
+                        end_date_column=row.end_date_column)
+                else:
+                    dropped_query = rdpt.EXPECTED_DROPPED_ROWS_QUERY.format(
+                        dataset_id=self.dataset_id,
+                        table_id=row.table,
+                        pid_table_id=self.tablename,
+                        pid=pid,
+                        date_column=row.date_column)
+                    kept_query = rdpt.EXPECTED_KEPT_ROWS_QUERY.format(
+                        dataset_id=self.dataset_id,
+                        table_id=row.table,
+                        pid_table_id=self.tablename,
+                        pid=pid,
+                        date_column=row.date_column)
+                dropped_row_count_queries.append({
+                    clean_consts.QUERY: dropped_query,
+                    clean_consts.DESTINATION_DATASET: self.dataset_id,
+                    clean_consts.DESTINATION_TABLE: row.table
+                })
+                kept_row_count_queries.append({
+                    clean_consts.QUERY: kept_query,
+                    clean_consts.DESTINATION_DATASET: self.dataset_id,
+                    clean_consts.DESTINATION_TABLE: row.table
+                })
+
+        # Use query results to count number of expected dropped row deletions
+        expected_dropped_row_count = {}
+        for query_dict in dropped_row_count_queries:
+            response = self.client.query(query_dict['query'])
+            result = response.result()
+            if query_dict['destination_table_id'] in expected_dropped_row_count:
+                expected_dropped_row_count[
+                    query_dict['destination_table_id']] += result.total_rows
+            else:
+                expected_dropped_row_count[
+                    query_dict['destination_table_id']] = result.total_rows
+
+        # Separate check to find number of actual deleted rows
+        q = rdpt.TABLE_ROWS_QUERY.format(dataset_id=self.dataset_id)
+        q_result = self.client.query(q)
+        row_count_before_retraction = {}
+        for row in q_result:
+            row_count_before_retraction[row['table_id']] = row['row_count']
+
+        # Use query results to count number of expected dropped row deletions
+        expected_kept_row_count = {}
+        for query_dict in kept_row_count_queries:
+            response = self.client.query(query_dict['query'])
+            result = response.result()
+            if query_dict['destination_table_id'] in expected_kept_row_count:
+                expected_kept_row_count[query_dict['destination_table_id']] -= (
+                    (row_count_before_retraction[
+                         query_dict['destination_table_id']] -
+                     result.total_rows))
+            else:
+                expected_kept_row_count[query_dict['destination_table_id']] = (
+                        row_count_before_retraction[
+                            query_dict['destination_table_id']] -
+                        (row_count_before_retraction[
+                             query_dict['destination_table_id']] -
+                         result.total_rows))
+
+        # Perform retraction
+        query_list = retract_deactivated_pids.create_queries(
+            self.project_id, self.ticket_number, self.project_id,
+            self.bq_dataset_id, self.pid_table_id)
+        retract_deactivated_pids.run_queries(query_list, self.client)
+
+        # Find actual deleted rows
+        q_result = self.client.query(q)
+        results = q_result.result()
+        row_count_after_retraction = {}
+        for row in results:
+            row_count_after_retraction[row['table_id']] = row['row_count']
+
+        for table in expected_dropped_row_count:
+            self.assertEqual(
+                expected_dropped_row_count[table],
+                row_count_before_retraction[table] -
+                row_count_after_retraction[table])
+
+        for table in expected_kept_row_count:
+            self.assertEqual(expected_kept_row_count[table],
+                             row_count_after_retraction[table])
+
+    def tearDown(self):
+        test_util.delete_all_tables(self.dataset_id)
+        test_util.delete_all_tables(self.sandbox_id)
+        self.client.delete_dataset(self.sandbox_id)
+
---
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_past_deactivation_date_test.py
@@ -0,0 +1,199 @@
+"""
+Unit test for the remove_ehr_data_past_deactivation_date module
+
+Original Issue: DC-686
+
+The intent is to sandbox and drop records dated after the date of deactivation for participants
+who have deactivated from the Program.
+"""
+
+# Python imports
+import unittest
+import mock
+
+# Third Party imports
+import pandas
+import pandas.testing
+
+# Project imports
+from constants import bq_utils as bq_consts
+from constants.cdr_cleaner import clean_cdr as clean_consts
+import retraction.retract_deactivated_pids as rdp
+import cdr_cleaner.cleaning_rules.remove_ehr_data_past_deactivation_date as red
+
+
+class RemoveEhrDataPastDeactivationDateTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+    def setUp(self):
+        # Input parameters expected by the class
+        self.project_id = 'foo_project'
+        self.pids_project_id = 'foo_pid_project_id'
+        self.pids_dataset_id = 'foo_pid_dataset_id'
+        self.tablename = 'foo_table'
+        self.ticket_number = 'DC12345'
+        self.columns = ['participantId', 'suspensionStatus', 'suspensionTime']
+
+        mock_bq_client_patcher = mock.patch(
+            'retraction.retract_deactivated_pids.get_client')
+        self.mock_bq_client = mock_bq_client_patcher.start()
+        self.addCleanup(mock_bq_client_patcher.stop)
+
+        self.deactivated_participants_data = {
+            'person_id': [1, 2],
+            'suspension_status': ['NO_CONTACT', 'NO_CONTACT'],
+            'suspension_time': ['2018-12-07T08:21:14', '2019-12-07T08:21:14']
+        }
+
+    @mock.patch(
+        'retraction.retract_deactivated_pids.get_date_info_for_pids_tables')
+    @mock.patch(
+        'retraction.retract_deactivated_pids.check_and_create_sandbox_dataset')
+    @mock.patch('retraction.retract_deactivated_pids.get_client')
+    @mock.patch('retraction.retract_deactivated_pids.check_pid_exist')
+    @mock.patch(
+        'utils.participant_summary_requests.get_deactivated_participants')
+    def test_remove_ehr_data_past_deactivation_date(
+        self, mock_get_deactivated_participants, mock_pid_exist, mock_client,
+            mock_check_sandbox, mock_date_info):
+        # Preconditions for participant summary module mocks
+        deactivated_participants_df = pandas.DataFrame(
+            columns=self.columns, data=self.deactivated_participants_data)
+        mock_get_deactivated_participants.return_value = deactivated_participants_df
+
+        # Preconditions for retraction module mocks
+        d = {
+            'project_id': [
+                self.project_id, self.project_id, self.project_id,
+                self.project_id, self.project_id, self.project_id
+            ],
+            'dataset_id': [
+                self.pids_dataset_id, self.pids_dataset_id,
+                self.pids_dataset_id, self.pids_dataset_id,
+                self.pids_dataset_id, self.pids_dataset_id
+            ],
+            'table': [
+                'fake_condition_occurrence', 'fake_drug_exposure',
+                'fake_measurement', 'fake_observation',
+                'fake_procedure_occurrence', 'fake_visit_occurrence'
+            ],
+            'date_column': [
+                None, None, 'measurement_date', 'observation_date',
+                'procedure_date', None
+            ],
+            'start_date_column': [
+                'condition_start_date', 'drug_exposure_start_date', None, None,
+                None, 'visit_start_date'
+            ],
+            'end_date_column': [
+                'condition_end_date', 'drug_exposure_end_date', None, None,
+                None, 'visit_end_date'
+            ]
+        }
+        retraction_info = pandas.DataFrame(data=d)
+        mock_date_info.return_value = retraction_info
+
+        client = mock_client.return_value = self.mock_bq_client
+        client.query.return_value.to_dataframe.return_value = deactivated_participants_df
+
+        mock_pid_exist.return_value = 1
+        sandbox = mock_check_sandbox.return_value = 'foo_pid_dataset_id_sandbox'
+
+        # test
+        returned_queries = red.remove_ehr_data(self.project_id,
+                                               self.ticket_number,
+                                               self.pids_project_id,
+                                               self.pids_dataset_id,
+                                               self.tablename)
+
+        # post conditions
+        expected_queries = []
+
+        for ehr_row in deactivated_participants_df.itertuples(index=False):
+            for retraction_row in retraction_info.itertuples(index=False):
+                sandbox_dataset = sandbox
+                pid = ehr_row.person_id
+
+                if pandas.isnull(retraction_row.date_column):
+                    sandbox_query = rdp.SANDBOX_QUERY_END_DATE.render(
+                        project=retraction_row.project_id,
+                        sandbox_dataset=sandbox_dataset,
+                        intermediary_table=self.ticket_number + '_' +
+                        retraction_row.table,
+                        dataset=retraction_row.dataset_id,
+                        table=retraction_row.table,
+                        pid=pid,
+                        deactivated_pids_project=self.pids_project_id,
+                        deactivated_pids_dataset=self.pids_dataset_id,
+                        deactivated_pids_table=self.tablename,
+                        end_date_column=retraction_row.end_date_column,
+                        start_date_column=retraction_row.start_date_column)
+                    clean_query = rdp.CLEAN_QUERY_END_DATE.render(
+                        project=retraction_row.project_id,
+                        dataset=retraction_row.dataset_id,
+                        table=retraction_row.table,
+                        pid=pid,
+                        deactivated_pids_project=self.pids_project_id,
+                        deactivated_pids_dataset=self.pids_dataset_id,
+                        deactivated_pids_table=self.tablename,
+                        end_date_column=retraction_row.end_date_column,
+                        start_date_column=retraction_row.start_date_column)
+                else:
+                    sandbox_query = rdp.SANDBOX_QUERY_DATE.render(
+                        project=retraction_row.project_id,
+                        sandbox_dataset=sandbox_dataset,
+                        intermediary_table=self.ticket_number + '_' +
+                        retraction_row.table,
+                        dataset=retraction_row.dataset_id,
+                        table=retraction_row.table,
+                        pid=pid,
+                        deactivated_pids_project=self.pids_project_id,
+                        deactivated_pids_dataset=self.pids_dataset_id,
+                        deactivated_pids_table=self.tablename,
+                        date_column=retraction_row.date_column)
+                    clean_query = rdp.CLEAN_QUERY_DATE.render(
+                        project=retraction_row.project_id,
+                        dataset=retraction_row.dataset_id,
+                        table=retraction_row.table,
+                        pid=pid,
+                        deactivated_pids_project=self.pids_project_id,
+                        deactivated_pids_dataset=self.pids_dataset_id,
+                        deactivated_pids_table=self.tablename,
+                        date_column=retraction_row.date_column)
+                expected_queries.append({
+                    clean_consts.QUERY:
+                        sandbox_query,
+                    clean_consts.DESTINATION:
+                        retraction_row.project_id + '.' + sandbox_dataset +
+                        '.' + (self.ticket_number + '_' + retraction_row.table),
+                    clean_consts.DESTINATION_DATASET:
+                        retraction_row.dataset_id,
+                    clean_consts.DESTINATION_TABLE:
+                        retraction_row.table,
+                    clean_consts.DISPOSITION:
+                        bq_consts.WRITE_APPEND,
+                    'type':
+                        'sandbox'
+                })
+                expected_queries.append({
+                    clean_consts.QUERY:
+                        clean_query,
+                    clean_consts.DESTINATION:
+                        retraction_row.project_id + '.' +
+                        retraction_row.dataset_id + '.' + retraction_row.table,
+                    clean_consts.DESTINATION_DATASET:
+                        retraction_row.dataset_id,
+                    clean_consts.DESTINATION_TABLE:
+                        retraction_row.table,
+                    clean_consts.DISPOSITION:
+                        bq_consts.WRITE_TRUNCATE,
+                    'type':
+                        'retraction'
+                })
+
+        self.assertEqual(returned_queries, expected_queries)
