849a8e18fe98466c09544538cd2a63c4b09af907
Krishna Kalluri <krishnasaikalluri@gmail.com>
2020-08-26 18:52:08 -0400

    [DC-1009] Truncate Data from RDR Export after a cutoff date
    Signed-off-by: Krishna Kalluri <krishnasaikalluri@gmail.com>

---
+++ data_steward/cdr_cleaner/cleaning_rules/truncate_rdr_using_date.py
@@ -0,0 +1,174 @@
+"""
+The data cutoff date for PPI data for the fall 2020 CDR is 8/1. However, due to delays caused by the COPE vocabulary
+ update, the RDR ETL was not able to run until several days later, and the ETL cannot truncate exported data by date.
+
+Original Issues: DC-1009
+
+All rows of data in the August RDR export with dates after 08/01/2020 should be moved from the RDR export to a
+ sandboxed dataset prior to use of the RDR export to create the CDR
+"""
+
+# Python imports
+import logging
+
+# Third party imports
+from jinja2 import Environment
+
+# Project imports
+import common
+import constants.cdr_cleaner.clean_cdr as cdr_consts
+from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
+
+LOGGER = logging.getLogger(__name__)
+
+jinja_env = Environment(
+    # help protect against cross-site scripting vulnerabilities
+    autoescape=True,
+    # block tags on their own lines
+    # will not cause extra white space
+    trim_blocks=True,
+    lstrip_blocks=True,
+    # syntax highlighting should be better
+    # with these comment delimiters
+    comment_start_string='--',
+    comment_end_string=' --')
+
+TRUNCATE_TABLES = [
+    common.VISIT_OCCURRENCE, common.OBSERVATION, common.MEASUREMENT,
+    common.PROCEDURE_OCCURRENCE
+]
+
+TABLES_DATES_FIELDS = {
+    common.VISIT_OCCURRENCE: 'visit_start_date',
+    common.OBSERVATION: 'observation_date',
+    common.MEASUREMENT: 'measurement_date',
+    common.PROCEDURE_OCCURRENCE: 'procedure_date'
+}
+
+CUTOFF_DATE = '2020-08-01'
+
+# Query to create tables in sandbox with the rows that will be removed per cleaning rule
+SANDBOX_QUERY = jinja_env.from_string("""
+CREATE OR REPLACE TABLE
+    `{{project}}.{{sandbox_dataset}}.{{intermediary_table}}` AS (
+SELECT *
+FROM
+    `{{project}}.{{dataset}}.{{table_name}}`
+    where {{field_name}} > {{cutoff_date}}
+)
+""")
+
+TRUNCATE_ROWS = jinja_env.from_string("""
+DELETE
+FROM
+  `{{project}}.{{dataset}}.{{table_name}}`
+WHERE
+ {{field_name}} > {{cutoff_date}}
+""")
+
+
+class TruncateRdrData(BaseCleaningRule):
+    """
+    Apply value ranges to ensure that values are reasonable and to minimize the likelihood
+    of sensitive information (like phone numbers) within the free text fields.
+    """
+
+    def __init__(self, project_id, dataset_id, sandbox_dataset_id):
+        """
+        Initialize the class with proper information.
+
+        Set the issue numbers, description and affected datasets. As other tickets may affect
+        this SQL, append them to the list of Jira Issues.
+        DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
+        """
+        desc = 'All rows of data in the August RDR export with dates after 08/01/2020 will be truncated.'
+        super().__init__(issue_numbers=['DC1009'],
+                         description=desc,
+                         affected_datasets=[cdr_consts.RDR],
+                         affected_tables=TRUNCATE_TABLES,
+                         project_id=project_id,
+                         dataset_id=dataset_id,
+                         sandbox_dataset_id=sandbox_dataset_id)
+
+    def get_query_specs(self):
+        """
+        Return a list of dictionary query specifications.
+
+        :return:  A list of dictionaries. Each dictionary contains a single query
+            and a specification for how to execute that query. The specifications
+            are optional but the query is required.
+        """
+        counter = 0
+        sandbox_queries = []
+        truncate_queries = []
+        for table in TRUNCATE_TABLES:
+
+            save_changed_rows = {
+                cdr_consts.QUERY:
+                    SANDBOX_QUERY.render(
+                        project=self.project_id,
+                        dataset=self.dataset_id,
+                        sandbox_dataset=self.sandbox_dataset_id,
+                        intermediary_table=self.get_sandbox_tablenames()
+                        [counter],
+                        table_name=table,
+                        field_name=TABLES_DATES_FIELDS[table],
+                        cutoff_date=CUTOFF_DATE)
+            }
+
+            sandbox_queries.append(save_changed_rows)
+
+            truncate_query = {
+                cdr_consts.QUERY:
+                    TRUNCATE_ROWS.render(project=self.project_id,
+                                         dataset=self.dataset_id,
+                                         table_name=table,
+                                         field_name=TABLES_DATES_FIELDS[table],
+                                         cutoff_date=CUTOFF_DATE),
+            }
+
+            truncate_queries.append(truncate_query)
+            counter += 1
+
+        return sandbox_queries + truncate_queries
+
+    def setup_rule(self, client):
+        """
+        Function to run any data upload options before executing a query.
+        """
+        pass
+
+    def setup_validation(self, client):
+        """
+        Run required steps for validation setup
+        """
+        pass
+
+    def validate_rule(self, client):
+        """
+        Validates the cleaning rule which deletes or updates the data from the tables
+        """
+        pass
+
+    def get_sandbox_tablenames(self):
+        sandbox_tables = []
+        for table in self.affected_tables:
+            sandbox_tables.append(f'{self._issue_numbers[0].lower()}_{table}')
+        return sandbox_tables
+
+
+if __name__ == '__main__':
+    import cdr_cleaner.args_parser as parser
+    import cdr_cleaner.clean_cdr_engine as clean_engine
+
+    ARGS = parser.parse_args()
+
+    clean_engine.add_console_logging(ARGS.console_log)
+    rdr_cleaner = TruncateRdrData(ARGS.project_id, ARGS.dataset_id,
+                                  ARGS.sandbox_dataset_id)
+    query_list = rdr_cleaner.get_query_specs()
+
+    if ARGS.list_queries:
+        rdr_cleaner.log_queries()
+    else:
+        clean_engine.clean_dataset(ARGS.project_id, query_list)
---
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/truncate_rdr_using_date_test.py
@@ -0,0 +1,118 @@
+# Python imports
+import unittest
+
+# Project imports
+from cdr_cleaner.cleaning_rules.truncate_rdr_using_date import (
+    TruncateRdrData, TABLES_DATES_FIELDS, CUTOFF_DATE, SANDBOX_QUERY,
+    TRUNCATE_ROWS)
+from constants.cdr_cleaner import clean_cdr as clean_consts
+
+
+class TruncateRdrDataTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+    def setUp(self):
+        self.project_id = 'foo_project'
+        self.dataset_id = 'bar_dataset'
+        self.sandbox_id = 'baz_sandbox'
+
+        self.query_class = TruncateRdrData(self.project_id, self.dataset_id,
+                                           self.sandbox_id)
+
+        self.assertEqual(self.query_class.project_id, self.project_id)
+        self.assertEqual(self.query_class.dataset_id, self.dataset_id)
+        self.assertEqual(self.query_class.sandbox_dataset_id, self.sandbox_id)
+
+    def test_get_query_specs(self):
+        # Pre conditions
+        self.assertEqual(self.query_class.affected_datasets, [clean_consts.RDR])
+
+        # Test
+        results_list = self.query_class.get_query_specs()
+
+        # Post conditions
+        counter = 0
+        sandbox_queries = []
+        truncate_queries = []
+        for table in self.query_class.affected_tables:
+            save_changed_rows = {
+                clean_consts.QUERY:
+                    SANDBOX_QUERY.render(project=self.project_id,
+                                         dataset=self.dataset_id,
+                                         sandbox_dataset=self.sandbox_id,
+                                         intermediary_table=self.query_class.
+                                         get_sandbox_tablenames()[counter],
+                                         table_name=table,
+                                         field_name=TABLES_DATES_FIELDS[table],
+                                         cutoff_date=CUTOFF_DATE)
+            }
+
+            sandbox_queries.append(save_changed_rows)
+
+            truncate_query = {
+                clean_consts.QUERY:
+                    TRUNCATE_ROWS.render(project=self.project_id,
+                                         dataset=self.dataset_id,
+                                         table_name=table,
+                                         field_name=TABLES_DATES_FIELDS[table],
+                                         cutoff_date=CUTOFF_DATE),
+            }
+
+            truncate_queries.append(truncate_query)
+            counter += 1
+
+        expected_list = sandbox_queries + truncate_queries
+
+        self.assertEqual(results_list, expected_list)
+
+    def test_log_queries(self):
+        # Pre conditions
+        self.assertEqual(self.query_class.affected_datasets, [clean_consts.RDR])
+
+        counter = 0
+        sandbox_queries = []
+        truncate_queries = []
+        for table in self.query_class.affected_tables:
+            save_changed_rows = {
+                clean_consts.QUERY:
+                    SANDBOX_QUERY.render(project=self.project_id,
+                                         dataset=self.dataset_id,
+                                         sandbox_dataset=self.sandbox_id,
+                                         intermediary_table=self.query_class.
+                                         get_sandbox_tablenames()[counter],
+                                         table_name=table,
+                                         field_name=TABLES_DATES_FIELDS[table],
+                                         cutoff_date=CUTOFF_DATE)
+            }
+
+            sandbox_queries.append(
+                f'INFO:cdr_cleaner.cleaning_rules.base_cleaning_rule:Generated SQL Query:\n'
+                f'{save_changed_rows[clean_consts.QUERY]}')
+
+            truncate_query = {
+                clean_consts.QUERY:
+                    TRUNCATE_ROWS.render(project=self.project_id,
+                                         dataset=self.dataset_id,
+                                         table_name=table,
+                                         field_name=TABLES_DATES_FIELDS[table],
+                                         cutoff_date=CUTOFF_DATE),
+            }
+
+            truncate_queries.append(
+                f'INFO:cdr_cleaner.cleaning_rules.base_cleaning_rule:Generated SQL Query:\n'
+                f'{truncate_query[clean_consts.QUERY]}')
+            counter += 1
+
+        # Test
+        with self.assertLogs(level='INFO') as cm:
+            self.query_class.log_queries()
+
+            expected = sandbox_queries + truncate_queries
+
+            # Post condition
+            self.assertEqual(cm.output, expected)
