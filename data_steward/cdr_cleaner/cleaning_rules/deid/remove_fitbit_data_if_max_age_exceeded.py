42d8c9020760dfd3afbe2e20ca22ea23c3757457
Nishanth Pavinkurve <npp267@nyu.edu>
2020-09-09 22:00:01 -0400

    [DC-984] Update to use mapping_dataset_id instead of combined where possible

--- data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded.py
@@ -174,12 +174,14 @@ if __name__ == '__main__':
     if ARGS.list_queries:
         clean_engine.add_console_logging()
         query_list = clean_engine.get_query_list(
-            ARGS.project_id, ARGS.dataset_id,
-            [(RemoveFitbitDataIfMaxAgeExceeded,)], ARGS.combined_dataset_id)
+            ARGS.project_id,
+            ARGS.dataset_id, [(RemoveFitbitDataIfMaxAgeExceeded,)],
+            combined_dataset_id=ARGS.combined_dataset_id)
         for query in query_list:
             LOGGER.info(query)
     else:
         clean_engine.add_console_logging(ARGS.console_log)
-        clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
+        clean_engine.clean_dataset(ARGS.project_id,
+                                   ARGS.dataset_id,
                                    [(RemoveFitbitDataIfMaxAgeExceeded,)],
-                                   ARGS.combined_dataset_id)
+                                   combined_dataset_id=ARGS.combined_dataset_id)
