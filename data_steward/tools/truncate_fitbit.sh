399f80041d610acc52501f6e0f945328aa00f509
dcampbell-vumc <danyel.campbell@vumc.org>
2020-09-17 07:20:46 -0500

    [DC-1078] Truncate FitBit Data
    Comment changes in `truncate_fitibit.sh` bash script

--- data_steward/tools/truncate_fitbit.sh
+++ data_steward/tools/truncate_fitbit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -ex
-# This Script automates the process of de-identification of the fitbit dataset
+# This Script automates the process of truncating data in the fitbit dataset
 # This script expects you to use the venv in curation directory
 
 USAGE="
-Usage: clean_fitbit.sh
+Usage: truncate_fitbit.sh
   --key_file <path to key file>
   --fitbit_dataset <fitbit_dataset_id>
   --dataset_release_tag <release tag for the CDR>
@@ -58,8 +58,8 @@ export BIGQUERY_DATASET_ID="${fitbit_dataset}"
 export PYTHONPATH="${PYTHONPATH}:${CLEANER_DIR}:${DATA_STEWARD_DIR}"
 
 # create empty fitbit staging dataset
-#bq mk --dataset --description "${dataset_release_tag} staging version of ${fitbit_dataset}" "${APP_ID}":"${fitbit_staging_dataset}"
-#"${TOOLS_DIR}"/table_copy.sh --sync "false" --source_app_id "${APP_ID}" --target_app_id "${APP_ID}" --source_dataset "${fitbit_dataset}" --target_dataset "${fitbit_staging_dataset}"
+bq mk --dataset --description "${dataset_release_tag} staging version of ${fitbit_dataset}" "${APP_ID}":"${fitbit_staging_dataset}"
+"${TOOLS_DIR}"/table_copy.sh --sync "false" --source_app_id "${APP_ID}" --target_app_id "${APP_ID}" --source_dataset "${fitbit_dataset}" --target_dataset "${fitbit_staging_dataset}"
 # Use the below command if copy fails
 #transfer_params='{"source_dataset_id":"'${registered_fitbit_deid}'","source_project_id":"'${APP_ID}'"'
 #bq mk --transfer_config --project_id="${APP_ID}" --data_source="cross_region_copy" --target_dataset="${registered_fitbit_deid}" --display_name='Create Fitbit Deid' --params="${transfer_params}"
