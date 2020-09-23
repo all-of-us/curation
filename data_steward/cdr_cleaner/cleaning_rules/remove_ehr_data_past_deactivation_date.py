4158e0a8edbc57c72609f8a04c414de66c9e180a
Mark Velez <mav7014@nyp.org>
2020-09-23 15:51:46 -0400

    Extract storage of data from get_deactivated_participants
     * Revert get_deactivated_participants to return dataframe
     * Deactivation cleaning rule calls store_participant_data

--- data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_past_deactivation_date.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_past_deactivation_date.py
@@ -41,10 +41,12 @@ def remove_ehr_data_queries(project_id, ticket_number, pids_project_id,
     ehr_union_dataset = bq_utils.get_unioned_dataset_id()
 
     # gets the deactivated participant dataset to ensure it's up-to-date
-    psr.get_deactivated_participants(pids_project_id, pids_dataset_id,
-                                     tablename,
-                                     DEACTIVATED_PARTICIPANTS_COLUMNS)
-
+    df = psr.get_deactivated_participants(pids_project_id, pids_dataset_id,
+                                          tablename,
+                                          DEACTIVATED_PARTICIPANTS_COLUMNS)
+    # To store dataframe in a BQ dataset table
+    destination_table = pids_dataset_id + '.' + tablename
+    psr.store_participant_data(df, project_id, destination_table)
     # creates sandbox and truncate queries to run for deactivated participant data drops
     queries = rdp.create_queries(project_id, ticket_number, pids_project_id,
                                  pids_dataset_id, tablename, ehr_union_dataset)
