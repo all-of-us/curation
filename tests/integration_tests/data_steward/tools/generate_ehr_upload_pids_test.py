2754aa853675c1501c6fc9aec4a55eeeb4c4db1f
Nishanth Pavinkurve <npp267@nyu.edu>
2020-11-09 18:31:16 -0500

    [DC-1166] Add tests

---
+++ tests/integration_tests/data_steward/tools/generate_ehr_upload_pids_test.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+
+import app_identity
+from utils import bq
+from constants.utils.bq import LOOKUP_TABLES_DATASET_ID, HPO_SITE_ID_MAPPINGS_TABLE_ID
+from tools import generate_ehr_upload_pids as eup
+
+
+class GenerateEhrUploadPids(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+    def setUp(self):
+        self.project_id = app_identity.get_application_id()
+        self.dataset_id = os.environ.get('UNIONED_DATASET_ID')
+        self.client = bq.get_client(self.project_id)
+
+    def test_generate_ehr_upload_pids_query(self):
+        hpo_query = f"SELECT hpo_id FROM `{self.project_id}.{LOOKUP_TABLES_DATASET_ID}.{HPO_SITE_ID_MAPPINGS_TABLE_ID}`"
+        hpo_query_job = self.client.query(hpo_query)
+        hpo_ids = hpo_query_job.result().to_dataframe()["hpo_id"].to_list()
+        hpo_ids.sort()
+        query = eup.generate_ehr_upload_pids_query(self.project_id,
+                                                   self.dataset_id)
+        queries = query.split("\nUNION ALL \n")
+        for i, query in enumerate(sorted(queries)):
+            self.assertIn(hpo_ids[i], query)
