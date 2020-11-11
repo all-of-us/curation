aaf7877044273dc69ee514c60630b10e6ea033ca
dcampbell-vumc <danyel.campbell@vumc.org>
2020-11-10 18:17:04 -0600

    [DC-1159] Slack Alert Write Access Update
    * Modified slack logging handler to test if channel name and token are
    valid in initialize slack logging function

--- data_steward/curation_logging/slack_logging_handler.py
+++ data_steward/curation_logging/slack_logging_handler.py
@@ -17,14 +17,13 @@ class SlackLoggingHandler(logging.Handler):
     def emit(self, record):
 
         try:
-            if check_channel_and_token and not _is_raised_from_itself(record):
+            if not _is_raised_from_itself(record):
                 post_message(record.msg % record.args)
         except SlackConfigurationError:
             logging.exception(
                 'Slack is not configured for posting messages, refer to playbook.'
             )
             raise
-
 
 def _is_raised_from_itself(record):
     return record.module in __name__
@@ -35,10 +34,11 @@ def initialize_slack_logging():
     Setup Slack logging
     """
     if SLACK_TOKEN in os.environ and SLACK_CHANNEL in os.environ:
-        # Configure root logger
-        root_logger = logging.getLogger()
-        # Configure slack logging handler
-        log_handler = SlackLoggingHandler()
-        log_handler.setLevel(logging.WARNING)
-        # Add slack logging handler to root logger.
-        root_logger.addHandler(log_handler)
+        if check_channel_and_token:
+            # Configure root logger
+            root_logger = logging.getLogger()
+            # Configure slack logging handler
+            log_handler = SlackLoggingHandler()
+            log_handler.setLevel(logging.WARNING)
+            # Add slack logging handler to root logger.
+            root_logger.addHandler(log_handler)
