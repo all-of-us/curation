2d2256fd805db219ba34e309b64e55f9e6ab12c4
dcampbell-vumc <danyel.campbell@vumc.org>
2020-11-12 15:56:24 -0600

    [DC-807] Update Invalid Foreign Key CR
    * Added NullInvalidForeignKey to combined_cleaning_classes CRs list

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -25,7 +25,6 @@ import cdr_cleaner.cleaning_rules.id_deduplicate as id_dedup
 import cdr_cleaner.cleaning_rules.maps_to_value_ppi_vocab_update as maps_to_value_vocab_update
 import cdr_cleaner.cleaning_rules.negative_ages as neg_ages
 import cdr_cleaner.cleaning_rules.no_data_30_days_after_death as no_data_30days_after_death
-import cdr_cleaner.cleaning_rules.null_invalid_foreign_keys as null_foreign_key
 import cdr_cleaner.cleaning_rules.populate_route_ids as populate_routes
 import cdr_cleaner.cleaning_rules.remove_aian_participants as remove_aian_participants
 import \
@@ -73,6 +72,7 @@ from cdr_cleaner.cleaning_rules.rdr_observation_source_concept_id_suppression im
 from cdr_cleaner.cleaning_rules.truncate_rdr_using_date import TruncateRdrData
 from cdr_cleaner.cleaning_rules.unit_normalization import UnitNormalization
 from cdr_cleaner.cleaning_rules.update_fields_numbers_as_strings import UpdateFieldsNumbersAsStrings
+from cdr_cleaner.cleaning_rules.null_invalid_foreign_keys import NullInvalidForeignKeys
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 from constants.cdr_cleaner.clean_cdr import DataStage
 from constants.cdr_cleaner import clean_cdr_engine as ce_consts
@@ -170,7 +170,7 @@ COMBINED_CLEANING_CLASSES = [
     (drop_duplicate_states.get_drop_duplicate_states_queries,),
     # TODO : Make null_invalid_foreign_keys able to run on de_identified dataset
     (
-        null_foreign_key.null_invalid_foreign_keys,),
+        NullInvalidForeignKeys,),
     (remove_aian_participants.get_queries,),
     (validate_missing_participants.delete_records_for_non_matching_participants,
     ),
