628d9734e60ed20232def3fa1d5a134a05a529ae
Mark Velez <mav7014@nyp.org>
2020-11-16 15:26:14 -0500

    Add notebook rdr_export_qc
     * Temp solution to specify parameters via URL

---
+++ data_steward/analytics/cdr_ops/rdr_export_qc.py
@@ -0,0 +1,254 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # QC for RDR Export
+#
+# Quality checks performed on a new RDR dataset and comparison with previous RDR dataset.
+# ## Parameters
+# This notebook contains parameters which must be set via the URL. 
+#
+# For example:
+#
+# `https://my_notebooks/../cdr_ops/rdr_export_qc.py?project_id=my_project&old_rdr=rdr1&new_rdr=rdr2&cope_version_map=dataset1.map_table`
+#
+# will set variables like these which are used to construct SQL queries:
+#
+#     project_id = "my_project"
+#     old_rdr = "rdr1"
+#     new_rdr = "rdr2"
+#     cope_version_map = "dataset1.map_table"
+
+# +
+import urllib
+import pandas as pd
+
+NOTEBOOK_URL = None
+
+# Parameters required by this notebook
+REQUIRED_PARAMS = [
+    # identifies the google cloud project
+    'project_id',
+    # dataset_id of a prior RDR export for comparison
+    'old_rdr',
+    # dataset_id of the RDR export being evaluated
+    'new_rdr',
+    # dataset_id.table_id mapping questionnaire IDs to survey versions
+    'cope_version_map'
+]
+
+
+# -
+
+# ## Parse URL parameters
+# Retrieve parameters from the query string of the notebook URL. This allows sensitive
+# analytical parameters (e.g. project_id, dataset_id) to be specified ad hoc without
+# storing them in the notebook, which must be version controlled.
+#
+# **This approach to parameterizing notebooks has some major limitations and may be replaced in
+# the future.**
+# 1. The user must remember to run the following cells every time the query string is changed
+# 2. It may not be possible to specify the parameters this way when executing the notebook
+# programmatically (i.e. for automation)
+
+# + language="javascript"
+# // conditional needed to prevent warning
+# if (IPython.notebook.kernel) {
+#     // set the Python variable NOTEBOOK_URL
+#     IPython.notebook.kernel.execute("NOTEBOOK_URL = '" + window.location + "'");
+# }
+
+# +
+def get_url_params(url):
+    """
+    Extract params from the query string of a URL
+    
+    :returns: dict where the keys are parameter names
+    """
+    split_url = urllib.parse.urlsplit(url)
+    params = urllib.parse.parse_qs(split_url.query)
+    for key, value in params.items():
+        if len(value) == 1:
+            params[key] = value[0]
+    return params
+
+
+def validate_params(params, required_params):
+    """
+    Raise an error if a required parameter is missing from the params dict
+    """
+    for required_param in required_params:
+        if required_param not in params:
+            raise ValueError(f'Missing query parameter `{required_param}`')
+    print(f'All required parameters are specified.\n{params}')
+
+
+# -
+
+params = get_url_params(NOTEBOOK_URL)
+validate_params(params, REQUIRED_PARAMS)
+
+project_id = params['project_id']
+old_rdr = params['old_rdr']
+new_rdr = params['new_rdr']
+cope_version_map = params['cope_version_map']
+
+# # Check if observation_source_value vs concept ids
+
+query = """
+SELECT 
+    SUM(CASE WHEN observation_source_concept_id IS NULL THEN 1 ELSE 0 END) as n_null_source_concept_id
+    , SUM(CASE WHEN observation_source_concept_id=0 THEN 1 ELSE 0 END) as n_zero_source_concept_id
+FROM `{dataset}.observation`
+WHERE observation_source_value IS NOT NULL
+and observation_source_value != ''
+"""
+pd.read_gbq(query.format(dataset=new_rdr),
+            project_id=project_id,
+            dialect='standard')
+
+query = """
+SELECT 
+    SUM(CASE WHEN observation_concept_id IS NULL THEN 1 ELSE 0 END) as n_null_concept_id
+    , SUM(CASE WHEN observation_concept_id=0 THEN 1 ELSE 0 END) as n_zero_concept_id
+FROM `{dataset}.observation`
+WHERE observation_source_value IS NOT NULL
+and observation_source_value != ''
+"""
+pd.read_gbq(query.format(dataset=new_rdr),
+            project_id=project_id,
+            dialect='standard')
+
+# # Check value_source_value unmapped to source
+
+query = """
+SELECT 
+    SUM(CASE WHEN value_source_concept_id IS NULL THEN 1 ELSE 0 END) as n_null_source_concept_id
+    , SUM(CASE WHEN value_source_concept_id=0 THEN 1 ELSE 0 END) as n_zero_source_concept_id
+FROM `{dataset}.observation`
+WHERE value_source_value IS NOT NULL
+and value_source_value != ''
+"""
+pd.read_gbq(query.format(dataset=new_rdr),
+            project_id=project_id,
+            dialect='standard')
+
+# # Check value_source_value unmapped to standard
+
+query = """
+SELECT 
+    SUM(CASE WHEN value_as_concept_id IS NULL THEN 1 ELSE 0 END) as n_null_concept_id
+    , SUM(CASE WHEN value_as_concept_id=0 THEN 1 ELSE 0 END) as n_zero_concept_id
+FROM `{dataset}.observation`
+WHERE value_source_value IS NOT NULL
+and value_source_value != ''
+"""
+pd.read_gbq(query.format(dataset=new_rdr),
+            project_id=project_id,
+            dialect='standard')
+
+# # check date = extract date datetime
+
+query = """
+SELECT 
+    SUM(CASE WHEN observation_date != EXTRACT(DATE FROM observation_datetime) THEN 1 ELSE 0 END) as n_date_datetime_issues
+FROM `{dataset}.observation`
+"""
+pd.read_gbq(query.format(dataset=new_rdr),
+            project_id=project_id,
+            dialect='standard')
+
+# # Check for duplicates
+
+query = f"""
+with duplicates AS (
+    SELECT
+        person_id
+        , observation_datetime
+        , observation_source_value
+        , value_source_value
+        , value_as_number
+        , value_as_string
+        --, questionnaire_response_id
+        , COUNT(1) as n_data
+    FROM `{new_rdr}.observation`
+    INNER JOIN `{cope_version_map}` USING(questionnaire_response_id) -- For COPE only
+    GROUP BY 1,2,3,4,5,6
+)
+SELECT 
+    n_data as duplicates
+    , COUNT(1) as n_duplicates
+FROM duplicates
+WHERE n_data > 1
+GROUP BY 1
+ORDER BY 2 DESC
+"""
+pd.read_gbq(query, project_id=project_id, dialect='standard')
+
+# +
+# select
+# *
+# from `rdr20200906.observation`
+# where person_id = 	182351412
+# and observation_source_value = "PIIAddress_StreetAddress"
+# and value_as_string = "hvgcfhgv"
+# -
+
+# # Check if numeric data in value_as_string
+
+query = f"""
+SELECT
+    observation_source_value
+    , COUNT(1) AS n
+FROM `{new_rdr}.observation`
+WHERE SAFE_CAST(value_as_string AS INT64) IS NOT NULL
+GROUP BY 1
+ORDER BY 2 
+"""
+pd.read_gbq(query, project_id=project_id, dialect='standard')
+
+# # Check all questionnaire_id in map
+
+query = f"""
+SELECT
+    COUNT(1)
+FROM `{new_rdr}.observation`
+INNER JOIN `pipeline_tables.cope_concepts` on observation_source_value = concept_code
+WHERE questionnaire_response_id NOT IN (SELECT questionnaire_response_id FROM `{cope_version_map}`)
+"""
+pd.read_gbq(query, project_id=project_id, dialect='standard')
+
+# # Check questionnaire_response_ids don't overlap amongst versions
+
+query = f"""
+SELECT
+    participant_id,
+    questionnaire_response_id,
+    cope_month
+FROM `{cope_version_map}`
+"""
+version = pd.read_gbq(query, project_id=project_id, dialect='standard')
+
+v_dict = {}
+v_dict['may'] = set(version[version['cope_month'] == 'may']
+                    ['questionnaire_response_id'].unique())
+v_dict['june'] = set(version[version['cope_month'] == 'june']
+                     ['questionnaire_response_id'].unique())
+v_dict['july'] = set(version[version['cope_month'] == 'july']
+                     ['questionnaire_response_id'].unique())
+
+# +
+from itertools import combinations
+
+for d1, d2 in combinations(v_dict.keys(), 2):
+    print(f"{d1}-{d2}: {len(v_dict[d1].intersection(v_dict[d2]))}")
