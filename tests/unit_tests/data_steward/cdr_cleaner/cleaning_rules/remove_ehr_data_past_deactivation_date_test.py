4bdf0a8c3156cb70bd1b27b3b524b88196fa6bef
Nishanth Pavinkurve <npp267@nyu.edu>
2021-01-04 17:28:28 -0500

    [DC-1184] Update test from patch file from Lauren

--- tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_past_deactivation_date_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_past_deactivation_date_test.py
@@ -12,6 +12,7 @@ import unittest
 import mock
 
 # Third Party imports
+import google.cloud.bigquery as gbq
 import pandas
 
 # Project imports
@@ -46,62 +47,30 @@ class RemoveEhrDataPastDeactivationDateTest(unittest.TestCase):
         self.deactivated_participants_df = pandas.DataFrame(
             columns=self.columns, data=self.deactivated_participants_data)
 
-    @mock.patch('retraction.retract_deactivated_pids.get_table_cols_df')
-    @mock.patch(
-        'retraction.retract_deactivated_pids.sb.check_and_create_sandbox_dataset'
-    )
+    @mock.patch('retraction.retract_deactivated_pids.generate_queries')
+    @mock.patch('utils.participant_summary_requests.store_participant_data')
     @mock.patch(
         'utils.participant_summary_requests.get_deactivated_participants')
-    @mock.patch('utils.participant_summary_requests.store_participant_data')
-    def test_remove_ehr_data_past_deactivation_date(
-        self, mock_store_participant_data, mock_get_deactivated_participants,
-        mock_check_sandbox, mock_date_info):
-        mock_store_participant_data.return_value = self.deactivated_participants_df
-        mock_get_deactivated_participants.return_value = self.deactivated_participants_df
-
-        # Preconditions for retraction module mocks
-        table_cols = {
-            'project_id': [self.project_id] * 31,
-            'dataset_id': [self.dataset_id] * 31,
-            'table_name': ['condition_occurrence'] * 5 + ['drug_exposure'] * 6 +
-                          ['measurement'] * 3 + ['observation'] * 3 +
-                          ['procedure_occurrence'] * 3 +
-                          ['visit_occurrence'] * 5 +
-                          ['_mapping_condition_occurrence'] +
-                          ['_mapping_drug_exposure'] +
-                          ['_mapping_measurement'] + ['_mapping_observation'] +
-                          ['_mapping_procedure_occurrence'] +
-                          ['_mapping_visit_occurrence'],
-            'column_name': [
-                'condition_start_date', 'condition_start_datetime',
-                'condition_end_date', 'condition_end_datetime', 'person_id',
-                'drug_exposure_start_date', 'drug_exposure_start_datetime',
-                'drug_exposure_end_date', 'drug_exposure_end_datetime',
-                'verbatim_end_date', 'person_id', 'measurement_date',
-                'measurement_datetime', 'person_id', 'observation_date',
-                'observation_datetime', 'person_id', 'procedure_date',
-                'procedure_datetime', 'person_id', 'visit_start_date',
-                'visit_start_datetime', 'visit_end_date', 'visit_end_datetime',
-                'person_id'
-            ] + ['None'] * 6
-        }
-        table_cols_df = pandas.DataFrame(data=table_cols)
-        mock_date_info.return_value = table_cols_df
-
-        expected_tables_to_query = [
-            'condition_occurrence', 'drug_exposure', 'measurement',
-            'observation', 'procedure_occurrence', 'visit_occurrence'
-        ]
-
-        self.mock_bq_client.query.return_value.to_dataframe.return_value = self.deactivated_participants_df
-
-        sandbox = mock_check_sandbox.return_value = 'foo_pid_dataset_id_sandbox'
+    def test_queries(self, mock_get_data, mock_store_data, mock_query_creation):
+        # test setup
+        mock_get_data.return_value = pandas.DataFrame()
+        mock_query_creation.return_value = []
+        sandbox_ds = f'{self.dataset_id}_sandbox'
 
         # test
-        returned_queries = red.remove_ehr_data_queries(self.mock_bq_client,
-                                                       self.project_id,
-                                                       self.dataset_id, sandbox,
-                                                       self.fq_deact_pids)
+        red.remove_ehr_data_queries(self.mock_bq_client, self.project_id,
+                                    self.dataset_id, sandbox_ds,
+                                    self.fq_deact_pids)
 
-        self.assertEqual(len(returned_queries),
-                         len(expected_tables_to_query) * 2)
+        # post conditions
+        mock_query_creation.assert_called_once_with(
+            self.mock_bq_client, self.project_id, self.dataset_id, sandbox_ds,
+            gbq.TableReference.from_string(self.fq_deact_pids))
+
+        mock_store_data.assert_called_once_with(
+            mock_get_data.return_value, self.project_id,
+            '.'.join(self.fq_deact_pids.split('.')[1:]))
+
+        mock_get_data.assert_called_once_with(
+            *self.fq_deact_pids.split('.'),
+            red.DEACTIVATED_PARTICIPANTS_COLUMNS)
