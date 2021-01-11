ff8a9c3f63fb0777c478a3ce35b3e830226721bd
aimeemcgrenera <aimee.mcgrenera@vumc.org>
2021-01-11 09:32:13 -0600

    [DC-1289] fixed linting errors

--- tests/unit_tests/data_steward/tools/create_tier_test.py
+++ tests/unit_tests/data_steward/tools/create_tier_test.py
@@ -121,7 +121,9 @@ class CreateTierTest(unittest.TestCase):
 
     def test_validate_deid_stage_param(self):
         # Preconditions
-        invalid_deid_stage_params = ['baseee', 'deid_base', 'deid_clean', 'clean_base']
+        invalid_deid_stage_params = [
+            'baseee', 'deid_base', 'deid_clean', 'clean_base'
+        ]
 
         # Test if invalid parameters are given
         for ds in invalid_deid_stage_params:
