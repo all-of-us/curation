8e8dab1fc3b0b125fd89a142fe4d7900db85762b
jp3477 <jp3477@cumc.columbia.edu>
2021-01-14 20:06:21 -0500

    split parser into two sequential parsers

--- data_steward/analytics/cdr_ops/execute_notebook.py
+++ data_steward/analytics/cdr_ops/execute_notebook.py
@@ -3,22 +3,25 @@ import papermill as pm
 import sys
 
 
-def parse_notebook_params(parser, expected_params):
-    # parser = argparse.ArgumentParser()
+def parse_notebook_params(notebook_path, expected_params):
+    parser = argparse.ArgumentParser(
+        prog=f"{sys.argv[0]} {notebook_path}",
+        description=f"Execute notebook {notebook_path} with parameters")
 
     for param in expected_params:
         parser.add_argument(f"--{param}", required=True)
 
     args = parser.parse_args()
-    print(args)
-    print(sys.argv)
-    if all([vars(args).get(arg) for arg in expected_params]):
-        print("contains all args")
+    # print(args)
+    # print(sys.argv)
+    # if all([vars(args).get(arg) for arg in expected_params]):
+    #     print("contains all args")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Execute a jupyter notebook with parameters")
+        description="Execute a jupyter notebook with parameters",
+        add_help=False)
     parser.add_argument('notebook_path', help='path to a jupyter notebook')
     # parser.add_argument('--vocab_path',
     #                     help='a text filing containing a list of terminologies')
@@ -29,7 +32,17 @@ def main():
     contents = pm.inspect_notebook(notebook_path)
     parameter_names = contents.keys()
 
-    parse_notebook_params(parser, parameter_names)
+    # parser2 = argparse.ArgumentParser(
+    #     prog=f"{sys.argv[0]} {notebook_path}",
+    #     description=f"Execute notebook {notebook_path} with parameters")
+
+    # for param in parameter_names:
+    #     parser2.add_argument(f"--{param}", required=True)
+
+    # args2 = parser2.parse_known_args()
+
+    # print(args2)
+    parse_notebook_params(notebook_path, parameter_names)
 
     # if not (set(parameter_names).issubset(set(args))
 
