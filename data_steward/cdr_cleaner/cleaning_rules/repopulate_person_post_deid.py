393a2c33b614c497a1f5e6020e0c15e96480932e
Krishna Kalluri <krishnasaikalluri@gmail.com>
2021-01-19 14:59:17 -0500

    [DC-836] [DC-1194] Update Person Table Repopulation CR to New Base Class (#704)
    * Refactors repopulate_person_post_deid CR to use base class
    * adds unittests
    * Fixes integration test
    
    Signed-off-by: Krishna Kalluri <krishnasaikalluri@gmail.com>

--- data_steward/cdr_cleaner/cleaning_rules/repopulate_person_post_deid.py
+++ data_steward/cdr_cleaner/cleaning_rules/repopulate_person_post_deid.py
@@ -25,114 +25,154 @@ sex_at_birth_source_value (extension)
 """
 import logging
 
-from jinja2 import Template
-
 import constants.bq_utils as bq_consts
 import constants.cdr_cleaner.clean_cdr as cdr_consts
+from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule, query_spec_list
+from common import JINJA_ENV, PERSON
 
 LOGGER = logging.getLogger(__name__)
 
-PERSON_TABLE = 'person'
+JIRA_ISSUE_NUMBERS = ['DC516', 'DC836']
+
 GENDER_CONCEPT_ID = 1585838
 SEX_AT_BIRTH_CONCEPT_ID = 1585845
 AOU_NONE_INDICATED_CONCEPT_ID = 2100000001
 
-REPOPULATE_PERSON_QUERY = Template("""
+REPOPULATE_PERSON_QUERY = JINJA_ENV.from_string("""
 WITH
   gender AS (
   SELECT
     p.person_id,
-    COALESCE(o.value_as_concept_id, 0) AS gender_concept_id,
-    COALESCE(o.value_source_concept_id, 0) AS gender_source_concept_id,
-    COALESCE(c.concept_code, "No matching concept") AS gender_source_value
-  FROM `{{project}}.{{dataset}}.person` p
-  LEFT JOIN `{{project}}.{{dataset}}.observation` o
-    ON p.person_id = o.person_id AND observation_source_concept_id = {{gender_concept_id}}
-  LEFT JOIN `{{project}}.{{dataset}}.concept` c
-    ON value_source_concept_id = concept_id
-  ),
+    COALESCE(o.value_as_concept_id,
+      0) AS gender_concept_id,
+    COALESCE(o.value_source_concept_id,
+      0) AS gender_source_concept_id,
+    COALESCE(c.concept_code,
+      "No matching concept") AS gender_source_value
+  FROM
+    `{{project}}.{{dataset}}.person` p
+  LEFT JOIN
+    `{{project}}.{{dataset}}.observation` o
+  ON
+    p.person_id = o.person_id
+    AND observation_source_concept_id = {{gender_concept_id}}
+  LEFT JOIN
+    `{{project}}.{{dataset}}.concept` c
+  ON
+    value_source_concept_id = concept_id ),
   sex_at_birth AS (
   SELECT
     p.person_id,
-    COALESCE(o.value_as_concept_id, 0) AS sex_at_birth_concept_id,
-    COALESCE(o.value_source_concept_id, 0) AS sex_at_birth_source_concept_id,
-    COALESCE(c.concept_code, "No matching concept") AS sex_at_birth_source_value
-  FROM `{{project}}.{{dataset}}.person` p
-  LEFT JOIN `{{project}}.{{dataset}}.observation` o
-    ON p.person_id = o.person_id AND observation_source_concept_id = {{sex_at_birth_concept_id}}
-  LEFT JOIN `{{project}}.{{dataset}}.concept` c
-    ON value_source_concept_id = concept_id
-  ),
-  repopulate_person_from_observation AS (
-  select DISTINCT * 
-  from 
-    (SELECT
-    per.person_id,
-    gender.gender_concept_id,
-    sex_at_birth.sex_at_birth_concept_id,
-    EXTRACT(YEAR from birth_datetime) as year_of_birth,
-    EXTRACT(MONTH from birth_datetime) as month_of_birth,
-    EXTRACT(DAY from birth_datetime) as day_of_birth,
-    birth_datetime,
-    --Case statement to get proper race domain matches
-    case race_ob.value_source_concept_id
-      when 1586142 then 8515 --asian
-      when 1586143 then 8516 --black/aa
-      when 1586146 then 8527 --white
-      --otherwise, just use the standard mapped answer (or 0)
-      else coalesce(race_ob.value_as_concept_id, 0) 
-      end AS race_concept_id,
-    --Hardcode to the standard non-hispanic or hispanic code as applicable.
-    if(ethnicity_ob.value_as_concept_id is null, 
-    --Case this out based on the race_ob (race) values, ie if it's a skip/pna respect that.
-    case race_ob.value_source_concept_id
-      when 0 then 0 --missing answer
-      when null then 0 --missing answer
-      when 903079 then 903079 --PNA
-      when 903096 then 903096 --Skip
-      when 1586148 then 1586148 --None of these
-      --otherwise, it's non-hispanic
-      else 38003564
-      end
-    --Assign HLS if it's present
-    , 38003563) AS ethnicity_concept_id,
-    location_id,
-    per.provider_id,
-    care_site_id,
-    cast(per.person_id as STRING) as person_source_value,
-    gender.gender_source_value,
-    gender.gender_source_concept_id,
-    sex_at_birth.sex_at_birth_source_value,
-    sex_at_birth.sex_at_birth_source_concept_id,
-    coalesce(race_ob.value_source_value, "No matching concept") AS race_source_value,
-    coalesce(race_ob.value_source_concept_id, 0) AS race_source_concept_id,
-    coalesce(ethnicity_ob.value_source_value, 
-    --fill in the skip/pna/none of these if needed
-    if(race_ob.value_source_concept_id in (903079,903096,1586148),race_ob.value_source_value,null)
-    --otherwise it is no matching
-    ,"No matching concept") AS ethnicity_source_value,
-    coalesce(ethnicity_ob.value_source_concept_id, 0) AS ethnicity_source_concept_id
+    COALESCE(o.value_as_concept_id,
+      0) AS sex_at_birth_concept_id,
+    COALESCE(o.value_source_concept_id,
+      0) AS sex_at_birth_source_concept_id,
+    COALESCE(c.concept_code,
+      "No matching concept") AS sex_at_birth_source_value
   FROM
-    `{{project}}.{{dataset}}.person` AS per
-  LEFT JOIN gender
-  ON
-    per.person_id = gender.person_id
-  LEFT JOIN sex_at_birth
-  ON
-    per.person_id = sex_at_birth.person_id
+    `{{project}}.{{dataset}}.person` p
   LEFT JOIN
-    `{{project}}.{{dataset}}.observation` race_ob
+    `{{project}}.{{dataset}}.observation` o
   ON
-    per.person_id = race_ob.person_id
-    AND race_ob.observation_concept_id = 1586140
-    AND race_ob.value_source_concept_id != 1586147
+    p.person_id = o.person_id
+    AND observation_source_concept_id = {{sex_at_birth_concept_id}}
   LEFT JOIN
-    `{{project}}.{{dataset}}.observation` ethnicity_ob
+    `{{project}}.{{dataset}}.concept` c
   ON
-    per.person_id = ethnicity_ob.person_id
-    AND ethnicity_ob.observation_concept_id=1586140
-    AND ethnicity_ob.value_source_concept_id = 1586147)
-    )
+    value_source_concept_id = concept_id ),
+  repopulate_person_from_observation AS (
+  SELECT
+    DISTINCT *
+  FROM (
+    SELECT
+      per.person_id,
+      gender.gender_concept_id,
+      sex_at_birth.sex_at_birth_concept_id,
+      EXTRACT(YEAR
+      FROM
+        birth_datetime) AS year_of_birth,
+      EXTRACT(MONTH
+      FROM
+        birth_datetime) AS month_of_birth,
+      EXTRACT(DAY
+      FROM
+        birth_datetime) AS day_of_birth,
+      birth_datetime,
+      /*Case statement to get proper race domain matches*/
+      CASE race_ob.value_source_concept_id
+        WHEN 1586142 THEN 8515 /*asian*/
+        WHEN 1586143 THEN 8516 /*black/aa*/
+        WHEN 1586146 THEN 8527 /*white*/
+      /*otherwise, just use the standard mapped answer (or 0)*/
+      ELSE
+      coalesce(race_ob.value_as_concept_id,
+        0)
+    END
+      AS race_concept_id,
+      /*Hardcode to the standard non-hispanic or hispanic code as applicable.*/
+    IF
+      (ethnicity_ob.value_as_concept_id IS NULL,
+        /*Case this out based on the race_ob (race) values, ie if it's a skip/pna respect that.*/
+        CASE race_ob.value_source_concept_id
+          WHEN  0 THEN 0 /*missing answer*/
+          WHEN  NULL THEN 0 /*missing answer*/
+          WHEN  903079 THEN 903079 /*PNA*/
+          WHEN  903096 THEN 903096 /*Skip*/
+          WHEN  1586148 THEN 1586148 /*None of these*/
+        /*otherwise, it's non-hispanic*/
+        ELSE
+        38003564
+      END
+        /*Assign HLS if it's present*/
+        ,
+        38003563) AS ethnicity_concept_id,
+      location_id,
+      per.provider_id,
+      care_site_id,
+      CAST(per.person_id AS STRING) AS person_source_value,
+      gender.gender_source_value,
+      gender.gender_source_concept_id,
+      sex_at_birth.sex_at_birth_source_value,
+      sex_at_birth.sex_at_birth_source_concept_id,
+      coalesce(race_ob.value_source_value,
+        "No matching concept") AS race_source_value,
+      coalesce(race_ob.value_source_concept_id,
+        0) AS race_source_concept_id,
+      coalesce(ethnicity_ob.value_source_value,
+        /*fill in the skip/pna/none of these if needed*/
+      IF
+        (race_ob.value_source_concept_id IN (903079,
+            903096,
+            1586148),
+          race_ob.value_source_value,
+          NULL)
+        /*otherwise it is no matching*/
+        ,
+        "No matching concept") AS ethnicity_source_value,
+      coalesce(ethnicity_ob.value_source_concept_id,
+        0) AS ethnicity_source_concept_id
+    FROM
+      `{{project}}.{{dataset}}.person` AS per
+    LEFT JOIN
+      gender
+    ON
+      per.person_id = gender.person_id
+    LEFT JOIN
+      sex_at_birth
+    ON
+      per.person_id = sex_at_birth.person_id
+    LEFT JOIN
+      `{{project}}.{{dataset}}.observation` race_ob
+    ON
+      per.person_id = race_ob.person_id
+      AND race_ob.observation_concept_id = 1586140
+      AND race_ob.value_source_concept_id != 1586147
+    LEFT JOIN
+      `{{project}}.{{dataset}}.observation` ethnicity_ob
+    ON
+      per.person_id = ethnicity_ob.person_id
+      AND ethnicity_ob.observation_concept_id=1586140
+      AND ethnicity_ob.value_source_concept_id = 1586147) )
 SELECT
   person_id,
   gender_concept_id,
@@ -170,32 +210,69 @@ FROM
 """)
 
 
-def get_repopulate_person_post_deid_queries(project_id,
-                                            dataset_id,
-                                            sandbox_dataset_id=None):
-    """
-    This Function returns a parsed query to repopulate the person table using observation.
+class RepopulatePersonPostDeid(BaseCleaningRule):
 
-    :param project_id: Name of the project
-    :param dataset_id: Name of the dataset where the queries should be run
-    :param sandbox_dataset_id: Identifies the sandbox dataset to store rows 
-    #TODO use sandbox_dataset_id for CR
-    :return: A list of query dictionaries.
-    """
-    queries_list = []
-    query = dict()
-    query[cdr_consts.QUERY] = REPOPULATE_PERSON_QUERY.render(
-        project=project_id,
-        dataset=dataset_id,
-        gender_concept_id=GENDER_CONCEPT_ID,
-        sex_at_birth_concept_id=SEX_AT_BIRTH_CONCEPT_ID,
-        aou_custom_concept=AOU_NONE_INDICATED_CONCEPT_ID)
-    query[cdr_consts.DESTINATION_TABLE] = PERSON_TABLE
-    query[cdr_consts.DESTINATION_DATASET] = dataset_id
-    query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
-    queries_list.append(query)
+    def __init__(self, project_id, dataset_id, sandbox_dataset_id):
+        """
+        Initialize the class with proper information.
 
-    return queries_list
+        Set the issue numbers, description and affected datasets. As other tickets may affect
+        this SQL, append them to the list of Jira Issues.
+        DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
+        """
+        desc = (
+            'Returns a parsed query to repopulate the person table using observation.'
+        )
+
+        super().__init__(issue_numbers=JIRA_ISSUE_NUMBERS,
+                         description=desc,
+                         affected_datasets=[cdr_consts.DEID_BASE],
+                         affected_tables=[PERSON],
+                         project_id=project_id,
+                         dataset_id=dataset_id,
+                         sandbox_dataset_id=sandbox_dataset_id)
+
+    def get_query_specs(self, *args, **keyword_args) -> query_spec_list:
+        """
+        Return a list of dictionary query specifications.
+
+        :return:  A list of dictionaries. Each dictionary contains a single query
+            and a specification for how to execute that query. The specifications
+            are optional but the query is required.
+        """
+
+        queries_list = []
+        query = dict()
+        query[cdr_consts.QUERY] = REPOPULATE_PERSON_QUERY.render(
+            project=self.project_id,
+            dataset=self.dataset_id,
+            gender_concept_id=GENDER_CONCEPT_ID,
+            sex_at_birth_concept_id=SEX_AT_BIRTH_CONCEPT_ID,
+            aou_custom_concept=AOU_NONE_INDICATED_CONCEPT_ID)
+        query[cdr_consts.DESTINATION_TABLE] = PERSON
+        query[cdr_consts.DESTINATION_DATASET] = self.dataset_id
+        query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
+        queries_list.append(query)
+
+        return queries_list
+
+    def setup_rule(self, client, *args, **keyword_args):
+        pass
+
+    def setup_validation(self, client):
+        """
+        Run required steps for validation setup
+        """
+        raise NotImplementedError("Please fix me.")
+
+    def validate_rule(self, client):
+        """
+        Validates the cleaning rule which deletes or updates the data from the tables
+        """
+        raise NotImplementedError("Please fix me.")
+
+    def get_sandbox_tablenames(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -206,13 +283,14 @@ if __name__ == '__main__':
 
     if ARGS.list_queries:
         clean_engine.add_console_logging()
-        query_list = clean_engine.get_query_list(
-            ARGS.project_id, ARGS.dataset_id, ARGS.sandbox_dataset_id,
-            [(get_repopulate_person_post_deid_queries,)])
+        query_list = clean_engine.get_query_list(ARGS.project_id,
+                                                 ARGS.dataset_id,
+                                                 ARGS.sandbox_dataset_id,
+                                                 [(RepopulatePersonPostDeid,)])
         for query in query_list:
             LOGGER.info(query)
     else:
         clean_engine.add_console_logging(ARGS.console_log)
         clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
                                    ARGS.sandbox_dataset_id,
-                                   [(get_repopulate_person_post_deid_queries,)])
+                                   [(RepopulatePersonPostDeid,)])
--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/repopulate_person_post_deid_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/repopulate_person_post_deid_test.py
@@ -2,21 +2,23 @@
 import unittest
 
 from googleapiclient.errors import HttpError
-# Third party imports
-from jinja2 import Template
 
 # Project imports
 import bq_utils
+import common
 from cdr_cleaner import clean_cdr_engine
-from cdr_cleaner.cleaning_rules import repopulate_person_post_deid
+from cdr_cleaner.cleaning_rules.repopulate_person_post_deid import (
+    RepopulatePersonPostDeid, GENDER_CONCEPT_ID, SEX_AT_BIRTH_CONCEPT_ID)
 from tests import test_util
+
+# Third party imports
 
 # Participant 1: has gender and sex at birth observations
 # Participant 2: no gender or sex at birth observations
 INSERT_FAKE_PARTICIPANTS_TMPLS = [
     # TODO(calbach): Ideally these tests should not manipulate concept table, not currently hermetic.
-    Template("""
-INSERT INTO `{{project_id}}.{{dataset_id}}.concept` (concept_id, concept_code)
+    common.JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{dataset_id}}.concept` (concept_id, concept_code)
 VALUES
   ({{gender_concept_id}}, "gender"),
   ({{gender_nonbinary_concept_id}}, "nonbinary"),
@@ -25,17 +27,27 @@ VALUES
   ({{sex_female_concept_id}}, "female"),
   ({{sex_female_source_concept_id}}, "female_src")
 """),
-    Template("""
-INSERT INTO `{{project_id}}.{{dataset_id}}.person` (person_id)
+    common.JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{dataset_id}}.person` (person_id)
 VALUES (1), (2)
 """),
-    Template("""
-INSERT INTO `{{project_id}}.{{dataset_id}}.observation` (person_id, observation_id, observation_source_concept_id, value_as_concept_id, value_source_concept_id)
+    common.JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{dataset_id}}.observation` (person_id, observation_id, observation_source_concept_id, value_as_concept_id, value_source_concept_id)
 VALUES
   (1, 100, {{gender_concept_id}}, {{gender_nonbinary_concept_id}}, {{gender_nonbinary_source_concept_id}}),
   (1, 101, {{sex_at_birth_concept_id}}, {{sex_female_concept_id}}, {{sex_female_source_concept_id}})
 """)
 ]
+
+
+def drop_concept_table(dataset_id):
+    if bq_utils.table_exists(common.CONCEPT):
+        q = "DROP TABLE {dataset}.concept;".format(dataset=dataset_id)
+        try:
+            bq_utils.query(q)
+        except HttpError as err:
+            if err.resp.status != 404:
+                raise
 
 
 class RepopulatePersonPostDeidTest(unittest.TestCase):
@@ -60,6 +72,9 @@ class RepopulatePersonPostDeidTest(unittest.TestCase):
         # clean up after itself so that we don't need this defensive check.
         test_util.delete_all_tables(self.dataset_id)
 
+        # drop concept table
+        drop_concept_table(self.dataset_id)
+
         create_tables = ['person', 'observation']
         table_fields = {
             'person': 'post_deid_person',
@@ -77,6 +92,16 @@ class RepopulatePersonPostDeidTest(unittest.TestCase):
 
     def tearDown(self):
         test_util.delete_all_tables(self.dataset_id)
+        # Delete concept table
+        drop_concept_table(self.dataset_id)
+
+        # re create concept table
+        q = """CREATE or REPLACE table `{project}.{dataset}.concept`  as (
+            SELECT * FROM `{project}.{vocab}.concept`)""".format(
+            project=self.project_id,
+            dataset=self.dataset_id,
+            vocab=common.VOCABULARY_DATASET)
+        bq_utils.query(q)
 
     def assertPersonFields(self, person, want):
         for k in want.keys():
@@ -92,12 +117,11 @@ class RepopulatePersonPostDeidTest(unittest.TestCase):
             query = tmpl.render(
                 project_id=self.project_id,
                 dataset_id=self.dataset_id,
-                gender_concept_id=repopulate_person_post_deid.GENDER_CONCEPT_ID,
+                gender_concept_id=GENDER_CONCEPT_ID,
                 gender_nonbinary_concept_id=gender_nonbinary_concept_id,
                 gender_nonbinary_source_concept_id=
                 gender_nonbinary_source_concept_id,
-                sex_at_birth_concept_id=repopulate_person_post_deid.
-                SEX_AT_BIRTH_CONCEPT_ID,
+                sex_at_birth_concept_id=SEX_AT_BIRTH_CONCEPT_ID,
                 sex_female_concept_id=sex_female_concept_id,
                 sex_female_source_concept_id=sex_female_source_concept_id)
             try:
@@ -107,10 +131,9 @@ class RepopulatePersonPostDeidTest(unittest.TestCase):
                     query, e.content))
             self.assertTrue(resp["jobComplete"])
 
-        clean_cdr_engine.clean_dataset(
-            self.project_id, self.dataset_id, self.sandbox_dataset_id,
-            [(repopulate_person_post_deid.
-              get_repopulate_person_post_deid_queries,)])
+        clean_cdr_engine.clean_dataset(self.project_id, self.dataset_id,
+                                       self.sandbox_dataset_id,
+                                       [(RepopulatePersonPostDeid,)])
 
         rows = bq_utils.response2rows(
             bq_utils.query("SELECT * FROM `{}.{}.person`".format(
