d7a146fa7857a781083e47d4aa9417ad02deca30
dcampbell-vumc <danyel.campbell@vumc.org>
2021-01-22 13:23:12 -0600

    [DC-1293] CT Create Datasets Function
    * removed unused import statements
    * corrected the description
    * dataset names now in dictionary
    * using consts.BACKUP/SANDBOX/STAGING strings in test function

--- tests/unit_tests/data_steward/tools/create_tier_test.py
+++ tests/unit_tests/data_steward/tools/create_tier_test.py
@@ -10,7 +10,7 @@ import mock
 # Third party imports
 
 # Project imports
-from utils import bq
+from constants.cdr_cleaner import clean_cdr as consts
 from tools.create_tier import parse_deid_args, validate_deid_stage_param, validate_tier_param, \
     validate_release_tag_param, create_datasets, get_dataset_name
 
@@ -33,7 +33,7 @@ class CreateTierTest(unittest.TestCase):
         self.deid_stage = 'deid'
         self.name = 'foo_name'
 
-        self.description = f'Dataset created for {self.release_tag} {self.tier} CDR run'
+        self.description = f'Dataset created for {self.tier}{self.release_tag} CDR run'
         self.labels_and_tags = {
             'release_tag': self.release_tag,
             'phase': self.deid_stage,
@@ -211,6 +211,13 @@ class CreateTierTest(unittest.TestCase):
         client = mock_client.return_value = self.mock_bq_client
         client.side_effects = create_datasets
 
+        datasets = {
+            self.name: self.name,
+            consts.BACKUP: f'{self.name}_{consts.BACKUP}',
+            consts.SANDBOX: f'{self.name}_{consts.SANDBOX}',
+            consts.STAGING: f'{self.name}_{consts.STAGING}'
+        }
+
         # Tests if incorrect parameters are given
         self.assertRaises(RuntimeError, create_datasets, None, self.name,
                           self.input_dataset, self.tier, self.release_tag,
@@ -235,31 +242,30 @@ class CreateTierTest(unittest.TestCase):
         # Post conditions
         client.create_dataset.assert_called()
 
-        self.assertEqual(expected, ('foo_name', 'backup_foo_name',
-                                    'staging_foo_name', 'sandbox_foo_name'))
+        self.assertEqual(expected, datasets)
 
         # Ensures datasets are created with the proper name, descriptions, and labels and tags
         self.assertEqual(mock_define_dataset.call_count, 4)
 
         mock_define_dataset.assert_has_calls([
-            mock.call(client.project, 'foo_name', self.description,
+            mock.call(client.project, datasets[self.name], self.description,
                       self.labels_and_tags),
-            mock.call(client.project, 'backup_foo_name', self.description,
+            mock.call(client.project, datasets[consts.BACKUP], self.description,
                       self.labels_and_tags),
-            mock.call(client.project, 'staging_foo_name', self.description,
-                      self.labels_and_tags),
-            mock.call(client.project, 'sandbox_foo_name', self.description,
-                      self.labels_and_tags)
+            mock.call(client.project, datasets[consts.STAGING],
+                      self.description, self.labels_and_tags),
+            mock.call(client.project, datasets[consts.SANDBOX],
+                      self.description, self.labels_and_tags)
         ])
 
         # Ensures datasets are updated with the proper labels and tags (if dataset is de-identified or not)
         self.assertEqual(mock_update_labels_tags.call_count, 3)
 
         mock_update_labels_tags.assert_has_calls([
-            mock.call('backup_foo_name', self.labels_and_tags,
+            mock.call(datasets[consts.BACKUP], self.labels_and_tags,
                       {'de-identified': 'false'}),
-            mock.call('staging_foo_name', self.labels_and_tags,
+            mock.call(datasets[consts.STAGING], self.labels_and_tags,
                       {'de-identified': 'true'}),
-            mock.call('foo_name', self.labels_and_tags,
+            mock.call(datasets[self.name], self.labels_and_tags,
                       {'de-identified': 'true'})
         ])
