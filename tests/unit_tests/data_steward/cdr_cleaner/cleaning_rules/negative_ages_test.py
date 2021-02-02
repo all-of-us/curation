ed3d76b2ae52546349ea9b17296ff0d232108b46
aimeemcgrenera <aimee.mcgrenera@vumc.org>
2021-02-02 16:01:57 -0600

    [DC-811] update unit test

--- tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/negative_ages_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/negative_ages_test.py
@@ -55,70 +55,11 @@ class NegativeAgesTest(unittest.TestCase):
         result_list = self.rule_instance.get_query_specs()
 
         # Post conditions
-        expected_sandbox_query_list = []
-        expected_queries = []
-        for table in date_fields:
-            sandbox_query = dict()
-            query_na = dict()
-            query_ma = dict()
-            person_table = person
-            sandbox_query[
-                cdr_consts.QUERY] = SANDBOX_NEGATIVE_AND_MAX_AGE_QUERY.render(
-                    project_id=self.project_id,
-                    dataset_id=self.dataset_id,
-                    sandbox_id=self.sandbox_dataset_id,
-                    intermediary_table=self.rule_instance.sandbox_table_for(
-                        table),
-                    table=table,
-                    person_table=person_table,
-                    table_date=date_fields[table],
-                    MAX_AGE=MAX_AGE)
-            expected_sandbox_query_list.append(sandbox_query)
-            query_na[cdr_consts.QUERY] = NEGATIVE_AGES_QUERY.render(
-                project_id=self.project_id,
-                dataset_id=self.dataset_id,
-                table=table,
-                person_table=person_table,
-                table_date=date_fields[table])
-            query_na[cdr_consts.DESTINATION_TABLE] = table
-            query_na[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
-            query_na[cdr_consts.DESTINATION_DATASET] = self.dataset_id
-            query_ma[cdr_consts.QUERY] = MAX_AGE_QUERY.render(
-                project_id=self.project_id,
-                dataset_id=self.dataset_id,
-                table=table,
-                person_table=person_table,
-                table_date=date_fields[table],
-                MAX_AGE=MAX_AGE)
-            query_ma[cdr_consts.DESTINATION_TABLE] = table
-            query_ma[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
-            query_ma[cdr_consts.DESTINATION_DATASET] = self.dataset_id
-            expected_queries.extend([query_na, query_ma])
-
-        # query for death before birthdate
-        death = common.DEATH
-        sandbox_query = dict()
-        query = dict()
-        person_table = person
-        sandbox_query[
-            cdr_consts.QUERY] = SANDBOX_NEGATIVE_AGE_DEATH_QUERY.render(
-                project_id=self.project_id,
-                dataset_id=self.dataset_id,
-                sandbox_id=self.sandbox_dataset_id,
-                intermediary_tatble=self.rule_instance.sandbox_table_for(death),
-                table=death,
-                person_table=person_table)
-        expected_sandbox_query_list.append(sandbox_query)
-        query[cdr_consts.QUERY] = NEGATIVE_AGE_DEATH_QUERY.render(
-            project_id=self.project_id,
-            dataset_id=self.dataset_id,
-            table=death,
-            person_table=person_table)
-        query[cdr_consts.DESTINATION_TABLE] = death
-        query[cdr_consts.DISPOSITION] = bq_consts.WRITE_TRUNCATE
-        query[cdr_consts.DESTINATION_DATASET] = self.dataset_id
-        expected_queries.append(query)
-
-        expected_list = [expected_sandbox_query_list, expected_queries]
-
-        self.assertEqual(result_list, expected_list)
+        self.assertIsInstance(result_list, list,
+                              'get_query_specs should return a list')
+        for query_spec in result_list:
+            self.assertTrue(
+                type(query_spec) is dict,
+                'Each item in get_query_specs should be a query spec dict')
+            self.assertTrue(cdr_consts.QUERY in query_spec.keys(),
+                            'Each query spec should specify a query')
