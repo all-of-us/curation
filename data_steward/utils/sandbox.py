833e5eeda42c05e3935dd956064a98641017eb96
jp3477 <jp3477@cumc.columbia.edu>
2021-02-09 12:27:08 -0500

    [DC-1329] create sandbox utility options function (#727)
    * generated options template
    
    * added error handling
    
    * added tests and docstrings
    
    * fixed linting error
    
    * added a test and minor refactors
    
    * added check for description non-emptiness
    
    * corrected typo
    
    * converted booleans to lowered strings

--- data_steward/utils/sandbox.py
+++ data_steward/utils/sandbox.py
@@ -1,6 +1,32 @@
 from utils.bq import list_datasets, create_dataset
+from common import JINJA_ENV
+from collections import OrderedDict
 
 SANDBOX_SUFFIX = 'sandbox'
+
+TABLE_LABELS_STRING = JINJA_ENV.from_string("""
+    {%- if labels %}
+    labels=[
+        {% for label_key, label_value in labels.items() %}
+        ("{{ label_key }}", "{{ label_value }}"){{ ", " if not loop.last}}
+        {% endfor %}
+    ]
+    {%- endif %}
+""")
+
+TABLE_DESCRIPTION_STRING = JINJA_ENV.from_string("""
+    {%- if description %}
+    description="{{ description }}"
+    {%- endif %}
+""")
+
+TABLE_OPTIONS_CLAUSE = JINJA_ENV.from_string("""
+{%- block options %}
+OPTIONS(
+{{ contents }}
+)
+{%- endblock %}
+""")
 
 
 def create_sandbox_dataset(project_id, dataset_id):
@@ -63,3 +89,85 @@ def check_and_create_sandbox_dataset(project_id, dataset_id):
     if sandbox_dataset not in datasets:
         create_sandbox_dataset(project_id, dataset_id)
     return sandbox_dataset
+
+
+def get_sandbox_labels_string(src_dataset_name,
+                              class_name,
+                              table_tag,
+                              shared_lookup=False):
+    """A helper function that formats a set of labels for BigQuery
+
+    :param str src_dataset_name: A dataset name
+    :param str class_name: A class name
+    :param str table_tag: A table tag
+    :param bool shared_lookup: A boolean that indicates if a table is a shared lookup, defaults to False
+    :raises ValueError: Raises if one of first three arguments is whitespace or None
+    :raises ValueError: Raises if the `shared_lookup` is not a boolean
+    :return: A formatted string of the arguments concatenated as labels
+    :rtype: str
+    """
+    string_labels = ['src_dataset', 'class_name', 'table_tag']
+    bool_labels = ['shared_lookup']
+
+    labels = OrderedDict([('src_dataset', src_dataset_name),
+                          ('class_name', class_name), ('table_tag', table_tag),
+                          ('shared_lookup', shared_lookup)])
+
+    for label in string_labels:
+        value = labels[label]
+        if not value or value.isspace():
+            raise ValueError(
+                f"Label '{label}' requires a non-empty string. Received the value '{value}'."
+            )
+
+    for label in bool_labels:
+        value = labels[label]
+
+        if type(value) != bool:
+            raise ValueError(
+                f"Label '{label}' requires a boolean value of True or False. Received the value '{value}'."
+            )
+
+        #Convert bools to lowered strings for BQ
+        labels[label] = str(value).lower()
+
+    return TABLE_LABELS_STRING.render(labels=labels)
+
+
+def get_sandbox_table_description_string(description):
+    """A helper function that returns a formatted description for BigQuery
+
+    :param str description: A table description
+    :return: A formatted table description
+    :rtype: str
+    """
+
+    if not description or description.isspace():
+        raise ValueError(
+            f"Description must be a non-empty string. Received the value '{description}'."
+        )
+
+    return TABLE_DESCRIPTION_STRING.render(description=description)
+
+
+def get_sandbox_options(dataset_name,
+                        class_name,
+                        table_tag,
+                        desc,
+                        shared_lookup=False):
+    """A function that assembles a BigQuery table options clause from labels and descriptions
+
+    :param str dataset_name: A dataset name
+    :param str class_name: A class name
+    :param str table_tag: A table tag
+    :param str desc: A table description
+    :param bool shared_lookup: A boolean that indicates if a table is a shared lookup, defaults to False
+    :return: A BigQuery table options clause 
+    :rtype: str
+    """
+    labels_text = get_sandbox_labels_string(dataset_name, class_name, table_tag,
+                                            shared_lookup)
+    description_text = get_sandbox_table_description_string(desc)
+
+    contents = ',\n'.join([description_text, labels_text])
+    return TABLE_OPTIONS_CLAUSE.render(contents=contents)
--- tests/unit_tests/data_steward/utils/sandbox_test.py
+++ tests/unit_tests/data_steward/utils/sandbox_test.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 from utils import sandbox
 
@@ -29,3 +30,88 @@ class SandboxTest(unittest.TestCase):
                          sandbox.get_sandbox_table_name('\t', base_name))
         self.assertEqual(base_name,
                          sandbox.get_sandbox_table_name('\n', base_name))
+
+    def test_get_sandbox_labels_string(self):
+        dataset_name = "my_dataset_name"
+        class_name = "my_class_name"
+        table_tag = "my_table_tag"
+        shared_lookup = True
+
+        #Test that standard arguments run without error
+        actual = sandbox.get_sandbox_labels_string(dataset_name,
+                                                   class_name,
+                                                   table_tag,
+                                                   shared_lookup=shared_lookup)
+        self.assertIn(f'("src_dataset", "{dataset_name}")', actual)
+        self.assertIn(f'("class_name", "{class_name}")', actual)
+        self.assertIn(f'("table_tag", "{table_tag}")', actual)
+        self.assertIn(f'("shared_lookup", "true")', actual)
+
+        #Test that an empty string argument will throw an error
+        empty_class_name = ""
+        with self.assertRaises(ValueError):
+            sandbox.get_sandbox_labels_string(dataset_name,
+                                              empty_class_name,
+                                              table_tag,
+                                              shared_lookup=shared_lookup)
+
+        #Test that a non-boolean shared_lookup argument will throw an error
+        non_bool_shared_lookup = "Yes"
+        with self.assertRaises(ValueError):
+            sandbox.get_sandbox_labels_string(
+                dataset_name,
+                empty_class_name,
+                table_tag,
+                shared_lookup=non_bool_shared_lookup)
+
+    def test_get_sandbox_table_description_string(self):
+        description = "This is a helpful description."
+
+        actual = sandbox.get_sandbox_table_description_string(description)
+        self.assertIn(f'description="{description}"', actual)
+
+        #Test description empty
+        description = "    "
+        with self.assertRaises(ValueError):
+            sandbox.get_sandbox_table_description_string(description)
+
+    @patch('utils.sandbox.get_sandbox_labels_string')
+    @patch('utils.sandbox.get_sandbox_table_description_string')
+    def test_get_sandbox_options(self,
+                                 mock_get_sandbox_table_description_string,
+                                 mock_get_sandbox_labels_string):
+
+        self.maxDiff = None
+
+        dataset_name = "my_dataset_name"
+        class_name = "my_class_name"
+        table_tag = "my_table_tag"
+        shared_lookup = True
+        description = "This is a helpful description."
+
+        labels_string = f"""
+            labels=[
+                ("src_dataset", "{dataset_name}"),
+                ("class_name", "{class_name}"),
+                ("table_tag", "{table_tag}"),
+                ("shared_lookup", "true")
+            ]
+        """
+
+        description_string = f"""
+            description="{description}"
+        """
+
+        mock_get_sandbox_labels_string.return_value = labels_string
+
+        mock_get_sandbox_table_description_string.return_value = description_string
+
+        #Test that contents are formatted as expected
+        actual_options = sandbox.get_sandbox_options(dataset_name, class_name,
+                                                     table_tag, description,
+                                                     shared_lookup)
+
+        expected_options = sandbox.TABLE_OPTIONS_CLAUSE.render(
+            contents=',\n'.join([description_string, labels_string]))
+
+        self.assertEqual(actual_options, expected_options)
