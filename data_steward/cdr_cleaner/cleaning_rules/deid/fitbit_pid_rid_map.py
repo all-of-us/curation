b064a12d8f28bc18e1a3f44a61def2bfeae89c50
Nishanth Pavinkurve <npp267@nyu.edu>
2021-02-09 11:33:20 -0500

    [DC-1333] Update tests and remove unnecessary methods

--- data_steward/cdr_cleaner/cleaning_rules/deid/fitbit_pid_rid_map.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/fitbit_pid_rid_map.py
@@ -35,37 +35,6 @@ class FitbitPIDtoRID(PIDtoRID):
                          affected_tables=FITBIT_TABLES,
                          issue_numbers=ISSUE_NUMBERS)
 
-    def get_query_specs(self):
-        """
-        Return a list of dictionary query specifications.
-
-        :return:  A list of dictionaries.  Each dictionary contains a
-            single query and a specification for how to execute that query.
-            The specifications are optional but the query is required.
-        """
-        pass
-
-    def get_sandbox_tablenames(self):
-        return []
-
-    def setup_rule(self, client):
-        """
-        Function to run any data upload options before executing a query.
-        """
-        pass
-
-    def setup_validation(self, client):
-        """
-        Run required steps for validation setup
-        """
-        raise NotImplementedError("Please fix me.")
-
-    def validate_rule(self, client):
-        """
-        Validates the cleaning rule which deletes or updates the data from the tables
-        """
-        pass
-
 
 if __name__ == '__main__':
     import cdr_cleaner.args_parser as parser
@@ -96,7 +65,7 @@ if __name__ == '__main__':
         query_list = clean_engine.get_query_list(
             ARGS.project_id,
             ARGS.dataset_id,
-            ARGS.sandbox_dataset_id, [(PIDtoRID,)],
+            ARGS.sandbox_dataset_id, [(FitbitPIDtoRID,)],
             mapping_dataset_id=ARGS.mapping_dataset_id,
             mapping_table_id=ARGS.mapping_table_id)
         for query in query_list:
@@ -105,6 +74,6 @@ if __name__ == '__main__':
         clean_engine.add_console_logging(ARGS.console_log)
         clean_engine.clean_dataset(ARGS.project_id,
                                    ARGS.dataset_id,
-                                   ARGS.sandbox_dataset_id, [(PIDtoRID,)],
+                                   ARGS.sandbox_dataset_id, [(FitbitPIDtoRID,)],
                                    mapping_dataset_id=ARGS.mapping_dataset_id,
                                    mapping_table_id=ARGS.mapping_table_id)
