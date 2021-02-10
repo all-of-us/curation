408af8ea9cc379cf5c38caaa6a91d9baba5c5948
Mark Velez <mav7014@nyp.org>
2021-02-10 16:29:56 -0500

    Reformat load_vocab_test (no functional changes)

--- tests/unit_tests/data_steward/tools/load_vocab_test.py
+++ tests/unit_tests/data_steward/tools/load_vocab_test.py
@@ -28,8 +28,10 @@ class LoadVocabTest(unittest.TestCase):
     def test_load_stage(self):
         # TODO check the calls to load_table_from_uri
         # TODO check that extra files are skipped
-        mock_list_blobs = [Blob(f'{table}.csv', self.bucket_name)
-                           for table in common.VOCABULARY_TABLES]
+        mock_list_blobs = [
+            Blob(f'{table}.csv', self.bucket_name)
+            for table in common.VOCABULARY_TABLES
+        ]
         with mock.patch.object(load_vocab.storage.Client,
                                'list_blobs',
                                return_value=mock_list_blobs):
@@ -37,17 +39,22 @@ class LoadVocabTest(unittest.TestCase):
             with mock.patch.object(load_vocab.Client,
                                    'load_table_from_uri',
                                    return_value=mock.MagicMock(spec=spec)):
-                result = load_vocab.load_stage(self.dst_dataset, self.bq_client, self.bucket_name)
+                result = load_vocab.load_stage(self.dst_dataset, self.bq_client,
+                                               self.bucket_name)
 
         # throws error when vocabulary files are missing
         expected_missing = [common.DOMAIN, common.CONCEPT_SYNONYM]
-        mock_list_blobs = [Blob(f'{table}.csv', self.bucket_name)
-                           for table in common.VOCABULARY_TABLES if table not in expected_missing]
+        mock_list_blobs = [
+            Blob(f'{table}.csv', self.bucket_name)
+            for table in common.VOCABULARY_TABLES
+            if table not in expected_missing
+        ]
         with mock.patch.object(load_vocab.storage.Client,
                                'list_blobs',
                                return_value=mock_list_blobs):
             expected_msg = f'Bucket {self.bucket_name} is missing files for tables {expected_missing}'
             with self.assertRaises(RuntimeError) as c:
-                load_vocab.load_stage(self.dst_dataset, self.bq_client, self.bucket_name)
+                load_vocab.load_stage(self.dst_dataset, self.bq_client,
+                                      self.bucket_name)
                 self.assertIsInstance(c.exception, RuntimeError)
                 self.assertEqual(str(c.exception), expected_msg)
