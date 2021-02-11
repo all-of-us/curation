ad1c827ccce336aef23437c96bb6060442d67284
dcampbell-vumc <62044448+dcampbell-vumc@users.noreply.github.com>
2021-02-11 12:33:19 -0600

    [DC-1347] Create questionnaire_response_id mapping rule (#737)
    * [DC-1347] Create Mapping Rule
    
    * created questionnaire_response_id_map module to:
        1. Create the qrid-rid mapping table if it doesn't exist in the
           shared sandbox dataset
        2. Perform the mapping of the qrid to rid in the observation table
    
    * [DC-1347] Create Mapping Rule
    
    * Added QUIDtoRID to `CONTROLLED_TIER_DEID_CLEANING_CLASSES`
    
    * [DC-1347] Create Mapping Rule
    
    * Modified affected_datasets
    
    * [DC-1347] Create Mapping Rule
    
    * Unit test to test questionnaire_response_id_map.py module
    
    * [DC-1347] Create Mapping Rule
    
    * Added clarifying comment
    
    * [DC-1347] Create Mapping Rule
    
    * Modified `LOOKUP_TABLE_CREATION_QUERY_` to start at 1m when generating
      the research_response_id
    
    * [DC-1347] Create Mapping Rule
    
    * Added clarifying comments to the `LOOKUP_TABLE_CREATION_QUERY`

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -69,6 +69,7 @@ from cdr_cleaner.cleaning_rules.update_fields_numbers_as_strings import UpdateFi
 from cdr_cleaner.cleaning_rules.temporal_consistency import TemporalConsistency
 from cdr_cleaner.cleaning_rules.valid_death_dates import ValidDeathDates
 from cdr_cleaner.cleaning_rules.negative_ages import NegativeAges
+from cdr_cleaner.cleaning_rules.deid.questionnaire_response_id_map import QRIDtoRID
 from cdr_cleaner.cleaning_rules.null_person_birthdate import NullPersonBirthdate
 from constants.cdr_cleaner import clean_cdr_engine as ce_consts
 from constants.cdr_cleaner.clean_cdr import DataStage
@@ -193,7 +194,10 @@ DEID_CLEAN_CLEANING_CLASSES = [
     (CleanMappingExtTables,)
 ]
 
-CONTROLLED_TIER_DEID_CLEANING_CLASSES = [(NullPersonBirthdate,)]
+CONTROLLED_TIER_DEID_CLEANING_CLASSES = [
+    (QRIDtoRID,),  # Should run before any row suppression rules
+    (NullPersonBirthdate,)
+]
 
 CONTROLLED_TIER_DEID_BASE_CLEANING_CLASSES = []
 
