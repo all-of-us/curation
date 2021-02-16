13dda085256aeef14f3ce9d7d1385e35296e76f5
jp3477 <jp3477@cumc.columbia.edu>
2021-02-16 15:58:15 -0500

    added generalize zip code rule to clean_cdr

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -71,6 +71,7 @@ from cdr_cleaner.cleaning_rules.valid_death_dates import ValidDeathDates
 from cdr_cleaner.cleaning_rules.negative_ages import NegativeAges
 from cdr_cleaner.cleaning_rules.deid.questionnaire_response_id_map import QRIDtoRID
 from cdr_cleaner.cleaning_rules.null_person_birthdate import NullPersonBirthdate
+from cdr_cleaner.cleaning_rules.generalize_zip_codes import GeneralizeZipCodes
 from constants.cdr_cleaner import clean_cdr_engine as ce_consts
 from constants.cdr_cleaner.clean_cdr import DataStage
 
@@ -196,7 +197,8 @@ DEID_CLEAN_CLEANING_CLASSES = [
 
 CONTROLLED_TIER_DEID_CLEANING_CLASSES = [
     (QRIDtoRID,),  # Should run before any row suppression rules
-    (NullPersonBirthdate,)
+    (NullPersonBirthdate,),
+    (GeneralizeZipCodes,)
 ]
 
 CONTROLLED_TIER_DEID_BASE_CLEANING_CLASSES = []
