93ae6e25a37560919c83c2dedfb358f2e0e3f941
dcampbell-vumc <danyel.campbell@vumc.org>
2021-03-04 08:59:59 -0600

    [DC-1445] Enforce Data Cutoff for EHR Submissions
    * Finished testing of `test_ehr_submission_data_cutoff` function
    * Removed unused import statements
    * Added mocking of `get_affected_tables` function since only testing on
      one OMOP table (visit_occurrence) not all
    * Removed testing of person table

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/ehr_submission_data_cutoff_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/ehr_submission_data_cutoff_test.py
@@ -9,17 +9,14 @@ Intent of this integration test is to ensure that the data cutoff for the PPI da
 
 # Python imports
 import os
+from mock import patch
 from dateutil.parser import parse
 
 # Project imports
 import common
 from app_identity import PROJECT_ID
-from constants import bq_utils as bq_consts
-from constants.cdr_cleaner import clean_cdr as cdr_consts
 from cdr_cleaner.cleaning_rules.ehr_submission_data_cutoff import EhrSubmissionDataCutoff
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
-
-tables = ['visit_occurrence', 'person']
 
 
 class EhrSubmissionDataCutoffTest(BaseTest.CleaningRulesTestBase):
@@ -49,12 +46,12 @@ class EhrSubmissionDataCutoffTest(BaseTest.CleaningRulesTestBase):
         cls.rule_instance = EhrSubmissionDataCutoff(project_id, dataset_id,
                                                     sandbox_id)
 
-        for table_name in tables:
+        for table_name in common.VISIT_OCCURRENCE:
             sandbox_table_name = cls.rule_instance.sandbox_table_for(table_name)
             cls.fq_sandbox_table_names.append(
                 f'{cls.project_id}.{cls.sandbox_id}.{sandbox_table_name}')
-            cls.fq_table_names.append(
-                f'{cls.project_id}.{dataset_id}.{table_name}')
+
+        cls.fq_table_names.append(f'{cls.project_id}.{cls.dataset_id}.{common.VISIT_OCCURRENCE}')
 
         # call super to set up the client, create datasets, and create
         # empty test tables
@@ -77,14 +74,17 @@ class EhrSubmissionDataCutoffTest(BaseTest.CleaningRulesTestBase):
 
         super().setUp()
 
-    def test_ehr_submission_data_cutoff(self):
+    @patch.object(EhrSubmissionDataCutoff, 'get_affected_tables')
+    def test_ehr_submission_data_cutoff(self, mock_get_affected_tables):
         """
         Validates pre conditions, tests execution, and post conditions based on the load
         statements and the tables_and_counts variable.
         """
+        # mocks the return value of get_affected_tables as we only want to loop through the
+        # visit_occurrence not all of the CDM tables
+        mock_get_affected_tables.return_value = [common.VISIT_OCCURRENCE]
 
         queries = []
-
         visit_occurrence_tmpl = self.jinja_env.from_string("""
             INSERT INTO `{{fq_dataset_name}}.{{cdm_table}}`
             (visit_occurrence_id, person_id, visit_concept_id, visit_start_date, 
@@ -104,22 +104,6 @@ class EhrSubmissionDataCutoffTest(BaseTest.CleaningRulesTestBase):
                         cdm_table=common.VISIT_OCCURRENCE)
         queries.append(visit_occurrence_tmpl)
 
-        # Because the perosn table is not being cleaned in this cleaning rule, all of these values
-        # will be returned, nothing will be sandboxed and dropped
-        person_tmpl = self.jinja_env.from_string("""
-        INSERT INTO `{{fq_dataset_name}}.{{cdm_table}}`
-        (person_id, gender_concept_id, year_of_birth, 
-            birth_datetime, race_concept_id, ethnicity_concept_id)
-        VALUES
-        (111, 2, 2018, timestamp('2018-03-06 11:00:00'), 3, 4),
-        (222, 2, 2019, timestamp('2019-03-06 11:00:00'), 3, 4),
-        (333, 2, 2020, timestamp('2020-03-06 11:00:00'), 3, 4),
-        (444, 2, 2021, timestamp('2021-03-06 11:00:00'), 3, 4),
-        (555, 2, 2022, timestamp('2022-03-06 11:00:00'), 3, 4)
-        """).render(fq_dataset_name=self.fq_dataset_name,
-                    cdm_table=common.PERSON)
-        queries.append(person_tmpl)
-
         self.load_test_data(queries)
 
         table_and_counts = [{
@@ -130,34 +114,18 @@ class EhrSubmissionDataCutoffTest(BaseTest.CleaningRulesTestBase):
             'loaded_ids': [111, 222, 333, 444, 555],
             'sandboxed_ids': [111, 222, 333],
             'fields': [
-                'visit_occurrence_id', 'person_id', 'visit_occurrence_id',
+                'visit_occurrence_id', 'person_id', 'visit_concept_id',
                 'visit_start_date', 'visit_start_datetime', 'visit_end_date',
                 'visit_end_datetime', 'visit_type_concept_id'
             ],
             'cleaned_values': [
-                (444, 555, parse('2021-03-06').date(),
-                 parse('2021-03-06 11:00:00'), parse('2021-03-07').date(),
-                 parse('2021-03-07 11:00:00'), 4),
+                (444, 555, 3, parse('2021-03-06').date(),
+                 parse('2021-03-06 11:00:00 UTC'), parse('2021-03-07').date(),
+                 parse('2021-03-07 11:00:00 UTC'), 4),
                 (555, 666, 3, parse('2022-03-06').date(),
-                 parse('2022-03-06 11:00:00'), parse('2022-03-07').date,
-                 parse('2022-03-07 11:00:00'), 4)
-            ]
-        }, {
-            'fq_table_name':
-                '.'.join([self.fq_dataset_name, 'person']),
-            'fq_sandbox_table_name':
-                f'{self.fq_sandbox_name}.{self.rule_instance.sandbox_table_for(common.PERSON)}',
-            'loaded_ids': [111, 222, 333, 444, 555],
-            'sandboxed_ids': [],
-            'fields': [
-                'person_id', 'gender_concept_id', 'year_of_birth',
-                'birth_datetime', 'race_concept_id', 'ethnicity_concept_id'
-            ],
-            'cleaned_values': [
-                (111, 2, 2018, parse('2018-03-06 11:00:00'), 3, 4),
-                (222, 2, 2019, parse('2019-03-06 11:00:00'), 3, 4),
-                (333, 2, 2020, parse('2020-03-06 11:00:00'), 3, 4),
-                (444, 2, 2021, parse('2021-03-06 11:00:00'), 3, 4),
-                (555, 2, 2022, parse('2022-03-06 11:00:00'), 3, 4)
+                 parse('2022-03-06 11:00:00 UTC'), parse('2022-03-07').date(),
+                 parse('2022-03-07 11:00:00 UTC'), 4)
             ]
         }]
+
+        self.default_test(table_and_counts)
