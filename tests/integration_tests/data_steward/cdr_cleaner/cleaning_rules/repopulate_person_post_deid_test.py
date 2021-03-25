e679fb9df779cea8cab712bdc8aceb23b7ea8fe4
jp3477 <jp3477@cumc.columbia.edu>
2021-03-25 09:34:08 -0700

    [DC-1198] Integration Test for Missing Race Responses CR (#806)
    * modified integration test to new base class format
    
    * added comments
    
    * removed unnecessary line

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/repopulate_person_post_deid_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/repopulate_person_post_deid_test.py
@@ -1,56 +1,52 @@
+"""
+Integration test for repopulate_person_post_deid module
+
+The de-id scripts removes all fields in the person table except for the person_id and the birthdate_time field.
+Before CDR handoff to the Workbench team, we need to repopulate the following fields with demographic information
+from the observation table.
+
+These are the following fields in the person table will be repopulated:
+
+gender_concept_id
+year_of_birth
+month_of_birth
+day_of_birth
+race_concept_id
+ethnicity_concept_id
+gender_source_value
+gender_source_concept_id
+race_source_value
+race_source_concept_id
+ethnicity_source_value
+ethnicity_source_concept_id
+sex_at_birth_concept_id (extension)
+sex_at_birth_source_concept_id (extension)
+sex_at_birth_source_value (extension)
+
+Original Issue: DC-516
+"""
+
 # Python imports
-import unittest
-
-from googleapiclient.errors import HttpError
-
-# Project imports
-import bq_utils
-import common
-from cdr_cleaner import clean_cdr_engine
-from cdr_cleaner.cleaning_rules.repopulate_person_post_deid import (
-    RepopulatePersonPostDeid, GENDER_CONCEPT_ID, SEX_AT_BIRTH_CONCEPT_ID)
-from tests import test_util
+from datetime import date
+import os
 
 # Third party imports
+from dateutil import parser
 
-# Participant 1: has gender and sex at birth observations
-# Participant 2: no gender or sex at birth observations
-INSERT_FAKE_PARTICIPANTS_TMPLS = [
-    # TODO(calbach): Ideally these tests should not manipulate concept table, not currently hermetic.
-    common.JINJA_ENV.from_string("""
-    INSERT INTO `{{project_id}}.{{dataset_id}}.concept` (concept_id, concept_code)
-VALUES
-  ({{gender_concept_id}}, "gender"),
-  ({{gender_nonbinary_concept_id}}, "nonbinary"),
-  ({{gender_nonbinary_source_concept_id}}, "nonbinary_src"),
-  ({{sex_at_birth_concept_id}}, "sex"),
-  ({{sex_female_concept_id}}, "female"),
-  ({{sex_female_source_concept_id}}, "female_src")
-"""),
-    common.JINJA_ENV.from_string("""
-    INSERT INTO `{{project_id}}.{{dataset_id}}.person` (person_id)
-VALUES (1), (2)
-"""),
-    common.JINJA_ENV.from_string("""
-    INSERT INTO `{{project_id}}.{{dataset_id}}.observation` (person_id, observation_id, observation_source_concept_id, value_as_concept_id, value_source_concept_id)
-VALUES
-  (1, 100, {{gender_concept_id}}, {{gender_nonbinary_concept_id}}, {{gender_nonbinary_source_concept_id}}),
-  (1, 101, {{sex_at_birth_concept_id}}, {{sex_female_concept_id}}, {{sex_female_source_concept_id}})
-""")
-]
+# Project imports
+from app_identity import PROJECT_ID
+from cdr_cleaner.cleaning_rules.repopulate_person_post_deid import (
+    RepopulatePersonPostDeid, GENDER_CONCEPT_ID, SEX_AT_BIRTH_CONCEPT_ID)
+from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
+from common import PERSON, OBSERVATION, CONCEPT
+
+GENDER_NONBINARY_CONCEPT_ID = 1585841
+GENDER_NONBINARY_SOURCE_CONCEPT_ID = 123
+SEX_FEMALE_CONCEPT_ID = 1585847
+SEX_FEMALE_SOURCE_CONCEPT_ID = 45878463
 
 
-def drop_concept_table(dataset_id):
-    if bq_utils.table_exists(common.CONCEPT):
-        q = "DROP TABLE {dataset}.concept;".format(dataset=dataset_id)
-        try:
-            bq_utils.query(q)
-        except HttpError as err:
-            if err.resp.status != 404:
-                raise
-
-
-class RepopulatePersonPostDeidTest(unittest.TestCase):
+class RepopulatePersonPostDeidTest(BaseTest.CleaningRulesTestBase):
 
     @classmethod
     def setUpClass(cls):
@@ -58,104 +54,119 @@ class RepopulatePersonPostDeidTest(unittest.TestCase):
         print(cls.__name__)
         print('**************************************************************')
 
+        super().initialize_class_vars()
+
+        # set the test project identifier
+        project_id = os.environ.get(PROJECT_ID)
+        cls.project_id = project_id
+
+        # set the expected test datasets
+        dataset_id = os.environ.get('COMBINED_DATASET_ID')
+        cls.dataset_id = dataset_id
+        sandbox_id = dataset_id + '_sandbox'
+        cls.sandbox_id = sandbox_id
+
+        cls.rule_instance = RepopulatePersonPostDeid(project_id, dataset_id,
+                                                     sandbox_id)
+
+        table_names = [PERSON, OBSERVATION, CONCEPT]
+
+        cls.fq_table_names = [
+            f'{project_id}.{dataset_id}.{table_name}'
+            for table_name in table_names
+        ]
+
+        cls.fq_sandbox_table_names += [
+            f'{cls.project_id}.{cls.sandbox_id}.{cls.rule_instance.sandbox_table_for(table_name)}'
+            for table_name in table_names
+        ]
+
+        # call super to set up the client, create datasets, and create
+        # empty test tables
+        # NOTE:  does not create empty sandbox tables.
+        super().setUpClass()
+
     def setUp(self):
-        self.project_id = bq_utils.app_identity.get_application_id()
-        self.dataset_id = bq_utils.get_combined_dataset_id()
-        self.sandbox_dataset_id = bq_utils.get_unioned_dataset_id()
-        if not self.project_id or not self.dataset_id:
-            # TODO: Fix handling of globals, push these assertions down if they are required.
-            raise ValueError(
-                f"missing configuration for project ('{self.project_id}') " +
-                f"and/or dataset ('{self.dataset_id}')")
+        fq_dataset_name = self.fq_table_names[0].split('.')
+        self.fq_dataset_name = '.'.join(fq_dataset_name[:-1])
 
-        # TODO: Reconcile this with a consistent integration testing model. Ideally each test should
-        # clean up after itself so that we don't need this defensive check.
-        test_util.delete_all_tables(self.dataset_id)
+        self.date = parser.parse('2020-05-05').date()
 
-        # drop concept table
-        drop_concept_table(self.dataset_id)
+        super().setUp()
 
-        create_tables = ['person', 'observation']
-        table_fields = {
-            'person': 'post_deid_person',
-            'observation': 'observation',
-            'concept': 'concept'
-        }
-        for tbl in ['concept']:
-            if not bq_utils.table_exists(tbl, dataset_id=self.dataset_id):
-                create_tables.append(tbl)
-        for tbl in create_tables:
-            bq_utils.create_standard_table(table_fields[tbl],
-                                           tbl,
-                                           dataset_id=self.dataset_id,
-                                           force_all_nullable=True)
+    def test_repopulate_person_post_deid(self):
+        """
+        Tests that the specifications for queries perform as designed.
 
-    def tearDown(self):
-        test_util.delete_all_tables(self.dataset_id)
-        # Delete concept table
-        drop_concept_table(self.dataset_id)
+        Validates pre conditions, tests execution, and post conditions based on the load
+        statements and the tables_and_counts variable.        
+        """
 
-        # re create concept table
-        q = """CREATE or REPLACE table `{project}.{dataset}.concept`  as (
-            SELECT * FROM `{project}.{vocab}.concept`)""".format(
-            project=self.project_id,
-            dataset=self.dataset_id,
-            vocab=common.VOCABULARY_DATASET)
-        bq_utils.query(q)
+        insert_concepts_query = self.jinja_env.from_string("""
+            INSERT INTO `{{fq_dataset_name}}.concept` (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, concept_code, valid_start_date, valid_end_date)
+            VALUES
+                ({{gender_concept_id}}, "some text", "some text", "some text", "some text", "gender", date('2020-05-05'), date('2020-05-05')),
+                ({{gender_nonbinary_concept_id}}, "some text", "some text", "some text", "some text", "nonbinary", date('2020-05-05'), date('2020-05-05')),
+                ({{gender_nonbinary_source_concept_id}}, "some text", "some text", "some text", "some text", "nonbinary_src", date('2020-05-05'), date('2020-05-05')),
+                ({{sex_at_birth_concept_id}}, "some text", "some text", "some text", "some text", "sex", date('2020-05-05'), date('2020-05-05')),
+                ({{sex_female_concept_id}}, "some text", "some text", "some text", "some text", "female", date('2020-05-05'), date('2020-05-05')),
+                ({{sex_female_source_concept_id}}, "some text", "some text", "some text", "some text", "female_src", date('2020-05-05'), date('2020-05-05'))
+        """).render(fq_dataset_name=self.fq_dataset_name,
+                    gender_concept_id=GENDER_CONCEPT_ID,
+                    gender_nonbinary_concept_id=GENDER_NONBINARY_CONCEPT_ID,
+                    gender_nonbinary_source_concept_id=
+                    GENDER_NONBINARY_SOURCE_CONCEPT_ID,
+                    sex_at_birth_concept_id=SEX_AT_BIRTH_CONCEPT_ID,
+                    sex_female_concept_id=SEX_FEMALE_CONCEPT_ID,
+                    sex_female_source_concept_id=SEX_FEMALE_SOURCE_CONCEPT_ID)
 
-    def assertPersonFields(self, person, want):
-        for k in want.keys():
-            self.assertIn(k, person)
-            self.assertEqual(person[k], want[k])
+        create_persons_query = self.jinja_env.from_string("""
+            INSERT INTO `{{fq_dataset_name}}.person` (person_id, gender_concept_id, birth_datetime, year_of_birth, race_concept_id, ethnicity_concept_id, gender_source_value, gender_source_concept_id)
+            VALUES
+                (1, 1, timestamp('1991-05-05'), 1991, 1, 1, "some text", 1),
+                (2, 2, timestamp('1976-05-05'), 1976, 2, 2, "some text", 2)
+        """).render(fq_dataset_name=self.fq_dataset_name)
 
-    def test_execute_queries(self):
-        gender_nonbinary_concept_id = 1585841
-        gender_nonbinary_source_concept_id = 123
-        sex_female_concept_id = 1585847
-        sex_female_source_concept_id = 45878463
-        for tmpl in INSERT_FAKE_PARTICIPANTS_TMPLS:
-            query = tmpl.render(
-                project_id=self.project_id,
-                dataset_id=self.dataset_id,
-                gender_concept_id=GENDER_CONCEPT_ID,
-                gender_nonbinary_concept_id=gender_nonbinary_concept_id,
-                gender_nonbinary_source_concept_id=
-                gender_nonbinary_source_concept_id,
-                sex_at_birth_concept_id=SEX_AT_BIRTH_CONCEPT_ID,
-                sex_female_concept_id=sex_female_concept_id,
-                sex_female_source_concept_id=sex_female_source_concept_id)
-            try:
-                resp = bq_utils.query(query)
-            except HttpError as e:
-                self.fail("failed to execute query '{}': {}".format(
-                    query, e.content))
-            self.assertTrue(resp["jobComplete"])
+        create_observations_query = self.jinja_env.from_string("""
+            INSERT INTO `{{fq_dataset_name}}.observation` (person_id, observation_id, observation_source_concept_id, value_as_concept_id, value_source_concept_id,
+                observation_date, observation_type_concept_id, observation_concept_id)
+            VALUES
+                (1, 100, {{gender_concept_id}}, {{gender_nonbinary_concept_id}}, {{gender_nonbinary_source_concept_id}}, date('2020-05-05'), 1, 1),
+                (1, 101, {{sex_at_birth_concept_id}}, {{sex_female_concept_id}}, {{sex_female_source_concept_id}}, date('2020-05-05'), 2, 2)
+        """).render(fq_dataset_name=self.fq_dataset_name,
+                    gender_concept_id=GENDER_CONCEPT_ID,
+                    gender_nonbinary_concept_id=GENDER_NONBINARY_CONCEPT_ID,
+                    gender_nonbinary_source_concept_id=
+                    GENDER_NONBINARY_SOURCE_CONCEPT_ID,
+                    sex_at_birth_concept_id=SEX_AT_BIRTH_CONCEPT_ID,
+                    sex_female_concept_id=SEX_FEMALE_CONCEPT_ID,
+                    sex_female_source_concept_id=SEX_FEMALE_SOURCE_CONCEPT_ID)
 
-        clean_cdr_engine.clean_dataset(self.project_id, self.dataset_id,
-                                       self.sandbox_dataset_id,
-                                       [(RepopulatePersonPostDeid,)])
+        queries = [
+            insert_concepts_query, create_persons_query,
+            create_observations_query
+        ]
+        self.load_test_data(queries)
 
-        rows = bq_utils.response2rows(
-            bq_utils.query("SELECT * FROM `{}.{}.person`".format(
-                self.project_id, self.dataset_id)))
-        self.assertEquals(len(rows), 2)
+        tables_and_counts = [{
+            'fq_table_name':
+                '.'.join([self.fq_dataset_name, 'person']),
+            'fq_sandbox_table_name':
+                '',
+            'loaded_ids': [1, 2],
+            'sandboxed_ids': [],
+            'fields': [
+                'person_id', 'gender_concept_id', 'year_of_birth',
+                'race_concept_id', 'ethnicity_concept_id',
+                'gender_source_value', 'gender_source_concept_id',
+                'sex_at_birth_concept_id', 'sex_at_birth_source_concept_id',
+                'sex_at_birth_source_value'
+            ],
+            'cleaned_values': [(1, 1585841, 1991, 0, 38003564, "nonbinary_src",
+                                123, SEX_FEMALE_CONCEPT_ID,
+                                SEX_FEMALE_SOURCE_CONCEPT_ID, "female_src"),
+                               (2, 0, 1976, 0, 38003564, "No matching concept",
+                                0, 0, 0, "No matching concept")]
+        }]
 
-        by_participant = {r["person_id"]: r for r in rows}
-        self.assertPersonFields(
-            by_participant[1], {
-                "gender_concept_id": gender_nonbinary_concept_id,
-                "gender_source_value": "nonbinary_src",
-                "gender_source_concept_id": gender_nonbinary_source_concept_id,
-                "sex_at_birth_concept_id": sex_female_concept_id,
-                "sex_at_birth_source_value": "female_src",
-                "sex_at_birth_source_concept_id": sex_female_source_concept_id
-            })
-        self.assertPersonFields(
-            by_participant[2], {
-                "gender_concept_id": 0,
-                "gender_source_value": "No matching concept",
-                "gender_source_concept_id": 0,
-                "sex_at_birth_concept_id": 0,
-                "sex_at_birth_source_value": "No matching concept",
-                "sex_at_birth_source_concept_id": 0
-            })
+        self.default_test(tables_and_counts)
