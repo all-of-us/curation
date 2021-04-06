1f094525efed0666946a5563831a4a5912b3a113
Chao Pang <ChaoPang229@gmail.com>
2021-04-06 09:38:00 -0400

    [DC-1050] [P2] Domain Alignment Cleaning Rule needs to update Mapping tables when records are moved to another table (#788)
    * added reroute mapping records to domain_alignment
    * modified the integration test to add the cases for rerouting mapping records

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/domain_alignment_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/domain_alignment_test.py
@@ -13,6 +13,7 @@ from google.cloud.bigquery import Table
 from common import VOCABULARY_TABLES
 from cdr_cleaner.cleaning_rules.domain_mapping import DOMAIN_TABLE_NAMES
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
+from tools.combine_ehr_rdr import mapping_table_for
 from utils import bq
 from app_identity import PROJECT_ID
 from cdr_cleaner.cleaning_rules.domain_alignment import domain_alignment, \
@@ -85,6 +86,9 @@ class DomainAlignmentTest(BaseTest.CleaningRulesTestBase):
 
         # Generates list of fully qualified mapping table names
         for table_name in DOMAIN_TABLE_NAMES:
+            cls.fq_table_names.append(
+                f'{cls.project_id}.{cls.dataset_id}.{mapping_table_for(table_name)}'
+            )
             sandbox_table = f'{cls.project_id}.{cls.sandbox_id}.{sandbox_name_for(table_name)}'
             cls.fq_sandbox_table_names.append(sandbox_table)
 
@@ -134,6 +138,19 @@ class DomainAlignmentTest(BaseTest.CleaningRulesTestBase):
                         (104, 5, 3009160, '2015-07-15', TIMESTAMP '2015-07-15T00:00:00', 42894222, 4)
                     """)
 
+        mapping_condition_occurrence_data_template = self.jinja_env.from_string(
+            """
+                    INSERT INTO `{{project_id}}.{{dataset_id}}._mapping_condition_occurrence`
+                    (condition_occurrence_id, src_dataset_id, src_condition_occurrence_id, 
+                     src_hpo_id, src_table_id)
+                    VALUES
+                        (100, '{{dataset_id}}', 1, 'hpo_1', 'condition_occurrence'),
+                        (101, '{{dataset_id}}', 2, 'hpo_2', 'condition_occurrence'),
+                        (102, '{{dataset_id}}', 3, 'hpo_3', 'condition_occurrence'),
+                        (103, '{{dataset_id}}', 4, 'hpo_4', 'condition_occurrence'),
+                        (104, '{{dataset_id}}', 5, 'hpo_5', 'condition_occurrence')
+                    """)
+
         procedure_occurrence_tmpl = self.jinja_env.from_string("""
                     INSERT INTO `{{project_id}}.{{dataset_id}}.procedure_occurrence`
                     (procedure_occurrence_id, person_id, procedure_concept_id, procedure_date, 
@@ -143,17 +160,34 @@ class DomainAlignmentTest(BaseTest.CleaningRulesTestBase):
                         (200, 5, 36676219, '2015-07-15', TIMESTAMP '2015-07-15T00:00:00', 42865906, 5),
                         (201, 6, 320128, '2015-08-15', TIMESTAMP '2015-08-15T00:00:00', 42894222, 6)
                         """)
+        mapping_procedure_occurrence_data_template = self.jinja_env.from_string(
+            """
+                    INSERT INTO `{{project_id}}.{{dataset_id}}._mapping_procedure_occurrence`
+                    (procedure_occurrence_id, src_dataset_id, src_procedure_occurrence_id, 
+                     src_hpo_id, src_table_id)
+                    VALUES
+                        (200, '{{dataset_id}}', 10, 'hpo_1', 'procedure_occurrence'),
+                        (201, '{{dataset_id}}', 20, 'hpo_2', 'procedure_occurrence')
+                    """)
 
         insert_condition_query = condition_occurrence_data_template.render(
+            project_id=self.project_id, dataset_id=self.dataset_id)
+
+        insert_condition_mapping_query = mapping_condition_occurrence_data_template.render(
             project_id=self.project_id, dataset_id=self.dataset_id)
 
         insert_procedure_query = procedure_occurrence_tmpl.render(
             project_id=self.project_id, dataset_id=self.dataset_id)
 
+        insert_procedure_mapping_query = mapping_procedure_occurrence_data_template.render(
+            project_id=self.project_id, dataset_id=self.dataset_id)
+
         # Load test data
         self.load_test_data([
             f'''{insert_condition_query};
-                {insert_procedure_query};'''
+                {insert_condition_mapping_query};
+                {insert_procedure_query};
+                {insert_procedure_mapping_query};'''
         ])
 
     def test_domain_alignment(self):
@@ -198,5 +232,34 @@ class DomainAlignmentTest(BaseTest.CleaningRulesTestBase):
                                (202, 2, 36676219, date(2015, 7, 15),
                                 parse('2015-07-15 00:00:00 UTC'), None, 2)]
         }]
+
+        mapping_tables_and_counts = [{
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}._mapping_condition_occurrence',
+            'loaded_ids': [100, 101, 102, 103, 104],
+            'fields': [
+                'condition_occurrence_id', 'src_dataset_id',
+                'src_condition_occurrence_id', 'src_hpo_id', 'src_table_id'
+            ],
+            'cleaned_values': [
+                (100, self.dataset_id, 1, 'hpo_1', 'condition_occurrence'),
+                (102, self.dataset_id, 3, 'hpo_3', 'condition_occurrence'),
+                (103, self.dataset_id, 4, 'hpo_4', 'condition_occurrence'),
+                (105, self.dataset_id, 20, 'hpo_2', 'procedure_occurrence')
+            ]
+        }, {
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}._mapping_procedure_occurrence',
+            'loaded_ids': [200, 201],
+            'fields': [
+                'procedure_occurrence_id', 'src_dataset_id',
+                'src_procedure_occurrence_id', 'src_hpo_id', 'src_table_id'
+            ],
+            'cleaned_values': [
+                (200, self.dataset_id, 10, 'hpo_1', 'procedure_occurrence'),
+                (202, self.dataset_id, 2, 'hpo_2', 'condition_occurrence')
+            ]
+        }]
+
         # Apply the cleaning rule to the dataset
-        self.default_test(tables_and_counts)
+        self.default_test(tables_and_counts + mapping_tables_and_counts)
