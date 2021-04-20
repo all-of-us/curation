f152448b7f50a32b8df12f3583105f91dedbb076
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2021-04-20 16:06:42 -0500

    Reorder columns in detailed output

--- data_steward/analytics/cdr_ops/controlled_tier_qc/code/controlled_tier_qc.py
+++ data_steward/analytics/cdr_ops/controlled_tier_qc/code/controlled_tier_qc.py
@@ -61,8 +61,16 @@ def display_check_summary_by_rule(checks_df):
         by_rule['n_row_violation'] = 0
         by_rule['note'] = 'NOT RUN'
     by_rule['n_row_violation'] = by_rule['n_row_violation'].fillna(0).astype(int)
+    col_order = [col for col in check_description] + ['n_row_violation', 'note']
+    by_rule = by_rule[col_order]
     return by_rule.style.apply(highlight, axis=1)
 
     
 def display_check_detail_of_rule(checks_df, rule):
-    return pretty_print(checks_df[checks_df['rule'] == rule].dropna(axis=1, how='all')) if rule in checks_df['rule'].values else 'Nothing to report or not run'
+    col_orders = ['table_name', 'column_name', 'concept_id', 'concept_code', 'n_row_violation', 'query']
+    if rule in checks_df['rule'].values:
+        to_print_df = checks_df[checks_df['rule'] == rule].dropna(axis=1, how='all')
+        columns = [col for col in col_orders if col in to_print_df]
+        return pretty_print(to_print_df[columns])
+    else:
+        return 'Nothing to report or not run'
