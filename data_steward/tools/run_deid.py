95e73970521eb6d3e4493880792b7e131086f699
amcgrenera-vumc <56938070+amcgrenera-vumc@users.noreply.github.com>
2021-05-11 13:07:01 -0500

    [DC-1530] specifically suppress provider table (#871)

--- data_steward/tools/run_deid.py
+++ data_steward/tools/run_deid.py
@@ -26,11 +26,12 @@ LOGGER = logging.getLogger(__name__)
 DEID_TABLES = [
     'person', 'observation', 'visit_occurrence', 'condition_occurrence',
     'drug_exposure', 'procedure_occurrence', 'device_exposure', 'death',
-    'measurement', 'location', 'care_site', 'specimen', 'observation_period'
+    'measurement', 'location', 'care_site', 'specimen', 'observation_period',
+    'provider'
 ]
 # these tables will be suppressed.  This means an empty table with the same schema will
 # exist.  It overrides the DEID_TABLES list
-SUPPRESSED_TABLES = ['note', 'note_nlp', 'location', 'care_site']
+SUPPRESSED_TABLES = ['note', 'note_nlp', 'location', 'care_site', 'provider']
 VOCABULARY_TABLES = [
     'concept', 'vocabulary', 'domain', 'concept_class', 'concept_relationship',
     'relationship', 'concept_synonym', 'concept_ancestor',
