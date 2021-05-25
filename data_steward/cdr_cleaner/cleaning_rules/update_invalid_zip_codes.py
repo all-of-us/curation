852d898363647c149635d0d2e2626567b81fb916
dcampbell-vumc <danyel.campbell@vumc.org>
2021-05-25 10:14:57 -0500

    [DC-1633] Update Invalid Zip Entries
    * Modified SANDBOX_INVALID_ZIP_CODES to check for more invalid zip code
      conditions
    * Created new query SANDBOX_ZIPS_WITH_WHITESPACE to sandbox any zip
      codes with leading/trailing whitespace
    * Created CLEAN_ZIPS_OF_WHITESPACE to clean any zips with
      leading/trailing whitespace in observation table before
      SANDBOX_INVALID_ZIP_CODES query is evaluated
    * Modified `get_query_specs` to render new queries

--- data_steward/cdr_cleaner/cleaning_rules/update_invalid_zip_codes.py
+++ data_steward/cdr_cleaner/cleaning_rules/update_invalid_zip_codes.py
@@ -3,9 +3,12 @@ Sandbox and update invalid zip codes found in the observation table.
 
 Original Issues: DC-1633, DC-1645
 
-The intent of this cleaning rule is to sandbox and update any invalid zip code in the observation table. A zip code is
-    considered invalid if it does not match a zip3 code in the zip3 master lookup table or if it less than 5 digits.
-    The record is sandboxed and updated to have the following information:
+The intent of this cleaning rule is to remove any leading/trailing whitespace in the zip code string then sandbox and
+update any invalid zip code in the observation table. A zip code is considered invalid if it:
+        Is less than 5 digits in length
+        Is alpha-numeric
+        Does not match any zip3 code in the master zip3 lookup table
+If zip code is deemed invalid the record is sandboxed and updated to have the following information:
         value_as_string and value_source_value = 'Response removed due to invalid value'
         value_as_number = 0
         value_source_concept_id = 2000000010
@@ -22,26 +25,41 @@ from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 
 LOGGER = logging.getLogger(__name__)
 
+ZIPS_WITH_WHITESPACE_SANDBOX = 'dc1633_zips_with_whitespace'
+
+# Creates sandbox that contains any zips that have leading/trailing whitespace
+SANDBOX_ZIPS_WITH_WHITESPACE = JINJA_ENV.from_string("""
+CREATE OR REPLACE TABLE `{{project_id}}.{{sandbox_dataset_id}}.{{whitespace_sandbox}}` AS
+SELECT * FROM `{{project_id}}.{{dataset_id}}.{{obs_table}}`
+WHERE observation_source_concept_id = 1585250 
+AND REGEXP_CONTAINS(value_as_string, ' ')
+""")
+
 # Creates sandbox that contains all invalid zip codes which are deemed invalid because they:
 # 1. Are not 5 digits in length
-# 2. Do not match a zip3 code in the master zip3 lookup table
+# 2. Are are alpha-numeric
+# 3. Do not match a zip3 code in the master zip3 lookup table
 SANDBOX_INVALID_ZIP_CODES = JINJA_ENV.from_string("""
 CREATE OR REPLACE TABLE `{{project_id}}.{{sandbox_dataset_id}}.{{sandbox_table}}` AS (
--- Only selects columns from one observation table --
-SELECT L.* FROM (
--- Selects all zips that are less than 5 digits in length --
+-- Selects all zips that are less than 5 digits in length and/or alpha-numeric --
 SELECT * FROM `{{project_id}}.{{dataset_id}}.{{obs_table}}`
-WHERE observation_source_concept_id = 1585250 AND LENGTH(value_as_string) < 5) AS L
--- Using left join in order not to get duplicate records for zips that are both less than 5 digits in length --
--- and do not match a zip in the master zip3 lookup table --
-LEFT JOIN (
+WHERE observation_source_concept_id = 1585250 
+AND (LENGTH(value_as_string) < 5 
+OR NOT REGEXP_CONTAINS(value_as_string, r'^[0-9]{5}(?:-[0-9]{4})?$'))
+-- Using union distinct to prevent duplicates --
+UNION DISTINCT (
 -- Selects all zips that do not match one in the master zip3 lookup table --
 SELECT o.* FROM `{{project_id}}.{{dataset_id}}.{{obs_table}}` o
 LEFT JOIN `{{project_id}}.{{pipeline_tables}}.{{zip3_lookup}}` z
-ON CAST(SUBSTR(o.value_as_string, 1, 3) AS INT64) = z.zip3
-WHERE (observation_source_concept_id = 1585250
-AND z.zip3 IS NULL)
-) AS R ON L.person_id = R.person_id)
+ON SUBSTR(o.value_as_string, 1, 3) = CAST(z.zip3 AS STRING)
+WHERE (observation_source_concept_id = 1585250 AND z.zip3 IS NULL)))
+""")
+
+CLEAN_ZIPS_OF_WHITESPACE = JINJA_ENV.from_string("""
+UPDATE `{{project_id}}.{{dataset_id}}.{{obs_table}}` SET
+value_as_string = TRIM(value_as_string)
+WHERE person_id IN (
+SELECT person_id FROM `{{project_id}}.{{sandbox_dataset_id}}.{{whitespace_sandbox}}`)
 """)
 
 UPDATE_INVALID_ZIP_CODES = JINJA_ENV.from_string("""
@@ -85,14 +103,14 @@ class UpdateInvalidZipCodes(BaseCleaningRule):
             and a specification for how to execute that query. The specifications
             are optional but the query is required.
         """
-        keep_valid_zip_codes = {
+        sandbox_zips_with_whitespace = {
             cdr_consts.QUERY:
-                UPDATE_INVALID_ZIP_CODES.render(
+                SANDBOX_ZIPS_WITH_WHITESPACE.render(
                     project_id=self.project_id,
-                    dataset_id=self.dataset_id,
-                    obs_table=OBSERVATION,
                     sandbox_dataset_id=self.sandbox_dataset_id,
-                    sandbox_table=self.sandbox_table_for(OBSERVATION))
+                    whitespace_sandbox=ZIPS_WITH_WHITESPACE_SANDBOX,
+                    dataset_id=self.dataset_id,
+                    obs_table=OBSERVATION)
         }
 
         sandbox_invalid_zip_codes = {
@@ -107,7 +125,30 @@ class UpdateInvalidZipCodes(BaseCleaningRule):
                     zip3_lookup=ZIP3_LOOKUP)
         }
 
-        return [sandbox_invalid_zip_codes, keep_valid_zip_codes]
+        clean_zips_of_whitespace = {
+            cdr_consts.QUERY:
+                CLEAN_ZIPS_OF_WHITESPACE.render(
+                    project_id=self.project_id,
+                    dataset_id=self.dataset_id,
+                    obs_table=OBSERVATION,
+                    sandbox_dataset_id=self.sandbox_dataset_id,
+                    whitespace_sandbox=ZIPS_WITH_WHITESPACE_SANDBOX)
+        }
+
+        update_zip_codes = {
+            cdr_consts.QUERY:
+                UPDATE_INVALID_ZIP_CODES.render(
+                    project_id=self.project_id,
+                    dataset_id=self.dataset_id,
+                    obs_table=OBSERVATION,
+                    sandbox_dataset_id=self.sandbox_dataset_id,
+                    sandbox_table=self.sandbox_table_for(OBSERVATION))
+        }
+
+        return [
+            sandbox_zips_with_whitespace, clean_zips_of_whitespace,
+            sandbox_invalid_zip_codes, update_zip_codes
+        ]
 
     def setup_rule(self, client, *args, **keyword_args):
         """
@@ -116,7 +157,10 @@ class UpdateInvalidZipCodes(BaseCleaningRule):
         pass
 
     def get_sandbox_tablenames(self):
-        return [self.sandbox_table_for(OBSERVATION)]
+        return [
+            ZIPS_WITH_WHITESPACE_SANDBOX,
+            self.sandbox_table_for(OBSERVATION)
+        ]
 
     def setup_validation(self, client, *args, **keyword_args):
         """
