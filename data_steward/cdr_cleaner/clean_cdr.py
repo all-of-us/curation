1de1fb315080c03a6bda2633930c6013849dbd75
Krishna Kalluri <krishnasaikalluri@gmail.com>
2021-05-26 13:43:20 -0400

    [DC-1642] Apply ValidDeathDates before NoDataAfterDeath. (#881)
    Signed-off-by: ksdkalluri <krishnasaikalluri@gmail.com>

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -181,8 +181,11 @@ COMBINED_CLEANING_CLASSES = [
     (DropParticipantsWithoutPPI,),
     (clean_years.get_year_of_birth_queries,),
     (NegativeAges,),
+    # Valid Death dates needs to be applied before no data after death as running no data after death is
+    # wiping out the needed consent related data for cleaning.
+    (
+        ValidDeathDates,),
     (NoDataAfterDeath,),
-    (ValidDeathDates,),
     (drug_refills_supply.get_days_supply_refills_queries,),
     # trying to load a table while creating query strings,
     # won't work with mocked strings.  should use base class
