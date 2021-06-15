cf0318b180ae6c8e680482acfc1ac92ccfa50158
Nishanth Pavinkurve <np2689@cumc.columbia.edu>
2021-06-15 18:21:40 -0400

    [DC-1689] Update email address on automated data submission emails (#895)
    * [DC-1689] Update reply to in email body and improve formatting
    * [DC-1689] Update test

--- data_steward/constants/validation/email_notification.py
+++ data_steward/constants/validation/email_notification.py
@@ -6,7 +6,8 @@ SELECT *
 FROM `{{project}}.{{dataset}}.{{contact_table}}`
 """
 
-DATA_CURATION = 'Data Curation'
+EHR_OPERATIONS = 'EHR Ops'
+EHR_OPS_ZENDESK = 'support@aou-ehr-ops.zendesk.com'
 DATA_CURATION_LISTSERV = 'datacuration@researchallofus.org'
 NO_REPLY_ADDRESS = 'noreply@researchallofus.org'
 NO_DATA_STEWARD = 'no data steward'
@@ -31,20 +32,19 @@ EMAIL_BODY = """
 {% endif %}
 Please review the <code>results.html</code> submission report attached to this email{% if submission_error %}<br>
 and resolve the errors before making a new submission{% endif %}.<br>
-If Your files have not been successfully uploaded to the DRC, Please run the local file check before making your
- submission.
-<a href="https://github.com/all-of-us/aou-ehr-file-check">aou-ehr-file-check</a><br>  
-To view the full set of curation reports, please visit the submission folder in your GCS bucket
- <a href="{{ submission_folder_url }}">here</a>.<br>
-For more information on the reports and how to download them, please refer to our 
-<a href="{{ ehr_ops_site_url }}">EHR Ops website</a>.</p>
+If any of your files have not been successfully uploaded, please run the
+ <a href="https://github.com/all-of-us/aou-ehr-file-check">local file check</a> before making your submission.<br>  
+To view the full set of curation reports, please visit the submission folder in your
+ GCS bucket <a href="{{ submission_folder_url }}">here</a>.<br>
+For more information on the reports and how to download them, please refer to our
+ <a href="{{ ehr_ops_site_url }}">EHR Ops website</a>.</p>
 
-<p style="font-size:115%;">You are receiving this email because you are listed as a point of contact<br>
-for HPO Site <em>{{ site_name }}</em>.<br>
-If you have additional questions or wish to no longer receive these emails,<br>
-please reply/send an email to <a href="mailto:{{ dc_listserv }}">{{ dc_listserv }}</a>.</p>
+<p style="font-size:115%;">You are receiving this email because you are listed as a point of contact
+ for HPO Site <em>{{ site_name }}</em>.<br>
+If you have additional questions or wish to no longer receive these emails, please reply/send an
+ email to <a href="mailto:{{ eo_zendesk }}">{{ eo_zendesk }}</a>.</p>
 
-<p style="font-size:115%;">Curation team, DRC<br>
+<p style="font-size:115%;">EHR Ops team, DRC<br>
 <em>All of Us</em> Research Program<br>
 <img src="cid:{{ aou_logo }}"/></p>
 """
