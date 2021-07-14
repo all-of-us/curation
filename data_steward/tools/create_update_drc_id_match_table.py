9220eb1e9bdb02db60e4f9190e4c78c6b46bfd97
dcampbell-vumc <danyel.campbell@vumc.org>
2021-07-14 14:09:16 -0500

    [DC-1216] Create Or Updated DRC Match Table
    * Modified module to populate the validation table with the default
      value 'missing_ehr' and 'missing_rdr' depending if the field contains
      data in the ps_value site table
    * Updated module summary

--- data_steward/tools/create_update_drc_id_match_table.py
+++ data_steward/tools/create_update_drc_id_match_table.py
@@ -6,7 +6,7 @@ There should be a record for each participant and the record should be filled wi
     phone_number, email, address_1, address_2, city, state, zip, birth_date, sex, and algorithm.
 
 The record for each of the above fields should default to `missing_rdr` if the joined record in the
-    ps_api_values_<hpo_id> table does not contain any information otherwise, it should default to `missing_rdr`
+    ps_api_values_<hpo_id> table does not contain any information otherwise, it should default to `missing_ehr`
 
 Original Issue: DC-1216
 """
@@ -31,40 +31,26 @@ CREATE TABLE `{{project_id}}.{{drc_dataset_id}}.{{id_match_table_id}}` ({{fields
 PARTITION BY DATE_TRUNC(_PARTITIONTIME, HOUR)
 """)
 
-INSERT_QUERY = JINJA_ENV.from_string("""
-MERGE `{{project_id}}.{{drc_dataset_id}}.{{id_match_table_id}}` b 
-USING `{{project_id}}.{{drc_dataset_id}}.{{ps_values_table_id}}` a
-ON a.person_id = b.person_id
-WHEN NOT MATCHED THEN 
-INSERT (_PARTITIONTIME, {{fields}}) 
-VALUES (TIMESTAMP_TRUNC(CURRENT_TIMESTAMP, HOUR), {{fields}})
+POPULATE_VALIDATION_TABLE = JINJA_ENV.from_string("""
+INSERT INTO `{{project_id}}.{{drc_dataset_id}}.{{id_match_table_id}}` (_PARTITIONTIME, {{fields}}) 
+SELECT TIMESTAMP_TRUNC(CURRENT_TIMESTAMP, HOUR), person_id, 
+{{case_statements}}
+FROM `{{project_id}}.{{drc_dataset_id}}.{{ps_values_table_id}}`
 """)
 
-MISSING_EHR_QUERY = JINJA_ENV.from_string("""
-UPDATE `{{project_id}}.{{drc_dataset_id}}.{{id_match_table_id}}`
-SET {{col}} = 'missing_ehr'
-WHERE {{col}} IS NULL
+CASE_EXPRESSION = JINJA_ENV.from_string("""
+CASE WHEN {{field}} IS NULL THEN 'missing_rdr' ELSE 'missing_ehr' END AS {{field}}
 """)
-
-MISSING_RDR_QUERY = JINJA_ENV.from_string("""
-UPDATE `{{project_id}}.{{drc_dataset_id}}.{{id_match_table_id}}`
-SET {{set_expression}}
-WHERE COALESCE({{fields}}) IS NULL
-""")
-
-SET_EXPRESSION = JINJA_ENV.from_string("""
-{{col}} = 'missing_rdr'""")
 
 
 def create_drc_validation_table(client, project_id, table_id):
     """
-    Creates the partitioned DRC validation table. Table will be partitioned by HOUR.
+    Creates the partitioned DRC validation table, partitioned by HOUR.
 
     :param client: bq client
     :param project_id: the project containing the dataset
     :param table_id: ID of the table
     """
-
     fields = resources.fields_for(IDENTITY_MATCH_TABLE)
 
     create_table = CREATE_TABLE.render(project_id=project_id,
@@ -78,9 +64,30 @@ def create_drc_validation_table(client, project_id, table_id):
     return table_id
 
 
-def copy_ps_values_data_to_id_match_table(client, project_id, table_id, hpo_id):
+def get_case_statements():
     """
-    Copies data from the ps_values table to the DRC identity match table.
+    This method generates the CASE_STATEMENT query
+    """
+    case_statements = []
+    field_list = []
+
+    schema_list = bq.get_table_schema(IDENTITY_MATCH_TABLE)
+    for item in schema_list:
+        field_list.append(item.name)
+
+    # this removes the person_id as it is primary key and will not be updated in case statement
+    field_list.remove('person_id')
+
+    for item in field_list:
+        case_statements.append(CASE_EXPRESSION.render(field=item))
+
+    return ', '.join(case_statements)
+
+
+def populate_validation_table(client, project_id, table_id, hpo_id):
+    """
+    Populates validation table with 'missing_rdr' or 'missing_ehr' data. Populated with 'missing_rdr' if data IS NOT
+        found in the ps_values table. Populated with 'missing_ehr' as default.
 
     :param client: bq client
     :param project_id: the project containing the dataset
@@ -92,85 +99,20 @@ def copy_ps_values_data_to_id_match_table(client, project_id, table_id, hpo_id):
     id_match_table_id = table_id
     ps_values_table_id = f'{PS_API_VALUES}_{hpo_id}'
 
-    fields_name_str = ',\n'.join([item.name for item in schema_list])
+    fields_name_str = ', '.join([item.name for item in schema_list])
 
-    insert_query = INSERT_QUERY.render(project_id=project_id,
-                                       drc_dataset_id=DRC_OPS,
-                                       id_match_table_id=id_match_table_id,
-                                       fields=fields_name_str,
-                                       ps_values_table_id=ps_values_table_id)
-
-    client.query(insert_query)
-
-    LOGGER.info(
-        f'Inserted data from `{ps_values_table_id}` to `{id_match_table_id}`')
-
-
-def get_set_expression():
-    """
-    This method generates the SET_EXPRESSION query
-    """
-    set_expression = []
-    schema_list = bq.get_table_schema(IDENTITY_MATCH_TABLE)
-    ', '.join([item.name for item in schema_list])
-
-    for item in schema_list:
-        set_expression.append(SET_EXPRESSION.render(col=item.name))
-
-    # Work around to remove person_id from the fields list since person_id is required
-    # and will never need to be updated to missing_rdr
-    set_expression.remove("\nperson_id = 'missing_rdr'")
-
-    return ', '.join(set_expression)
-
-
-def update_site_drc_table(client, project_id, table_id):
-    """
-    Updates the site's DRC identity match table. If the record is empty, each field should default
-        to `missing_rdr`, else `missing_ehr` for each field that does not have any data.
-
-    :param client: bq client
-    :param project_id: the project containing the dataset
-    :param table_id: ID of the table
-    """
-    schema_list = bq.get_table_schema(IDENTITY_MATCH_TABLE)
-    fields_name_str = ',\n'.join([item.name for item in schema_list])
-
-    # person_id will not need to be updated since it will always be in table since it
-    # is a required field
-    fields = fields_name_str.replace('person_id,', '')
-    id_match_table_id = table_id
-
-    # Updates missing rows to contain 'missing_rdr'
-    missing_rdr_query = MISSING_RDR_QUERY.render(
+    populate_query = POPULATE_VALIDATION_TABLE.render(
         project_id=project_id,
         drc_dataset_id=DRC_OPS,
         id_match_table_id=id_match_table_id,
-        set_expression=get_set_expression(),
-        fields=fields)
+        fields=fields_name_str,
+        case_statements=get_case_statements(),
+        ps_values_table_id=ps_values_table_id)
 
-    job = client.query(missing_rdr_query)
+    job = client.query(populate_query)
     job.result()
-    LOGGER.info(
-        f'Updated missing row values in `{id_match_table_id}` with \'missing_rdr\''
-    )
 
-    # Updates remaining fields to contain 'missing_ehr' in site's drc id match table
-    for item in schema_list:
-        if item.name is not 'person_id':
-            missing_ehr_query = MISSING_EHR_QUERY.render(
-                project_id=project_id,
-                drc_dataset_id=DRC_OPS,
-                id_match_table_id=id_match_table_id,
-                col=item.name)
-
-            job = client.query(missing_ehr_query)
-            job.result()
-        LOGGER.info(
-            f'Updated missing column values in `{id_match_table_id}` with \'missing_ehr\''
-        )
-
-    LOGGER.info(f'Completed column update of `{id_match_table_id}` table.')
+    LOGGER.info(f'Populated values in `{id_match_table_id}`')
 
 
 def get_arg_parser():
@@ -187,12 +129,6 @@ def get_arg_parser():
                         dest='hpo_id',
                         help='awardee name of the site',
                         required=True)
-    parser.add_argument('-s',
-                        '--console_log',
-                        dest='console_log',
-                        action='store_true',
-                        required=False,
-                        help='Log to the console as well as to a file.')
     parser.add_argument('-e',
                         '--run_as_email',
                         action='store',
@@ -219,10 +155,8 @@ def main():
     if not table_exists(table_id, DRC_OPS):
         create_drc_validation_table(client, args.project_id, table_id)
 
-    # Copy values from ps_values table and updates site DRC id match table
-    copy_ps_values_data_to_id_match_table(client, args.project_id, table_id,
-                                          args.hpo_id)
-    update_site_drc_table(client, args.project_id, table_id)
+    # Populates the validation table for the site
+    populate_validation_table(client, args.project_id, table_id, args.hpo_id)
 
 
 if __name__ == '__main__':
