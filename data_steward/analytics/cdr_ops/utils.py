b73ef893fd461fe0d1f3b0ad3f6098eb329e4e88
Nishanth Pavinkurve <np2689@cumc.columbia.edu>
2021-07-21 17:28:24 -0400

    [DC-1756] Create notebook for EHR snapshot validation (#935)
    * [DC-1756] Add and update notebook for EHR snapshot
    * [DC-1756] Move execute to bq and parametrize client
    * [DC-1756] Move util function to separate script

---
+++ data_steward/analytics/cdr_ops/utils.py
@@ -0,0 +1,15 @@
+"""
+Utility functions for notebooks related to CDR generation
+"""
+
+
+def execute(client, query):
+    """
+    Execute a bigquery command and return the results in a dataframe
+
+    :param client: an instantiated bigquery client object
+    :param query: the query to execute
+    :return pandas dataframe object
+    """
+    print(query)
+    return client.query(query).to_dataframe()
