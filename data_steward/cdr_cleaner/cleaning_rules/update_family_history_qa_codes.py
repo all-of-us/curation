50f9c321c7d92d73c8498eab43918e5e9c140b45
Lauren Biggers <lauren.r.biggers@vumc.org>
2021-08-13 10:52:56 -0500

    [DC-844] update family history qa codes
    * adding validation checks to the cleaning rule

--- data_steward/cdr_cleaner/cleaning_rules/update_family_history_qa_codes.py
+++ data_steward/cdr_cleaner/cleaning_rules/update_family_history_qa_codes.py
@@ -39,6 +39,15 @@ WHERE observation_source_concept_id IN (43529632, 43529637, 43529636)
 AND value_source_concept_id IN (43529091, 43529094, 702787)
 """)
 
+### Validation Query ####
+COUNTS_QUERY = JINJA_ENV.from_string("""
+SELECT
+COUNTIF(observation_source_concept_id = 43529632 AND value_source_concept_id = 43529091) AS n_old_43529632,
+COUNTIF(observation_source_concept_id = 43529637 AND value_source_concept_id = 43529094) AS n_old_43529637,
+COUNTIF(observation_source_concept_id = 43529636 AND value_source_concept_id = 702787) AS n_old_43529636,
+COUNT(*) AS total
+FROM `{{project_id}}.{{dataset_id}}.{{obs_table}}`
+""")
 
 class UpdateFamilyHistoryCodes(BaseCleaningRule):
     """
@@ -71,7 +80,12 @@ class UpdateFamilyHistoryCodes(BaseCleaningRule):
                          dataset_id=dataset_id,
                          sandbox_dataset_id=sandbox_dataset_id,
                          table_namer=table_namer)
-        print(f'init table namer is:  `{self.table_namer}`')
+
+        self.counts_query = COUNTS_QUERY.render(
+            project_id=self.project_id,
+            dataset_id=self.dataset_id,
+            obs_table=OBSERVATION)
+
 
     def get_query_specs(self):
         """
@@ -88,16 +102,11 @@ class UpdateFamilyHistoryCodes(BaseCleaningRule):
                     sandbox_tablename=self.sandbox_table_for(OBSERVATION))
         }
 
-        print(f'table namer is:  `{self.table_namer}`')
         update_query = {
             cdr_consts.QUERY:
                 UPDATE_FAMILY_HISTORY_QUERY.render(dataset_id=self.dataset_id,
                                                    project_id=self.project_id)
         }
-
-        print(sandbox_query.get('query'))
-        print('\n\n')
-        print(update_query.get('query'))
 
         return [sandbox_query, update_query]
 
@@ -111,16 +120,63 @@ class UpdateFamilyHistoryCodes(BaseCleaningRule):
         """
         Run required steps for validation setup
         """
-        raise NotImplementedError("Please fix me.")
+        self.init_counts = self._get_counts(client)
+
+        if self.init_counts.get('total_rows') == 0:
+            raise RuntimeError('NO DATA EXISTS IN OBSERVATION TABLE')
+
 
     def validate_rule(self, client):
         """
         Validates the cleaning rule which deletes or updates the data from the tables
         """
-        raise NotImplementedError("Please fix me.")
+        clean_counts = self._get_counts(client)
+
+        if clean_counts.get('total_rows') == 0:
+            raise RuntimeError('LOST ALL DATA IN OBSERVATION TABLE')
+
+        if self.init_counts.get('total_rows') != clean_counts.get('total_rows'):
+            raise RuntimeError(
+                f'{self.__class__.__name__} unexpectedly changed table counts.\n'
+                f'initial count is: {self.init_counts.get("total_rows")}\n'
+                f'clean count is: {clean_counts.get("total_rows")}')
+
+        if clean_counts.get('43529632_count')
+        or clean_counts.get('43529637_count')
+        or clean_counts.get('43529636_count'):
+            raise RuntimeError(
+                f'{self.__class__.__name__} did not clean as expected.\n'
+                f'Found data in: {clean_counts}')
 
     def get_sandbox_tablenames(self):
+        """
+        Create sandbox table names and return as list.
+        """
         return [self.sandbox_table_for(OBSERVATION)]
+
+    def _get_counts(self, client):
+        """
+        Counts query.
+
+        Used for job validation.
+        """
+        job = client.query(self.counts_query)
+        response = job.result()
+
+        errors = []
+        if job.exception():
+            errors.append(job.exception())
+            LOGGER.error(f"FAILURE:  {job.exception()}\n"
+                         f"Problem exceuting query:\n{self.counts_query}")
+        else:
+            for item in response:
+                total = item.get('total', 0)
+                n_43529632 = item.get('n_old_43529632', 0)
+                n_43529637 = item.get('n_old_43529637', 0)
+                n_43529636 = item.get('n_old_43529636', 0)
+
+        return {'total_rows': total, '43529632_count': n_43529632,
+                '43529637_count': n_43529637, '43529636_count': n_43529636}
 
 
 if __name__ == '__main__':
@@ -132,12 +188,14 @@ if __name__ == '__main__':
     # only stream to console if running as the main script
     pipeline_logging.configure(add_console_handler=True)
 
+    table_namer = '_'.join(ARGS.dataset_id.split('_')[1:])
+
     if ARGS.list_queries:
         query_list = clean_engine.get_query_list(ARGS.project_id,
                                                  ARGS.dataset_id,
                                                  ARGS.sandbox_dataset_id,
                                                  [(UpdateFamilyHistoryCodes,)],
-                                                 table_namer='rdr')
+                                                 table_namer=table_namer)
         for query in query_list:
             LOGGER.info(query)
     else:
@@ -145,4 +203,4 @@ if __name__ == '__main__':
                                    ARGS.dataset_id,
                                    ARGS.sandbox_dataset_id,
                                    [(UpdateFamilyHistoryCodes,)],
-                                   table_namer='rdr')
+                                   table_namer=table_namer)
