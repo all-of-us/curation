6432a174cb54a8a27ac5cc97dfea2e92845ab559
ksdkalluri <krishnasaikalluri@gmail.com>
2021-08-26 11:10:47 -0400

    [DC-1801] Removes cdm.csv from resource_files and all references to it
    Signed-off-by: ksdkalluri <krishnasaikalluri@gmail.com>

--- tests/unit_tests/data_steward/resources_test.py
+++ tests/unit_tests/data_steward/resources_test.py
@@ -15,25 +15,6 @@ class ResourcesTest(unittest.TestCase):
         print(cls.__name__)
         print('**************************************************************')
 
-    def test_cdm_csv(self):
-        cdm_data_rows = resources.cdm_csv()
-        expected_keys = {
-            'table_name', 'column_name', 'is_nullable', 'data_type',
-            'description'
-        }
-        expected_table_names = {
-            'person', 'visit_occurrence', 'condition_occurrence',
-            'procedure_occurrence', 'drug_exposure', 'measurement'
-        }
-        actual_table_names = set()
-        for row in cdm_data_rows:
-            keys = set(row.keys())
-            self.assertSetEqual(expected_keys, keys)
-            actual_table_names.add(row['table_name'])
-
-        for expected_table_name in expected_table_names:
-            self.assertIn(expected_table_name, actual_table_names)
-
     def test_cdm_schemas(self):
         schemas = resources.cdm_schemas()
         table_names = schemas.keys()
