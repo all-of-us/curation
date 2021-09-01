7c045dfd3515f6d0ae8e3f0908d2edcb98980c08
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-09-01 07:14:48 -0500

    [DC-1811] Building our CircleCI build using Docker on VM - Creating base image from which all dev containers should derive - Creating simpler tests image to speed up dev time

--- docker/tests/scripts/funcs.sh
+++ docker/tests/scripts/funcs.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/env bash
 
-function in_ci() {
-  if [[ -n "${CIRCLECI}" ]] && [[ "${CIRCLECI}" == "true" ]]; then
+function in_circleci {
+  if [[ -n "${CIRCLECI}" ]] && [[ "${CIRCLECI}" == "true" ]];
+  then
     return 0
-  elif [[ -n "${CI}" ]] && [[ "${CI}" == "true" ]]; then
+  elif [[ -n "${CI}" ]] && [[ "${CI}" == "true" ]];
+  then
     return 0
   else
     return 1
   fi
 }
 
-function uppercase() {
+function uppercase {
   echo -n "${1}" | tr '[:lower:]' '[:upper:]'
 }
 
-function lowercase() {
+function lowercase {
   echo -n "${1}" | tr '[:upper:]' '[:lower:]'
 }
 
-function underscore_me() {
+function underscore_me {
   # matches: [/- ]+
   # replace: "_"
   echo -n "${1}" | sed -e 's#/#_#g; s/-/_/g; s/ /_/g'
 }
 
-function escape_newlines() {
+function escape_newlines {
   echo -n "${1}" | sed ':a;N;$!ba;s/\n/\\n/g'
 }
 
@@ -32,11 +34,14 @@ function escape_newlines() {
 function define_username() {
   # if USERNAME is already set, use it.
   local out
-  if [ -n "${USERNAME}" ]; then
+  if [ -n "${USERNAME}" ];
+  then
     out="${USERNAME}"
-  elif [ -n "${GH_USERNAME}" ]; then
+  elif [ -n "${GH_USERNAME}" ];
+  then
     out="${GH_USERNAME}"
-  elif [ -n "${CIRCLE_USERNAME}" ]; then
+  elif [ -n "${CIRCLE_USERNAME}" ];
+  then
     out="${CIRCLE_USERNAME}"
   else
     out="${USER:-$(whoami)}"
@@ -50,11 +55,12 @@ function define_username() {
   return 0
 }
 
-function set_env() {
+function set_env {
   local name=$1
   local default=$2
   local value
-  if [[ -z $3 ]]; then
+  if [[ -z $3 ]];
+  then
     value="${!name:-$default}"
   else
     value="${default}"
@@ -63,10 +69,11 @@ function set_env() {
   export "${name}"="${value}"
 }
 
-function require_ok() {
+function require_ok {
   # shellcheck source=/dev/null
-  if ! . "${CURATION_SCRIPTS_DIR}/${1}"; then
-    echo "Error running script \"${CURATION_SCRIPTS_DIR}/${1}\""
+  if ! . "${CURATION_SCRIPTS_DIR}/$1" ;
+  then
+    echo "Error running \$1"
     exit 1
   fi
   return 0
