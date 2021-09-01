f9fb2e81fdb0e93cb1c89232f27c5a374ba16cb7
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-09-01 07:14:48 -0500

    [DC-1811] Building our CircleCI build using Docker on VM - Creating base image from which all dev containers should derive - Creating simpler tests image to speed up dev time

--- docker/tests/scripts/tests_integration_00_execute.sh
+++ docker/tests/scripts/tests_integration_00_execute.sh
@@ -3,87 +3,72 @@
 # this script is slightly more complex than the unit test script
 # as there isn't a great way to terminate a circle-ci job early
 
-set -e
-set +x
+set -ex
 
-function is_forced_run() {
-  if [[ -n "${FORCE_RUN_INTEGRATION}" ]] && [[ "${FORCE_RUN_INTEGRATION}" == "1" ]]; then
+function is_forced_run {
+  if [[ -n "${FORCE_RUN_INTEGRATION}" ]] && [[ "${FORCE_RUN_INTEGRATION}" == "1" ]];
+  then
+    echo "FORCE_RUN_INTEGRATION=${FORCE_RUN_INTEGRATION}"
+    echo "Integration run forced"
     return 0
   else
     return 1
   fi
 }
 
-function is_dev_branch() {
-  if ! in_ci; then
-    return 1
-  fi
-
-  local branch
-  branch="${CIRCLE_BRANCH}"
-  if [[ "${branch}" == "develop" ]] || [[ "${branch}" == "master" ]] ||
-    [[ "${branch}" == "main" ]]; then
-    return 0
-  else
-    return 1
-  fi
-
-}
-
-function is_circle_pr() {
-  if ! in_ci; then
-    return 1
-  fi
-
-  if [[ -n "${CIRCLE_PULL_REQUEST}" ]] || [[ -n "${CIRCLE_PULL_REQUESTS}" ]]; then
+function is_dev_branch {
+  if [[ "${CIRCLE_BRANCH}" == "develop" ]] || [[ "${CIRCLE_BRANCH}" == "master" ]] \
+    || [[ "${CIRCLE_BRANCH}" == "main" ]];
+  then
+    echo "CIRCLE_BRANCH=${CIRCLE_BRANCH}"
+    echo "We're in branch master or develop"
     return 0
   else
     return 1
   fi
 }
 
-function is_manually_run() {
-  if [[ "${GIT_LAST_LOG}" == *"all tests"* ]]; then
+function is_circle_pr {
+  if [[ -n "${CIRCLE_PULL_REQUEST}" ]] || [[ -n "${CIRCLE_PULL_REQUESTS}" ]];
+  then
+    echo "CIRCLE_PULL_REQUEST=${CIRCLE_PULL_REQUEST}"
+    echo "CIRCLE_PULL_REQUESTS=${CIRCLE_PULL_REQUESTS}"
+    echo "We are in a Circle-CI pull request"
     return 0
   else
     return 1
   fi
 }
 
-is_forced_run
-forced=$?
-is_dev_branch
-dev_branch=$?
-is_circle_pr
-circle_pr=$?
-is_manually_run
-manual_run=$?
+function is_manually_run {
+  if [[ "${GIT_LAST_LOG}" == *"all tests"* ]];
+  then
+    echo "GIT_LAST_LOG=${GIT_LAST_LOG}"
+    echo "GIT_LAST_LOG contains \"all tests\""
+    return 0
+  else
+    return 1
+  fi
+}
 
-if [[ $forced -eq 0 ]]; then
-  echo "FORCE_RUN_INTEGRATION is true"
-  should_run=0
-elif [[ $dev_branch -eq 0 ]]; then
-  echo "We're in a dev branch"
-  should_run=0
-elif [[ $circle_pr -eq 0 ]]; then
-  echo "We're in a PR in CircleCI"
-  should_run=0
-elif [[ $manual_run -eq 0 ]]; then
-  echo "Last git commit message contains phrase\"all tests\""
-  should_run=0
-else
-  should_run=1
-fi
+function should_run {
+  if is_forced_run -eq 0 || is_dev_branch -eq 0  ||  is_circle_pr -eq 0  \
+    || is_manually_run -eq 0 ;
+  then
+    return 0
+  else
+    return 1
+  fi
+}
 
-if [[ $should_run -eq 0 ]]; then
-  echo "Running integration tests..."
-
+if should_run ;
+then
   # todo: this inline PYTHONPATH modification is silly.
-  cd "${CIRCLE_WORKING_DIRECTORY}"/data_steward &&
-    PYTHONPATH=./:$PYTHONPATH python ./ci/test_setup.py
+  cd "${CIRCLE_WORKING_DIRECTORY}"/data_steward \
+    && PYTHONPATH=./:$PYTHONPATH python ./ci/test_setup.py
 
-  cd "${CIRCLE_WORKING_DIRECTORY}" &&
-    ./tests/run_tests.sh -s integration
+  cd "${CIRCLE_WORKING_DIRECTORY}" \
+    && ./tests/run_tests.sh -s integration
 else
   echo "Skipping integration tests"
 fi
