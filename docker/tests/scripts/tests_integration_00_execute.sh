f06d61e204587f223912a4262254ca514d96943f
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-09-14 16:02:56 -0500

    formatting scripts and tweaking integration test runner script some more

--- docker/tests/scripts/tests_integration_00_execute.sh
+++ docker/tests/scripts/tests_integration_00_execute.sh
@@ -6,26 +6,23 @@
 set -e
 set +x
 
-function is_forced_run {
-  if [[ -n "${FORCE_RUN_INTEGRATION}" ]] && [[ "${FORCE_RUN_INTEGRATION}" == "1" ]];
-  then
+function is_forced_run() {
+  if [[ -n "${FORCE_RUN_INTEGRATION}" ]] && [[ "${FORCE_RUN_INTEGRATION}" == "1" ]]; then
     return 0
   else
     return 1
   fi
 }
 
-function is_dev_branch {
-  if ! in_ci ;
-  then
+function is_dev_branch() {
+  if ! in_ci; then
     return 1
   fi
 
   local branch
   branch="${CIRCLE_BRANCH}"
-  if [[ "${branch}" == "develop" ]] || [[ "${branch}" == "master" ]] \
-    || [[ "${branch}" == "main" ]];
-  then
+  if [[ "${branch}" == "develop" ]] || [[ "${branch}" == "master" ]] ||
+    [[ "${branch}" == "main" ]]; then
     return 0
   else
     return 1
@@ -33,23 +30,20 @@ function is_dev_branch {
 
 }
 
-function is_circle_pr {
-  if ! in_ci ;
-  then
+function is_circle_pr() {
+  if ! in_ci; then
     return 1
   fi
 
-  if [[ -n "${CIRCLE_PULL_REQUEST}" ]] || [[ -n "${CIRCLE_PULL_REQUESTS}" ]];
-  then
+  if [[ -n "${CIRCLE_PULL_REQUEST}" ]] || [[ -n "${CIRCLE_PULL_REQUESTS}" ]]; then
     return 0
   else
     return 1
   fi
 }
 
-function is_manually_run {
-  if [[ "${GIT_LAST_LOG}" == *"all tests"* ]];
-  then
+function is_manually_run() {
+  if [[ "${GIT_LAST_LOG}" == *"all tests"* ]]; then
     return 0
   else
     return 1
@@ -65,32 +59,31 @@ circle_pr=$?
 is_manually_run
 manual_run=$?
 
-if [ $forced -eq 0 ];
-then
+if [[ $forced -eq 0 ]]; then
   echo "FORCE_RUN_INTEGRATION is true"
   should_run=0
-elif [ $dev_branch -eq 0 ];
-then
+elif [[ $dev_branch -eq 0 ]]; then
   echo "We're in a dev branch"
-elif [ $circle_pr -eq 0 ];
-then
+  should_run=0
+elif [[ $circle_pr -eq 0 ]]; then
   echo "We're in a PR in CircleCI"
-elif [ $manual_run -eq 0 ];
-then
+  should_run=0
+elif [[ $manual_run -eq 0 ]]; then
   echo "Last git commit message contains phrase\"all tests\""
+  should_run=0
+else
+  should_run=1
 fi
 
-
-if [ $should_run -eq 0 ] ;
-then
+if [[ $should_run -eq 0 ]]; then
   echo "Running integration tests..."
 
   # todo: this inline PYTHONPATH modification is silly.
-  cd "${CIRCLE_WORKING_DIRECTORY}"/data_steward \
-    && PYTHONPATH=./:$PYTHONPATH python ./ci/test_setup.py
+  cd "${CIRCLE_WORKING_DIRECTORY}"/data_steward &&
+    PYTHONPATH=./:$PYTHONPATH python ./ci/test_setup.py
 
-  cd "${CIRCLE_WORKING_DIRECTORY}" \
-    && ./tests/run_tests.sh -s integration
+  cd "${CIRCLE_WORKING_DIRECTORY}" &&
+    ./tests/run_tests.sh -s integration
 else
   echo "Skipping integration tests"
 fi
