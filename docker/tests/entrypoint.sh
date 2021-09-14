319dbb9c5fe2f0c018995739ecafa792201c7b80
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-09-14 16:02:56 -0500

    formatting scripts and tweaking integration test runner script some more

--- docker/tests/entrypoint.sh
+++ docker/tests/entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 source "${CURATION_SCRIPTS_DIR}"/funcs.sh
 
-HELP_TEXT=$(cat << EOT
+HELP_TEXT=$(
+  cat <<EOT
 Must provide at least one of "lint", "unit", and / or "integration" as argument to execution
 EOT
 )
@@ -15,28 +16,27 @@ RUN_LINT=0
 
 for i in "$@"; do
   case $i in
-    unit)
-      RUN_UNIT=1
-      shift
-      ;;
-    integration)
-      RUN_INTEGRATION=1
-      shift
-      ;;
-    lint)
-      RUN_LINT=1
-      shift
-      ;;
-    *)
-      echo "Unknown option ${i}"
-      echo "${HELP_TEXT}"
-      exit 1
-      ;;
+  unit)
+    RUN_UNIT=1
+    shift
+    ;;
+  integration)
+    RUN_INTEGRATION=1
+    shift
+    ;;
+  lint)
+    RUN_LINT=1
+    shift
+    ;;
+  *)
+    echo "Unknown option ${i}"
+    echo "${HELP_TEXT}"
+    exit 1
+    ;;
   esac
 done
 
-if [ "${RUN_LINT}" -ne 1 ] && [ "${RUN_UNIT}" -ne 1 ] && [ "${RUN_INTEGRATION}" -ne 1 ];
-then
+if [ "${RUN_LINT}" -ne 1 ] && [ "${RUN_UNIT}" -ne 1 ] && [ "${RUN_INTEGRATION}" -ne 1 ]; then
   echo "${HELP_TEXT}"
   exit 0
 fi
@@ -44,13 +44,11 @@ fi
 echo "Initializing envvars..."
 require_ok "00_init_env.sh"
 
-if [ "${RUN_LINT}" -eq 1 ];
-then
+if [ "${RUN_LINT}" -eq 1 ]; then
   echo "Running linting checks..."
 
   # if we're running inside CI, execute some additional lint checks
-  if in_ci;
-  then
+  if in_ci; then
     require_ok "lint_00_validate_commit_message.sh"
     require_ok "lint_10_validate_pr_title.sh"
   fi
@@ -60,31 +58,26 @@ then
   require_ok "lint_30_pylint.sh"
 fi
 
-if [ "${RUN_UNIT}" -eq 1 ] || [ "${RUN_INTEGRATION}" -eq 1 ];
-then
+if [ "${RUN_UNIT}" -eq 1 ] || [ "${RUN_INTEGRATION}" -eq 1 ]; then
   echo "Running test setup script(s)..."
 
   require_ok "tests_00_prep_output_paths.sh"
   require_ok "tests_10_init_gcloud_auth.sh"
 fi
 
-if [ "${RUN_UNIT}" -eq 1 ];
-then
+if [ "${RUN_UNIT}" -eq 1 ]; then
   require_ok "tests_unit_00_execute.sh"
 fi
 
-if [ "${RUN_INTEGRATION}" -eq 1 ];
-then
-  if ! in_ci ;
-  then
-      export FORCE_RUN_INTEGRATION=1
+if [ "${RUN_INTEGRATION}" -eq 1 ]; then
+  if ! in_ci; then
+    export FORCE_RUN_INTEGRATION=1
   fi
   require_ok "tests_integration_00_execute.sh"
   require_ok "tests_integration_99_teardown.sh"
 fi
 
-if [ "${RUN_UNIT}" -eq 1 ] || [ "${RUN_INTEGRATION}" -eq 1 ];
-then
+if [ "${RUN_UNIT}" -eq 1 ] || [ "${RUN_INTEGRATION}" -eq 1 ]; then
   require_ok "tests_50_combine_coverage.sh"
 fi
 
