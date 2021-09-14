97c84e63c19df3f5fb67cbddcabd1ed7ee012014
Lauren Biggers <lauren.r.biggers@vumc.org>
2021-09-14 15:36:06 -0500

    [DC-1839] adding soon to be deprecated requirements file
    * adding curation/data_steward/deid/requirements.txt to pip install
    * this should be removed by a future PR
    * specifically setting PYTHONPATH and removing debug statements from bash script

--- tools/pylint_checks.sh
+++ tools/pylint_checks.sh
@@ -4,15 +4,8 @@
 
 set +e
 
-echo "-----------------------"
-echo "${PYTHONPATH}"
-echo "-----------------------"
 pushd $(git rev-parse --show-toplevel) > /dev/null
-lint_out=$(PYTHONPATH=./data_steward:./tests pylint -E data_steward tests)
-
-echo "-----------------------"
-echo "${lint_out}"
-echo "-----------------------"
+lint_out=$(PYTHONPATH=$PYTHONPATH pylint -E data_steward tests)
 
 if [ -z "$lint_out" ]; then
   echo "Passed pylint linter!"
