84b6cf3a4d73d97f625c2311eafbc26c850c0980
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-09-14 16:02:56 -0500

    formatting scripts and tweaking integration test runner script some more

--- docker/tests/scripts/tests_10_init_gcloud_auth.sh
+++ docker/tests/scripts/tests_10_init_gcloud_auth.sh
@@ -8,26 +8,23 @@ source "${CURATION_SCRIPTS_DIR}"/funcs.sh
 echo "Activating Google Cloud Credentials..."
 
 # this must be defined whether we're running in CircleCI or
-if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]];
-then
+if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
   echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS is missing or empty"
   exit 1
 fi
 
 # test if we're running within CircleCI
-if in_ci;
-then
-  if [[ -z "${GCLOUD_CREDENTIALS_KEY}" ]] || [[ -z "${GCLOUD_CREDENTIALS}" ]];
-  then
+if in_ci; then
+  if [[ -z "${GCLOUD_CREDENTIALS_KEY}" ]] || [[ -z "${GCLOUD_CREDENTIALS}" ]]; then
     echo "Environment variables GCLOUD_CREDENTIALS and/or GCLOUD_CREDENTIALS_KEY are missing or empty"
     exit 1
   fi
 
   echo "Writing decoded CircleCI GCP key to ${GOOGLE_APPLICATION_CREDENTIALS}"
 
-  echo "${GCLOUD_CREDENTIALS}" | \
+  echo "${GCLOUD_CREDENTIALS}" |
     openssl enc -d -aes-256-cbc -base64 -A -md md5 -k "${GCLOUD_CREDENTIALS_KEY}" \
-    -out "${GOOGLE_APPLICATION_CREDENTIALS}"
+      -out "${GOOGLE_APPLICATION_CREDENTIALS}"
 fi
 
-gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}" ;
+gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
