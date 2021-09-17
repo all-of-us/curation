384313b75fb102d0928233483322463f148e813e
Lauren Biggers <47253368+lrwb-aou@users.noreply.github.com>
2021-09-17 10:03:05 -0500

    [DC-1839] yapf linting failure fixes (#973)
    * yapf run on only one file that is causing lint failures
    * moving pylint checks to a script
    * adding curation/data_steward/deid/requirements.txt to pip install
    * this should be removed by a future PR
    * specifically setting PYTHONPATH directly where needed and removing debug statements from bash script

---
+++ tools/pylint_checks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Applies pylint -E to the entire curation repository. 
+# Fails if there are any errors.
+
+set +e
+
+pushd $(git rev-parse --show-toplevel) > /dev/null
+lint_out=$(PYTHONPATH=./data_steward/:./tests pylint -E data_steward tests)
+
+if [ -z "$lint_out" ]; then
+  echo "Passed pylint linter!"
+  exit 0
+else
+  echo "Full pylint output:"
+  echo "${lint_out}"
+  echo ""
+	echo "Failed pylint linter.  Fix errors and commit changes."
+	exit 1
+fi
