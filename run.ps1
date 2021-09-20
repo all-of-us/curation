5de1f381f05072abbbf533329f3a4710ee8a5693
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-09-20 14:11:24 -0500

    cleanup of scripts

---
+++ run.ps1
@@ -0,0 +1,80 @@
+# this _should_ make powershell stop on first error, but apparently there are lots of edge cases.  yay.
+$ErrorActionPreference = "Stop"
+
+function in_ci
+{
+  return (Test-Path "env:CI") -and ($env:CI -eq "true")
+}
+
+function missing_required_env
+{
+  param ([string]$name)
+
+  write-host "Required environment variable ""$name"" is missing or empty"
+  exit 1
+}
+
+if (($args.Count) -eq 0)
+{
+  echo "At least one argument must be provided, and one must be the name of a service within docker-compose.yml"
+  exit 1
+}
+
+# args defined in docker-compose.yml
+$uid=1000
+$gid=1000
+$user=(split-path -path $(Get-WMIObject -class Win32_ComputerSystem | select username).username -leaf)
+
+write-host "Running ""$service"" as user ""$user""..."
+
+$pwd=(get-location)
+
+$run_args = @(
+  "compose",
+  "run",
+  "-v", """${pwd}/.git:/home/curation/project/curation/.git""",
+  "-v", """${pwd}/data_steward:/home/curation/project/curation/data_steward""",
+  "-v", """${pwd}/tests:/home/curation/project/curation/tests""",
+  "-v", """${pwd}/tools:/home/curation/project/curation/tools"""
+)
+
+if (-not (in_ci))
+{
+  write-host "Running outside of CI"
+
+  if ((-not (Test-Path "env:GOOGLE_APPLICATION_CREDENTIALS")) -or ($env:GOOGLE_APPLICATION_CREDENTIALS -eq ""))
+  {
+    (missing_required_env "GOOGLE_APPLICATION_CREDENTIALS")
+  }
+
+  #$gcreds=(unixize_path $env:GOOGLE_APPLICATION_CREDENTIALS)
+  $gcreds=$env:GOOGLE_APPLICATION_CREDENTIALS
+
+  # write-host "Ensuring base image is up to date..."
+
+  docker compose build --build-arg uid=1000 --build-arg gid=1000 base
+  if ($lastexitcode -ne 0)
+  {
+    write-host "Error(s) occurred while building base image"
+    exit 1
+  }
+
+  # add gcreds path volume mount to runtime args
+  $run_args += "-v"
+  $run_args += """${gcreds}:/home/curation/project/curation/aou-res-curation-test.json"""
+
+#  write-host "Adding volume mount: $volumes"
+}
+else
+{
+  write-host "Running in CI."
+}
+
+# finally add any / all remaining args provided to this script as args to pass into container
+for ($a in $args)
+{
+  $run_args += $a
+}
+
+# do the thing.
+& docker @run_args
--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ function missing_required_env() {
   exit 1
 }
 
-if [ -z "$1" ]; then
+if [ "$#" -eq 0 ]; then
   echo At least one argument must be provided, and \
     it must be the name of a service within docker-compose.yml
 
@@ -41,6 +41,18 @@ gid=$(id -g)
 whoiis=$(whoami)
 
 echo Running "$1" as user "${whoiis}" \("${uid}:${gid}"\)...
+
+run_args=(
+  "run"
+  "-v"
+  "$(pwd)/.git:/home/curation/project/curation/.git"
+  "-v"
+  "$(pwd)/data_steward:/home/curation/project/curation/data_steward"
+  "-v"
+  "$(pwd)/tests:/home/curation/project/curation/tests"
+  "-v"
+  "$(pwd)/tools:/home/curation/project/curation/tools"
+)
 
 # when run on a developer's machine, we need to do some extra things like:
 # 1. ensure base container image is up to date
@@ -82,33 +94,20 @@ if ! in_ci; then
     exit 1
   fi
 
-  # execute tests image build
-  set +e
-  docker compose build "${1}"
-  build_ok=$?
-  set -e
-
-  if [ $build_ok -ne 0 ]; then
-    echo "Build tests step failed"
-    exit 1
-  fi
-
-  echo Image build successful.
-
-  # TODO: docker-compose vs. docker compose have different full-name volume flags
-  # docker-compose -> --volume
-  # docker compose -> --volumes
-  # this is dumb.
-  VOLUMES="-v ${GOOGLE_APPLICATION_CREDENTIALS}:/home/curation/project/curation/aou-res-curation-test.json"
-  echo Adding credential volume mount: \'"${VOLUMES}"\'
+  # add google cred volume to arg list
+  run_args+=("-v")
+  run_args+=("${GOOGLE_APPLICATION_CREDENTIALS}:/home/curation/project/curation/aou-res-curation-test.json")
 else
   echo Running in CI.
   # when operating in CI, utilize compose v1
   COMPOSE_EXEC="docker-compose"
 fi
 
+# finally, add any / all remaining args provided to this script as args to pass into docker
+for v in "$@"
+do
+  run_args+=("${v}")
+done
+
 # run service
-exec ${COMPOSE_EXEC} run \
-  -u "${uid}":"${gid}" \
-  ${VOLUMES} \
-  "$@"
+exec ${COMPOSE_EXEC} "${run_args[@]}"
