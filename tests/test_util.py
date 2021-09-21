c511c73649fd7d408962fa82d51a96ee7e655608
Mark Velez <mav7014@nyp.org>
2021-09-21 13:27:29 -0400

    Remove Drive-related functionality from test_util

--- tests/test_util.py
+++ tests/test_util.py
@@ -219,49 +219,6 @@ def delete_all_tables(dataset_id):
     return deleted
 
 
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-
-def get_synpuf_results_files():
-    files = [('0B8QSHCLE8g4JV1Q4UHFRLXNhM2c', 'achilles_results.csv'),
-             ('0B8QSHCLE8g4JeUUxZEh0SS1YNlk', 'achilles_results_dist.csv'),
-             ('0B8QSHCLE8g4JQUE1dGJLd1RpWEk', 'achilles_heel_results.csv')]
-    for file_id, file_name in files:
-        dest_path = os.path.join(TEST_DATA_EXPORT_SYNPUF_PATH, file_name)
-        if not os.path.exists(dest_path):
-            download_file_from_google_drive(
-                file_id, os.path.join(TEST_DATA_EXPORT_SYNPUF_PATH, file_name))
-
-
 def read_cloud_file(bucket, name):
     return gcs_utils.get_object(bucket, name)
 
