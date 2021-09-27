57f3715a40760b47e4f7452f8ebaa5089aab9e8b
Nishanth Pavinkurve <npp267@nyu.edu>
2021-09-27 18:48:38 -0400

    [DC-1855] Add some review changes

--- data_steward/tools/load_submission.py
+++ data_steward/tools/load_submission.py
@@ -3,12 +3,14 @@ Loads a submission into bq from the archive
 """
 import logging
 import argparse
+from typing import List
 
-from google.cloud.bigquery import LoadJobConfig, Table
-from google.cloud.storage import Client
+from google.cloud.bigquery import LoadJobConfig, LoadJob, Table, Client as BQClient
+from google.cloud.storage import Client as GCSClient
+from google.cloud.exceptions import GoogleCloudError
+from concurrent.futures import TimeoutError
 
 from common import AOU_REQUIRED, JINJA_ENV
-from utils import pipeline_logging
 from utils.bq import get_table_schema, get_client
 import constants.bq_utils as bq_consts
 
@@ -21,30 +23,58 @@ WHERE hpo_id = '{{hpo_id}}'
 """)
 
 
-def get_bucket(client, hpo_id):
+def get_bucket(client: BQClient, hpo_id: str) -> str:
+    """
+    Retrieves bucket name for site
+
+    :param client: Bigquery Client object
+    :param hpo_id: Identifies the HPO site
+    :return: bucket name for the HPO site as a string
+    :raises GoogleCloudError/TimeoutError
+    """
     bucket_name_query = BUCKET_NAME_QUERY.render(
         project=client.project,
         dataset=bq_consts.LOOKUP_TABLES_DATASET_ID,
         bucket_names_table=bq_consts.HPO_ID_BUCKET_NAME_TABLE_ID,
         hpo_id=hpo_id.upper())
 
-    bucket_name_result = client.query(bucket_name_query).result()
-    bucket_name = bucket_name_result.to_dataframe()['bucket_name'].to_list()[0]
+    try:
+        bucket_names_result = client.query(bucket_name_query).result()
+        bucket_names = bucket_names_result.to_dataframe(
+        )['bucket_name'].to_list()
+        if len(bucket_names) > 1:
+            LOGGER.warning(
+                f'Found more than one bucket name for site {hpo_id}: {bucket_names}'
+            )
+        bucket_name = bucket_names[0]
+    except (GoogleCloudError, TimeoutError) as e:
+        LOGGER.error(f'Job failed with error {str(e)}')
+        raise e
     return bucket_name
 
 
 def _filename_to_table_name(filename: str) -> str:
+    """
+    Converts a file path/URI to a table name
+
+    :param filename: filename or URI as /path/to/file.csv
+    :return: table_name as string, e.g. 'file' for /path/to/file.csv
+    """
     return filename.split('/')[-1].replace('.csv', '').lower()
 
 
-def load_stage(dst_dataset, bq_client, bucket_name, prefix, gcs_client, hpo_id):
+def load_stage(dst_dataset: str, bq_client: BQClient, bucket_name: str,
+               prefix: str, gcs_client: GCSClient,
+               hpo_id: str) -> List[LoadJob]:
     """
     Stage files from a bucket to a dataset
 
-    :param dst_dataset: reference to destination dataset object
+    :param dst_dataset: Identifies the destination dataset
     :param bq_client: a BigQuery client object
-    :param bucket_name: the location in GCS containing the vocabulary files
+    :param bucket_name: the bucket in GCS containing the archive files
+    :param prefix: prefix of the filepath URI
     :param gcs_client: a Cloud Storage client object
+    :param hpo_id: Identifies the HPO site
     :return: list of completed load jobs
     """
     blobs = list(gcs_client.list_blobs(bucket_name, prefix=prefix))
@@ -53,9 +83,10 @@ def load_stage(dst_dataset, bq_client, bucket_name, prefix, gcs_client, hpo_id):
     for blob in blobs:
         table_name = _filename_to_table_name(blob.name)
         if table_name not in AOU_REQUIRED:
+            LOGGER.debug(f'Skipping file for {table_name}')
             continue
         schema = get_table_schema(table_name)
-        hpo_table_name = f'{hpo_id}_{_filename_to_table_name(blob.name)}'
+        hpo_table_name = f'{hpo_id}_{table_name}'
         fq_hpo_table = f'{bq_client.project}.{dst_dataset}.{hpo_table_name}'
         destination = Table(fq_hpo_table, schema=schema)
         destination = bq_client.create_table(destination, exists_ok=True)
@@ -111,13 +142,15 @@ def get_arg_parser():
 
 def main(project_id, dataset_id, bucket_name, hpo_id, folder_name):
     bq_client = get_client(project_id)
-    gcs_client = Client(project_id)
+    gcs_client = GCSClient(project_id)
     site_bucket = get_bucket(bq_client, hpo_id)
     prefix = f'{hpo_id}/{site_bucket}/{folder_name}'
     load_stage(dataset_id, bq_client, bucket_name, prefix, gcs_client, hpo_id)
 
 
 if __name__ == '__main__':
+    from utils import pipeline_logging
+
     ARGS = get_arg_parser().parse_args()
     pipeline_logging.configure(add_console_handler=True)
     main(ARGS.project_id, ARGS.dataset_id, ARGS.bucket_name, ARGS.hpo_id,
