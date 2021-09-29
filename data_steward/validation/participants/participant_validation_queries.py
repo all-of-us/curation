3c6b7e8d483525dba4c631464fff04e05d967534
Krishna Kalluri <krishnasaikalluri@gmail.com>
2021-09-29 11:06:15 -0400

    [DC-1236] Perform DRC Phone Number Comparisons (#982)
    * [DC-1236] Add phone number comparison to participant validation
    * add integration test cases
    
    Signed-off-by: ksdkalluri <krishnasaikalluri@gmail.com>

--- data_steward/validation/participants/participant_validation_queries.py
+++ data_steward/validation/participants/participant_validation_queries.py
@@ -24,8 +24,38 @@ CREATE_EMAIL_COMPARISON_FUNCTION = JINJA_ENV.from_string("""
     ));
 """)
 
-#Contains list of create function queries to execute
+CREATE_PHONE_NUMBER_COMPARISON_FUNCTION = JINJA_ENV.from_string("""
+CREATE FUNCTION IF NOT EXISTS
+  `{{project_id}}.{{drc_dataset_id}}.ComparePhoneNumber`(rdr_phone_number string,
+    ehr_phone_number string)
+  RETURNS string AS ((
+    WITH
+      normalized_rdr_phone_number AS (
+      SELECT
+        REPLACE(REPLACE(REPLACE(REPLACE(rdr_phone_number,'-',''),'+', ''),'(',''),')','') AS rdr_phone_number ),
+      normalized_ehr_phone_number AS (
+      SELECT
+        REPLACE(REPLACE(REPLACE(REPLACE(ehr_phone_number,'-',''),'+', ''),'(',''),')','') AS ehr_phone_number )
+    SELECT
+      CASE
+        WHEN normalized_rdr_phone_number.rdr_phone_number = normalized_ehr_phone_number.ehr_phone_number THEN '{{match}}'
+        WHEN normalized_rdr_phone_number.rdr_phone_number IS NOT NULL
+      AND normalized_ehr_phone_number.ehr_phone_number IS NOT NULL THEN '{{no_match}}'
+        WHEN normalized_rdr_phone_number.rdr_phone_number IS NULL THEN '{{missing_rdr}}'
+      ELSE
+      '{{missing_ehr}}'
+    END
+      AS phone_number
+    FROM
+      normalized_rdr_phone_number,
+      normalized_ehr_phone_number ));
+""")
+
+# Contains list of create function queries to execute
 CREATE_COMPARISON_FUNCTION_QUERIES = [{
     'name': 'CompareEmail',
     'query': CREATE_EMAIL_COMPARISON_FUNCTION
+}, {
+    'name': 'ComparePhoneNumber',
+    'query': CREATE_PHONE_NUMBER_COMPARISON_FUNCTION
 }]
