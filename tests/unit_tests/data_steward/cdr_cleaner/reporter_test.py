2612e5555a3d1dad44a2e8888b8ef075749ea320
Daniel Carbone <daniel.p.carbone@vumc.org>
2021-10-18 09:35:38 -0500

    reverting unintended changes to reporter_test.py

--- tests/unit_tests/data_steward/cdr_cleaner/reporter_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/reporter_test.py
@@ -183,77 +183,77 @@ class CleanRulesReporterTest(unittest.TestCase):
             actual = reporter.get_stage_elements(data_stage, fields_list)
 
             # post conditions
-            # for class_info in actual:
-            #     keys = list(class_info.keys())
-            #     self.assertEqual(keys, fields_list)
+            for class_info in actual:
+                keys = list(class_info.keys())
+                self.assertEqual(keys, fields_list)
 
-    # def test_write_csv_report_to_non_csv_file(self):
-    #     self.assertRaises(RuntimeError, reporter.write_csv_report, 'foo.bar',
-    #                       ['rdr'], ['description'])
-    #
-    # @mock.patch('cdr_cleaner.reporter.format_values')
-    # @mock.patch('cdr_cleaner.reporter.get_stage_elements')
-    # def test_write_csv_report(self, mock_elements, mock_format):
-    #     # preconditions
-    #     fields_list = ['description']
-    #     stage_list = ['rdr']
-    #     output_file = 'foo.csv'
-    #     list_of_dicts = [{'description': 'mock desc'}]
-    #     mock_elements.return_value = list_of_dicts
-    #     mock_format.return_value = list_of_dicts
-    #     open_mock = mock.mock_open()
-    #
-    #     # test
-    #     with mock.patch("cdr_cleaner.reporter.open", open_mock, create=True):
-    #         reporter.write_csv_report(output_file, stage_list, fields_list)
-    #
-    #     # post conditions
-    #     mock_elements.assert_called_once_with(stage_list[0], fields_list)
-    #
-    #     mock_format.assert_called_once_with(list_of_dicts, fields_list)
-    #
-    #     open_mock.assert_called_once_with(output_file, 'w', newline='')
-    #     mock_writer = open_mock()
-    #     mock_writer.write.assert_any_call('"description"\n')
-    #     mock_writer.write.assert_any_call('"mock desc"\n')
-    #
-    # @mock.patch('cdr_cleaner.reporter.engine.add_console_logging')
-    # @mock.patch('cdr_cleaner.reporter.write_csv_report')
-    # def test_main(self, mock_writer, mock_logger):
-    #     # preconditions
-    #     args_list = [
-    #         '--data-stage', 'rdr', 'ehr', '--fields', 'name', 'sandbox-tables',
-    #         'description', '--output-file', 'temp.csv'
-    #     ]
-    #
-    #     # test
-    #     reporter.main(args_list)
-    #
-    #     # post conditions
-    #     mock_writer.assert_called_once_with(
-    #         'temp.csv', ['rdr', 'ehr'],
-    #         ['name', 'sandbox-tables', 'description'])
-    #     mock_logger.assert_called_once_with(False)
-    #
-    # @mock.patch('cdr_cleaner.reporter.engine.add_console_logging')
-    # @mock.patch('cdr_cleaner.reporter.write_csv_report')
-    # def test_main_unspecified(self, mock_writer, mock_logger):
-    #     # preconditions
-    #     args_list = [
-    #         '--data-stage', 'unspecified', '--fields', 'name', 'sandbox-tables',
-    #         'description', '--output-file', 'temp.csv', '-c'
-    #     ]
-    #
-    #     data_stages = [
-    #         s.value
-    #         for s in cdr_consts.DataStage
-    #         if s is not cdr_consts.DataStage.UNSPECIFIED
-    #     ]
-    #
-    #     # test
-    #     reporter.main(args_list)
-    #
-    #     # post conditions
-    #     mock_writer.assert_called_once_with(
-    #         'temp.csv', data_stages, ['name', 'sandbox-tables', 'description'])
-    #     mock_logger.assert_called_once_with(True)
+    def test_write_csv_report_to_non_csv_file(self):
+        self.assertRaises(RuntimeError, reporter.write_csv_report, 'foo.bar',
+                          ['rdr'], ['description'])
+
+    @mock.patch('cdr_cleaner.reporter.format_values')
+    @mock.patch('cdr_cleaner.reporter.get_stage_elements')
+    def test_write_csv_report(self, mock_elements, mock_format):
+        # preconditions
+        fields_list = ['description']
+        stage_list = ['rdr']
+        output_file = 'foo.csv'
+        list_of_dicts = [{'description': 'mock desc'}]
+        mock_elements.return_value = list_of_dicts
+        mock_format.return_value = list_of_dicts
+        open_mock = mock.mock_open()
+
+        # test
+        with mock.patch("cdr_cleaner.reporter.open", open_mock, create=True):
+            reporter.write_csv_report(output_file, stage_list, fields_list)
+
+        # post conditions
+        mock_elements.assert_called_once_with(stage_list[0], fields_list)
+
+        mock_format.assert_called_once_with(list_of_dicts, fields_list)
+
+        open_mock.assert_called_once_with(output_file, 'w', newline='')
+        mock_writer = open_mock()
+        mock_writer.write.assert_any_call('"description"\n')
+        mock_writer.write.assert_any_call('"mock desc"\n')
+
+    @mock.patch('cdr_cleaner.reporter.engine.add_console_logging')
+    @mock.patch('cdr_cleaner.reporter.write_csv_report')
+    def test_main(self, mock_writer, mock_logger):
+        # preconditions
+        args_list = [
+            '--data-stage', 'rdr', 'ehr', '--fields', 'name', 'sandbox-tables',
+            'description', '--output-file', 'temp.csv'
+        ]
+
+        # test
+        reporter.main(args_list)
+
+        # post conditions
+        mock_writer.assert_called_once_with(
+            'temp.csv', ['rdr', 'ehr'],
+            ['name', 'sandbox-tables', 'description'])
+        mock_logger.assert_called_once_with(False)
+
+    @mock.patch('cdr_cleaner.reporter.engine.add_console_logging')
+    @mock.patch('cdr_cleaner.reporter.write_csv_report')
+    def test_main_unspecified(self, mock_writer, mock_logger):
+        # preconditions
+        args_list = [
+            '--data-stage', 'unspecified', '--fields', 'name', 'sandbox-tables',
+            'description', '--output-file', 'temp.csv', '-c'
+        ]
+
+        data_stages = [
+            s.value
+            for s in cdr_consts.DataStage
+            if s is not cdr_consts.DataStage.UNSPECIFIED
+        ]
+
+        # test
+        reporter.main(args_list)
+
+        # post conditions
+        mock_writer.assert_called_once_with(
+            'temp.csv', data_stages, ['name', 'sandbox-tables', 'description'])
+        mock_logger.assert_called_once_with(True)
