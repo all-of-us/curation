b0a7186248d9c7579d9eb56c6e92d3346ce8bbb4
Michael Schmidt <Michael.Schmidt@VUMC.org>
2021-10-27 14:40:10 -0500

    initial commit

---
+++ data_steward/gcloud/gcs/__init__.py
@@ -0,0 +1,47 @@
+"""
+Interact with Google Cloud Storage (GCS)
+"""
+# Python stl imports
+
+# Project imports
+
+# Third-party imports
+from google.api_core import page_iterator
+from google.cloud.storage.client import Client
+
+
+class StorageClient(Client):
+    """
+    An (extended) client for interfacing with GCS
+    See https://googleapis.dev/python/storage/latest/client.html
+    """
+
+    def list_sub_prefixes(self, bucket: str, prefix: str) -> list[str]:
+        """
+        List sub folders in folder specified by prefix
+
+        SO link: https://stackoverflow.com/a/59008580
+        :param bucket: GCS bucket name as string
+        :param prefix: path to directory to look into e.g. a/b/c/
+        :return: list of strings of sub-directories e.g. [a/b/c/v1/, a/b/c/v2/]
+        """
+        if not prefix.endswith('/'):
+            prefix += '/'
+
+        extra_params: dict = {
+            'projection': 'noAcl',
+            'prefix': prefix,
+            'delimiter': '/'
+        }
+
+        path: str = f'/b/{bucket}/o'
+
+        pg_iterator = page_iterator.HTTPIterator(
+            client=self,
+            api_request=self._connection.api_request,
+            path=path,
+            items_key='prefixes',
+            item_to_value=lambda _, x: x,
+            extra_params=extra_params,
+        )
+        return list(pg_iterator)
