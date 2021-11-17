0f7443f095e53ce6c3256c4f1831e33394af3442
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2021-11-17 15:42:47 -0500

    [DC-1899] Format the script with yapf
    Updated the script based on the error message in the last circleci run.

--- data_steward/tools/delete_stale_test_buckets.py
+++ data_steward/tools/delete_stale_test_buckets.py
@@ -1,15 +1,16 @@
 """
-Work in progress.
+Delete stale buckets in test environment. 
 """
 from google.cloud import storage
 from datetime import datetime, timezone
 import logging
 
 # Project imports
-from utils import pipeline_logging 
+from utils import pipeline_logging
 
 LOGGER = logging.getLogger(__name__)
 CLIENT = storage.Client(project='aou-res-curation-test')
+
 
 def _filter_stale_buckets(buckets: list, first_n: int = None):
     """
@@ -27,25 +28,32 @@ def _filter_stale_buckets(buckets: list, first_n: int = None):
 
     for bucket in buckets:
 
-        if first_n and n >= first_n: break
-        
-        if (now - bucket.time_created).days > 90 and len(list(CLIENT.list_blobs(bucket.name))) == 0:
+        if first_n and n >= first_n:
+            break
+
+        if (now - bucket.time_created).days > 90 and len(
+                list(CLIENT.list_blobs(bucket.name))) == 0:
             stale_buckets.append(bucket.name)
-            LOGGER.info(f"{n}: stale_bucket={bucket.name}, time_created={bucket.time_created}.")
+            LOGGER.info(
+                f"{n}: stale_bucket={bucket.name}, time_created={bucket.time_created}."
+            )
             n += 1
-            
+
     return stale_buckets
+
 
 def main():
 
     pipeline_logging.configure(logging.INFO, add_console_handler=True)
 
-    stale_buckets = _filter_stale_buckets(buckets=CLIENT.list_buckets(), first_n=200)
+    stale_buckets = _filter_stale_buckets(buckets=CLIENT.list_buckets(),
+                                          first_n=200)
 
     for stale_bucket in stale_buckets:
         LOGGER.info(f"Running - CLIENT.get_bucket({stale_bucket}).delete()")
         # Uncomment the following before release:
         # CLIENT.get_bucket(stale_bucket).delete()
+
 
 if __name__ == "__main__":
     main()
