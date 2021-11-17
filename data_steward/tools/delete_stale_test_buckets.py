0567373279583d120dc2e002088a128a70a1ed9a
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2021-11-17 13:38:16 -0500

    [DC-1899] Add new script, update config
    Updated config.yml for circleci, and added a new python script for deleting stale test buckets. The delete command in this script is currently commented out.

---
+++ data_steward/tools/delete_stale_test_buckets.py
@@ -0,0 +1,51 @@
+"""
+Work in progress.
+"""
+from google.cloud import storage
+from datetime import datetime, timezone
+import logging
+
+# Project imports
+from utils import pipeline_logging 
+
+LOGGER = logging.getLogger(__name__)
+CLIENT = storage.Client(project='aou-res-curation-test')
+
+def _filter_stale_buckets(buckets: list, first_n: int = None):
+    """
+    Given a list of buckets, get the first n buckets that meet all of the following criteria:
+    1. Buckets older than 90 days
+    2. Empty buckets
+
+    :param buckets: list of buckets to filter
+    :param first_n: number of buckets to return. If not specified, return all.
+    :return: list of bucket names that are stale
+    """
+    stale_buckets, n = [], 0
+
+    now = datetime.now(timezone.utc)
+
+    for bucket in buckets:
+
+        if first_n and n >= first_n: break
+        
+        if (now - bucket.time_created).days > 90 and len(list(CLIENT.list_blobs(bucket.name))) == 0:
+            stale_buckets.append(bucket.name)
+            LOGGER.info(f"{n}: stale_bucket={bucket.name}, time_created={bucket.time_created}.")
+            n += 1
+            
+    return stale_buckets
+
+def main():
+
+    pipeline_logging.configure(logging.INFO, add_console_handler=True)
+
+    stale_buckets = _filter_stale_buckets(buckets=CLIENT.list_buckets(), first_n=200)
+
+    for stale_bucket in stale_buckets:
+        LOGGER.info(f"Running - CLIENT.get_bucket({stale_bucket}).delete()")
+        # Uncomment the following before release:
+        # CLIENT.get_bucket(stale_bucket).delete()
+
+if __name__ == "__main__":
+    main()
