6c59309327e4426b71e3e706226648f6d0da20b6
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2021-12-06 18:15:52 -0500

    [DC-1901] Update docker command script for circleci

--- docker/develop/commands/delete-stale-test-datasets.sh
+++ docker/develop/commands/delete-stale-test-datasets.sh
@@ -4,4 +4,4 @@ set -e
 
 CMD_NAME="delete-stale-test-datasets"
 
-python3 /home/circleci/project/data_steward/tools/delete_stale_test_datasets.py
+python3 ./data_steward/tools/delete_stale_test_datasets.py
