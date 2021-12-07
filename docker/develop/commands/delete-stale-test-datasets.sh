dc0ff31d91462910f6e51e2e5b71bd3eda906dd5
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2021-12-07 11:20:25 -0500

    [DC-1901] Update docker command script

--- docker/develop/commands/delete-stale-test-datasets.sh
+++ docker/develop/commands/delete-stale-test-datasets.sh
@@ -10,5 +10,6 @@ source "${CURATION_SCRIPTS_DIR}"/funcs.sh
 echo "Initializing envvars..."
 require_ok "run-tests/00_init_env.sh"
 require_ok "run-tests/10_prep_output_paths.sh"
+activate_gcloud
 
 python3 ./data_steward/tools/delete_stale_test_datasets.py
