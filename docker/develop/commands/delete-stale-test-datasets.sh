76c7edb26ada68e617fb9b86f176fb6ec9dd37f9
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2021-12-09 08:04:36 -0500

    [DC-1901] Cleaned up shell script based on Codacy static code analysis

--- docker/develop/commands/delete-stale-test-datasets.sh
+++ docker/develop/commands/delete-stale-test-datasets.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-CMD_NAME="delete-stale-test-datasets"
-
 source "${CURATION_SCRIPTS_DIR}"/funcs.sh
 
 echo "Initializing envvars..."
 require_ok "run-tests/00_init_env.sh"
-require_ok "run-tests/10_prep_output_paths.sh" # Is this needed?
+require_ok "run-tests/10_prep_output_paths.sh"
 activate_gcloud
 
 python3 ./data_steward/tools/delete_stale_test_datasets.py
