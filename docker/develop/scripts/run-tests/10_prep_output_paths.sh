bffdaedc6d2a6220f1c24861d4ced0a12ea0f52c
Nishanth Pavinkurve <npp267@nyu.edu>
2021-12-10 17:05:11 -0500

    [DC-2022] Move check for filepaths

--- docker/develop/scripts/run-tests/10_prep_output_paths.sh
+++ docker/develop/scripts/run-tests/10_prep_output_paths.sh
@@ -3,6 +3,11 @@
 # don't suppress errors
 set -e
 
+# If env var is set containing test filepaths, include it and update paths to mount location
+if [[ -n "${CURATION_TESTS_FILEPATH}" ]]; then
+  sed -i 's/.*curation/\./g' "${CURATION_TESTS_FILEPATH}"
+fi
+
 # store test results in junit format to allow CircleCI Test Summary reporting
 #  https://circleci.com/docs/2.0/collect-test-data/
 mkdir -p tests/results/coverage/unit/xml \
