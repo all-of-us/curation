046595ddd439b13490cc5484b16050805abda558
Lauren Biggers <lauren.r.biggers@vumc.org>
2021-12-20 11:32:10 -0600

    [DC-2050] yapf formatting to fix main_test formatting issues.

--- tests/unit_tests/data_steward/validation/main_test.py
+++ tests/unit_tests/data_steward/validation/main_test.py
@@ -327,12 +327,12 @@ class ValidationMainTest(TestCase):
     @mock.patch('gcs_utils.list_bucket')
     @mock.patch('gcs_utils.get_hpo_bucket')
     def test_process_hpo_ignore_dirs(
-            self, mock_hpo_bucket, mock_bucket_list, mock_valid_rdr,
-            mock_first_validation, mock_has_all_required_files,
-            mock_folder_items, mock_validation, mock_get_hpo_name,
-            mock_upload_string_to_gcs, mock_get_duplicate_counts_query,
-            mock_query_rows, mock_all_required_files_loaded, mock_upload,
-            mock_run_achilles, mock_export, mock_valid_folder_name, mock_query):
+        self, mock_hpo_bucket, mock_bucket_list, mock_valid_rdr,
+        mock_first_validation, mock_has_all_required_files, mock_folder_items,
+        mock_validation, mock_get_hpo_name, mock_upload_string_to_gcs,
+        mock_get_duplicate_counts_query, mock_query_rows,
+        mock_all_required_files_loaded, mock_upload, mock_run_achilles,
+        mock_export, mock_valid_folder_name, mock_query):
         """
         Test process_hpo with directories we want to ignore.
 
