58121b7102c2fd961d12bd0ae5835b1f903d6f16
lledezma <luis.ledezma@vumc.org>
2022-01-14 12:52:52 -0600

    [DC-2069] Merge branch 'develop' into ll/dc-2069

--- data_steward/validation/main.py
+++ data_steward/validation/main.py
@@ -69,7 +69,8 @@ def save_datasources_json(datasource_id=None,
         bucket assigned to hpo_id.
     :return:
     """
-    storage_client = StorageClient()
+    project_id = app_identity.get_application_id()
+    storage_client = StorageClient(project_id)
     if datasource_id is None:
         if target_bucket is None:
             raise RuntimeError(
@@ -100,7 +101,8 @@ def run_export(datasource_id=None, folder_prefix="", target_bucket=None):
     :param target_bucket: Bucket to save report. If None, use bucket associated with hpo_id.
     """
     results = []
-    storage_client = StorageClient()
+    project_id = app_identity.get_application_id()
+    storage_client = StorageClient(project_id)
     # Using separate var rather than hpo_id here because hpo_id None needed in calls below
     if datasource_id is None and target_bucket is None:
         raise RuntimeError(
