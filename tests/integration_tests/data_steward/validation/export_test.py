8d7f5b7ff55249be7d63208b7c5b6d969ef0574d
lledezma <luis.ledezma@vumc.org>
2022-01-26 11:35:07 -0600

    [DC-2069] Add Bucket import

--- tests/integration_tests/data_steward/validation/export_test.py
+++ tests/integration_tests/data_steward/validation/export_test.py
@@ -9,6 +9,7 @@ import app_identity
 import bq_utils
 import common
 from gcloud.gcs import StorageClient
+from google.cloud.storage.bucket import Bucket
 from tests import test_util
 from tests.test_util import FAKE_HPO_ID
 from validation import export, main
@@ -31,7 +32,6 @@ class ExportTest(unittest.TestCase):
     def setUp(self):
         self.project_id = app_identity.get_application_id()
         self.storage_client = StorageClient(self.project_id)
-
         self.hpo_bucket = self.storage_client.get_hpo_bucket(FAKE_HPO_ID)
 
     def _test_report_export(self, report):
@@ -125,13 +125,11 @@ class ExportTest(unittest.TestCase):
         # validation/main.py INTEGRATION TEST
         mock_is_hpo_id.return_value = True
         folder_prefix: str = 'dummy-prefix-2018-03-24/'
-
-        target_bucket = self.storage_client.get_hpo_bucket('nyc')
+        target_bucket: Bucket = self.storage_client.get_hpo_bucket('nyc')
         objects: Iterable = target_bucket.list_blobs()
         main.run_export(datasource_id=FAKE_HPO_ID,
                         folder_prefix=folder_prefix,
                         target_bucket=target_bucket.name)
-
         actual_names: list = [obj.name for obj in objects]
         for report in common.ALL_REPORT_FILES:
             prefix: str = f'{folder_prefix}{common.ACHILLES_EXPORT_PREFIX_STRING}{FAKE_HPO_ID}/'
