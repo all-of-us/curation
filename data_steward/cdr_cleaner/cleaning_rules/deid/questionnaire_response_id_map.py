5399f4ea7618d2bc370cc5593df847ad6097d2b5
jp3477 <jp3477@cumc.columbia.edu>
2022-02-16 11:15:54 -0500

    moved logic and fixed unit test

--- data_steward/cdr_cleaner/cleaning_rules/deid/questionnaire_response_id_map.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/questionnaire_response_id_map.py
@@ -14,7 +14,7 @@ import logging
 
 # Project imports
 from utils import pipeline_logging
-from common import OBSERVATION, JINJA_ENV
+from common import OBSERVATION, DEID_QUESTIONNAIRE_RESPONSE_MAP, JINJA_ENV
 from constants.bq_utils import WRITE_TRUNCATE
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
@@ -22,20 +22,6 @@ from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 LOGGER = logging.getLogger(__name__)
 
 ISSUE_NUMBERS = ['DC1347', 'DC518']
-
-# Creates _deid_questionnaire_response_map lookup table and populates with the questionnaire_response_id's
-# from the observation table as well as randomly generates values for hte research_response_id column
-LOOKUP_TABLE_CREATION_QUERY = JINJA_ENV.from_string("""
-CREATE TABLE IF NOT EXISTS `{{project_id}}.{{shared_sandbox_id}}._deid_questionnaire_response_map` 
-(questionnaire_response_id INT64, research_response_id INT64)
-OPTIONS (description='lookup table for questionnaire response ids') AS
--- 1000000 used to start the research_response_id generation at 1mil --
-SELECT DISTINCT questionnaire_response_id AS questionnaire_response_id, 1000000 + ROW_NUMBER() 
-    OVER(ORDER BY GENERATE_UUID()) AS research_response_id
-FROM `{{project_id}}.{{dataset_id}}.observation`
-WHERE questionnaire_response_id IS NOT NULL
-GROUP BY questionnaire_response_id
-""")
 
 # Map the research_response_id from _deid_questionnaire_response_map lookup table to the questionnaire_response_id in
 # the observation table
@@ -45,7 +31,7 @@ SELECT
     d.research_response_id as questionnaire_response_id,
 FROM
     `{{project_id}}.{{dataset_id}}.observation` t
-LEFT JOIN `{{project_id}}.{{shared_sandbox_id}}._deid_questionnaire_response_map` d
+LEFT JOIN `{{project_id}}.{{deid_questionnaire_response_map_dataset_id}}.{{deid_questionnaire_response_map}}` d
 ON t.questionnaire_response_id = d.questionnaire_response_id
 """)
 
@@ -60,7 +46,8 @@ class QRIDtoRID(BaseCleaningRule):
                  project_id,
                  dataset_id,
                  sandbox_dataset_id,
-                 table_namer=None):
+                 table_namer=None,
+                 deid_questionnaire_response_map_dataset=None):
         """
         Initialize the class with proper info.
 
@@ -71,6 +58,13 @@ class QRIDtoRID(BaseCleaningRule):
         desc = f'Create a deid questionnaire response mapping lookup table and remap the QID ' \
                f'(questionnaire_response_id) from the observation table to the RID (research_response_id) found in ' \
                f'the deid questionnaire response mapping lookup table.'
+
+        if not deid_questionnaire_response_map_dataset:
+            raise TypeError(
+                "`deid_questionnaire_response_map_dataset` cannot be empty")
+
+        self.deid_questionnaire_response_map_dataset = deid_questionnaire_response_map_dataset
+
         super().__init__(issue_numbers=ISSUE_NUMBERS,
                          description=desc,
                          affected_datasets=[
@@ -92,20 +86,15 @@ class QRIDtoRID(BaseCleaningRule):
             The specifications are optional but the query is required.
         """
 
-        lookup_table_creation = {
-            cdr_consts.QUERY:
-                LOOKUP_TABLE_CREATION_QUERY.render(
-                    project_id=self.project_id,
-                    shared_sandbox_id=self.sandbox_dataset_id,
-                    dataset_id=self.dataset_id)
-        }
-
         mapping_query = {
             cdr_consts.QUERY:
                 QRID_RID_MAPPING_QUERY.render(
                     project_id=self.project_id,
                     dataset_id=self.dataset_id,
-                    shared_sandbox_id=self.sandbox_dataset_id),
+                    deid_questionnaire_response_map=
+                    DEID_QUESTIONNAIRE_RESPONSE_MAP,
+                    deid_questionnaire_response_map_dataset_id=self.
+                    deid_questionnaire_response_map_dataset),
             cdr_consts.DESTINATION_TABLE:
                 OBSERVATION,
             cdr_consts.DESTINATION_DATASET:
@@ -114,7 +103,7 @@ class QRIDtoRID(BaseCleaningRule):
                 WRITE_TRUNCATE
         }
 
-        return [lookup_table_creation, mapping_query]
+        return [mapping_query]
 
     def setup_rule(self, client, *args, **keyword_args):
         """
@@ -142,18 +131,33 @@ if __name__ == '__main__':
     import cdr_cleaner.args_parser as parser
     import cdr_cleaner.clean_cdr_engine as clean_engine
 
-    ARGS = parser.parse_args()
+    ext_parser = parser.get_argument_parser()
+    ext_parser.add_argument(
+        '-q',
+        '--deid_questionnaire_response_map_dataset',
+        action='store',
+        dest='deid_questionnaire_response_map_dataset',
+        help=
+        'Identifies the dataset containing the _deid_questionnaire_response_map lookup table',
+        required=True)
+    ARGS = ext_parser.parse_args()
+
     pipeline_logging.configure(level=logging.DEBUG, add_console_handler=True)
 
     if ARGS.list_queries:
         clean_engine.add_console_logging()
-        query_list = clean_engine.get_query_list(ARGS.project_id,
-                                                 ARGS.dataset_id,
-                                                 ARGS.sandbox_dataset_id,
-                                                 [(QRIDtoRID,)])
+        query_list = clean_engine.get_query_list(
+            ARGS.project_id,
+            ARGS.dataset_id,
+            ARGS.sandbox_dataset_id, [(QRIDtoRID,)],
+            deid_questionnaire_response_map_dataset=ARGS.
+            deid_questionnaire_response_map_dataset)
         for query in query_list:
             LOGGER.info(query)
     else:
         clean_engine.add_console_logging(ARGS.console_log)
-        clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
-                                   ARGS.sandbox_dataset_id, [(QRIDtoRID,)])
+        clean_engine.clean_dataset(ARGS.project_id,
+                                   ARGS.dataset_id,
+                                   ARGS.sandbox_dataset_id, [(QRIDtoRID,)],
+                                   deid_questionnaire_response_map_dataset=ARGS.
+                                   deid_questionnaire_response_map_dataset)
