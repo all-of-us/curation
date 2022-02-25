9f38b3d0ae542af872894ae8a7e917c6d3fb33c0
lledezma <luis.ledezma@vumc.org>
2022-02-25 11:32:07 -0600

    [DC-2179] Add test_define_dataset method

--- tests/integration_tests/data_steward/gcloud/bq_client_test.py
+++ tests/integration_tests/data_steward/gcloud/bq_client_test.py
@@ -3,6 +3,8 @@ Test the Google Cloud Big Query Client and associated helper functions
 """
 # Python stl imports
 from unittest import TestCase
+from unittest.mock import patch
+import os
 
 # Project imports
 import app_identity
@@ -20,3 +22,19 @@ class BqClientTest(TestCase):
     def setUp(self):
         self.project_id = app_identity.get_application_id()
         self.bq_client = BigQueryClient(self.project_id)
+        unioned_dataset_id = os.environ.get('UNIONED_DATASET_ID')
+        self.dataset_id = f'{unioned_dataset_id}_bq_test'
+        self.description = f'Dataset for {__name__} integration tests'
+        self.label_or_tag = {'test': 'bq'}
+
+    def test_define_dataset(self):
+        self.assertRaises(RuntimeError, self.bq_client.define_dataset, False,
+                          self.description, self.label_or_tag)
+        self.assertRaises(RuntimeError, self.bq_client.define_dataset,
+                          self.dataset_id, ' ', self.label_or_tag)
+        self.assertRaises(RuntimeError, self.bq_client.define_dataset,
+                          self.dataset_id, self.description, None)
+        dataset = self.bq_client.define_dataset(self.dataset_id,
+                                                self.description,
+                                                self.label_or_tag)
+        self.assertEqual(dataset.dataset_id, self.dataset_id)
