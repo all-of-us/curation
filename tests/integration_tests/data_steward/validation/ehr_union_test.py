73a4775c90c442654fe9b9f8d09014c60dcc626c
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2022-03-07 08:23:02 -0500

    [DC-2220] Update test

--- tests/integration_tests/data_steward/validation/ehr_union_test.py
+++ tests/integration_tests/data_steward/validation/ehr_union_test.py
@@ -117,6 +117,9 @@ class EhrUnionTest(unittest.TestCase):
                     expected_tables[output_table] += list(csv_rows)
                 elif hpo_id != EXCLUDED_HPO_ID and cdm_table == common.VISIT_DETAIL:
                     mapping_table: str = ehr_union.mapping_table_for(cdm_table)
+                    if mapping_table not in expected_tables:
+                        expected_tables[mapping_table] = []
+
                     expected_tables[mapping_table] += list(csv_rows)
                     # Rows with invalid visit_occurrence_id are included in
                     # the mapping table for visit_detail, but those are excluded
