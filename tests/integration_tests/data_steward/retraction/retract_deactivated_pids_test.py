944b4ed65a03731fad277b36dba50ada6749bfc4
lledezma <luis.ledezma@vumc.org>
2022-03-08 15:59:39 -0600

    [DC-2185] Replace bq.get_client call, rename client object

--- tests/integration_tests/data_steward/retraction/retract_deactivated_pids_test.py
+++ tests/integration_tests/data_steward/retraction/retract_deactivated_pids_test.py
@@ -12,6 +12,7 @@ import app_identity
 from common import JINJA_ENV
 from retraction import retract_deactivated_pids as rdp
 from utils import bq, sandbox as sb
+from gcloud.bq import BigQueryClient
 
 DEACTIVATED_PIDS = JINJA_ENV.from_string("""
 INSERT INTO `{{deact_table.project}}.{{deact_table.dataset_id}}.{{deact_table.table_id}}` 
@@ -116,7 +117,7 @@ class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
         self.dataset_id = os.environ.get('UNIONED_DATASET_ID')
         self.deact_dataset_id = os.environ.get('COMBINED_DATASET_ID')
         self.deact_table = f'{self.project_id}.{self.deact_dataset_id}._deactivated_participants'
-        self.client = bq.get_client(self.project_id)
+        self.bq_client = BigQueryClient(self.project_id)
         self.bq_sandbox_dataset_id = sb.get_sandbox_dataset_id(self.dataset_id)
         self.tables = {**TABLE_ROWS, **MAPPING_TABLE_ROWS, **EXT_TABLE_ROWS}
         self.setup_data()
@@ -125,30 +126,30 @@ class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
         self.tearDown()
         # setup deactivated participants table
         deact_table_ref = gbq.TableReference.from_string(self.deact_table)
-        bq.create_tables(self.client,
+        bq.create_tables(self.bq_client,
                          self.project_id, [self.deact_table],
                          exists_ok=True)
         job_config = gbq.QueryJobConfig()
-        job = self.client.query(
+        job = self.bq_client.query(
             DEACTIVATED_PIDS.render(deact_table=deact_table_ref), job_config)
         job.result()
 
         # create omop tables and mapping/ext tables
         for table in self.tables:
             fq_table = f'{self.project_id}.{self.dataset_id}.{table}'
-            bq.create_tables(self.client,
+            bq.create_tables(self.bq_client,
                              self.project_id, [fq_table],
                              exists_ok=True)
             table_ref = gbq.TableReference.from_string(fq_table)
             job_config = gbq.QueryJobConfig()
-            job = self.client.query(self.tables[table].render(table=table_ref),
-                                    job_config)
+            job = self.bq_client.query(
+                self.tables[table].render(table=table_ref), job_config)
             job.result()
 
     @mock.patch('retraction.retract_utils.is_deid_label_or_id')
     def test_queries_to_retract_from_fake_dataset(self, mock_deid):
         mock_deid.return_value = False
-        rdp.run_deactivation(self.client, self.project_id, [self.dataset_id],
+        rdp.run_deactivation(self.bq_client, self.project_id, [self.dataset_id],
                              self.deact_table)
         person_cols = [
             'person_id', 'gender_concept_id', 'year_of_birth', 'birth_datetime',
@@ -198,7 +199,7 @@ class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
         for table in TABLE_ROWS:
             query = f"SELECT * FROM `{self.project_id}.{self.dataset_id}.{table}`"
             job_config = gbq.QueryJobConfig()
-            job = self.client.query(query, job_config)
+            job = self.bq_client.query(query, job_config)
             result_df = job.result().to_dataframe()
             result_df = result_df.dropna(how='all', axis=1)
             date_cols = [col for col in result_df.columns if 'date' in col]
@@ -217,8 +218,8 @@ class RetractDeactivatedEHRDataBqTest(unittest.TestCase):
         for table in self.tables:
             fq_table = f'{self.project_id}.{self.dataset_id}.{table}'
             table_ref = gbq.TableReference.from_string(fq_table)
-            self.client.delete_table(table_ref, not_found_ok=True)
-        self.client.delete_table(self.deact_table, not_found_ok=True)
-        self.client.delete_dataset(self.bq_sandbox_dataset_id,
-                                   delete_contents=True,
-                                   not_found_ok=True)
+            self.bq_client.delete_table(table_ref, not_found_ok=True)
+        self.bq_client.delete_table(self.deact_table, not_found_ok=True)
+        self.bq_client.delete_dataset(self.bq_sandbox_dataset_id,
+                                      delete_contents=True,
+                                      not_found_ok=True)
