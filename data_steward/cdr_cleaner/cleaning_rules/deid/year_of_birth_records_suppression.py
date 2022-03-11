2ee4b2f5f5143f5ee33c2b90823953a4fde88938
molly-giddings <molly.giddings@vumc.org>
2022-03-10 18:17:51 -0600

    [DC-2205] Add cleaning rule for CT from DC-1977 for DOB related dates

---
+++ data_steward/cdr_cleaner/cleaning_rules/deid/year_of_birth_records_suppression.py
@@ -0,0 +1,292 @@
+"""
+Sandbox and record suppress all records with a concept_id or concept_code relating to delivery or birthing concepts.
+
+Original Issue: DC-1977
+
+suppress all records (specifically in the CT) if a concept is associated with a delivery or birth concept
+and the record date/start_date is on a participant's birthday.
+and also covers all the mapped standard concepts for non standard concepts that the regex filters.
+"""
+
+# Python Imports
+import logging
+
+# Third Party Imports
+from google.cloud.exceptions import GoogleCloudError
+
+# Project Imports
+from common import AOU_REQUIRED, JINJA_ENV
+import constants.cdr_cleaner.clean_cdr as cdr_consts
+from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
+
+LOGGER = logging.getLogger(__name__)
+ISSUE_NUMBERS = ['DC1977', 'DC2205']
+BIRTH_DELIVERY_SUPPRESSION_CONCEPT_TABLE = '_birth_concepts'
+
+EXCLUDED_CONCEPTS = [4013886, 4135376, 4271761]
+all_concept_ids = [str(x) for x in EXCLUDED_CONCEPTS]
+all_concept_ids = ",".join(all_concept_ids)
+
+LOOKUP_TABLE = 'birth_columns_lookup'
+
+
+class YearOfBirthRecordsSuppression(BaseCleaningRule):
+
+    def __init__(self, project_id, dataset_id, sandbox_dataset_id, table_namer=''):
+        """
+        Initialize the class with proper info.
+
+        Set the issue numbers, description and affected datasets.  As other
+        tickets may affect this SQL, append them to the list of Jira Issues.
+        DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
+        """
+        desc = (
+            'Sandbox and record suppress all records within one year of a '
+            'participant\'s birth.')
+        super().__init__(issue_numbers=ISSUE_NUMBERS,
+                         description=desc,
+                         affected_datasets=[cdr_consts.CONTROLLED_TIER_DEID_BASE, cdr_consts.CONTROLLED_TIER_DEID_CLEAN],
+                         project_id=project_id,
+                         dataset_id=dataset_id,
+                         sandbox_dataset_id=sandbox_dataset_id,
+                         affected_tables=AOU_REQUIRED,
+                         table_namer=table_namer)
+
+        # default values
+        self.tables_and_columns = {}
+        self.observation_concept_id_columns = []
+
+    def get_sandbox_tablenames(self):
+        return [self.sandbox_table_for(table) for table in AOU_REQUIRED]
+
+
+    def setup_rule(self, client):
+        self._get_time_columns(client)
+        self._get_observation_concept_id_columns(client)
+
+    def _get_observation_concept_id_columns(self, client):
+        tables_columns_query_template = JINJA_ENV.from_string("""
+        SELECT 
+           column_name
+         FROM
+           `{{project}}.{{dataset}}.INFORMATION_SCHEMA.COLUMNS`
+         WHERE
+            (lower(data_type) in ("int64") and REGEXP_CONTAINS(column_name, r'(?i)(concept_id)'))
+            -- tables we are not interested in cleaning --
+           and lower(table_name) = 'observation'
+        """)
+        tables_columns_query = tables_columns_query_template.render(
+            project=self.project_id,
+            dataset=self.dataset_id
+            )
+
+        try:
+            response = client.query(tables_columns_query,
+                                    job_id_prefix='ct_yob_setup_')
+        except (GoogleCloudError, TOError) as exc:
+            raise exc
+        else:
+            response_list = list(response.result())
+
+            # move all the columns to key/value dictionary list for JINJA templating
+            new_response_list = []
+            for item in response_list:
+                new_response_list.append(item[0])
+
+        self.observation_concept_id_columns = new_response_list
+
+    def _get_time_columns(self, client):
+        tables_columns_query_template = JINJA_ENV.from_string("""
+        CREATE OR REPLACE TABLE `{{project}}.{{sandbox_dataset}}.{{lookup_table}}` AS (
+            SELECT 
+               table_name, column_name
+             FROM
+               `{{project}}.{{dataset}}.INFORMATION_SCHEMA.COLUMNS`
+             WHERE
+                (lower(data_type) in ("date", "datetime", "time", "timestamp") and not REGEXP_CONTAINS(column_name, r'(?i)(partitiontime)'))
+                -- tables we are not interested in cleaning --
+               and not REGEXP_CONTAINS(table_name, '(?i)(_ext)|(person)|(activity_summary)|(steps_intraday)|(heart_rate)')
+
+               -- make sure table has a person_id column --
+                and table_name in (
+                    select table_name 
+                    from `{{project}}.{{dataset}}.INFORMATION_SCHEMA.COLUMNS` 
+                    where lower(column_name) = 'person_id'
+                )
+            ORDER BY 1,2
+        )
+        """)
+        tables_columns_query = tables_columns_query_template.render(
+            project=self.project_id,
+            dataset=self.dataset_id,
+            sandbox_dataset=self.sandbox_dataset_id,
+            lookup_table=self.sandbox_table_for(LOOKUP_TABLE)
+            )
+
+        try:
+            client.query(tables_columns_query,
+                         job_id_prefix='ct_yob_setup_')
+        except (GoogleCloudError, TOError) as exc:
+            raise exc
+        else:
+            hold_response_template = JINJA_ENV.from_string("""
+                SELECT table_name, column_name
+                FROM `{{project}}.{{sandbox_dataset}}.{{lookup_table}}`""")
+
+            hold_response_query = hold_response_template.render(
+                project=self.project_id,
+                sandbox_dataset=self.sandbox_dataset_id,
+                lookup_table=self.sandbox_table_for(LOOKUP_TABLE)
+            )
+
+            try:
+                response = client.query(hold_response_query,
+                                        job_id_prefix='ct_yob_setup_get_')
+            except (GoogleCloudError, TOError) as exc:
+                raise exc
+            else:
+                response_list = list(response.result())
+                print(response_list)
+
+                # move all the columns to key/value dictionary list for JINJA templating
+                response_dict = {}
+                for item in response_list:
+                    table_name = item[0]
+                    column_name = item[1]
+                    current_value = response_dict.get(table_name, [])
+                    current_value.append(column_name)
+                    response_dict[table_name] = current_value
+
+        self.tables_and_columns = response_dict
+
+    def get_sandbox_queries(self):
+        """
+        Sandbox records in the given table whose concept id fields contain any concepts in the
+        suppression concept table
+
+        :param table_name:
+        :return:
+        """
+        bq_lookup_table_sandbox_query_template = JINJA_ENV.from_string("""
+            CREATE OR REPLACE TABLE `{{project}}.{{sandbox_dataset}}.{{sandbox_table}}` AS (
+                SELECT
+                  d.*
+                FROM `{{project}}.{{dataset}}.{{domain_table}}` AS d
+                JOIN `{{project}}.{{dataset}}.person` AS p
+                USING (person_id)
+                WHERE ( 
+                {% for column in columns_list %}
+                    {% if loop.index > 1 %}
+                        OR
+                    {% endif %}
+                    DATE(d.{{column}}) < DATE(CONCAT(p.year_of_birth + 2, '-01-01'))
+                {% endfor %}
+                )
+                {% if domain_table == 'observation' %}
+                    AND (
+                    {% for column in obs_columns %}
+                        {% if loop.index > 1 %}
+                         AND
+                        {% endif %}
+                        {{column}} not in ({{exceptions}})
+                    {% endfor %}
+                    )
+                {% endif %}
+            )
+        """)
+
+        sandbox_queries = []
+        print(f"\n--------\n{self.tables_and_columns}\n------\n")
+        for table_name, columns_list in self.tables_and_columns.items():
+            suppression_record_sandbox_query = bq_lookup_table_sandbox_query_template.render(
+                project=self.project_id,
+                dataset=self.dataset_id,
+                sandbox_dataset=self.sandbox_dataset_id,
+                sandbox_table=self.sandbox_table_for(table_name),
+                domain_table=table_name,
+                columns_list=columns_list,
+                obs_columns=self.observation_concept_id_columns,
+                exceptions=all_concept_ids)
+
+            sandbox_queries.append({
+                cdr_consts.QUERY: suppression_record_sandbox_query
+            })
+
+        return sandbox_queries
+
+    def get_suppression_queries(self):
+        suppression_record_query_template = JINJA_ENV.from_string("""
+        DELETE
+        FROM `{{project}}.{{dataset}}.{{domain_table}}` d 
+        WHERE d.{{domain_table}}_id in (
+            SELECT
+                distinct {{domain_table}}_id
+            FROM
+                `{{project}}.{{sandbox_dataset}}.{{sandbox_table}}`)
+        """)
+
+        suppression_queries = []
+        for table_name, _ in self.tables_and_columns.items():
+            suppression_record_query = suppression_record_query_template.render(
+                project=self.project_id,
+                dataset=self.dataset_id,
+                sandbox_dataset=self.sandbox_dataset_id,
+                domain_table=table_name,
+                sandbox_table=self.sandbox_table_for(table_name))
+
+            suppression_queries.append({
+                cdr_consts.QUERY: suppression_record_query,
+            })
+
+        return suppression_queries
+
+    def get_query_specs(self):
+
+        sandbox_queries = self.get_sandbox_queries()
+        suppression_queries = self.get_suppression_queries()
+
+        all_queries = sandbox_queries + suppression_queries
+        print("\n\n------------------------------------------------\n\n")
+
+        for q in all_queries:
+            print(f'{q.get("query")};\n')
+
+        return all_queries
+
+    def setup_validation(self, client, *args, **keyword_args):
+        pass
+
+    def validate_rule(self, client, *args, **keyword_args):
+        pass
+
+
+if __name__ == '__main__':
+    import cdr_cleaner.args_parser as parser
+    from utils import pipeline_logging
+    import cdr_cleaner.clean_cdr_engine as clean_engine
+
+    pipeline_logging.configure(level=logging.DEBUG, add_console_handler=True)
+
+    additional_args = [{
+                        parser.SHORT_ARGUMENT: '-a',
+                        parser.LONG_ARGUMENT: '--data-stage',
+                        parser.DEST: 'data_stage',
+                        parser.ACTION: 'store',
+                        parser.HELP: 'The data stage you will execute on.',
+                        parser.REQUIRED: True}]
+    ARGS = parser.default_parse_args(additional_args)
+
+    if ARGS.list_queries:
+        clean_engine.add_console_logging()
+        query_list = clean_engine.get_query_list(
+            ARGS.project_id, ARGS.dataset_id, ARGS.sandbox_dataset_id,
+            [(YearOfBirthRecordsSuppression,)],
+            ARGS.data_stage)
+        for query in query_list:
+            LOGGER.info(query)
+    else:
+        clean_engine.add_console_logging(ARGS.console_log)
+        clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
+                                   ARGS.sandbox_dataset_id,
+                                   [(YearOfBirthRecordsSuppression,)],
+                                   ARGS.data_stage)
---
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/deid/suppress_year_of_birth_records_test.py
@@ -0,0 +1,162 @@
+"""
+Integration test for year_of_birth_records_suppression module
+
+Original Issues: DC-1977, DC-2205
+
+The intent is to suppress all records (specifically in the CT) if a concept is associated with a delivery or birth concept
+and the record date/start_date is within a participant's year of birth."""
+
+# Python Imports
+import os
+import datetime
+
+# Project Imports
+from common import VISIT_OCCURRENCE, OBSERVATION
+from common import JINJA_ENV
+from app_identity import PROJECT_ID
+from cdr_cleaner.cleaning_rules.deid.year_of_birth_records_suppression import \
+    YearOfBirthRecordsSuppression
+from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import \
+    BaseTest
+
+AFFECTED_TABLES = ['observation', 'visit_occurrence', 'person']
+
+# Required fields for person table
+PERSON_DATA_TEMPLATE = JINJA_ENV.from_string("""
+INSERT INTO `{{project_id}}.{{dataset_id}}.person`
+(person_id, birth_datetime, gender_concept_id, year_of_birth, race_concept_id, ethnicity_concept_id)
+VALUES
+      /* Adding participants with different ranges of birthdays.*/
+      /* The data belonging to participants with delivery or birth concepts with a start date < birth year + 2 */
+      /* should be dropped from all the domain tables.*/  
+      (1, '1970-01-01 00:00:00 UTC', 0, 1970, 0, 0),
+      (2, '2002-01-01 00:00:00 UTC', 0, 2002, 0, 0),
+      (3, '2003-01-01 00:00:00 UTC', 0, 2003, 0, 0),
+      (4, '2015-01-01 00:00:00 UTC', 0, 2015, 0, 0)
+""")
+VISIT_OCCURRENCE_DATA_TEMPLATE = JINJA_ENV.from_string("""
+INSERT INTO `{{project_id}}.{{dataset_id}}.visit_occurrence`
+      (visit_occurrence_id, person_id, visit_start_date, visit_start_datetime, visit_end_date, visit_end_datetime, 
+      visit_concept_id, visit_type_concept_id)
+VALUES
+    /* For this data, visit_occurrence_id 2 and 4 should be dropped.*/
+    /* visit_occurrence_id 2 is within the same year as participant's year of birth.*/
+    /* visit_occurrence_id 4 exactly matches the participant's birthday.*/    
+      (1, 1, '2020-01-01', '2020-01-01 00:00:00 UTC', '2020-01-02', '2020-01-02 00:00:00 UTC', 0, 0),
+      (2, 3, '2003-07-03', '2003-07-03 00:00:00 UTC', '2003-07-30', '2003-07-30 00:00:00 UTC', 0, 0),
+      (3, 2, '2020-01-01', '2020-01-01 00:00:00 UTC', '2020-03-01', '2020-03-01 00:00:00 UTC', 0, 0),
+      (4, 4, '2015-01-01', '2015-01-01 00:00:00 UTC', '2015-01-01', '2015-01-01 00:00:00 UTC', 0, 0)
+""")
+OBSERVATION_DATA_TEMPLATE = JINJA_ENV.from_string("""
+INSERT INTO `{{project_id}}.{{dataset_id}}.observation`
+    (observation_id, person_id, observation_date, observation_datetime, observation_concept_id, 
+    observation_type_concept_id, value_as_concept_id, qualifier_concept_id, unit_concept_id, 
+    observation_source_concept_id, value_source_concept_id)
+    /* For this data, observation_id 2 and 5 should be dropped.*/
+    /* observation_id 2 is within the same year as participant's year of birth.*/
+    /* observation_id 5 is the last possible date that is less than participant's birth year + 2 (2005-01-01) */
+VALUES
+      (1, 1, '2020-06-01', '2020-06-01 00:00:00 UTC', 0, 0, 0, 0, 0, 0, 0),
+      (2, 2, '2002-06-01', '2002-06-01 00:00:00 UTC', 0, 0, 0, 0, 0, 0, 0),
+      (3, 3, '2020-03-01', '2020-03-01 00:00:00 UTC', 0, 0, 0, 0, 0, 0, 0),
+      (4, 4, '2020-01-05', '2020-01-05 00:00:00 UTC', 0, 0, 0, 0, 0, 0, 0),
+      (5, 3, '2004-12-31', '2004-12-31 00:00:00 UTC', 0, 0, 0, 0, 0, 0, 0)
+""")
+
+
+class SuppressYearOfBirthRecordsTest(BaseTest.CleaningRulesTestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+        super().initialize_class_vars()
+
+        # Set the test project identifier
+        cls.project_id = os.environ.get(PROJECT_ID)
+
+        # Set the expected test datasets
+        cls.dataset_id = os.environ.get('COMBINED_DATASET_ID')
+        cls.sandbox_id = cls.dataset_id + '_sandbox'
+
+        cls.rule_instance = YearOfBirthRecordsSuppression(
+            cls.project_id, cls.dataset_id, cls.sandbox_id)
+
+        # Generates list of fully qualified table names and their corresponding sandbox table names
+        # adding death table name for setup/cleanup operations
+        for table_name in AFFECTED_TABLES:
+            cls.fq_table_names.append(
+                f'{cls.project_id}.{cls.dataset_id}.{table_name}')
+            sandbox_table_name = cls.rule_instance.sandbox_table_for(
+                table_name)
+            cls.fq_sandbox_table_names.append(
+                f'{cls.project_id}.{cls.sandbox_id}.{sandbox_table_name}')
+
+        # call super to set up the client, create datasets
+        cls.up_class = super().setUpClass()
+
+    def setUp(self):
+        """
+        Create empty tables for the rule to run on
+        """
+        # Create the observation, concept, and concept_relationship tables required for the test
+        super().setUp()
+
+        person_data_query = PERSON_DATA_TEMPLATE.render(
+            project_id=self.project_id, dataset_id=self.dataset_id)
+        visit_occurrence_data_query = VISIT_OCCURRENCE_DATA_TEMPLATE.render(
+            project_id=self.project_id, dataset_id=self.dataset_id)
+        observation_data_query = OBSERVATION_DATA_TEMPLATE.render(
+            project_id=self.project_id, dataset_id=self.dataset_id)
+
+        # Load test data
+        self.load_test_data([
+            f'''{person_data_query};
+                {visit_occurrence_data_query};
+                {observation_data_query}'''
+        ])
+
+    def test_suppress_year_of_birth_records(self):
+        # Expected results list
+        tables_and_counts = [{
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}.{VISIT_OCCURRENCE}',
+            'fq_sandbox_table_name':
+                f'{self.project_id}.{self.sandbox_id}.{self.rule_instance.sandbox_table_for(VISIT_OCCURRENCE)}',
+            'loaded_ids': [1, 2, 3, 4],
+            'sandboxed_ids': [2, 4],
+            'fields': [
+                'visit_occurrence_id', 'person_id', 'visit_start_date',
+                'visit_end_date'
+            ],
+            'cleaned_values': [
+                (1, 1, datetime.datetime.strptime('2020-01-01',
+                                                  '%Y-%m-%d').date(),
+                 datetime.datetime.strptime('2020-01-02',
+                                            '%Y-%m-%d').date()),
+                (3, 2, datetime.datetime.strptime('2020-01-01',
+                                                  '%Y-%m-%d').date(),
+                 datetime.datetime.strptime('2020-03-01',
+                                            '%Y-%m-%d').date())
+            ]
+        }, {
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}.{OBSERVATION}',
+            'fq_sandbox_table_name':
+                f'{self.project_id}.{self.sandbox_id}.{self.rule_instance.sandbox_table_for(OBSERVATION)}',
+            'loaded_ids': [1, 2, 3, 4, 5],
+            'sandboxed_ids': [2, 5],
+            'fields': ['observation_id', 'person_id', 'observation_date'],
+            'cleaned_values': [
+                (1, 1, datetime.datetime.strptime('2020-06-01',
+                                                  '%Y-%m-%d').date()),
+                (3, 3, datetime.datetime.strptime('2020-03-01',
+                                                  '%Y-%m-%d').date()),
+                (4, 4, datetime.datetime.strptime('2020-01-05',
+                                                  '%Y-%m-%d').date())
+            ]
+        }]
+
+        self.default_test(tables_and_counts)
