4269fe4b995f398e61b5d345d741c4d221e8adb7
Krishna Kalluri <krishnasaikalluri@gmail.com>
2022-03-17 16:44:07 -0400

    [DC-2253] Set max_rows filter in analytics/cdr_ops/notebook_utils (#1175)

--- data_steward/analytics/cdr_ops/notebook_utils.py
+++ data_steward/analytics/cdr_ops/notebook_utils.py
@@ -8,17 +8,19 @@ IMPERSONATION_SCOPES = [
 ]
 
 
-def execute(client, query):
+def execute(client, query, max_rows=False):
     """
     Execute a bigquery command and return the results in a dataframe
 
     :param client: an instantiated bigquery client object
     :param query: the query to execute
+    :param max_rows: Boolean option to manually turn on max rows display(default -> false)
     :return pandas dataframe object
     """
     import pandas as pd
     print(query)
 
     res = client.query(query).to_dataframe()
-    pd.set_option('display.max_rows', res.shape[0] + 1)
+    if max_rows:
+        pd.set_option('display.max_rows', res.shape[0] + 1)
     return res
