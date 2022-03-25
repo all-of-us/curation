ae4a825cc7e7ca474824c8698c300dba256312d9
Nishanth Pavinkurve <npp267@nyu.edu>
2022-03-25 16:18:50 -0400

    [DC-2287] Snapshot validation script runs into errors (#1186)
    [DC-2287] Snapshot validation bug fixes

--- data_steward/constants/validation/participants/snapshot_validaiton_dataset.py
+++ data_steward/constants/validation/participants/snapshot_validaiton_dataset.py
@@ -6,16 +6,15 @@ FROM (SELECT
     table_name,
     partition_id,
     ROW_NUMBER() OVER(PARTITION BY table_name ORDER BY partition_id DESC) r
-FROM {{project_id}}.{{drc_ops_dataset}}.INFORMATION_SCHEMA.PARTITIONS
+FROM `{{project_id}}.{{drc_ops_dataset}}.INFORMATION_SCHEMA.PARTITIONS`
 WHERE table_name LIKE '%identity_match%'
 AND partition_id NOT IN ("__NULL__", "__UNPARTITIONED__")
 ) WHERE r = 1
 """)
 
-CREATE_TABLE_QUERY = JINJA_ENV.from_string("""
-CREATE TABLE {{project_id}}.{{dest_table}}
-LIKE {{project_id}}.{{source_table}}
-AS SELECT *
-FROM {{project_id}}.{{source_table}}
+SNAPSHOT_TABLE_QUERY = JINJA_ENV.from_string("""
+INSERT `{{fq_dest_table}}`
+SELECT *
+FROM `{{fq_source_table}}`
 WHERE FORMAT_TIMESTAMP("%Y%m%d%H", _PARTITIONTIME) = "{{partition_date}}"
 """)
