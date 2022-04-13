7105e317c7191f3e80c624a44343dc68f0211308
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2022-04-13 09:36:18 -0400

    [DC-850] Update in progress

--- data_steward/cdr_cleaner/cleaning_rules/remove_aian_participants.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_aian_participants.py
@@ -2,7 +2,6 @@
 Background
 
 DC-685 - removing participants who identified as AI/AN in PPI.
-DC-850
 
 There is a requirement from the NIH that participants who identified as AI/AN in PPI cannot have ANY of their data
 included in released datasets for the Data Browser or Workbench/Cohort Builder. These participants have had their data
@@ -19,7 +18,7 @@ import utils.bq
 from cdr_cleaner.cleaning_rules import sandbox_and_remove_pids
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 from constants.cdr_cleaner import clean_cdr as cdr_consts
-from common import JINJA_ENV
+from common import JINJA_ENV, OBSERVATION
 
 LOGGER = logging.getLogger(__name__)
 
@@ -93,6 +92,34 @@ class RemoveAianParticipants(BaseCleaningRule):
             sandbox_and_remove_pids.get_remove_pids_queries(
                 project_id, dataset_id, self.get_pids_list()))
         return queries_list
+
+    def get_query_specs(self):
+        """
+        Create queries for updating family history questions and answers
+        :return: list of query dicts
+        """
+        sandbox_query = {
+            cdr_consts.QUERY:
+                SANDBOX_QUERY.render(
+                    project_id=self.project_id,
+                    dataset_id=self.dataset_id,
+                    sandbox_dataset_id=self.sandbox_dataset_id,
+                    sandbox_tablename=self.sandbox_table_for(OBSERVATION))
+        }
+
+        update_query = {
+            cdr_consts.QUERY:
+                UPDATE_FAMILY_HISTORY_QUERY.render(dataset_id=self.dataset_id,
+                                                   project_id=self.project_id)
+        }
+
+        return [sandbox_query, update_query]
+
+    def get_sandbox_tablenames(self):
+        """
+        Create sandbox table names and return as list.
+        """
+        return [self.sandbox_table_for(OBSERVATION)]
 
 
 if __name__ == '__main__':
