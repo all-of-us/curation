a0c2d16fa15570fdcfdc8da0b5dfbd062aa2cf45
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2022-04-14 18:28:44 -0400

    [DC-823] Update script

--- data_steward/cdr_cleaner/cleaning_rules/remove_non_matching_participant.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_non_matching_participant.py
@@ -1,29 +1,36 @@
-"""Participant ID validation must be performed on site submission data for it to be included in the CDR. Ideally, 
-sites conduct participant matching and only submit data for matched participants, along with a table detailing their 
-matching process. The DRC also performs a matching process on data received to validate matching done at the site 
-level. 
+"""
+Participant ID validation must be performed on site submission data for it to be included in the CDR. 
+Ideally, sites conduct participant matching and only submit data for matched participants,
+along with a table detailing their matching process. 
+The DRC also performs a matching process on data received to validate matching done at the site level.
 
-Some sites may not provide participant matching tables for the launch dataset. In these cases, the DRC matching 
-algorithm should run and any non-matching PIDs identified by the algorithm should be dropped from the launch dataset. 
-This will ensure at least one level of identity validation is occurring. """
+Some sites may not provide participant matching tables for the launch dataset.
+In these cases, the DRC matching algorithm should run and any non-matching PIDs identified 
+by the algorithm should be dropped from the launch dataset. 
+This will ensure at least one level of identity validation is occurring.
+"""
+
+# Python imports
 import logging
-import bq_utils
-import resources
+
+# Third party imports
 import oauth2client
 import googleapiclient
 from google.cloud.exceptions import NotFound
+
+# Project imports
+import bq_utils
+import resources
+from common import JINJA_ENV, PARTICIPANT_MATCH
 from gcloud.bq import BigQueryClient
 from validation.participants import readers
 from cdr_cleaner.cleaning_rules import sandbox_and_remove_pids as remove_pids
+from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 from constants.validation.participants.identity_match import (PERSON_ID_FIELD,
                                                               FIRST_NAME_FIELD,
                                                               LAST_NAME_FIELD,
                                                               BIRTH_DATE_FIELD)
 from constants.validation.participants.writers import ALGORITHM_FIELD
-from common import PARTICIPANT_MATCH
-
-# Project imports
-from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 
 IDENTITY_MATCH = 'identity_match'
 
@@ -42,21 +49,20 @@ CAST_MISSING_COLUMN = "CAST({column} <> 'match' AS int64)"
 
 CRITERION_COLUMN_TEMPLATE = "({column_expr}) >= {num_of_missing}"
 
-SELECT_NON_MATCH_PARTICIPANTS_QUERY = """
+SELECT_NON_MATCH_PARTICIPANTS_QUERY = JINJA_ENV.from_string("""
 WITH non_match_participants AS
 (
-SELECT 
+  SELECT 
     *,
     ({key_fields_criteria}) AS key_fields_criteria,
     ({all_fields_criteria}) AS all_fields_criteria
-FROM `{project_id}.{validation_dataset_id}.{identity_match_table}`
+  FROM `{project_id}.{validation_dataset_id}.{identity_match_table}`
 )
-
-SELECT
-    person_id
+SELECT person_id
 FROM non_match_participants
-WHERE key_fields_criteria IS TRUE OR all_fields_criteria IS TRUE
-"""
+WHERE key_fields_criteria IS TRUE 
+OR all_fields_criteria IS TRUE
+""")
 
 
 class RemoveNonMatchingParticipant(BaseCleaningRule):
@@ -64,34 +70,39 @@ class RemoveNonMatchingParticipant(BaseCleaningRule):
     (Description to be added here)
     """
 
-    def __init__(self, project_id, dataset_id, sandbox_dataset_id):
+    def __init__(self,
+                 project_id,
+                 dataset_id,
+                 sandbox_dataset_id,
+                 ehr_dataset_id=None,
+                 validation_dataset_id=None):
         """
         (Description to be added here)
         """
 
-        self.ehr_dataset_id = 'xyz'
-        self.validation_dataset_id = 'xyz'
+        self.ehr_dataset_id = ehr_dataset_id
+        self.validation_dataset_id = validation_dataset_id
 
-        super().__init__(issue_numbers=['XYZ'],
+        super().__init__(issue_numbers=['DC468', 'DC823'],
                          project_id=project_id,
                          dataset_id=dataset_id,
                          sandbox_dataset_id=sandbox_dataset_id)
 
     def setup_rule(self, client):
         """
-        (Description to be added here)
+        Function to run any data upload options before executing a query.
         """
         pass
 
     def setup_validation(self, client):
         """
-        (Description to be added here)
+        Run required steps for validation setup.
         """
         raise NotImplementedError("Please fix me.")
 
     def validate_rule(self, client):
         """
-        (Description to be added here)
+        Validates the cleaning rule which deletes or updates the data from the tables.
         """
         raise NotImplementedError("Please fix me.")
 
@@ -105,7 +116,7 @@ class RemoveNonMatchingParticipant(BaseCleaningRule):
 
     def exist_participant_match(self, ehr_dataset_id, hpo_id):
         """
-        This function checks if the hpo has submitted the participant_match data
+        This function checks if the hpo has submitted the participant_match data.
 
         :param ehr_dataset_id:
         :param hpo_id:
@@ -215,7 +226,7 @@ class RemoveNonMatchingParticipant(BaseCleaningRule):
             column_expr=self.get_missing_criterion(identity_match_fields),
             num_of_missing=NUM_OF_MISSING_ALL_FIELDS)
         # instantiate the query for identifying the non-match participants in the validation_dataset
-        select_non_match_participants_query = SELECT_NON_MATCH_PARTICIPANTS_QUERY.format(
+        select_non_match_participants_query = SELECT_NON_MATCH_PARTICIPANTS_QUERY.render(
             project_id=project_id,
             validation_dataset_id=validation_dataset_id,
             identity_match_table=identity_match_table,
@@ -268,25 +279,20 @@ class RemoveNonMatchingParticipant(BaseCleaningRule):
                     'The hpo site {hpo_id} submitted the participant_match data'
                     .format(hpo_id=hpo_id))
 
-        queries = []
-
         if non_matching_person_ids:
             LOGGER.info(
                 'Participants: {person_ids} and their data will be dropped from {combined_dataset_id}'
                 .format(person_ids=non_matching_person_ids,
                         combined_dataset_id=self.dataset_id))
 
-            queries.extend(
-                remove_pids.get_sandbox_queries(self.project_id,
-                                                self.dataset_id,
-                                                non_matching_person_ids,
-                                                TICKET_NUMBER))
-            queries.extend(
-                remove_pids.get_remove_pids_queries(self.project_id,
-                                                    self.dataset_id,
-                                                    non_matching_person_ids))
+            sandbox_queries = remove_pids.get_sandbox_queries(
+                self.project_id, self.dataset_id, non_matching_person_ids,
+                TICKET_NUMBER)
 
-        return queries
+            remove_pids_queries = remove_pids.get_remove_pids_queries(
+                self.project_id, self.dataset_id, non_matching_person_ids)
+
+        return sandbox_queries + remove_pids_queries
 
 
 def parse_args():
@@ -324,12 +330,18 @@ if __name__ == '__main__':
     if ARGS.list_queries:
         clean_engine.add_console_logging()
         query_list = clean_engine.get_query_list(
-            ARGS.project_id, ARGS.dataset_id, ARGS.sandbox_dataset_id,
-            [(RemoveNonMatchingParticipant,)])
+            ARGS.project_id,
+            ARGS.dataset_id,
+            ARGS.sandbox_dataset_id, [(RemoveNonMatchingParticipant,)],
+            ehr_dataset_id=ARGS.ehr_dataset_id,
+            validation_dataset_id=ARGS.validation_dataset_id)
         for query in query_list:
             LOGGER.info(query)
     else:
         clean_engine.add_console_logging(ARGS.console_log)
-        clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
-                                   ARGS.sandbox_dataset_id,
-                                   [(RemoveNonMatchingParticipant,)])
+        clean_engine.clean_dataset(
+            ARGS.project_id,
+            ARGS.dataset_id,
+            ARGS.sandbox_dataset_id, [(RemoveNonMatchingParticipant,)],
+            ehr_dataset_id=ARGS.ehr_dataset_id,
+            validation_dataset_id=ARGS.validation_dataset_id)
