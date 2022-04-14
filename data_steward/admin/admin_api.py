713cdd4a207dce49bb1b80c7b0cd8234f3b31699
Lauren Biggers <lauren.r.biggers@vumc.org>
2022-04-14 14:33:59 -0500

    [DC-2115] add api logging setup to the admin service
    * this should allow logging to occur
    * logging will allow logging metrics to happen
    * logging metrics will allow GCP notifications

--- data_steward/admin/admin_api.py
+++ data_steward/admin/admin_api.py
@@ -13,6 +13,9 @@ from flask import Flask
 import api_util
 import app_identity
 from admin import key_rotation
+from curation_logging.curation_gae_handler import (begin_request_logging,
+                                                   end_request_logging,
+                                                   initialize_logging)
 
 PREFIX = '/admin/v1/'
 REMOVE_EXPIRED_KEYS_RULE = f'{PREFIX}RemoveExpiredServiceAccountKeys'
@@ -80,6 +83,16 @@ def remove_expired_keys():
     return 'remove-expired-keys-complete'
 
 
+@app.before_first_request
+def set_up_logging():
+    initialize_logging()
+
+
 app.add_url_rule(REMOVE_EXPIRED_KEYS_RULE,
                  view_func=remove_expired_keys,
                  methods=['GET'])
+
+app.before_request(
+    begin_request_logging)  # Must be first before_request() call.
+
+app.teardown_request(end_request_logging)
