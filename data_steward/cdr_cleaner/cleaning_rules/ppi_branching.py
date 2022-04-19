beb96bb3f93d5356ab7202496367472ef740b326
lledezma <luis.ledezma@vumc.org>
2022-04-19 11:51:47 -0500

    [DC-2318] Remove project_id argument

--- data_steward/cdr_cleaner/cleaning_rules/ppi_branching.py
+++ data_steward/cdr_cleaner/cleaning_rules/ppi_branching.py
@@ -319,7 +319,6 @@ class PpiBranching(BaseCleaningRule):
         query = BACKUP_ROWS_QUERY.render(lookup_table=self.lookup_table,
                                          src_table=self.observation_table)
         return self.bq_client.get_create_or_replace_table_ddl(
-            project_id=self.backup_table.project,
             dataset_id=self.backup_table.dataset_id,
             table_id=self.backup_table.table_id,
             schema=observation_schema,
@@ -338,7 +337,6 @@ class PpiBranching(BaseCleaningRule):
         query = CLEANED_ROWS_QUERY.render(src=self.observation_table,
                                           backup=self.backup_table)
         return self.bq_client.get_create_or_replace_table_ddl(
-            project_id=self.stage_table.project,
             dataset_id=self.stage_table.dataset_id,
             table_id=self.stage_table.table_id,
             schema=observation_schema,
@@ -364,7 +362,6 @@ class PpiBranching(BaseCleaningRule):
         stage = self.stage_table
         query = f'''SELECT * FROM `{stage.project}.{stage.dataset_id}.{stage.table_id}`'''
         return self.bq_client.get_create_or_replace_table_ddl(
-            project_id=self.observation_table.project,
             dataset_id=self.observation_table.dataset_id,
             schema=observation_schema,
             table_id=self.observation_table.table_id,
