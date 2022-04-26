b440f68002c132fa29501bc0533732e23a7c1d78
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2022-04-26 12:54:18 -0400

    [DC-823] [DC-1192] [skip ci] Update test

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/remove_non_matching_participant_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/remove_non_matching_participant_test.py
@@ -17,7 +17,54 @@ from tests import test_util
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 import resources
 
+# HPO_1 and HPO_2 have both participant_match and identity_match.
+# HPO_3 has only identity_match.
+# HPO_4 has only participant_match.
 HPO_1, HPO_2, HPO_3, HPO_4 = 'nyc', 'pitt', 'chs', 'vcu'
+
+POPULATE_PERSON = JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{dataset_id}}.{{person_table_id}}` 
+    (person_id, gender_concept_id, birth_datetime)
+    VALUES
+        (1, 0, timestamp('2000-01-01')),
+        (2, 0, timestamp('2000-01-02')),
+        (3, 0, timestamp('2000-01-03')),
+        (4, 0, timestamp('2000-01-04')),
+        (5, 0, timestamp('2000-01-05'))
+""")
+
+POPULATE_OBSERVATION = JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{dataset_id}}.{{observation_table_id}}` 
+    (observation_id, person_id, observation_concept_id, observation_date, observation_type_concept_id)
+    VALUES
+        (11, 1, 0, date('2022-01-01'), 0),
+        (12, 2, 0, date('2022-01-02'), 0),
+        (13, 3, 0, date('2022-01-03'), 0),
+        (14, 4, 0, date('2022-01-04'), 0),
+        (15, 5, 0, date('2022-01-05'), 0)
+""")
+
+POPULATE_PII_ADDRESS = JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{drc_dataset_id}}.{{pii_address_table_id}}`
+    (person_id, location_id)
+    VALUES
+        (1, 101),
+        (2, 102),
+        (3, 103),
+        (4, 104),
+        (5, 105)
+""")
+
+POPULATE_LOCATION = JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{drc_dataset_id}}.{{location_table_id}}` 
+    (location_id, address_1, address_2, city, state, zip)
+    VALUES
+        (101, 'xyz', '', 'New York', 'NY', '12345'),
+        (102, 'xyz', '', 'New York', 'NY', '12345'),
+        (103, 'xyz', '', 'New York', 'NY', '12345'),
+        (104, 'xyz', '', 'New York', 'NY', '12345'),
+        (105, 'xyz', '', 'New York', 'NY', '12345')
+""")
 
 
 class RemoveNonMatchingParticipantTest(BaseTest.CleaningRulesTestBase):
@@ -101,16 +148,27 @@ class RemoveNonMatchingParticipantTest(BaseTest.CleaningRulesTestBase):
         fq_dataset_name = self.fq_table_names[0].split('.')
         self.fq_dataset_name = '.'.join(fq_dataset_name[:-1])
 
+        self.create_idendity_match_tables([HPO_1, HPO_2, HPO_3])
+        self.create_participant_match_tables([HPO_1, HPO_2, HPO_4])
+
+        super().setUp()
+
+    def create_idendity_match_tables(self, hpo_ids):
+        """_summary_
+        """
         self.id_match_table_ids = [
-            f'{hpo_id}_{IDENTITY_MATCH}' for hpo_id in [HPO_1, HPO_2, HPO_3]
+            f'{hpo_id}_{IDENTITY_MATCH}' for hpo_id in hpo_ids
         ]
 
         for id_match_table_id in self.id_match_table_ids:
             create_drc_validation_table(self.client, id_match_table_id,
                                         self.dataset_id)
 
+    def create_participant_match_tables(self, hpo_ids):
+        """_summary_
+        """
         self.participant_match_table_ids = [
-            f'{hpo_id}_{PARTICIPANT_MATCH}' for hpo_id in [HPO_1, HPO_2, HPO_4]
+            f'{hpo_id}_{PARTICIPANT_MATCH}' for hpo_id in hpo_ids
         ]
 
         schema = resources.fields_for(IDENTITY_MATCH)
@@ -119,8 +177,6 @@ class RemoveNonMatchingParticipantTest(BaseTest.CleaningRulesTestBase):
                 f'{self.project_id}.{self.dataset_id}.{participant_match_table_id}',
                 schema=schema)
             table = self.client.create_table(table, exists_ok=True)
-
-        super().setUp()
 
     def test_dummy(self):
         self.assertEqual(1, 1)
