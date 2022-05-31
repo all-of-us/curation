ef454f38ce289155fe03d544b27157bc7ac9362b
lledezma <luis.ledezma@vumc.org>
2022-05-31 13:22:51 -0500

    [DC-2474] Refactoring

--- data_steward/validation/metrics/required_labs.py
+++ data_steward/validation/metrics/required_labs.py
@@ -25,7 +25,7 @@ SELECT table_id, row_count
 FROM `{{project_id}}.{{dataset_id}}.__TABLES__`""")
 
 
-def check_and_copy_tables(project_id, dataset_id):
+def check_and_copy_tables(client, dataset_id):
     """
     Will check that all the required tables exist and if not, they will be created
     or copied from another table.
@@ -35,12 +35,11 @@ def check_and_copy_tables(project_id, dataset_id):
     :return: None
     """
 
-    descendants_table_name = f'{project_id}.{dataset_id}.{MEASUREMENT_CONCEPT_SETS_DESCENDANTS_TABLE}'
-    concept_sets_table_name = f'{project_id}.{dataset_id}.{MEASUREMENT_CONCEPT_SETS_TABLE}'
+    descendants_table_name = f'{client.project}.{dataset_id}.{MEASUREMENT_CONCEPT_SETS_DESCENDANTS_TABLE}'
+    concept_sets_table_name = f'{client.project}.{dataset_id}.{MEASUREMENT_CONCEPT_SETS_TABLE}'
 
-    bq_client = BigQueryClient(project_id)
-    dataset = bq_client.dataset(dataset_id)
-    vocab_dataset = bq_client.dataset(common.VOCABULARY_DATASET)
+    dataset = client.dataset(dataset_id)
+    vocab_dataset = client.dataset(common.VOCABULARY_DATASET)
 
     # concept table and concept ancestor table source tables
     concept_source_table = vocab_dataset.table(common.CONCEPT)
@@ -51,44 +50,44 @@ def check_and_copy_tables(project_id, dataset_id):
     concept_ancestor_dest_table = dataset.table(common.CONCEPT_ANCESTOR)
 
     # query will check the row counts of each table in the specified dataset
-    row_count_query = ROW_COUNT_QUERY.render(project_id=project_id,
+    row_count_query = ROW_COUNT_QUERY.render(project_id=client.project,
                                              dataset_id=dataset_id)
 
-    results_dataframe = bq_client.query(row_count_query).to_dataframe()
+    results_dataframe = client.query(row_count_query).to_dataframe()
     empty_results_dataframe = results_dataframe[(
         results_dataframe['row_count'] == 0)]
 
     # checks if CONCEPT and CONCEPT_ANCESTOR tables exist, if they don't, they are copied from the
     # CONCEPT and CONCEPT_ANCESTOR tables in common.VOCABULARY
     if common.CONCEPT not in (results_dataframe['table_id']).values:
-        bq_client.copy_table(concept_source_table, concept_dest_table)
+        client.copy_table(concept_source_table, concept_dest_table)
     if common.CONCEPT_ANCESTOR not in (results_dataframe['table_id']).values:
-        bq_client.copy_table(concept_ancestor_source_table,
-                             concept_ancestor_dest_table)
+        client.copy_table(concept_ancestor_source_table,
+                          concept_ancestor_dest_table)
 
     # checks if CONCEPT and CONCEPT_ANCESTOR tables are empty, if they are, they are copied from the CONCEPT and
     # CONCEPT_ANCESTOR tables in common.VOCABULARY
     if common.CONCEPT in (empty_results_dataframe['table_id']).values:
-        bq_client.copy_table(concept_source_table, concept_dest_table)
+        client.copy_table(concept_source_table, concept_dest_table)
     if common.CONCEPT_ANCESTOR in (empty_results_dataframe['table_id']).values:
-        bq_client.copy_table(concept_ancestor_source_table,
-                             concept_ancestor_dest_table)
+        client.copy_table(concept_ancestor_source_table,
+                          concept_ancestor_dest_table)
 
     # checks if MEASUREMENT_CONCEPT_SETS_TABLE and MEASUREMENT_CONCEPT_SETS_DESCENDANTS_TABLE exist, if they
     # do not exist, they will be created
     if MEASUREMENT_CONCEPT_SETS_TABLE not in results_dataframe[
             'table_id'].values:
-        bq_client.create_tables(fq_table_names=[concept_sets_table_name],
-                                exists_ok=True,
-                                fields=None)
+        client.create_tables(fq_table_names=[concept_sets_table_name],
+                             exists_ok=True,
+                             fields=None)
     if MEASUREMENT_CONCEPT_SETS_DESCENDANTS_TABLE not in results_dataframe[
             'table_id'].values:
-        bq_client.create_tables(fq_table_names=[descendants_table_name],
-                                exists_ok=True,
-                                fields=None)
+        client.create_tables(fq_table_names=[descendants_table_name],
+                             exists_ok=True,
+                             fields=None)
 
 
-def load_measurement_concept_sets_table(project_id, dataset_id):
+def load_measurement_concept_sets_table(client, dataset_id):
     """
     Loads the required lab table from resource_files/measurement_concept_sets.csv
     into project_id.ehr_ops
@@ -98,7 +97,7 @@ def load_measurement_concept_sets_table(project_id, dataset_id):
     :return: None
     """
 
-    check_and_copy_tables(project_id, dataset_id)
+    check_and_copy_tables(client, dataset_id)
 
     try:
         LOGGER.info(
@@ -106,9 +105,9 @@ def load_measurement_concept_sets_table(project_id, dataset_id):
             .format(
                 measurement_concept_sets_table=MEASUREMENT_CONCEPT_SETS_TABLE,
                 dataset_id=dataset_id,
-                project_id=project_id))
+                project_id=client.project))
 
-        bq_utils.load_table_from_csv(project_id, dataset_id,
+        bq_utils.load_table_from_csv(client.project, dataset_id,
                                      MEASUREMENT_CONCEPT_SETS_TABLE)
 
     except (oauth2client.client.HttpAccessTokenRefreshError,
@@ -118,7 +117,7 @@ def load_measurement_concept_sets_table(project_id, dataset_id):
             f"FAILED:  CSV file could not be uploaded:\n{app_identity}")
 
 
-def load_measurement_concept_sets_descendants_table(project_id, dataset_id):
+def load_measurement_concept_sets_descendants_table(client, dataset_id):
     """
     Loads the measurement_concept_sets_descendants table using LOINC group and LOINC hierarchy
     into project_id.ehr_ops
@@ -128,10 +127,10 @@ def load_measurement_concept_sets_descendants_table(project_id, dataset_id):
     :return: None
     """
 
-    check_and_copy_tables(project_id, dataset_id)
+    check_and_copy_tables(client, dataset_id)
 
     identify_labs_query = IDENTIFY_LABS_QUERY.format(
-        project_id=project_id,
+        project_id=client.project,
         ehr_ops_dataset_id=dataset_id,
         vocab_dataset_id=dataset_id,
         measurement_concept_sets=MEASUREMENT_CONCEPT_SETS_TABLE)
@@ -161,28 +160,27 @@ def load_measurement_concept_sets_descendants_table(project_id, dataset_id):
         )
 
 
-def get_lab_concept_summary_query(hpo_id):
+def get_lab_concept_summary_query(client, hpo_id):
     """
     Get the query that checks if the HPO site has submitted the required labs
     :param hpo_id: Identifies the HPO site
     :return: 
     """
-    project_id = app_identity.get_application_id()
     dataset_id = bq_utils.get_dataset_id()
     hpo_measurement_table = resources.get_table_id(common.MEASUREMENT,
                                                    hpo_id=hpo_id)
 
     # Create measurement_concept_sets_table if not exist
     if not bq_utils.table_exists(MEASUREMENT_CONCEPT_SETS_TABLE, dataset_id):
-        load_measurement_concept_sets_table(project_id, dataset_id)
+        load_measurement_concept_sets_table(client, dataset_id)
 
     # Create measurement_concept_sets_descendants_table if not exist
     if not bq_utils.table_exists(MEASUREMENT_CONCEPT_SETS_DESCENDANTS_TABLE,
                                  dataset_id):
-        load_measurement_concept_sets_descendants_table(project_id, dataset_id)
+        load_measurement_concept_sets_descendants_table(client, dataset_id)
 
     return CHECK_REQUIRED_LAB_QUERY.format(
-        project_id=project_id,
+        project_id=client.project,
         ehr_ops_dataset_id=dataset_id,
         hpo_measurement_table=hpo_measurement_table,
         measurement_concept_sets_descendants=
@@ -193,9 +191,10 @@ if __name__ == '__main__':
     import cdr_cleaner.args_parser as parser
 
     ARGS = parser.parse_args()
+    bq_client = BigQueryClient(ARGS.project_id)
     # Upload the required lab lookup table
-    load_measurement_concept_sets_table(project_id=ARGS.project_id,
+    load_measurement_concept_sets_table(client=bq_client,
                                         dataset_id=ARGS.dataset_id)
     # Create the measurement concept sets descendant table
-    load_measurement_concept_sets_descendants_table(project_id=ARGS.project_id,
+    load_measurement_concept_sets_descendants_table(client=bq_client,
                                                     dataset_id=ARGS.dataset_id)
