d687dac5aa0e2432a57803f09ea2297a3907abad
Molly Giddings <molly.giddings@vumc.org>
2022-06-09 11:19:16 -0500

    [DC-2429] Modify RemoveFitbitDataIfMaxAgeExceeded rule to use table_namer for get_sandbox_tablenames

--- data_steward/validation/main.py
+++ data_steward/validation/main.py
@@ -185,8 +185,7 @@ def _upload_achilles_files(hpo_id: str = None,
 
     for filename in resources.ACHILLES_INDEX_FILES:
         logging.info(
-            f"Uploading achilles file '{filename}' to bucket {target_bucket}"
-        )
+            f"Uploading achilles file '{filename}' to bucket {target_bucket}")
         bucket_file_name = filename.split(resources.resource_files_path +
                                           os.sep)[1].strip().replace('\\', '/')
         with open(filename, 'rb') as fp:
