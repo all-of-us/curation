f90b59236904528865ade2e170b7c31796f91c9d
lledezma <luis.ledezma@vumc.org>
2022-06-28 08:59:46 -0500

    [DC-2518] Remove duplicate method

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/populate_route_ids_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/populate_route_ids_test.py
@@ -98,30 +98,3 @@ class PopulateRouteIdsTest(unittest.TestCase):
         self.assertEqual(queries[0][cdr_consts.DESTINATION_TABLE],
                          common.DRUG_EXPOSURE)
         self.assertEqual(queries[0][cdr_consts.DISPOSITION], 'WRITE_TRUNCATE')
-
-    def _test_integration_create_drug_route_mappings_table(self):
-        if self.bq_client.table_exists(populate_route_ids.DRUG_ROUTES_TABLE_ID,
-                                       dataset_id=self.dataset_id):
-            bq_utils.delete_table(populate_route_ids.DRUG_ROUTES_TABLE_ID,
-                                  dataset_id=self.dataset_id)
-
-        if not self.bq_client.table_exists(
-                populate_route_ids.DOSE_FORM_ROUTES_TABLE_ID,
-                dataset_id=self.dataset_id):
-            populate_route_ids.create_dose_form_route_mappings_table(
-                self.project_id, self.dataset_id)
-
-        populate_route_ids.create_drug_route_mappings_table(
-            self.project_id, self.dataset_id,
-            populate_route_ids.DOSE_FORM_ROUTES_TABLE_ID,
-            self.route_mapping_prefix)
-        time.sleep(10)
-        query = ("SELECT COUNT(*) AS n "
-                 "FROM `{project_id}.{dataset_id}.{table_id}`").format(
-                     project_id=self.project_id,
-                     dataset_id=self.dataset_id,
-                     table_id=populate_route_ids.DRUG_ROUTES_TABLE_ID)
-
-        result = bq_utils.query(query)
-        actual = bq_utils.response2rows(result)
-        self.assertGreater(actual[0]["n"], 0)
