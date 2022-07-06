a8aacb63d17e5a4801dc6652bdf45b2cea0ec7ab
Nishanth Pavinkurve <npp267@nyu.edu>
2022-07-06 11:21:54 -0400

    [DC-1930] Fully convert deactivation data script to cleaning rule (#1289)
    * [DC-1930] Update deactivation CR
    
    * [DC-1930] Migrate constants and unit tests
    
    * [DC-1930] Migrate integration tests
    
    * [DC-2478] Update from review comments

--- data_steward/cdr_cleaner/cleaning_rules/remove_participant_data_past_deactivation_date.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_participant_data_past_deactivation_date.py
@@ -5,17 +5,22 @@ Original Issue: DC-686
 
 The intent is to sandbox and drop records dated after the date of deactivation for participants
 who have deactivated from the Program.
+
+Subsequent issues: DC-686, DC-1184, DC-1799, DC-1791, DC-1896
 """
 
 # Python imports
 import logging
 
+# Third-party imports
+from pandas import DataFrame
+
 # Project imports
-import common
+from common import CDM_TABLES, FITBIT_TABLES, JINJA_ENV
 import constants.cdr_cleaner.clean_cdr as cdr_consts
 import utils.participant_summary_requests as psr
-import retraction.retract_deactivated_pids as rdp
-from constants.retraction.retract_deactivated_pids import DEACTIVATED_PARTICIPANTS
+from constants.bq_utils import WRITE_TRUNCATE
+from resources import fields_for
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 
 # Third-Party imports
@@ -26,6 +31,76 @@ LOGGER = logging.getLogger(__name__)
 DEACTIVATED_PARTICIPANTS_COLUMNS = [
     'participantId', 'suspensionStatus', 'suspensionTime'
 ]
+
+# For reference
+DEACTIVATION_ISSUE_NUMBERS = ['DC686', 'DC1184', 'DC1799']
+ISSUE_NUMBERS = ['DC1791', 'DC1896']
+
+TABLE_INFORMATION_SCHEMA = JINJA_ENV.from_string(  # language=JINJA2
+    """
+SELECT * except(is_generated, generation_expression, is_stored, is_hidden,
+is_updatable, is_system_defined, clustering_ordinal_position)
+FROM `{{project}}.{{dataset}}.INFORMATION_SCHEMA.COLUMNS`
+""")
+
+# Queries to create tables in associated sandbox with rows that will be removed per cleaning rule
+SANDBOX_QUERY = JINJA_ENV.from_string(  # language=JINJA2
+    """
+CREATE OR REPLACE TABLE `{{sandbox_ref.project}}.{{sandbox_ref.dataset_id}}.{{sandbox_ref.table_id}}` AS (
+SELECT t.*
+FROM `{{table_ref.project}}.{{table_ref.dataset_id}}.{{table_ref.table_id}}` t
+
+JOIN `{{deact_pids_table.project}}.{{deact_pids_table.dataset_id}}.{{deact_pids_table.table_id}}` d
+USING (person_id)
+
+{% if has_start_date %}
+WHERE (({{end_datetime}} IS NOT NULL AND {{end_datetime}} >= d.deactivated_datetime)
+OR ({{end_datetime}} IS NULL AND {{end_date}} IS NOT NULL AND {{end_date}} >= DATE(d.deactivated_datetime))
+OR ({{end_datetime}} IS NULL AND {{end_date}} IS NULL AND {{start_datetime}} IS NOT NULL AND {{start_datetime}} >= d.deactivated_datetime)
+OR ({{end_datetime}} IS NULL AND {{end_date}} IS NULL AND {{start_datetime}} IS NULL AND {{start_date}} IS NOT NULL AND {{start_date}} >= DATE(d.deactivated_datetime))
+{% if table_ref.table_id == 'drug_exposure' %}
+OR verbatim_end_date >= DATE(d.deactivated_datetime))
+{% else %} )
+{% endif %}
+{% elif table_ref.table_id == 'death' %}
+WHERE (death_datetime IS NOT NULL AND death_datetime >= d.deactivated_datetime)
+OR (death_datetime IS NULL AND death_date >= DATE(d.deactivated_datetime))
+{% elif table_ref.table_id in ['activity_summary', 'heart_rate_summary'] %}
+WHERE date >= DATE(d.deactivated_datetime)
+{% elif table_ref.table_id in ['heart_rate_minute_level', 'steps_intraday']  %}
+WHERE datetime >= PARSE_DATETIME('%F', CAST(d.deactivated_datetime as STRING))
+{% elif table_ref.table_id in ['payer_plan_period', 'observation_period']  %}
+WHERE COALESCE({{table_ref.table_id + '_end_date'}},
+{{table_ref.table_id + '_start_date'}}) >= DATE(d.deactivated_datetime)
+{% elif table_ref.table_id in ['drug_era', 'condition_era', 'dose_era']  %}
+WHERE COALESCE({{table_ref.table_id + '_end_date'}},
+{{table_ref.table_id + '_start_date'}}) >= d.deactivated_datetime
+{% else %}
+WHERE ({{datetime}} IS NOT NULL AND {{datetime}} >= d.deactivated_datetime)
+OR ({{datetime}} IS NULL AND {{date}} >= DATE(d.deactivated_datetime))
+{% endif %})
+""")
+
+# Queries to truncate existing tables to remove deactivated EHR PIDS, two different queries for
+# tables with standard entry dates vs. tables with start and end dates
+CLEAN_QUERY = JINJA_ENV.from_string(  # language=JINJA2
+    """
+SELECT *
+FROM `{{table_ref.project}}.{{table_ref.dataset_id}}.{{table_ref.table_id}}`
+EXCEPT DISTINCT
+SELECT *
+FROM `{{sandbox_ref.project}}.{{sandbox_ref.dataset_id}}.{{sandbox_ref.table_id}}`
+""")
+
+DATETIME = 'datetime'
+START_DATETIME = f'start_{DATETIME}'
+END_DATETIME = f'end_{DATETIME}'
+
+DATE = 'date'
+START_DATE = f'start_{DATE}'
+END_DATE = f'end_{DATE}'
+
+DEACTIVATED_PARTICIPANTS = '_deactivated_participants'
 
 
 class RemoveParticipantDataPastDeactivationDate(BaseCleaningRule):
@@ -54,22 +129,112 @@ class RemoveParticipantDataPastDeactivationDate(BaseCleaningRule):
         if not api_project_id:
             raise TypeError("`api_project_id` cannot be empty")
 
-        super().__init__(issue_numbers=['DC-1791', 'DC-1896'],
+        super().__init__(issue_numbers=ISSUE_NUMBERS,
                          description=desc,
                          affected_datasets=[cdr_consts.COMBINED],
                          project_id=project_id,
                          dataset_id=dataset_id,
                          sandbox_dataset_id=sandbox_dataset_id,
-                         affected_tables=common.CDM_TABLES +
-                         common.FITBIT_TABLES,
+                         affected_tables=CDM_TABLES + FITBIT_TABLES,
                          table_namer=table_namer)
         self.api_project_id = api_project_id
         self.destination_table = (f'{self.project_id}.{self.sandbox_dataset_id}'
                                   f'.{DEACTIVATED_PARTICIPANTS}')
+        self.deact_table_ref = gbq.TableReference.from_string(
+            self.destination_table)
+        self.table_cols_df = self.get_table_cols_df(None, self.project_id,
+                                                    self.dataset_id)
+        self.table_dates_info = self.get_table_dates_info(self.table_cols_df)
 
-        # initialized to None so that if setup_rule is skipped, it will not
-        # query live datasets for table information
-        self.client = None
+    def get_date_cols_dict(self, date_cols_list):
+        """
+        Converts list of date/datetime columns into dictionary mappings
+
+        Assumes each date column has a corresponding datetime column due to OMOP specifications
+        If a date column does not have a corresponding datetime column, skips it
+        Used for determining available dates based on order of precedence stated in the SANDBOX_QUERY
+        end_date > end_datetime > start_date > start_datetime. Non-conforming dates are factored into
+        the query separately, e.g. verbatim_end_date in drug_exposure
+        :param date_cols_list: list of date/datetime columns
+        :return: dictionary with mappings for START_DATE, START_DATETIME, END_DATE, END_DATETIME
+            or DATE, DATETIME
+        """
+        date_cols_dict = {}
+        for field in date_cols_list:
+            if field.endswith(START_DATETIME):
+                date_cols_dict[START_DATETIME] = field
+            elif field.endswith(END_DATETIME):
+                date_cols_dict[END_DATETIME] = field
+            elif field.endswith(DATETIME):
+                date_cols_dict[DATETIME] = field
+        for field in date_cols_list:
+            if field.endswith(START_DATE):
+                if date_cols_dict.get(START_DATETIME, '').startswith(field):
+                    date_cols_dict[START_DATE] = field
+            elif field.endswith(END_DATE):
+                if date_cols_dict.get(END_DATETIME, '').startswith(field):
+                    date_cols_dict[END_DATE] = field
+            elif field.endswith(DATE):
+                if date_cols_dict.get(DATETIME, '').startswith(field):
+                    date_cols_dict[DATE] = field
+        return date_cols_dict
+
+    def get_table_cols_df(self, client, project_id, dataset_id):
+        """
+        Returns a df of dataset's INFORMATION_SCHEMA.COLUMNS
+
+        :param project_id: bq name of project_id
+        :param dataset_id: ba name of dataset_id
+        :param client: bq client object
+        :return: dataframe of columns from INFORMATION_SCHEMA
+        """
+        table_cols_df = DataFrame()
+        if client:
+            LOGGER.info(
+                f"Getting column information from live dataset: `{self.dataset_id}`"
+            )
+            # if possible, read live table schemas
+            table_cols_query = TABLE_INFORMATION_SCHEMA.render(
+                project=project_id, dataset=dataset_id)
+            table_cols_df = client.query(table_cols_query).to_dataframe()
+        else:
+            # if None is passed to the client, read the table data from JSON schemas
+            # generate a dataframe from schema files
+            LOGGER.info("Getting column information from schema files")
+            table_dict_list = []
+            for table in self.affected_tables:
+                table_fields = fields_for(table)
+                for field in table_fields:
+                    field['table_name'] = table
+                table_dict_list.extend(table_fields)
+
+            table_cols_df = DataFrame(table_dict_list)
+            table_cols_df = table_cols_df.rename(
+                columns={"name": "column_name"})
+
+        return table_cols_df
+
+    def get_table_dates_info(self, table_cols_df):
+        """
+        Returns a dict with tables containing pids and date columns
+
+        :param table_cols_df: dataframe of columns from INFORMATION_SCHEMA
+        :return: dict with key table and date columns as values
+        """
+        pids_tables = table_cols_df[table_cols_df['column_name'] ==
+                                    'person_id']['table_name']
+        date_tables_df = table_cols_df[
+            table_cols_df['column_name'].str.contains("date")]
+
+        dates_info = {}
+        for table in pids_tables:
+            date_cols = date_tables_df[date_tables_df['table_name'] ==
+                                       table]['column_name']
+            # exclude person since it does not contain EHR data
+            if date_cols.any() and table != 'person':
+                dates_info[table] = date_cols.to_list()
+
+        return dates_info
 
     def get_query_specs(self):
         """
@@ -80,17 +245,38 @@ class RemoveParticipantDataPastDeactivationDate(BaseCleaningRule):
 
         :return: a list of query dicts
         """
-        deact_table_ref = gbq.TableReference.from_string(self.destination_table)
-        # creates sandbox and truncate queries to run for deactivated participant data drops
-        # setup_rule must be run before this to ensure the client is properly
-        # configured.
-        queries = rdp.generate_queries(self.client,
-                                       self.project_id,
-                                       self.dataset_id,
-                                       self.sandbox_dataset_id,
-                                       deact_table_ref,
-                                       data_stage_id=self.table_namer)
-        return queries
+        sandbox_queries = []
+        clean_queries = []
+        for table in self.table_dates_info:
+            table_ref = gbq.TableReference.from_string(
+                f"{self.project_id}.{self.dataset_id}.{table}")
+            sandbox_table = self.sandbox_table_for(table)
+            sandbox_ref = gbq.TableReference.from_string(
+                f"{self.project_id}.{self.sandbox_dataset_id}.{sandbox_table}")
+            date_cols = self.get_date_cols_dict(self.table_dates_info[table])
+            has_start_date = START_DATE in date_cols
+            sandbox_queries.append({
+                cdr_consts.QUERY:
+                    SANDBOX_QUERY.render(table_ref=table_ref,
+                                         table_id=f'{table}_id',
+                                         sandbox_ref=sandbox_ref,
+                                         deact_pids_table=self.deact_table_ref,
+                                         has_start_date=has_start_date,
+                                         **date_cols)
+            })
+
+            clean_queries.append({
+                cdr_consts.QUERY:
+                    CLEAN_QUERY.render(table_ref=table_ref,
+                                       sandbox_ref=sandbox_ref),
+                cdr_consts.DESTINATION_TABLE:
+                    table,
+                cdr_consts.DESTINATION_DATASET:
+                    self.dataset_id,
+                cdr_consts.DISPOSITION:
+                    WRITE_TRUNCATE
+            })
+        return sandbox_queries + clean_queries
 
     def setup_rule(self, client):
         """
@@ -111,28 +297,18 @@ class RemoveParticipantDataPastDeactivationDate(BaseCleaningRule):
         LOGGER.info(f"Finished storing participant records in: "
                     f"`{self.destination_table}`")
 
-        LOGGER.debug("instantiating class BigQueryClient object")
-        self.client = client
-
         # reinitializing self.affected_tables
-        LOGGER.debug(
-            "reinitializing self.affected_tables to actual tables available")
-        tables_list = self.client.list_tables(self.dataset_id)
-        self.affected_tables = [
-            table_item.table_id for table_item in tables_list
-        ]
+        LOGGER.debug("Narrow down to actual tables with dates and pids")
+        table_cols_df = self.get_table_cols_df(client, self.project_id,
+                                               self.dataset_id)
+        self.table_dates_info = self.get_table_dates_info(table_cols_df)
+        self.affected_tables = table_cols_df['table_name'].to_list()
 
     def get_sandbox_tablenames(self):
         """
         Return a list table names created to backup deleted data.
         """
-        LOGGER.info("Generating list of possible sandbox table names "
-                    "from self.affected_tables")
-
-        return [
-            rdp.get_deactivated_sandbox_table_name(table, self.table_namer)
-            for table in self.affected_tables
-        ]
+        return [self.sandbox_table_for(table) for table in self.affected_tables]
 
     def setup_validation(self, client):
         """
--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/remove_participant_data_past_deactivation_date_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/remove_participant_data_past_deactivation_date_test.py
@@ -9,18 +9,19 @@ This test will mock calling the PS API and provide a returned value.  Everything
 within the bounds of our team will be tested.
 """
 # Python imports
-import mock
 import os
+from unittest import mock
 
 # Third party imports
 import pandas as pd
+from google.cloud.bigquery import TableReference
 
 # Project imports
+from common import JINJA_ENV, OBSERVATION, DRUG_EXPOSURE, DEATH, PERSON
 from app_identity import PROJECT_ID
-from common import OBSERVATION
 from cdr_cleaner.cleaning_rules.remove_participant_data_past_deactivation_date import (
-    RemoveParticipantDataPastDeactivationDate)
-from constants.retraction.retract_deactivated_pids import DEACTIVATED_PARTICIPANTS
+    RemoveParticipantDataPastDeactivationDate, DEACTIVATED_PARTICIPANTS, DATE,
+    DATETIME, START_DATE, START_DATETIME, END_DATE, END_DATETIME)
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
 
@@ -46,7 +47,7 @@ class RemoveParticipantDataPastDeactivationDateTest(
         cls.sandbox_id = sandbox_id
 
         cls.kwargs = {
-            'table_namer': 'bar_ds',
+            'table_namer': 'table_namer',
             'api_project_id': 'foo-project-id'
         }
         cls.rule_instance = RemoveParticipantDataPastDeactivationDate(
@@ -80,37 +81,125 @@ class RemoveParticipantDataPastDeactivationDateTest(
         """
         Add data to the tables for the rule to run on.
         """
-        insert_fake_data_tmpls = [
-            self.jinja_env.from_string("""
-        INSERT INTO `{{fq_table_name}}`
-        (observation_id, person_id, observation_concept_id, observation_date,
-         observation_type_concept_id, observation_source_concept_id)
+        TABLE_ROWS = {
+            PERSON:
+                JINJA_ENV.from_string("""
+        INSERT INTO `{{table.project}}.{{table.dataset_id}}.{{table.table_id}}`
+        (person_id, gender_concept_id, year_of_birth, birth_datetime, race_concept_id, ethnicity_concept_id)
         VALUES
-        -- Values to exist after running the cleaning rule --
-        -- 801 is before the user deactivates --
-        -- 802, the user doesn't deactivate --
-          (801, 1, 1585899, date('2019-05-01'), 45905771, 111111),
-          (802, 2, 1585899, date('2019-05-01'), 45905771, 222222),
-        -- Values that should be removed by the cleaning rule --
-        -- 804 is after person 1 deactivates --
-        -- 805 is after user 3 deactivates --
-          (804, 1, 1585899, date('2020-05-01'), 45905771, null),
-          (805, 3, 1585899, date('2020-05-01'), 45905771, 45)
+        (1,8507,1989,'1989-07-25 01:00:00 UTC', 8527, 38003563),
+        (2,8507,1975,'1975-03-14 02:00:00 UTC', 8527, 38003564),
+        (3,8507,1981,'1981-11-18 05:00:00 UTC', 8527, 38003564),
+        (4,8507,1991,'1991-11-25 08:00:00 UTC', 8527, 38003564),
+        (5,8507,2001,'2001-09-20 11:00:00 UTC', 8527, 38003564),
+        (6,8507,1979,'1979-10-06 11:00:00 UTC', 8527, 38003564)
+        """),
+            OBSERVATION:
+                JINJA_ENV.from_string("""
+        INSERT INTO `{{table.project}}.{{table.dataset_id}}.{{table.table_id}}`
+        (observation_id, person_id, observation_concept_id, observation_date, observation_datetime, observation_type_concept_id)
+        VALUES
+        (1001,1,0,'2008-07-25','2008-07-25 01:00:00 UTC',45905771),
+        (1005,2,0,'2008-03-14','2008-03-14 02:00:00 UTC',45905771),
+        (1002,3,0,'2009-11-18','2009-11-18 05:00:00 UTC',45905771),
+        (1004,4,0,'2009-11-25','2009-11-25 08:00:00 UTC',45905771),
+        (1003,5,0,'2010-09-20','2010-09-20 11:00:00 UTC',45905771),
+        (1006,3,0,'2009-11-18','2009-11-18 00:30:00 UTC',45905771),
+        (1007,4,0,'2009-11-25',NULL,45905771)
+        """),
+            DEATH:
+                JINJA_ENV.from_string("""
+        INSERT INTO `{{table.project}}.{{table.dataset_id}}.{{table.table_id}}`
+        (person_id, death_date, death_datetime, death_type_concept_id)
+        VALUES
+        (2,'2008-03-12','2008-03-12 05:00:00 UTC',8),
+        (3,'2011-01-18','2011-01-18 05:00:00 UTC',6),
+        (4,'2009-11-25','2009-11-25 00:30:00 UTC',6),
+        (5,'2009-09-20',NULL,6)
+        """),
+            DRUG_EXPOSURE:
+                JINJA_ENV.from_string("""
+        INSERT INTO `{{table.project}}.{{table.dataset_id}}.{{table.table_id}}`
+        (drug_exposure_id, person_id, drug_concept_id, drug_exposure_start_date, drug_exposure_start_datetime, 
+        drug_exposure_end_date, drug_exposure_end_datetime, verbatim_end_date, drug_type_concept_id)
+        VALUES
+        (2002,1,50,'2008-06-05','2008-06-05 01:00:00 UTC','2010-07-05','2008-06-05 01:00:00 UTC','2011-04-11',87),
+        (2003,2,21,'2008-11-22','2008-11-22 02:00:00 UTC',NULL,NULL,'2010-06-18',51),
+        (2004,3,5241,'2009-08-03','2009-08-03 05:00:00 UTC',NULL,NULL,'2009-11-26',2754),
+        (2005,4,76536,'2010-02-17','2010-02-17 08:00:00 UTC',NULL,NULL,'2008-03-04',24),
+        (2006,5,274,'2009-04-19','2009-04-19 11:00:00 UTC',NULL,'2010-11-19 01:00:00 UTC','2011-10-22',436),
+        (2007,3,50,'2009-11-18','2009-11-18 00:30:00 UTC',NULL,NULL,'2009-11-18',87),
+        (2008,4,50,'2009-11-25','2009-11-25 00:30:00 UTC','2009-11-25','2009-11-25 00:45:00 UTC','2009-11-24',87),
+        (2009,6,50,'2009-10-06','2009-10-06 01:30:00 UTC',NULL,NULL,'2009-10-05',87),
+        (2010,5,274,'2009-09-20','2009-09-20 11:00:00 UTC', '2009-09-20', NULL, NULL, 436)
         """)
-        ]
+        }
 
         self.load_statements = []
         # create the string(s) to load the data
-        for tmpl in insert_fake_data_tmpls:
-            query = tmpl.render(fq_table_name=self.fq_obs_table)
+        for table in TABLE_ROWS:
+            fq_table = TableReference.from_string(
+                f'{self.project_id}.{self.dataset_id}.{table}')
+            query = TABLE_ROWS[table].render(table=fq_table)
             self.load_statements.append(query)
 
         super().setUp()
 
+    def get_dates_info(self):
+        # preconditions
+        data = {
+            'table_catalog': ['project'] * 13,
+            'table_schema': ['dataset'] * 13,
+            'table_name': ['observation'] * 5 + ['location'] * 2 +
+                          ['drug_exposure'] * 6,
+            'column_name': [
+                'observation_id', 'person_id', 'observation_concept_id',
+                'observation_date', 'observation_datetime', 'location_id',
+                'city', 'person_id', 'drug_exposure_start_date',
+                'drug_exposure_start_datetime', 'drug_exposure_end_date',
+                'drug_exposure_end_datetime', 'verbatim_date'
+            ],
+        }
+        table_cols_df = pd.DataFrame(data,
+                                     columns=[
+                                         'table_catalog', 'table_schema',
+                                         'table_name', 'column_name'
+                                     ])
+
+        expected_dict = {
+            'observation': ['observation_date', 'observation_datetime'],
+            'drug_exposure': [
+                'drug_exposure_start_date', 'drug_exposure_start_datetime',
+                'drug_exposure_end_date', 'drug_exposure_end_datetime',
+                'verbatim_date'
+            ]
+        }
+        actual_dict = self.rule_instance.get_table_dates_info(table_cols_df)
+
+        self.assertDictEqual(actual_dict, expected_dict)
+
+    def get_date_cols_dict(self):
+        date_cols = ["visit_date", "measurement_date", "measurement_datetime"]
+        expected = {DATE: "measurement_date", DATETIME: "measurement_datetime"}
+        actual = self.rule_instance.get_date_cols_dict(date_cols)
+        self.assertDictEqual(expected, actual)
+
+        date_cols = [
+            "verbatim_date", "condition_end_date", "condition_end_datetime",
+            "condition_start_datetime", "condition_start_date"
+        ]
+        expected = {
+            START_DATE: "condition_start_date",
+            START_DATETIME: "condition_start_datetime",
+            END_DATE: "condition_end_date",
+            END_DATETIME: "condition_end_datetime"
+        }
+        actual = self.rule_instance.get_date_cols_dict(date_cols)
+        self.assertDictEqual(expected, actual)
+
     @mock.patch(
         'utils.participant_summary_requests.get_deactivated_participants')
-    @mock.patch('retraction.retract_utils.is_deid_label_or_id')
-    def test_removing_data_past_deactivated_date(self, mock_deid, mock_func):
+    def test_removing_data_past_deactivated_date(self, mock_get_deact):
         """
         Validate deactivated participant records are dropped via cleaning rule.
 
@@ -119,33 +208,55 @@ class RemoveParticipantDataPastDeactivationDateTest(
         return a staged data frame object for this test instead of calling
         the PS API.
         """
-        columns = ['deactivated_datetime', 'person_id', 'suspension_status']
-        values = [
-            ['2020-01-01 01:00:00 UTC', 1,
-             'NO_CONTACT'],  # corresponds with record 804
-            ['2020-01-01 01:00:00 UTC', 3,
-             'NO_CONTACT']  # corresponds with record 805
-        ]
-        deactivated_df = pd.DataFrame(values, columns=columns)
 
-        mock_func.return_value = deactivated_df
-        mock_deid.return_value = False
+        self.get_date_cols_dict()
+        self.get_dates_info()
+        mock_get_deact.return_value = pd.DataFrame(
+            [(1, 'NO_CONTACT', '2009-07-25 01:00:00 UTC'),
+             (2, 'NO_CONTACT', '2009-03-14 01:00:00 UTC'),
+             (3, 'NO_CONTACT', '2009-11-18 01:00:00 UTC'),
+             (4, 'NO_CONTACT', '2009-11-25 01:00:00 UTC'),
+             (5, 'NO_CONTACT', '2009-09-20 01:00:00 UTC'),
+             (6, 'NO_CONTACT', '2009-10-06 01:00:00 UTC')],
+            columns=['person_id', 'suspension_status', 'deactivated_datetime'])
+
         self.load_test_data(self.load_statements)
 
-        # Using the 0 position because there is only one sandbox table and
-        # one affected OMOP table
-        obs_sandbox = [
-            table for table in self.fq_sandbox_table_names
-            if 'observation' in table
-        ][0]
         tables_and_counts = [{
-            'name': 'observation',
-            'fq_table_name': self.fq_obs_table,
-            'fq_sandbox_table_name': obs_sandbox,
+            'name':
+                OBSERVATION,
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}.{OBSERVATION}',
+            'fq_sandbox_table_name':
+                f'{self.project_id}.{self.sandbox_id}.{self.rule_instance.sandbox_table_for(OBSERVATION)}',
             'fields': ['observation_id'],
-            'loaded_ids': [801, 802, 804, 805],
-            'sandboxed_ids': [804, 805],
-            'cleaned_values': [(801,), (802,)]
+            'loaded_ids': [1001, 1002, 1003, 1004, 1005, 1006, 1007],
+            'sandboxed_ids': [1002, 1003, 1004, 1007],
+            'cleaned_values': [(1001,), (1005,), (1006,)]
+        }, {
+            'name':
+                DRUG_EXPOSURE,
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}.{DRUG_EXPOSURE}',
+            'fq_sandbox_table_name':
+                f'{self.project_id}.{self.sandbox_id}.{self.rule_instance.sandbox_table_for(DRUG_EXPOSURE)}',
+            'fields': ['drug_exposure_id'],
+            'loaded_ids': [
+                2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
+            ],
+            'sandboxed_ids': [2002, 2003, 2004, 2005, 2006, 2007, 2009, 2010],
+            'cleaned_values': [(2008,)]
+        }, {
+            'name':
+                DEATH,
+            'fq_table_name':
+                f'{self.project_id}.{self.dataset_id}.{DEATH}',
+            'fq_sandbox_table_name':
+                f'{self.project_id}.{self.sandbox_id}.{self.rule_instance.sandbox_table_for(DEATH)}',
+            'fields': ['person_id'],
+            'loaded_ids': [2, 3, 4, 5],
+            'sandboxed_ids': [3, 5],
+            'cleaned_values': [(2,), (4,)]
         }]
 
         self.default_test(tables_and_counts)
