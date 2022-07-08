8e2b2d4ba207ef1b787608919930c3c3ce35bcc5
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2022-07-08 15:20:36 -0400

    [DC-821] [DC-1174] Update test

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/backfill_pmi_skip_codes_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/backfill_pmi_skip_codes_test.py
@@ -2,9 +2,51 @@
 
 """
 
+# Python Imports
+import os
+
+# Third party imports
+
+# Project imports
+from app_identity import PROJECT_ID
 from cdr_cleaner.cleaning_rules.backfill_pmi_skip_codes import BackfillPmiSkipCodes
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
 
 class BackfillPmiSkipCodesTest(BaseTest.CleaningRulesTestBase):
-    pass
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+        super().initialize_class_vars()
+
+        # set the test project identifier
+        project_id = os.environ.get(PROJECT_ID)
+        cls.project_id = project_id
+
+        # set the expected test datasets
+        dataset_id = os.environ.get('COMBINED_DATASET_ID')
+        cls.dataset_id = dataset_id
+        sandbox_id = dataset_id + '_sandbox'
+        cls.sandbox_id = sandbox_id
+
+        cls.rule_instance = BackfillPmiSkipCodes(project_id, dataset_id,
+                                                 sandbox_id)
+
+        sb_table_names = cls.rule_instance.sandbox_table_for(OBSERVATION)
+
+        cls.fq_sandbox_table_names.append(
+            f'{cls.project_id}.{cls.sandbox_id}.{sb_table_names}')
+
+        cls.fq_table_names = [
+            f'{project_id}.{dataset_id}.{OBSERVATION}',
+            f'{project_id}.{dataset_id}.{CONCEPT}',
+        ]
+
+        # call super to set up the client, create datasets, and create
+        # empty test tables
+        # NOTE:  does not create empty sandbox tables.
+        super().setUpClass()
