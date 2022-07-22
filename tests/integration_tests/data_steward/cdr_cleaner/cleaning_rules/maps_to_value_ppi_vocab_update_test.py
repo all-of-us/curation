2cbfe7d5bd7cedfe3f2aab46cf9ff1185f79ef7f
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2022-07-22 17:28:39 -0400

    [DC-2590] Initial push

---
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/maps_to_value_ppi_vocab_update_test.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+# Python imports
+import os
+
+# Project imports
+from app_identity import PROJECT_ID
+from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
+
+
+class MapsToValuePpiVocabUpdateTest(BaseTest.CleaningRulesTestBase):
+    pass
