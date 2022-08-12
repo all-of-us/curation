d60dd52312f5d3551aee74a8b0be5287a9fffe66
lledezma <luis.ledezma@vumc.org>
2022-08-12 15:13:35 -0500

    [DC-2135] Add ticket number

--- data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/remove_fitbit_data_if_max_age_exceeded.py
@@ -1,7 +1,7 @@
 """
 Remove all FitBit data for participants exceeding the maximum age of 89
 
-Original Issue: DC-1001, DC-1037, DC-2429
+Original Issue: DC-1001, DC-1037, DC-2429, DC-2135
 
 The intent is to ensure there is no data for participants over the age of 89 in
 Activity Summary, Heart Rate Minute Level, Heart Rate Summary, and Steps Intraday tables
