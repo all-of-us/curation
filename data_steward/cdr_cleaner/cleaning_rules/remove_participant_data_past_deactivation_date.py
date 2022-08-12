1dc18d5c17c5d8b563c4a48041b3e0511491923a
lledezma <luis.ledezma@vumc.org>
2022-08-12 14:39:06 -0500

    [DC-2129] Review the fitbit script for any modifications needed to ensure these tables are copied (#1339)
    * [DC-2129] Add sleep_daily_summary and sleep_level tables
    
    * [DC-2129] Add sleep_daily_summary and sleep_level tables
    
    * [DC-2129] Update FITBIT_TABLES_DATETIME_FIELDS and FITBIT_TABLES_DATE_FIELDS dictionaries
    
    * [DC-2129] Update SQL
    
    * [DC-2129] Revert changes
    
    * [DC-2129] Revert changes
    
    * [DC-2129] Add ticket number

--- data_steward/cdr_cleaner/cleaning_rules/remove_participant_data_past_deactivation_date.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_participant_data_past_deactivation_date.py
@@ -6,7 +6,7 @@ Original Issue: DC-686
 The intent is to sandbox and drop records dated after the date of deactivation for participants
 who have deactivated from the Program.
 
-Subsequent issues: DC-686, DC-1184, DC-1799, DC-1791, DC-1896
+Subsequent issues: DC-686, DC-1184, DC-1799, DC-1791, DC-1896, DC-2129
 """
 
 # Python imports
@@ -34,7 +34,7 @@ DEACTIVATED_PARTICIPANTS_COLUMNS = [
 
 # For reference
 DEACTIVATION_ISSUE_NUMBERS = ['DC686', 'DC1184', 'DC1799']
-ISSUE_NUMBERS = ['DC1791', 'DC1896']
+ISSUE_NUMBERS = ['DC1791', 'DC1896', 'DC2129']
 
 TABLE_INFORMATION_SCHEMA = JINJA_ENV.from_string(  # language=JINJA2
     """
@@ -75,6 +75,11 @@ WHERE COALESCE({{table_ref.table_id + '_end_date'}},
 {% elif table_ref.table_id in ['drug_era', 'condition_era', 'dose_era']  %}
 WHERE COALESCE({{table_ref.table_id + '_end_date'}},
 {{table_ref.table_id + '_start_date'}}) >= d.deactivated_datetime
+{% elif table_ref.table_id == 'sleep_level' %}
+ WHERE (start_datetime IS NOT NULL AND TIMESTAMP(start_datetime) >= d.deactivated_datetime)
+     OR (sleep_date IS NOT NULL AND sleep_date >= DATE(d.deactivated_datetime))
+ {% elif table_ref.table_id == 'sleep_daily_summary' %}
+ WHERE (sleep_date IS NOT NULL AND sleep_date >= DATE(d.deactivated_datetime))
 {% else %}
 WHERE ({{datetime}} IS NOT NULL AND {{datetime}} >= d.deactivated_datetime)
 OR ({{datetime}} IS NULL AND {{date}} >= DATE(d.deactivated_datetime))
