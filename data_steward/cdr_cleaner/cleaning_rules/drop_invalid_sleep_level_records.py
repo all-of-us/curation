335426317e77d4ae7bccc79b71dbb108dafc67c5
lledezma <luis.ledezma@vumc.org>
2022-08-31 11:57:11 -0500

    [DC-2605] Update sql

--- data_steward/cdr_cleaner/cleaning_rules/drop_invalid_sleep_level_records.py
+++ data_steward/cdr_cleaner/cleaning_rules/drop_invalid_sleep_level_records.py
@@ -23,17 +23,21 @@ CREATE OR REPLACE TABLE
         *
     FROM
         `{{project_id}}.{{dataset_id}}.{{sleep_level_table}}`
-    WHERE level NOT IN ('awake','light','asleep','deep','restless','wake','rem','unknown') or level is NULL
+    WHERE level NOT IN 
+        ('awake','light','asleep','deep','restless','wake','rem','unknown') or level is NULL
 )
  """)
 
 DELETE_INVALID_LEVEL_RECORDS = JINJA_ENV.from_string("""
 DELETE
-FROM `{{project_id}}.{{dataset_id}}.{{sleep_level_table}}`
+FROM 
+    `{{project_id}}.{{dataset_id}}.{{sleep_level_table}}`
 WHERE person_id IN
 (
-    SELECT person_id
-    FROM `{{project_id}}.{{sandbox_dataset}}.{{sandbox_table}}`
+    SELECT 
+        person_id
+    FROM 
+        `{{project_id}}.{{sandbox_dataset}}.{{sandbox_table}}`
 )
  """)
 
