c9ca884eded82eafcde6df3d99ce57dfc1e294b7
lledezma <luis.ledezma@vumc.org>
2022-09-14 15:54:07 -0500

    [DC-838] Add GeneralizeSexGenderConcepts class

--- data_steward/cdr_cleaner/cleaning_rules/generalize_sex_gender_concepts.py
+++ data_steward/cdr_cleaner/cleaning_rules/generalize_sex_gender_concepts.py
@@ -4,8 +4,36 @@ import logging
 # Project imports
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 import constants.cdr_cleaner.clean_cdr as cdr_consts
+from common import JINJA_ENV, OBSERVATION
 
 LOGGER = logging.getLogger(__name__)
+
+GENERALIZE_GENDER_CONCEPT_ID = 2000000002
+WOMAN_CONCEPT_ID = 1585840
+MAN_CONCEPT_ID = 1585839
+SEX_AT_BIRTH_MALE_CONCEPT_ID = 1585846
+SEX_AT_BIRTH_FEMALE_CONCEPT_ID = 1585847
+
+NEW_GENERALIZED_CONCEPT_ID_QUERY_TEMPLATE = JINJA_ENV.from_string("""
+UPDATE
+  `{{project_id}}.{{dataset_id}}.observation`
+SET
+  value_as_concept_id = {{generalized_gender_concept_id}},
+  value_source_concept_id = {{generalized_gender_concept_id}}
+WHERE
+  observation_source_concept_id = 1585838 -- the concept for gender identity
+  AND value_source_concept_id = {{gender_value_source_concept_id}}
+  AND person_id IN 
+  (
+    SELECT
+      person_id
+    FROM
+      `{{project_id}}.{{dataset_id}}.observation`
+    WHERE
+      observation_source_concept_id = 1585845 -- the concept for biological sex at birth
+        AND value_source_concept_id = {{biological_sex_birth_concept_id}}
+  )
+""")
 
 GENERALIZED_CONCEPT_ID_QUERY_TEMPLATE = '''
 UPDATE
@@ -26,15 +54,89 @@ WHERE
       AND value_source_concept_id = {biological_sex_birth_concept_id})
 '''
 
-GENERALIZE_GENDER_CONCEPT_ID = 2000000002
 
-WOMAN_CONCEPT_ID = 1585840
+class GeneralizeSexGenderConcepts(BaseCleaningRule):
 
-MAN_CONCEPT_ID = 1585839
+    def __init__(self,
+                 project_id,
+                 dataset_id,
+                 sandbox_dataset_id=None,
+                 table_namer=None):
+        """
+        Initialize the class with proper information.
 
-SEX_AT_BIRTH_MALE_CONCEPT_ID = 1585846
+        Set the issue numbers, description and affected datasets. As other tickets may affect
+        this SQL, append them to the list of Jira Issues.
+        DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
+        """
+        desc = ('Rule to add generalizations for gender/sex mismatch')
+        super().__init__(issue_numbers=ISSUE_NUMBERS,
+                         description=desc,
+                         affected_datasets=[cdr_consts.FITBIT],
+                         affected_tables=[OBSERVATION],
+                         project_id=project_id,
+                         dataset_id=dataset_id,
+                         sandbox_dataset_id=sandbox_dataset_id,
+                         table_namer=table_namer)
 
-SEX_AT_BIRTH_FEMALE_CONCEPT_ID = 1585847
+    def get_query_specs(self, *args, **keyword_args):
+        """
+        Return a list of dictionary query specifications.
+
+        :return: A list of dictionaries. Each dictionary contains a single query
+             and a specification for how to execute that query. The specifications
+             are optional but the query is required.
+        """
+
+        updating_woman_to_generalized_concept_id = {
+            cdr_consts.QUERY:
+                NEW_GENERALIZED_CONCEPT_ID_QUERY_TEMPLATE.render(
+                    project_id=self.project_id,
+                    dataset_id=self.dataset_id,
+                    gender_value_source_concept_id=WOMAN_CONCEPT_ID,
+                    biological_sex_birth_concept_id=SEX_AT_BIRTH_MALE_CONCEPT_ID,
+                    generalized_gender_concept_id=GENERALIZE_GENDER_CONCEPT_ID)
+        }
+
+        updating_man_to_generalized_concept_id = {
+            cdr_consts.QUERY:
+                NEW_GENERALIZED_CONCEPT_ID_QUERY_TEMPLATE.render(
+                    project_id=self.project_id,
+                    dataset_id=self.dataset_id,
+                    gender_value_source_concept_id=MAN_CONCEPT_ID,
+                    biological_sex_birth_concept_id=
+                    SEX_AT_BIRTH_FEMALE_CONCEPT_ID,
+                    generalized_gender_concept_id=GENERALIZE_GENDER_CONCEPT_ID)
+        }
+
+        return [
+            updating_woman_to_generalized_concept_id,
+            updating_man_to_generalized_concept_id
+        ]
+
+    def setup_rule(self, client):
+        """
+        Function to run any data upload options before executing a query.
+        """
+        pass
+
+    def setup_validation(self, client):
+        """
+        Run required steps for validation setup
+        """
+        raise NotImplementedError("Please fix me.")
+
+    def validate_rule(self, client):
+        """
+        Validates the cleaning rule which deletes or updates the data from the tables
+        """
+        raise NotImplementedError("Please fix me.")
+
+    def get_sandbox_tablenames(self):
+        """
+        generates sandbox table names
+        """
+        raise NotImplementedError("Please fix me.")
 
 
 def parse_query_for_updating_woman_to_generalized_concept_id(
