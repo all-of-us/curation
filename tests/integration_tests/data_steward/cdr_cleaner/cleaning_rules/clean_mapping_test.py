649c487a2a98865d2cdb46fd976325253e82ba6d
lledezma <luis.ledezma@vumc.org>
2022-10-13 15:58:27 -0500

    [DC-1528] Add python and project imports

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/clean_mapping_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/clean_mapping_test.py
@@ -3,3 +3,11 @@ Integration test for clean_mapping mmodule
 
 DC-1528
 """
+
+# Python imports
+import os
+
+# Project imports
+from app_identity import PROJECT_ID
+from cdr_cleaner.cleaning_rules.clean_mapping import CleanMappingExtTables
+from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
