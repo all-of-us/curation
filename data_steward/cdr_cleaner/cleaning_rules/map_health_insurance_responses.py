1b54ea5acd93d685771d3620f6c02254b3950fb3
brendagutman <77469967+brendagutman@users.noreply.github.com>
2022-11-21 14:05:00 -0600

    [DC-2746] Long Term fix for MapHealthInsuranceResponses cleaning rule and add integration test (#1445)
    * [DC-2746] Bug fix add test and schemas
    
    * [DC-2746] Make minor format changes
    
    * [DC-2746] Make a minor change for the lint check.
    
    * [DC-2746] Make suggested changes
    
    * [DC-2746] Make suggested changes
    
    * [DC-2746] Add pipeline table to common
    
    * [DC-2746] Correct lint errors
    
    * [DC-2746] Correct lint errors
    
    * [DC-2746] remove maxDiff

--- data_steward/cdr_cleaner/cleaning_rules/map_health_insurance_responses.py
+++ data_steward/cdr_cleaner/cleaning_rules/map_health_insurance_responses.py
@@ -1,13 +1,24 @@
 """
-For all answers for the survey question (43528428) and given pids,
-    1. Mark answers as invalid for all participants
-    2. Use the second survey (1384450) to generate valid answers for a subset of pids who took the second survey
+Clean insurance data from Spanish Basics and the HCAU follow-up.
+
+For a short time the spanish basics survey did not display the answers to the insurance question (43528428) correctly.
+The question was later asked again in the HCAU survey but with the correct branching logic.
+
+For PIDs who took the original spanish basics insurance question (43528428), invalidate their answers.
+FOR PIDs who took the both insurance questions, update the answer given with the HCAU survey to use the codes/ids used
+in the basics survey)
 """
+
+# Python imports
 import logging
 import os
+from datetime import datetime
 
+# Project imports
+from google.cloud import bigquery
+from gcloud.bq import BigQueryClient
 import resources
-from common import JINJA_ENV, OBSERVATION, RDR
+from common import JINJA_ENV, OBSERVATION, RDR, PIPELINE_TABLES, HEALTH_INSURANCE_PIDS
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule, query_spec_list
 
@@ -16,68 +27,55 @@ LOGGER = logging.getLogger(__name__)
 ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID = 43528428
 HCAU_OBSERVATION_SOURCE_CONCEPT_ID = 1384450
 
-ISSUE_NUMBERS = ['dc826']
+ISSUE_NUMBERS = ['DC826', 'DC2746']
 
 INSURANCE_LOOKUP = 'insurance_lookup'
 NEW_INSURANCE_ROWS = 'new_insurance_rows'
-PIDS_LOOKUP_TABLE = 'health_insurance_pids'
-PIPELINE_TABLES = 'pipeline_tables'
 
-INSURANCE_LOOKUP_FIELDS = [{
-    "type": "string",
-    "name": "answer_for_obs_src_c_id_43528428",
-    "mode": "nullable",
-    "description": "Answers for observation_source_concept_id = 43528428"
-}, {
-    "type": "string",
-    "name": "basics_value_source_value",
-    "mode": "nullable",
-    "description": "value_source_value field for the basics survey answer"
-}, {
-    "type": "integer",
-    "name": "basics_value_source_concept_id",
-    "mode": "nullable",
-    "description": "value_source_concept_id for the basics survey answer"
-}, {
-    "type": "string",
-    "name": "hcau_value_source_value",
-    "mode": "nullable",
-    "description": "value_source_value field for the HCAU survey answer"
-}, {
-    "type": "integer",
-    "name": "hcau_value_source_concept_id",
-    "mode": "nullable",
-    "description": "value_source_concept_id for the HCAU survey answer"
-}]
+# Sandbox basics rows to be invalidated, and the hcau rows to be updated.
+SANDBOX_CHANGES_QUERY = JINJA_ENV.from_string("""
+CREATE OR REPLACE TABLE `{{project_id}}.{{sandbox_dataset_id}}.{{observation_sandbox}}`
+AS
+SELECT 
+    *
+FROM 
+    `{{project_id}}.{{dataset_id}}.observation` 
+WHERE 
+    observation_source_concept_id IN ({{ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID}},{{HCAU_OBSERVATION_SOURCE_CONCEPT_ID}})
+    AND person_id IN (SELECT person_id FROM `{{project_id}}.{{pipeline_tables}}.{{health_insurance_pids}}`)
+""")
 
-SANDBOX_CREATE_QUERY = JINJA_ENV.from_string("""
-CREATE TABLE `{{project_id}}.{{sandbox_dataset_id}}.{{new_insurance_rows}}`
+# Invalidate where PIDs took the original insurance question.
+UPDATE_INVALID_QUERY = JINJA_ENV.from_string("""
+UPDATE 
+    `{{project_id}}.{{dataset_id}}.observation` 
+SET
+    value_as_concept_id = 46237613,
+    value_as_string = 'Invalid',
+    value_source_concept_id = 46237613,
+    value_source_value = 'Invalid'
+WHERE 
+    observation_source_concept_id IN ({{ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID}})
+AND person_id IN (SELECT person_id FROM
+    `{{project_id}}.{{pipeline_tables}}.{{health_insurance_pids}}`)
+""")
+
+# Create a temp table of the HCAU answers with the codes/ids replaced by codes/ids used in the basics survey.
+CREATE_NEW_INSURANCE_ROWS = JINJA_ENV.from_string("""
+CREATE OR REPLACE TABLE `{{project_id}}.{{sandbox_dataset_id}}.{{new_insurance_rows}}`
 AS
 SELECT
   *
 FROM
 (SELECT 
-  ob.observation_id, 
-  ob.person_id, 
-  ob.observation_concept_id, 
-  ob.observation_date, 
-  ob.observation_datetime, 
-  ob.observation_type_concept_id, 
-  ob.value_as_number, 
+  observation_id,
   new_value_source_value AS value_as_string, 
   new_value_as_concept_id AS value_as_concept_id, 
-  ob.qualifier_concept_id, 
-  ob.unit_concept_id, 
-  ob.provider_id, 
-  ob.visit_occurrence_id, 
   'HealthInsurance_InsuranceTypeUpdate' AS observation_source_value, 
   {{ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID}} AS observation_source_concept_id, 
-  ob.unit_source_value, 
-  ob.qualifier_source_value, 
   new_value_source_concept_id AS value_source_concept_id, 
-  new_value_source_value AS value_source_value, 
-  ob.questionnaire_response_id
-FROM `{{project_id}}.{{combined_dataset_id}}.observation` ob
+  new_value_source_value AS value_source_value
+FROM `{{project_id}}.{{dataset_id}}.observation` ob
 JOIN (
   SELECT DISTINCT
     hcau_value_source_concept_id,
@@ -87,69 +85,47 @@ JOIN (
     AS new_value_as_concept_id
   FROM 
     `{{project_id}}.{{sandbox_dataset_id}}.{{insurance_lookup}}`
-  JOIN `{{project_id}}.{{combined_dataset_id}}.concept` source_c ON (basics_value_source_concept_id=source_c.concept_id)
-  JOIN `{{project_id}}.{{combined_dataset_id}}.concept_relationship` c_r ON (source_c.concept_id=c_r.concept_id_1)
-  JOIN `{{project_id}}.{{combined_dataset_id}}.concept` standard_c ON (standard_c.concept_id=c_r.concept_id_2)
+  JOIN `{{project_id}}.{{dataset_id}}.concept` source_c ON (basics_value_source_concept_id=source_c.concept_id)
+  JOIN `{{project_id}}.{{dataset_id}}.concept_relationship` c_r ON (source_c.concept_id=c_r.concept_id_1)
+  JOIN `{{project_id}}.{{dataset_id}}.concept` standard_c ON (standard_c.concept_id=c_r.concept_id_2)
   WHERE source_c.vocabulary_id='PPI' 
   AND c_r.relationship_id LIKE 'Maps to%'  --prefers the 'maps to value', but will take 'maps to' if necessary --
 )
 ON ob.value_source_concept_id = hcau_value_source_concept_id 
 WHERE observation_source_concept_id IN ({{HCAU_OBSERVATION_SOURCE_CONCEPT_ID}})
 AND person_id IN (SELECT person_id FROM
-     `{{project_id}}.{{pipeline_tables}}.{{pids_lookup_table}}`)
+     `{{project_id}}.{{pipeline_tables}}.{{health_insurance_pids}}`)
 )
 """)
 
-UPDATE_INVALID_QUERY = JINJA_ENV.from_string("""
-UPDATE 
- `{{project_id}}.{{combined_dataset_id}}.observation` ob
+# Where possible, update the basics rows with the data in the temp table(new_insurance_rows).
+UPDATE_HCAU_ROWS = JINJA_ENV.from_string("""
+UPDATE
+    `{{project_id}}.{{dataset_id}}.observation` ob
 SET
-  ob.value_as_concept_id = 46237613,
-  ob.value_as_string = 'Invalid',
-  ob.value_source_concept_id = 46237613,
-  ob.value_source_value = 'Invalid'
-WHERE ob.observation_source_concept_id IN ({{ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID}})
-AND ob.person_id IN (
-    SELECT person_id FROM
-     `{{project_id}}.{{pipeline_tables}}.{{pids_lookup_table}}`
-     )
+    value_as_concept_id = nir.value_as_concept_id,
+    value_as_string = nir.value_as_string,
+    value_source_concept_id = nir.value_source_concept_id,
+    value_source_value = nir.value_source_value,
+    observation_source_value = nir.observation_source_value,
+    observation_source_concept_id = nir.observation_source_concept_id 
+FROM  
+    `{{project_id}}.{{sandbox_dataset_id}}.{{new_insurance_rows}}` nir 
+WHERE 
+    ob.observation_id = nir.observation_id    
 """)
 
-DELETE_ORIGINAL_FOR_HCAU_PARTICIPANTS = JINJA_ENV.from_string("""
-DELETE
-FROM `{{project_id}}.{{combined_dataset_id}}.observation` ob
-WHERE ob.observation_source_concept_id IN ({{ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID}})
-AND ob.person_id IN 
-(SELECT DISTINCT
-  person_id
-FROM `{{project_id}}.{{sandbox_dataset_id}}.{{new_insurance_rows}}`)
-""")
-
-INSERT_ANSWERS_FOR_HCAU_PARTICIPANTS = JINJA_ENV.from_string("""
-INSERT INTO `{{project_id}}.{{combined_dataset_id}}.observation`
-  (observation_id,
-  person_id,
-  observation_concept_id,
-  observation_date,
-  observation_datetime,
-  observation_type_concept_id,
-  value_as_number,
-  value_as_string,
-  value_as_concept_id,
-  qualifier_concept_id,
-  unit_concept_id,
-  provider_id,
-  visit_occurrence_id,
-  observation_source_value,
-  observation_source_concept_id,
-  unit_source_value,
-  qualifier_source_value,
-  value_source_concept_id,
-  value_source_value,
-  questionnaire_response_id)
-SELECT
-  *
-FROM `{{project_id}}.{{sandbox_dataset_id}}.{{new_insurance_rows}}`
+### Validation Query ####
+COUNTS_QUERY = JINJA_ENV.from_string("""
+SELECT 
+    COUNT(*) as count_valid
+FROM 
+    `{{project_id}}.{{dataset_id}}.observation` 
+WHERE 
+    observation_source_concept_id IN ({{ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID}})
+AND person_id IN (SELECT person_id FROM
+     `{{project_id}}.{{pipeline_tables}}.{{health_insurance_pids}}`
+AND value_source_value = 'Invalid'
 """)
 
 
@@ -167,9 +143,7 @@ class MapHealthInsuranceResponses(BaseCleaningRule):
         DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
         """
         desc = (
-            'For observation_concept_id 43528428 and the given subset of pids -'
-            'Marks answers as invalid for all participants and Use the second survey (1384450) '
-            'to generate valid answers for the given participants')
+            'Clean insurance data from Spanish Basics and the HCAU follow-up.')
 
         super().__init__(issue_numbers=ISSUE_NUMBERS,
                          description=desc,
@@ -179,6 +153,14 @@ class MapHealthInsuranceResponses(BaseCleaningRule):
                          dataset_id=dataset_id,
                          sandbox_dataset_id=sandbox_dataset_id,
                          table_namer=table_namer)
+
+        self.counts_query = COUNTS_QUERY.render(
+            project_id=self.project_id,
+            dataset_id=self.dataset_id,
+            ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID=
+            ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID,
+            pipeline_tables=PIPELINE_TABLES,
+            health_insurance_pids=HEALTH_INSURANCE_PIDS)
 
     def setup_rule(self, client, *args, **keyword_args):
         """
@@ -193,82 +175,134 @@ class MapHealthInsuranceResponses(BaseCleaningRule):
         the logic to get the row counts of the tables prior to applying cleaning rule
 
         """
-        client.create_tables(
-            [f'{self.project_id}.{self.sandbox_dataset_id}.{INSURANCE_LOOKUP}'],
-            False, [INSURANCE_LOOKUP_FIELDS])
-        fq_file_path = os.path.join(resources.resource_files_path,
-                                    INSURANCE_LOOKUP + ".csv")
-        client.upload_csv_data_to_bq_table(self.sandbox_dataset_id,
-                                           INSURANCE_LOOKUP, fq_file_path,
-                                           'write_empty')
+
+        table_path = os.path.join(resources.resource_files_path,
+                                  f"{INSURANCE_LOOKUP}.csv")
+        with open(table_path, 'rb') as csv_file:
+            schema_list = client.get_table_schema(INSURANCE_LOOKUP)
+            table_id = f'{self.project_id}.{self.sandbox_dataset_id}.{INSURANCE_LOOKUP}'
+            job_config = bigquery.LoadJobConfig(
+                schema=schema_list,
+                skip_leading_rows=1,
+                source_format=bigquery.SourceFormat.CSV,
+                field_delimiter=',',
+                allow_quoted_newlines=True,
+                quote_character='"',
+                write_disposition=bigquery.job.WriteDisposition.WRITE_TRUNCATE)
+
+            # job_id defined to the second precision
+            job_id = f'{self.dataset_id}_{self.__class__.__name__}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+            LOGGER.info(f'Loading `{table_id}`')
+            try:
+                load_job = client.load_table_from_file(
+                    csv_file, table_id, job_config=job_config,
+                    job_id=job_id)  # Make an API request.
+
+                load_job.result()  # Waits for the job to complete.
+            except (ValueError, TypeError) as exc:
+                LOGGER.info(
+                    'Something went wrong and the table did not load correctly')
+                raise exc
+            else:
+                LOGGER.info(f'Loading of `{table_id}` completed.')
 
     def get_query_specs(self, *args, **keyword_args) -> query_spec_list:
         queries = []
 
         sandbox_query = dict()
-        sandbox_query[cdr_consts.QUERY] = SANDBOX_CREATE_QUERY.render(
+        sandbox_query[cdr_consts.QUERY] = SANDBOX_CHANGES_QUERY.render(
             project_id=self.project_id,
-            combined_dataset_id=self.dataset_id,
+            dataset_id=self.dataset_id,
             sandbox_dataset_id=self.sandbox_dataset_id,
-            new_insurance_rows=NEW_INSURANCE_ROWS,
-            insurance_lookup=INSURANCE_LOOKUP,
             ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID=
             ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID,
             HCAU_OBSERVATION_SOURCE_CONCEPT_ID=
             HCAU_OBSERVATION_SOURCE_CONCEPT_ID,
+            observation_sandbox=self.sandbox_table_for(OBSERVATION),
             pipeline_tables=PIPELINE_TABLES,
-            pids_lookup_table=PIDS_LOOKUP_TABLE)
+            health_insurance_pids=HEALTH_INSURANCE_PIDS)
         queries.append(sandbox_query)
 
         invalidate_query = dict()
         invalidate_query[cdr_consts.QUERY] = UPDATE_INVALID_QUERY.render(
             project_id=self.project_id,
-            combined_dataset_id=self.dataset_id,
+            dataset_id=self.dataset_id,
             ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID=
             ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID,
             pipeline_tables=PIPELINE_TABLES,
-            pids_lookup_table=PIDS_LOOKUP_TABLE)
+            health_insurance_pids=HEALTH_INSURANCE_PIDS)
         queries.append(invalidate_query)
 
-        delete_query = dict()
-        delete_query[
-            cdr_consts.QUERY] = DELETE_ORIGINAL_FOR_HCAU_PARTICIPANTS.render(
+        new_insurance_rows_query = dict()
+        new_insurance_rows_query[
+            cdr_consts.QUERY] = CREATE_NEW_INSURANCE_ROWS.render(
                 project_id=self.project_id,
-                combined_dataset_id=self.dataset_id,
+                dataset_id=self.dataset_id,
                 sandbox_dataset_id=self.sandbox_dataset_id,
-                new_insurance_rows=NEW_INSURANCE_ROWS,
+                new_insurance_rows=self.sandbox_table_for(NEW_INSURANCE_ROWS),
+                insurance_lookup=INSURANCE_LOOKUP,
                 ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID=
-                ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID)
-        queries.append(delete_query)
+                ORIGINAL_OBSERVATION_SOURCE_CONCEPT_ID,
+                HCAU_OBSERVATION_SOURCE_CONCEPT_ID=
+                HCAU_OBSERVATION_SOURCE_CONCEPT_ID,
+                pipeline_tables=PIPELINE_TABLES,
+                health_insurance_pids=HEALTH_INSURANCE_PIDS)
+        queries.append(new_insurance_rows_query)
 
-        insert_query = dict()
-        insert_query[
-            cdr_consts.QUERY] = INSERT_ANSWERS_FOR_HCAU_PARTICIPANTS.render(
-                project_id=self.project_id,
-                combined_dataset_id=self.dataset_id,
-                sandbox_dataset_id=self.sandbox_dataset_id,
-                new_insurance_rows=NEW_INSURANCE_ROWS)
-        queries.append(insert_query)
+        update_hcau_query = dict()
+        update_hcau_query[cdr_consts.QUERY] = UPDATE_HCAU_ROWS.render(
+            project_id=self.project_id,
+            dataset_id=self.dataset_id,
+            sandbox_dataset_id=self.sandbox_dataset_id,
+            new_insurance_rows=self.sandbox_table_for(NEW_INSURANCE_ROWS))
+        queries.append(update_hcau_query)
 
         return queries
 
-    def get_sandbox_tablenames(self):
-        """
-        generates sandbox table names
-        """
-        raise NotImplementedError("Please fix me.")
+    def get_sandbox_tablenames(self) -> list:
+        return [
+            self.sandbox_table_for(OBSERVATION),
+            self.sandbox_table_for(NEW_INSURANCE_ROWS)
+        ]
 
-    def setup_validation(self, client):
+    def setup_validation(self, client) -> None:
         """
         Run required steps for validation setup
         """
-        raise NotImplementedError("Please fix me.")
+        init_counts = self._get_counts(client)
 
-    def validate_rule(self, client):
+        if init_counts.get('count_valid') == 0:
+            raise RuntimeError(
+                'NO DATA TO UPDATE OR INVALIDATE IN OBSERVATION TABLE ')
+
+    def validate_rule(self, client) -> None:
         """
         Validates the cleaning rule which deletes or updates the data from the tables
         """
-        raise NotImplementedError("Please fix me.")
+        clean_counts = self._get_counts(client)
+
+        if clean_counts.get('count_valid') == 0:
+            raise RuntimeError(
+                f'{self.__class__.__name__} did not clean as expected.\n'
+                f'Found data in: {clean_counts}')
+
+    def _get_counts(self, client: BigQueryClient) -> dict:
+        """
+        Counts query, used for job validation.
+        """
+        job = client.query(self.counts_query)
+        response = job.result()
+
+        errors = []
+        if job.exception():
+            errors.append(job.exception())
+            LOGGER.error(f"FAILURE:  {job.exception()}\n"
+                         f"Problem executing query:\n{self.counts_query}")
+        else:
+            for item in response:
+                count_valid = item.get('count_valid', 0)
+
+        return {'count_valid': count_valid}
 
 
 if __name__ == '__main__':
---
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/map_health_insurance_responses_test.py
@@ -0,0 +1,187 @@
+"""
+    Integration test for MapHealthInsuranceResponses
+
+"""
+# Python imports
+import os
+
+# Third party imports
+import mock
+from datetime import date
+from dateutil.parser import parse
+import pytz
+
+# Third party imports
+
+# Project imports
+from app_identity import PROJECT_ID
+from cdr_cleaner.cleaning_rules.map_health_insurance_responses import (
+    MapHealthInsuranceResponses, HEALTH_INSURANCE_PIDS, INSURANCE_LOOKUP)
+from common import JINJA_ENV, OBSERVATION, VOCABULARY_TABLES
+from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
+
+LOAD_QUERY = JINJA_ENV.from_string("""
+        INSERT INTO `{{project_id}}.{{dataset_id}}.observation` (
+        observation_id,
+        person_id,
+        observation_concept_id, 
+        value_as_string,
+        value_as_concept_id,
+        observation_source_value,
+        observation_source_concept_id,
+        value_source_value,
+        value_source_concept_id,
+        observation_date,
+        observation_type_concept_id,
+        observation_datetime,
+        questionnaire_response_id
+    )
+    VALUES
+        -- For selected participants, hcau survey rows should be sandboxed and updated in observation --
+    (1,1,40766241,
+    "Medicaid, Medical Assistance, or any kind of government-assistance plan for those with low incomes or disability",
+    0,"Insurance_InsuranceType",1384450,"InsuranceType_GovernmentAssistancePlan", 1384441,'2000-01-01',0,
+   TIMESTAMP('2000-01-01'),1),
+    (2,1,40766241,"TRICARE or other military health care",0,"Insurance_InsuranceType",1384450,
+    "InsuranceType_TricareOrMilitary", 1384550,'2000-01-01',0,TIMESTAMP('2000-01-01'),2), 
+    
+        -- For selected participants, original basics survey rows should be sandboxed and invalidated --
+    (3,2,43528428,
+    "Medicaid, Medical Assistance, or any kind of government-assistance plan for those with low incomes or disability"
+    ,0,"HealthInsurance_InsuranceTypeUpdate",43528428,"InsuranceTypeUpdate_Medicaid",43529209,'2000-01-01',0,
+    TIMESTAMP('2000-01-01'),3),
+    
+        -- For other participants, original basics survey rows should be unaffected --
+    (4,3,43528428,
+    "Medicaid, Medical Assistance, or any kind of government-assistance plan for those with low incomes or disability"
+    ,0,"HealthInsurance_InsuranceTypeUpdate",43528428,"InsuranceTypeUpdate_Medicaid",43529209,'2000-01-01',0,
+    TIMESTAMP('2000-01-01'),4),
+    
+        -- For other participants, hcau survey rows should be unaffected. --
+     (5,4,40766241,
+    "Medicaid, Medical Assistance, or any kind of government-assistance plan for those with low incomes or disability",
+    0,"Insurance_InsuranceType",1384450,"InsuranceType_GovernmentAssistancePlan", 1384441,'2000-01-01',0,
+    TIMESTAMP('2000-01-01'),5)
+        """)
+
+HEALTH_INSURANCE_PIDS_QUERY = JINJA_ENV.from_string("""
+    INSERT INTO `{{project_id}}.{{dataset_id}}.health_insurance_pids` (person_id)
+    VALUES
+        (1), (2)
+    """)
+
+
+class MapHealthInsuranceResponsesTest(BaseTest.CleaningRulesTestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+        super().initialize_class_vars()
+
+        cls.project_id = os.environ.get(PROJECT_ID)
+        cls.dataset_id = os.environ.get('RDR_DATASET_ID')
+        cls.sandbox_id = f'{cls.dataset_id}_sandbox'
+        cls.vocabulary_id = os.environ.get('VOCABULARY_DATASET')
+
+        cls.rule_instance = MapHealthInsuranceResponses(cls.project_id,
+                                                        cls.dataset_id,
+                                                        cls.sandbox_id)
+
+        sb_table_names = cls.rule_instance.get_sandbox_tablenames()
+        for table_name in sb_table_names:
+            cls.fq_sandbox_table_names.append(
+                f'{cls.project_id}.{cls.sandbox_id}.{table_name}')
+
+        cls.fq_sandbox_table_names.append(
+            f'{cls.project_id}.{cls.sandbox_id}.{INSURANCE_LOOKUP}')
+
+        cls.fq_table_names = [
+            f'{cls.project_id}.{cls.dataset_id}.{table}'
+            for table in [OBSERVATION] + VOCABULARY_TABLES +
+            [HEALTH_INSURANCE_PIDS]
+        ]
+
+        # call super to set up the client, create datasets, and create
+        # empty test tables
+        # NOTE:  does not create empty sandbox tables.
+        super().setUpClass()
+
+    def setUp(self):
+
+        super().setUp()
+
+        self.copy_vocab_tables(self.vocabulary_id)
+
+        self.load_test_data([
+            LOAD_QUERY.render(project_id=self.project_id,
+                              dataset_id=self.dataset_id),
+            HEALTH_INSURANCE_PIDS_QUERY.render(project_id=self.project_id,
+                                               dataset_id=self.dataset_id)
+        ])
+
+    def test_setup_rule(self):
+        # run setup_rule and see if the affected_table is updated
+        self.rule_instance.setup_rule(self.client)
+
+        # sees that setup worked and reset affected_tables as expected
+        self.assertEqual(set([OBSERVATION]),
+                         set(self.rule_instance.affected_tables))
+
+    def test_map_health_insurance_responses(self):
+        """
+        Use the default drop rows test function.
+
+        Validates pre-conditions, test execution and post conditions based on
+        the load statements and the tables_and_counts variable.
+        """
+
+        tables_and_counts = [{
+            'fq_table_name':
+                self.fq_table_names[0],
+            'fq_sandbox_table_name':
+                self.fq_sandbox_table_names[0],
+            'fields': [
+                'observation_id', 'person_id', 'observation_concept_id',
+                'value_as_string', 'value_as_concept_id',
+                'observation_source_value', 'observation_source_concept_id',
+                'value_source_value', 'value_source_concept_id',
+                'observation_date', 'observation_type_concept_id',
+                'observation_datetime', 'questionnaire_response_id'
+            ],
+            'loaded_ids': [1, 2, 3, 4, 5],
+            'sandboxed_ids': [1, 2, 3],
+            'cleaned_values': [
+                (1, 1, 40766241, "InsuranceTypeUpdate_Medicaid", 43529209,
+                 "HealthInsurance_InsuranceTypeUpdate", 43528428,
+                 "InsuranceTypeUpdate_Medicaid", 43529209, date(2000, 1, 1), 0,
+                 parse('2000-01-01 00:00:00 UTC').astimezone(pytz.utc), 1),
+                (2, 1, 40766241, "InsuranceTypeUpdate_Military", 45876394,
+                 "HealthInsurance_InsuranceTypeUpdate", 43528428,
+                 "InsuranceTypeUpdate_Military", 43529920, date(2000, 1, 1), 0,
+                 parse('2000-01-01 00:00:00 UTC').astimezone(pytz.utc), 2),
+                (3, 2, 43528428, "Invalid", 46237613,
+                 "HealthInsurance_InsuranceTypeUpdate", 43528428,
+                 "Invalid", 46237613, date(2000, 1, 1), 0,
+                 parse('2000-01-01 00:00:00 UTC').astimezone(pytz.utc), 3),
+                (4, 3, 43528428,
+                 "Medicaid, Medical Assistance, or any kind of government-assistance plan for those with low incomes or disability",
+                 0, "HealthInsurance_InsuranceTypeUpdate", 43528428,
+                 "InsuranceTypeUpdate_Medicaid", 43529209, date(2000, 1, 1), 0,
+                 parse('2000-01-01 00:00:00 UTC').astimezone(pytz.utc), 4),
+                (5, 4, 40766241,
+                 "Medicaid, Medical Assistance, or any kind of government-assistance plan for those with low incomes or disability",
+                 0, "Insurance_InsuranceType", 1384450,
+                 "InsuranceType_GovernmentAssistancePlan", 1384441,
+                 date(2000, 1, 1), 0,
+                 parse('2000-01-01 00:00:00 UTC').astimezone(pytz.utc), 5),
+            ]
+        }]
+
+        with mock.patch(
+                'cdr_cleaner.cleaning_rules.map_health_insurance_responses.PIPELINE_TABLES',
+                self.dataset_id):
+
+            self.default_test(tables_and_counts)
