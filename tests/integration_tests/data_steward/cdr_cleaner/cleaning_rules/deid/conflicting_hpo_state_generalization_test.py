d2bde4f8d902466768758a766abc82e729a4968c
ratuagga <ratul.aggarwal@vumc.org>
2023-01-13 11:44:51 -0600

    [DC-1221] Updated integration test.

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/deid/conflicting_hpo_state_generalization_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/deid/conflicting_hpo_state_generalization_test.py
@@ -7,11 +7,12 @@
 import os
 
 # Third Party Imports
+from dateutil.parser import parse
 
 # Project Imports
 from app_identity import PROJECT_ID
 from cdr_cleaner.cleaning_rules.deid.conflicting_hpo_state_generalization import ConflictingHpoStateGeneralize
-from common import JINJA_ENV, OBSERVATION, VOCABULARY_TABLES
+from common import JINJA_ENV, OBSERVATION
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
 INSERT_RAW_DATA_OBS = JINJA_ENV.from_string("""
@@ -20,13 +21,16 @@ INSERT_RAW_DATA_OBS = JINJA_ENV.from_string("""
        person_id,
        observation_concept_id,
        observation_date,
-       observation_type_concept_id
+       observation_type_concept_id,
+       value_as_concept_id,
+       observation_source_concept_id,
+       value_source_concept_id
        )
      VALUES
-       (1,101,1001,date('2020-01-01'),1),
-       (2,102,1002,date('2020-01-01'),1),
-       (3,103,1003,date('2020-01-01'),1),
-       (4,104,1004,date('2020-01-01'),1)
+       (1,101,1001,2020-01-01,1,,1585249,),
+       (2,102,1002,2020-01-01,1,100,1585250,),
+       (3,103,1003,2020-01-01,1,100,1585249,),
+       (4,104,1004,2020-01-01,1,100,1585248,)
  """)
 
 INSERT_RAW_DATA_MAPPING = JINJA_ENV.from_string("""
@@ -56,19 +60,18 @@ class ConflictingHpoStateGeneralizeTest(BaseTest.CleaningRulesTestBase):
         cls.project_id = os.environ.get(PROJECT_ID)
 
         # Set the expected test datasets
-        cls.dataset_id = os.environ.get('UNIONED_DATASET_ID')
+        cls.dataset_id = os.environ.get('COMBINED_DATASET_ID')
         cls.sandbox_id = f"{cls.dataset_id}_sandbox"
-        cls.vocabulary_id = os.environ.get('VOCABULARY_DATASET')
 
         cls.rule_instance = ConflictingHpoStateGeneralize(
             cls.project_id, cls.dataset_id, cls.sandbox_id)
 
         # Generates list of fully qualified table names and their corresponding sandbox table names
-        cls.fq_table_names.append(
-            f'{cls.project_id}.{cls.dataset_id}.{OBSERVATION}')
-        for table in VOCABULARY_TABLES:
-            cls.fq_table_names.append(
-                f'{cls.project_id}.{cls.dataset_id}.{table}')
+        for table in [OBSERVATION]:
+            cls.fq_table_names.extend([
+                f'{cls.project_id}.{cls.dataset_id}.{table}',
+                f'{cls.project_id}.{cls.dataset_id}._mapping_{table}'
+            ])
 
         for table in cls.rule_instance.get_sandbox_tablenames():
             cls.fq_sandbox_table_names.append(
@@ -80,11 +83,13 @@ class ConflictingHpoStateGeneralizeTest(BaseTest.CleaningRulesTestBase):
     def setUp(self):
         # Set the test project identifier
         super().setUp()
-        self.copy_vocab_tables(self.vocabulary_id)
+
         raw_data_load_query_obs = INSERT_RAW_DATA_OBS.render(
             project_id=self.project_id, dataset_id=self.dataset_id)
         raw_data_load_query_mapping = INSERT_RAW_DATA_MAPPING. \
             render(project_id=self.project_id, dataset_id=self.dataset_id)
+
+        self.date = parse('2020-01-01').date()
 
         # Load test data
         self.load_test_data([
@@ -104,9 +109,17 @@ class ConflictingHpoStateGeneralizeTest(BaseTest.CleaningRulesTestBase):
                     f'{self.project_id}.{self.dataset_id}._mapping_{OBSERVATION}',
                 ]),
             'loaded_ids': [1, 2, 3, 4],
-            'sandboxed_ids': [1, 4],
-            'fields': ['person_id', 'src_hpo_id'],
-            'cleaned_values': [(101, 'hpo_100'), (104, 'hpo_103')]
+            'sandboxed_ids': [1],
+            'fields': ['observation_id', 'person_id', 'observation_concept_id',
+                       'observation_date', 'observation_type_concept_id',
+                       'value_as_concept_id', 'observation_source_concept_id',
+                       'value_source_concept_id'],
+            'cleaned_values': [
+                (1, 101, 1001, self.date, 1, 2000000011, 1585249, 2000000011),
+                (2, 102, 1002, self.date, 1, 100, 1585249,),
+                (3, 103, 1003, self.date, 1, 100, 1585249,),
+                (4, 104, 1004, self.date, 1, 100, 1585248,),
+            ]
         }]
 
         self.default_test(tables_and_counts)
