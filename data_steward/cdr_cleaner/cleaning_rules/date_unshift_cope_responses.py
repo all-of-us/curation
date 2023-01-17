99ee2307f82585a9f83ce5282e850622e0cc1752
brendagutman <77469967+brendagutman@users.noreply.github.com>
2023-01-17 11:03:45 -0600

    [DC-2839] Refactor queries to use research_ids and update test. (#1464)
    * [DC-2839] Refactor queries to use research_ids and update test.
    
    * [DC-2839] Add the required dependency.
    
    * [DC-2839] Account for NULLS

--- data_steward/cdr_cleaner/cleaning_rules/date_unshift_cope_responses.py
+++ data_steward/cdr_cleaner/cleaning_rules/date_unshift_cope_responses.py
@@ -2,20 +2,15 @@
 Reverse Date Shift for COPE Responses.
 
 The COPE survey is longitudinal (administered monthly), so researchers will need a way to identify with which survey
-monthly iteration data is associated. Although there is a long-term plan to allow for survey versioning,
-this is anticipated to provide a short-term solution to support inclusion of COPE data in the CDR.
+monthly iteration data is associated. Because the surveys are known to have been taken in a particular month, these
+observations being date shifted could allow all other date shifting for these participants to be broken.
 
-The Date unshift is applied on the cope survey responses by following steps:
+History: In past versions of this cleaning rule, the concept_relationship table was used to gather all concepts related
+to the singular OMOP module concept_id(cope - 1333342). Then the data in `pipeline_tables.cope_concepts` was used to
+assign individual module ids(AoU_Custom concepts). These data would also have their date fields unshifted.
 
-1)lookup table with all the COPE survey module descendants were generated using the concept_relationship table.
-
-2) Created a CTE with filtering out the observation table with only observation_concept_ids that were cope_concept_ids
- using the `pipeline_tables.cope_concepts` and then apply the date_shit to those records in the CTE.
-
-3) Then Left joined this CTE temp table to the observation table post_deid using the observation_id.
-
-4) Using the Left join used coalesce function to get the date_unshifted observation_date and observation_datetime for
- cope survey responses.
+This cleaning rule now uses the observation_ext table to determine cope data in the observation table, and the
+survey_concept_id fields in the survey_conduct table for the same purpose.
 
 Original Issue: DC-938
 """
@@ -26,8 +21,9 @@ import logging
 # Project Imports
 from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
 from constants.cdr_cleaner import clean_cdr as cdr_consts
-from common import JINJA_ENV, OBSERVATION, SURVEY_CONDUCT
+from common import JINJA_ENV, OBSERVATION, SURVEY_CONDUCT, DEID_MAP
 from cdr_cleaner.cleaning_rules.deid.survey_version_info import COPESurveyVersionTask
+from cdr_cleaner.cleaning_rules.deid.questionnaire_response_id_map import QRIDtoRID
 
 LOGGER = logging.getLogger(__name__)
 
@@ -49,14 +45,23 @@ SELECT *
 FROM `{{project_id}}.{{dataset_id}}.{{survey_conduct_table}}`
 WHERE survey_source_concept_id IN (2100000002, 2100000003, 2100000004, 2100000005, 2100000006,
   2100000007, 905047, 905055, 765936)
+OR survey_concept_id IN (2100000002, 2100000003, 2100000004, 2100000005, 2100000006,
+  2100000007, 905047, 905055, 765936)
 """)
 
 DATE_UNSHIFT_OBS_QUERY = JINJA_ENV.from_string("""
 UPDATE `{{project_id}}.{{dataset_id}}.{{observation_table}}` ob
-SET observation_date = coalesce(po.observation_date, ob.observation_date),
-    observation_datetime = coalesce(po.observation_datetime, ob.observation_datetime)
-FROM `{{project_id}}.{{pre_deid_dataset}}.{{observation_table}}` po
-WHERE ob.observation_id = po.observation_id
+SET observation_date       = unshifted_observation_date,
+    observation_datetime   = unshifted_observation_datetime
+FROM (SELECT 
+        observation_id,
+        COALESCE(DATE_ADD(observation_date, INTERVAL shift DAY), observation_date) as unshifted_observation_date,
+        COALESCE(DATE_ADD(observation_datetime, INTERVAL shift DAY), observation_datetime) as unshifted_observation_datetime
+    FROM `{{project_id}}.{{dataset_id}}.{{observation_table}}` ob
+    LEFT JOIN `{{project_id}}.{{mapping_dataset}}.{{_deid_map}}` prm
+    ON ob.person_id = prm.research_id  -- reminder: obs has been deid --
+    ) AS ref 
+WHERE ob.observation_id = ref.observation_id
 AND ob.observation_id IN (
     SELECT observation_id
     FROM `{{project_id}}.{{sandbox_dataset}}.{{intermediary_obs_table}}`)
@@ -64,12 +69,20 @@ AND ob.observation_id IN (
 
 DATE_UNSHIFT_SC_QUERY = JINJA_ENV.from_string("""
 UPDATE `{{project_id}}.{{dataset_id}}.{{survey_conduct_table}}` sc
-SET survey_start_date = coalesce(psc.survey_start_date, sc.survey_start_date),
-    survey_start_datetime = coalesce(psc.survey_start_datetime, sc.survey_start_datetime),
-    survey_end_date = coalesce(psc.survey_end_date, sc.survey_end_date),
-    survey_end_datetime = coalesce(psc.survey_end_datetime, sc.survey_end_datetime)
-FROM `{{project_id}}.{{pre_deid_dataset}}.{{survey_conduct_table}}` psc
-WHERE sc.survey_conduct_id = psc.survey_conduct_id
+SET survey_start_date = unshifted_start_date,
+    survey_start_datetime = unshifted_start_datetime,
+    survey_end_date = unshifted_end_date,
+    survey_end_datetime = unshifted_end_datetime
+FROM (SELECT
+        survey_conduct_id,
+        COALESCE(DATE_ADD(survey_start_date, INTERVAL shift DAY),survey_start_date) as unshifted_start_date,
+        COALESCE(DATE_ADD(survey_start_datetime, INTERVAL shift DAY),survey_start_datetime) as unshifted_start_datetime,
+        COALESCE(DATE_ADD(survey_end_date, INTERVAL shift DAY),survey_end_date) as unshifted_end_date,
+        COALESCE(DATE_ADD(survey_end_datetime, INTERVAL shift DAY),survey_end_datetime) as unshifted_end_datetime
+    FROM `{{project_id}}.{{dataset_id}}.{{survey_conduct_table}}`  sc
+    LEFT JOIN `{{project_id}}.{{mapping_dataset}}.{{_deid_map}}`  prm
+    ON sc.person_id = prm.research_id) AS ref -- reminder: sc has been deid --
+WHERE sc.survey_conduct_id = ref.survey_conduct_id
 AND sc.survey_conduct_id IN (
     SELECT survey_conduct_id
     FROM `{{project_id}}.{{sandbox_dataset}}.{{intermediary_sc_table}}`)
@@ -85,6 +98,7 @@ class DateUnShiftCopeResponses(BaseCleaningRule):
                  project_id,
                  dataset_id,
                  sandbox_dataset_id,
+                 mapping_dataset_id,
                  table_namer=None):
         """
         Initialize the class with proper information.
@@ -94,15 +108,20 @@ class DateUnShiftCopeResponses(BaseCleaningRule):
         DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
         """
         desc = 'Reverse Date Shift for COPE Responses'
+
+        self.mapping_dataset_id = mapping_dataset_id
+
         super().__init__(
-            issue_numbers=['DC938', 'DC982', 'DC970', 'DC2438', 'DC2717'],
+            issue_numbers=[
+                'DC938', 'DC982', 'DC970', 'DC2438', 'DC2717', 'DC2839'
+            ],
             description=desc,
             affected_datasets=[cdr_consts.REGISTERED_TIER_DEID_BASE],
             affected_tables=[OBSERVATION, SURVEY_CONDUCT],
             project_id=project_id,
             dataset_id=dataset_id,
             sandbox_dataset_id=sandbox_dataset_id,
-            depends_on=[COPESurveyVersionTask],
+            depends_on=[COPESurveyVersionTask, QRIDtoRID],
             table_namer=table_namer)
 
     def get_combined_dataset_from_deid_dataset(self, dataset_name):
@@ -151,12 +170,12 @@ class DateUnShiftCopeResponses(BaseCleaningRule):
             update_query_obs = dict()
             update_query_obs[cdr_consts.QUERY] = DATE_UNSHIFT_OBS_QUERY.render(
                 project_id=self.project_id,
-                pre_deid_dataset=self.get_combined_dataset_from_deid_dataset(
-                    self.dataset_id),
                 dataset_id=self.dataset_id,
+                observation_table=OBSERVATION,
+                mapping_dataset=self.mapping_dataset_id,
+                _deid_map=DEID_MAP,
                 sandbox_dataset=self.sandbox_dataset_id,
-                intermediary_obs_table=self.sandbox_table_for(OBSERVATION),
-                observation_table=OBSERVATION)
+                intermediary_obs_table=self.sandbox_table_for(OBSERVATION))
             update_queries.append(update_query_obs)
 
         if SURVEY_CONDUCT in self.affected_tables:
@@ -174,12 +193,12 @@ class DateUnShiftCopeResponses(BaseCleaningRule):
             update_query_sc = dict()
             update_query_sc[cdr_consts.QUERY] = DATE_UNSHIFT_SC_QUERY.render(
                 project_id=self.project_id,
-                pre_deid_dataset=self.get_combined_dataset_from_deid_dataset(
-                    self.dataset_id),
                 dataset_id=self.dataset_id,
+                survey_conduct_table=SURVEY_CONDUCT,
+                mapping_dataset=self.mapping_dataset_id,
+                _deid_map=DEID_MAP,
                 sandbox_dataset=self.sandbox_dataset_id,
-                intermediary_sc_table=self.sandbox_table_for(SURVEY_CONDUCT),
-                survey_conduct_table=SURVEY_CONDUCT)
+                intermediary_sc_table=self.sandbox_table_for(SURVEY_CONDUCT))
             update_queries.append(update_query_sc)
         return sandbox_queries + update_queries
 
@@ -203,18 +222,30 @@ if __name__ == '__main__':
     import cdr_cleaner.args_parser as parser
     import cdr_cleaner.clean_cdr_engine as clean_engine
 
-    ARGS = parser.parse_args()
+    mapping_dataset_arg = {
+        parser.SHORT_ARGUMENT: '-m',
+        parser.LONG_ARGUMENT: '--mapping_dataset_id',
+        parser.ACTION: 'store',
+        parser.DEST: 'mapping_dataset_id',
+        parser.HELP: 'Identifies the dataset containing pid-rid map table',
+        parser.REQUIRED: True
+    }
+
+    ARGS = parser.default_parse_args([mapping_dataset_arg])
 
     if ARGS.list_queries:
         clean_engine.add_console_logging()
-        query_list = clean_engine.get_query_list(ARGS.project_id,
-                                                 ARGS.dataset_id,
-                                                 ARGS.sandbox_dataset_id,
-                                                 [(DateUnShiftCopeResponses,)])
+        query_list = clean_engine.get_query_list(
+            ARGS.project_id,
+            ARGS.dataset_id,
+            ARGS.sandbox_dataset_id, [(DateUnShiftCopeResponses,)],
+            mapping_dataset_id=ARGS.mapping_dataset_id)
         for query in query_list:
             LOGGER.info(query)
     else:
         clean_engine.add_console_logging(ARGS.console_log)
-        clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
+        clean_engine.clean_dataset(ARGS.project_id,
+                                   ARGS.dataset_id,
                                    ARGS.sandbox_dataset_id,
-                                   [(DateUnShiftCopeResponses,)])
+                                   [(DateUnShiftCopeResponses,)],
+                                   mapping_dataset_id=ARGS.mapping_dataset_id)
--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/date_unshift_cope_responses_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/date_unshift_cope_responses_test.py
@@ -12,7 +12,7 @@ from unittest.mock import patch
 # Project imports
 from app_identity import PROJECT_ID
 from cdr_cleaner.cleaning_rules.date_unshift_cope_responses import DateUnShiftCopeResponses
-from common import SURVEY_CONDUCT
+from common import SURVEY_CONDUCT, DEID_MAP
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
 
@@ -35,17 +35,25 @@ class DateUnShiftCopeResponsesTest(BaseTest.CleaningRulesTestBase):
         sandbox_id = f'{dataset_id}_sandbox'
         cls.sandbox_id = sandbox_id
 
+        # include the mapping dataset
+        mapping_dataset_id = os.environ.get('COMBINED_DATASET_ID')
+        cls.mapping_dataset_id = mapping_dataset_id
+        cls.kwargs.update({'mapping_dataset_id': mapping_dataset_id})
+        cls.fq_deid_map_table_name = f'{cls.project_id}.{mapping_dataset_id}.{DEID_MAP}'
+
         cls.combined_dataset_id = os.environ.get('COMBINED_DATASET_ID')
 
         cls.rule_instance = DateUnShiftCopeResponses(cls.project_id, dataset_id,
-                                                     sandbox_id)
+                                                     sandbox_id,
+                                                     mapping_dataset_id)
 
         cls.rule_instance.affected_tables = [SURVEY_CONDUCT]
 
         # can test the full functionality with one table
         cls.fq_table_names = [
             f"{cls.project_id}.{cls.combined_dataset_id}.{cls.rule_instance.affected_tables[0]}",
-            f"{cls.project_id}.{dataset_id}.{cls.rule_instance.affected_tables[0]}"
+            f"{cls.project_id}.{dataset_id}.{cls.rule_instance.affected_tables[0]}",
+            cls.fq_deid_map_table_name
         ]
 
         cls.fq_sandbox_table_names = [
@@ -61,6 +69,13 @@ class DateUnShiftCopeResponsesTest(BaseTest.CleaningRulesTestBase):
         Add data to the tables for the rule to run on.
         """
 
+        deid_map_query = self.jinja_env.from_string("""
+             INSERT INTO `{{fq_table_name}}` (person_id, research_id, shift)
+             VALUES
+             (81, 801, 7),
+             (82, 802, 4)
+             """)
+
         self.client.create_tables(self.fq_table_names)
 
         # load statement for the test data to unshift
@@ -71,8 +86,8 @@ class DateUnShiftCopeResponsesTest(BaseTest.CleaningRulesTestBase):
         assisted_concept_id, respondent_type_concept_id, timing_concept_id,
         collection_method_concept_id, survey_source_concept_id, validated_survey_concept_id)
         VALUES
-        (10, 801, 200, date(2016, 05, 11), timestamp(datetime(2016, 05, 11, 12, 45, 00)), date(2017, 05, 11), timestamp(datetime(2017, 05, 11, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6),
-        (11, 802, 201, date(2016, 05, 10), timestamp(datetime(2016, 05, 10, 12, 45, 00)), date(2017, 05, 11), timestamp(datetime(2017, 05, 11, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6)
+        (8, 801, 200, date(2016, 05, 11), timestamp(datetime(2016, 05, 11, 12, 45, 00)), date(2017, 05, 11), timestamp(datetime(2017, 05, 11, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6),
+        (9, 802, 201, date(2016, 05, 10), timestamp(datetime(2016, 05, 10, 12, 45, 00)), date(2017, 05, 10), timestamp(datetime(2017, 05, 10, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6)
         """)
 
         query_deid = self.jinja_env.from_string("""
@@ -82,13 +97,22 @@ class DateUnShiftCopeResponsesTest(BaseTest.CleaningRulesTestBase):
         assisted_concept_id, respondent_type_concept_id, timing_concept_id,
         collection_method_concept_id, survey_source_concept_id, validated_survey_concept_id)
         VALUES
-        (10, 81, 200, date(2016, 05, 4), timestamp(datetime(2016, 05, 4, 12, 45, 00)), date(2017, 05, 4), timestamp(datetime(2017, 05, 4, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6),
-        (11, 82, 201, date(2016, 05, 6), timestamp(datetime(2016, 05, 6, 12, 45, 00)), date(2017, 05, 6), timestamp(datetime(2017, 05, 6, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6)
+        -- cope concept in survey_source_concept_id. Should be unshifted. --
+        (10, 801, 200, date(2016, 05, 4),timestamp(datetime(2016, 05, 4, 12, 45, 00)), date(2017, 05, 4), timestamp(datetime(2017, 05, 4, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6),
+        -- cope concept in survey_concept_id. Should be unshifted. --
+        (11, 802, 2100000005, date(2016, 05, 6),timestamp(datetime(2016, 05, 6, 12, 45, 00)), date(2017, 05, 6), timestamp(datetime(2017, 05, 6, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6),
+        -- Multiple cope records for the same participant. All cope data should be unshifted. --
+        (12, 802, 201, date(2016, 05, 7),timestamp(datetime(2016, 05, 7, 12, 45, 00)), date(2017, 05, 7), timestamp(datetime(2017, 05, 7, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6),
+        -- Multiple records for the same participant. Non-cope data should remain shifted. --
+        (13, 802, 201, date(2016, 05, 7),timestamp(datetime(2016, 05, 7, 12, 45, 00)), date(2017, 05, 7), timestamp(datetime(2017, 05, 7, 12, 45, 00)), 1, 2, 3, 4, 201, 6),
+        -- Handling nulls in nullable fields --
+        (14, 802, 201, NULL, NULL, NULL, timestamp(datetime(2017, 05, 7, 12, 45, 00)), 1, 2, 3, 4, 2100000005, 6)
         """)
 
         load_statements = [
             query_combined.render(fq_table_name=self.fq_table_names[0]),
             query_deid.render(fq_table_name=self.fq_table_names[1]),
+            deid_map_query.render(fq_table_name=self.fq_table_names[2])
         ]
         self.load_test_data(load_statements)
 
@@ -114,17 +138,27 @@ class DateUnShiftCopeResponsesTest(BaseTest.CleaningRulesTestBase):
                 'survey_start_date', 'survey_start_datetime', 'survey_end_date',
                 'survey_end_datetime'
             ],
-            'loaded_ids': [10, 11],
-            'sandboxed_ids': [10, 11],
+            'loaded_ids': [10, 11, 12, 13, 14],
+            'sandboxed_ids': [10, 11, 12, 14],
             'cleaned_values': [
-                (10, 81, 200, date.fromisoformat('2016-05-11'),
+                (10, 801, 200, date.fromisoformat('2016-05-11'),
                  datetime.fromisoformat('2016-05-11 12:45:00+00:00'),
                  date.fromisoformat('2017-05-11'),
                  datetime.fromisoformat('2017-05-11 12:45:00+00:00')),
-                (11, 82, 201, date.fromisoformat('2016-05-10'),
+                (11, 802, 2100000005, date.fromisoformat('2016-05-10'),
                  datetime.fromisoformat('2016-05-10 12:45:00+00:00'),
+                 date.fromisoformat('2017-05-10'),
+                 datetime.fromisoformat('2017-05-10 12:45:00+00:00')),
+                (12, 802, 201, date.fromisoformat('2016-05-11'),
+                 datetime.fromisoformat('2016-05-11 12:45:00+00:00'),
                  date.fromisoformat('2017-05-11'),
-                 datetime.fromisoformat('2017-05-11 12:45:00+00:00'))
+                 datetime.fromisoformat('2017-05-11 12:45:00+00:00')),
+                (13, 802, 201, date.fromisoformat('2016-05-07'),
+                 datetime.fromisoformat('2016-05-07 12:45:00+00:00'),
+                 date.fromisoformat('2017-05-07'),
+                 datetime.fromisoformat('2017-05-07 12:45:00+00:00')),
+                (14, 802, 201, None, None, None,
+                 datetime.fromisoformat('2017-05-11 12:45:00+00:00')),
             ]
         }]
 
