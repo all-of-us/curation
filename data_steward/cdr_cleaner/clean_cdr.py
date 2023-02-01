b8a4f897e8d065be2f1dc0988dfc7cc3fa63d002
ratuagga <ratul.aggarwal@vumc.org>
2023-02-01 14:51:24 -0600

    [DC-1221] bug fix for circleci test fail.

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -249,8 +249,8 @@ REGISTERED_TIER_DEID_CLEANING_CLASSES = [
     # Data generalizations
     ####################################
     (
-        GeneralizeStateByPopulation,),
-    (ConflictingHpoStateGeneralize,),
+        ConflictingHpoStateGeneralize,),
+    (GeneralizeStateByPopulation,),
     (GeneralizeCopeInsuranceAnswers,),
     # (GeneralizeSexGenderConcepts,),
 
