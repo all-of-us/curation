6f363525f429dade4c2b352c6d5452825380e1cd
brendagutman <brenda.gutman@vumc.org>
2023-02-13 15:20:18 -0600

    [DC-3052] Account for Skips and whitespace in clean_ppi_numeric_fields_using_parameters.py

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/clean_ppi_numeric_fields_using_parameters_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/clean_ppi_numeric_fields_using_parameters_test.py
@@ -79,6 +79,8 @@ class CleanPPINumericFieldsUsingParameterTest(BaseTest.CleaningRulesTestBase):
         statements and the tables_and_counts variable.
         """
 
+        self.maxDiff = None
+
         queries = []
 
         invalid_values_tmpl = self.jinja_env.from_string(
@@ -131,6 +133,21 @@ class CleanPPINumericFieldsUsingParameterTest(BaseTest.CleaningRulesTestBase):
         ).render(fq_dataset_name=self.fq_dataset_name)
         queries.append(six_plus_tmpl)
 
+        skips_tmpl = self.jinja_env.from_string("""
+            INSERT INTO `{{fq_dataset_name}}.observation`
+            (observation_id, person_id, observation_concept_id, observation_source_concept_id, observation_date,
+             observation_type_concept_id, value_as_number, value_as_string, value_as_concept_id, value_source_concept_id)
+            VALUES
+                -- Skip values should not be invalidated --
+                (123, 23, 1333023,1333023, date('2015-07-15'), 0, NULL, '', 0, 903096),
+                (124, 24, 1333023,1333023, date('2015-07-15'), 0, NULL, ' ', 0, 903096),
+                (125, 25, 1333023,1333023, date('2015-07-15'), 0, NULL, 'PMI_Skip', 0, 903096),
+                (126, 26, 1585890,1585890, date('2015-07-15'), 0, NULL, '', 903096, 903096),
+                (127, 27, 1585890,1585890, date('2015-07-15'), 0, NULL, ' ', 903096, 903096),
+                (128, 28, 1585890,1585890, date('2015-07-15'), 0, NULL, 'PMI_Skip', 903096, 903096)
+                """).render(fq_dataset_name=self.fq_dataset_name)
+        queries.append(skips_tmpl)
+
         self.load_test_data(queries)
 
         # Expected results list
@@ -141,11 +158,11 @@ class CleanPPINumericFieldsUsingParameterTest(BaseTest.CleaningRulesTestBase):
                 self.fq_sandbox_table_names[0],
             'loaded_ids': [
                 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115,
-                116, 117, 118, 119, 120, 121, 122
+                116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128
             ],
             'sandboxed_ids': [
                 103, 104, 105, 106, 107, 108, 112, 113, 115, 116, 118, 119, 120,
-                121, 122
+                121, 122, 123, 124, 125
             ],
             'fields': [
                 'observation_id', 'observation_concept_id',
@@ -206,7 +223,14 @@ class CleanPPINumericFieldsUsingParameterTest(BaseTest.CleaningRulesTestBase):
                  self.invalid_values_value_as_concept_id),
                 (120, 1333023, 1333023, None, None,
                  self.six_plus_value_as_concept_id,
-                 self.six_plus_value_as_concept_id)
+                 self.six_plus_value_as_concept_id),
+                # Test skips
+                (123, 1333023, 1333023, None, None, 0, 903096),
+                (124, 1333023, 1333023, None, None, 0, 903096),
+                (125, 1333023, 1333023, None, None, 0, 903096),
+                (126, 1585890, 1585890, None, '', 903096, 903096),
+                (127, 1585890, 1585890, None, ' ', 903096, 903096),
+                (128, 1585890, 1585890, None, 'PMI_Skip', 903096, 903096)
             ]
         }]
 
