79c5160495046aa7e117a752c29f11ee3c7a5d53
Michael Schmidt <michael.schmidt@vumc.org>
2023-02-20 15:03:08 -0600

    [DC-3060] Correctly mock and test parser

--- tests/unit_tests/data_steward/cdm_test.py
+++ tests/unit_tests/data_steward/cdm_test.py
@@ -1,7 +1,7 @@
 import unittest
 from mock import patch
 
-import cdm, common
+import cdm
 
 
 class CDMTest(unittest.TestCase):
@@ -18,12 +18,12 @@ class CDMTest(unittest.TestCase):
         self.mock_table = 'mock_table'
         self.mock_component = 'mock_component'
 
-        self.mock_achilles = common.ACHILLES
+        self.mock_achilles = 'mock_achilles'
         self.mock_vocabulary = 'mock_vocabulary'
 
-    @patch('common.CDM_COMPONENTS')
-    @patch('resources.CDM_TABLES')
-    def test_parser_with_bad_args(self, mock_cdm_tables, mock_cdm_components):
+    @patch('cdm.common')  # components
+    @patch('cdm.resources')  # tables
+    def test_parser_with_bad_args(self, mock_resources, mock_common):
         """
         Case 1: No dataset
         Case 2: Bad table
@@ -32,8 +32,9 @@ class CDMTest(unittest.TestCase):
         Case 5: table/component mutual exclusion violation with good args
         """
 
-        mock_cdm_tables.return_value = [self.mock_table]
-        mock_cdm_components.return_value = [self.mock_component]
+        mock_common.CDM_COMPONENTS = [self.mock_component, self.mock_achilles]
+        mock_common.ACHILLES = [self.mock_achilles]
+        mock_resources.CDM_TABLES = [self.mock_table]
 
         parser = cdm.get_parser()
 
@@ -49,9 +50,11 @@ class CDMTest(unittest.TestCase):
         with self.assertRaises(SystemExit):
             _ = parser.parse_args(test_args)
 
-        test_args = [self.mock_dataset, '--component', self.mock_achilles]
-        with self.assertRaises(SystemExit):
-            _ = parser.parse_args(test_args)
+        # Incorrect, targets parser, not main()
+        # test_args = [self.mock_dataset, '--component', self.mock_achilles]
+        # with self.assertRaises(NotImplementedError):
+        #     _ = parser.parse_args(test_args)
+        #     cdm.main()
 
         test_args = [
             self.mock_dataset, '--component', self.mock_component, '--table',
@@ -60,27 +63,30 @@ class CDMTest(unittest.TestCase):
         with self.assertRaises(SystemExit):
             _ = parser.parse_args(test_args)
 
-    @patch('common.VOCABULARY')
-    def test_parser_with_good_args(self, mock_vocabulary):
+    @patch('cdm.common')  # components
+    @patch('cdm.resources')  # tables
+    def test_parser_with_good_args(self, mock_resources, mock_common):
         """
-        Case 1: Only a dataset specified
-        Case 2: Dataset with table
-        Case 3: Dataset with common.vocabulary component
+        Case 1: Only a dataset specified -> create_all()
+        Case 2: Dataset with table -> create_table()
+        Case 3: Dataset with component -> create_vocabulary()
         Case 4: Dataset with no matching component
         """
 
-        mock_vocabulary.return_value = self.mock_vocabulary
+        mock_common.CDM_COMPONENTS = [self.mock_component, self.mock_achilles]
+        mock_common.ACHILLES = [self.mock_achilles]
+        mock_resources.CDM_TABLES = [self.mock_table]
 
         parser = cdm.get_parser()
 
         test_args = [self.mock_dataset]
-        result_args = parser.parse_args(test_args)
+        result = parser.parse_args(test_args)
+        self.assertIn(self.mock_dataset, list(vars(result).values()))
 
-        test_args = [self.mock_dataset, '--component', 'test_vocabulary']
-        result_args = parser.parse_args(test_args)
+        test_args = [self.mock_dataset, '--table', self.mock_table]
+        result = parser.parse_args(test_args)
+        self.assertIn(self.mock_table, list(vars(result).values()))
 
-        test_args = [
-            self.mock_dataset,
-            '--table',
-        ]
-        result_args = parser.parse_args(test_args)
+        test_args = [self.mock_dataset, '--component', self.mock_component]
+        result = parser.parse_args(test_args)
+        self.assertIn(self.mock_component, list(vars(result).values()))
