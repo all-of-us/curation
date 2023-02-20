22f72f88dff710b1f06fbcc510c40a534d5076f6
Michael Schmidt <michael.schmidt@vumc.org>
2023-02-20 16:48:49 -0600

    [DC-3060] Fixed bad args test, completed good args test

--- tests/unit_tests/data_steward/cdm_test.py
+++ tests/unit_tests/data_steward/cdm_test.py
@@ -1,5 +1,6 @@
 import unittest
 from mock import patch
+from types import SimpleNamespace
 
 import cdm
 
@@ -13,7 +14,7 @@ class CDMTest(unittest.TestCase):
         print('**************************************************************')
 
     def setUp(self):
-        self.mock_dataset = 'mock_dataset'
+        self.mock_dataset_id = 'mock_dataset'
 
         self.mock_table = 'mock_table'
         self.mock_component = 'mock_component'
@@ -25,39 +26,48 @@ class CDMTest(unittest.TestCase):
     @patch('cdm.resources')  # tables
     def test_parser_with_bad_args(self, mock_resources, mock_common):
         """
-        Case 1: No dataset
-        Case 2: Bad table
-        Case 3: Bad component
-        Case 4: Using Achilles (NYI)
-        Case 5: table/component mutual exclusion violation with good args
+        Case 1: No dataset, good table
+        Case 2: Bad table, good dataset
+        Case 3: Bad component, good dataset
+        Case 4: Create a NotYetImplemented case, good dataset
+        Case 5: table/component mutual exclusion violation, good args
         """
 
         mock_common.CDM_COMPONENTS = [self.mock_component, self.mock_achilles]
-        mock_common.ACHILLES = [self.mock_achilles]
+        mock_common.ACHILLES = self.mock_achilles
         mock_resources.CDM_TABLES = [self.mock_table]
 
         parser = cdm.get_parser()
 
-        test_args = []
+        # Case 1
+        test_args = ['--table', self.mock_table]
         with self.assertRaises(SystemExit):
             _ = parser.parse_args(test_args)
 
-        test_args = [self.mock_dataset, '--table', 'bad_table']
+        # Case 2
+        test_args = [self.mock_dataset_id, '--table', 'bad_table']
         with self.assertRaises(SystemExit):
             _ = parser.parse_args(test_args)
 
-        test_args = [self.mock_dataset, '--component', 'bad_component']
+        # Case 3
+        test_args = [self.mock_dataset_id, '--component', 'bad_component']
         with self.assertRaises(SystemExit):
             _ = parser.parse_args(test_args)
 
-        # Incorrect, targets parser, not main()
-        # test_args = [self.mock_dataset, '--component', self.mock_achilles]
-        # with self.assertRaises(NotImplementedError):
-        #     _ = parser.parse_args(test_args)
-        #     cdm.main()
+        # Case 4
+        sn = SimpleNamespace()
+        sn.table = None
+        sn.component = self.mock_achilles
+        sn.dataset_id = self.mock_dataset_id
+        with self.assertRaises(NotImplementedError), patch(
+                'argparse.ArgumentParser.parse_args') as mock_args:
 
+            mock_args.return_value = sn
+            cdm.main()
+
+        # Case 5
         test_args = [
-            self.mock_dataset, '--component', self.mock_component, '--table',
+            self.mock_dataset_id, '--component', self.mock_component, '--table',
             self.mock_table
         ]
         with self.assertRaises(SystemExit):
@@ -67,26 +77,53 @@ class CDMTest(unittest.TestCase):
     @patch('cdm.resources')  # tables
     def test_parser_with_good_args(self, mock_resources, mock_common):
         """
-        Case 1: Only a dataset specified -> create_all()
-        Case 2: Dataset with table -> create_table()
+        Case 1: Only a dataset specified -> create_all_tables(dataset_id)
+        Case 2: Dataset with no matching component -> pass
         Case 3: Dataset with component -> create_vocabulary()
-        Case 4: Dataset with no matching component
+        Case 4: Dataset with table -> create_table()
         """
 
-        mock_common.CDM_COMPONENTS = [self.mock_component, self.mock_achilles]
-        mock_common.ACHILLES = [self.mock_achilles]
+        mock_common.VOCABULARY = self.mock_vocabulary
+        mock_common.CDM_COMPONENTS = [self.mock_component]
         mock_resources.CDM_TABLES = [self.mock_table]
 
-        parser = cdm.get_parser()
+        sn = SimpleNamespace()
+        sn.table = None
+        sn.component = None
+        sn.dataset_id = self.mock_dataset_id
 
-        test_args = [self.mock_dataset]
-        result = parser.parse_args(test_args)
-        self.assertIn(self.mock_dataset, list(vars(result).values()))
+        # Case 1
+        with patch('cdm.create_all_tables') as mock_create_all, patch(
+                'argparse.ArgumentParser.parse_args') as mock_args:
 
-        test_args = [self.mock_dataset, '--table', self.mock_table]
-        result = parser.parse_args(test_args)
-        self.assertIn(self.mock_table, list(vars(result).values()))
+            mock_args.return_value = sn  # Pre
+            cdm.main()  # Test
+            mock_create_all.assert_called_once_with(
+                self.mock_dataset_id)  # Post
 
-        test_args = [self.mock_dataset, '--component', self.mock_component]
-        result = parser.parse_args(test_args)
-        self.assertIn(self.mock_component, list(vars(result).values()))
+        # Case 2
+        sn.component = 'no_matching_table'
+        with patch('argparse.ArgumentParser.parse_args') as mock_args:
+
+            mock_args.return_value = sn  # Pre
+            cdm.main()  # Test
+            pass  # Post
+
+        # Case 3
+        sn.component = self.mock_vocabulary
+        with patch('cdm.create_vocabulary_tables') as mock_create_vocab, patch(
+                'argparse.ArgumentParser.parse_args') as mock_args:
+
+            mock_args.return_value = sn  # Pre
+            cdm.main()  # Test
+            mock_create_vocab.assert_called_once_with(self.mock_dataset_id)
+
+        # Case 4
+        sn.table = self.mock_table
+        with patch('cdm.create_table') as mock_create_table, patch(
+                'argparse.ArgumentParser.parse_args') as mock_args:
+
+            mock_args.return_value = sn  # Pre
+            cdm.main()  # Test
+            mock_create_table.assert_called_once_with(self.mock_table,
+                                                      self.mock_dataset_id)
