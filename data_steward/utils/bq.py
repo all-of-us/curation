68c47c0c56596267376b9c53e148f8e2522e631b
lledezma <luis.ledezma@vumc.org>
2023-03-06 13:24:13 -0600

    [DC-3102] Update deprecation warning

--- data_steward/utils/bq.py
+++ data_steward/utils/bq.py
@@ -646,7 +646,7 @@ def copy_datasets(client: bigquery.Client, input_dataset, output_dataset):
         client.copy_table(table, staging_table)
 
 
-@deprecated(reason='Use resources.validate_bq_date_string(date_string) instead')
+@deprecated(reason='Use resources.validate_date_string(date_string) instead')
 def validate_bq_date_string(date_string):
     """
     Validates the date string is a valid date in the YYYY-MM-DD format.
