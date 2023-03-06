c433fdf15e240e93976c2d4d7ba49a0728cfcb4f
lledezma <luis.ledezma@vumc.org>
2023-03-06 10:22:41 -0600

    [DC-3114] Add bq_client argument

--- tests/integration_tests/data_steward/cdr_cleaner/clean_cdr_engine_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/clean_cdr_engine_test.py
@@ -24,10 +24,10 @@ class CleanCDREngineTest(TestCase):
 
     def setUp(self):
         self.project_id = get_application_id()
+        self.bq_client = BigQueryClient(self.project_id)
         self.dataset_id = os.environ.get('UNIONED_DATASET_ID')
         self.sandbox_dataset_id = sandbox.check_and_create_sandbox_dataset(
-            self.project_id, self.dataset_id)
-        self.bq_client = BigQueryClient(self.project_id)
+            self.bq_client, self.dataset_id)
         self.delete_sandbox()
 
     def test_clean_dataset(self):
