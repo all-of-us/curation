09a4effd4a0d5b83268f288212ba96737db1ec80
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2023-03-17 08:08:32 -0400

    [DC-3099] Add questionnaire_response_id to test

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/backfill_lifestyle_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/backfill_lifestyle_test.py
@@ -15,23 +15,23 @@ from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_te
 
 OBSERVATION_TMPL = JINJA_ENV.from_string("""
 INSERT INTO `{{project}}.{{dataset}}.observation`
-(observation_id, person_id, observation_concept_id, observation_date, observation_type_concept_id, observation_source_concept_id)
+(observation_id, person_id, observation_concept_id, observation_date, observation_type_concept_id, observation_source_concept_id, questionnaire_response_id)
 VALUES
     -- person_id=1 has all Lifestyle records. Nothing happens. --
-    (101, 1, 40766306, date('2020-01-01'), 45905771, 1585857),
-    (102, 1, 1585636, date('2020-01-01'), 45905771, 1585636),
-    (103, 1, 1586166, date('2020-01-01'), 45905771, 1586166),
-    (104, 1, 1586174, date('2020-01-01'), 45905771, 1586174),
-    (105, 1, 1586182, date('2020-01-01'), 45905771, 1586182),
-    (106, 1, 1586190, date('2020-01-01'), 45905771, 1586190),
-    (107, 1, 40766357, date('2020-01-01'), 45905771, 1586198),
+    (101, 1, 40766306, date('2020-01-01'), 45905771, 1585857, 111),
+    (102, 1, 1585636, date('2020-01-01'), 45905771, 1585636, 111),
+    (103, 1, 1586166, date('2020-01-01'), 45905771, 1586166, 111),
+    (104, 1, 1586174, date('2020-01-01'), 45905771, 1586174, 111),
+    (105, 1, 1586182, date('2020-01-01'), 45905771, 1586182, 111),
+    (106, 1, 1586190, date('2020-01-01'), 45905771, 1586190, 111),
+    (107, 1, 40766357, date('2020-01-01'), 45905771, 1586198, 111),
     -- person_id=2 has NO Lifestyle records. Nothing happens. --
-    (201, 2, 9999999, date('2020-01-01'), 99999999, 9999999),
+    (201, 2, 9999999, date('2020-01-01'), 99999999, 9999999, 222),
     -- person_id=3 has some missing Lifestyle records. Backfill happens. --
     -- Backfilled skip records have the MAX date ('2022-01-01') as its date. --
-    (301, 3, 40766306, date('2020-01-01'), 45905771, 1585857),
-    (302, 3, 1585636, date('2021-01-01'), 45905771, 1585636),
-    (303, 3, 1586166, date('2022-01-01'), 45905771, 1586166)
+    (301, 3, 40766306, date('2020-01-01'), 45905771, 1585857, 333),
+    (302, 3, 1585636, date('2021-01-01'), 45905771, 1585636, 333),
+    (303, 3, 1586166, date('2022-01-01'), 45905771, 1586166, 333)
 """)
 
 PERSON_TMPL = JINJA_ENV.from_string("""
@@ -109,24 +109,24 @@ class BackfillLifestyleTest(BaseTest.CleaningRulesTestBase):
             'fields': [
                 'observation_id', 'person_id', 'observation_concept_id',
                 'observation_date', 'observation_type_concept_id',
-                'observation_source_concept_id'
+                'observation_source_concept_id', 'questionnaire_response_id'
             ],
             'cleaned_values': [
-                (101, 1, 40766306, self.date_2020, 45905771, 1585857),
-                (102, 1, 1585636, self.date_2020, 45905771, 1585636),
-                (103, 1, 1586166, self.date_2020, 45905771, 1586166),
-                (104, 1, 1586174, self.date_2020, 45905771, 1586174),
-                (105, 1, 1586182, self.date_2020, 45905771, 1586182),
-                (106, 1, 1586190, self.date_2020, 45905771, 1586190),
-                (107, 1, 40766357, self.date_2020, 45905771, 1586198),
-                (201, 2, 9999999, self.date_2020, 99999999, 9999999),
-                (301, 3, 40766306, self.date_2020, 45905771, 1585857),
-                (302, 3, 1585636, self.date_2021, 45905771, 1585636),
-                (303, 3, 1586166, self.date_2022, 45905771, 1586166),
-                (304, 3, 1586174, self.date_2022, 45905771, 1586174),
-                (305, 3, 1586182, self.date_2022, 45905771, 1586182),
-                (306, 3, 1586190, self.date_2022, 45905771, 1586190),
-                (307, 3, 40766357, self.date_2022, 45905771, 1586198)
+                (101, 1, 40766306, self.date_2020, 45905771, 1585857, 111),
+                (102, 1, 1585636, self.date_2020, 45905771, 1585636, 111),
+                (103, 1, 1586166, self.date_2020, 45905771, 1586166, 111),
+                (104, 1, 1586174, self.date_2020, 45905771, 1586174, 111),
+                (105, 1, 1586182, self.date_2020, 45905771, 1586182, 111),
+                (106, 1, 1586190, self.date_2020, 45905771, 1586190, 111),
+                (107, 1, 40766357, self.date_2020, 45905771, 1586198, 111),
+                (201, 2, 9999999, self.date_2020, 99999999, 9999999, 222),
+                (301, 3, 40766306, self.date_2020, 45905771, 1585857, 333),
+                (302, 3, 1585636, self.date_2021, 45905771, 1585636, 333),
+                (303, 3, 1586166, self.date_2022, 45905771, 1586166, 333),
+                (304, 3, 1586174, self.date_2022, 45905771, 1586174, None),
+                (305, 3, 1586182, self.date_2022, 45905771, 1586182, None),
+                (306, 3, 1586190, self.date_2022, 45905771, 1586190, None),
+                (307, 3, 40766357, self.date_2022, 45905771, 1586198, None)
             ]
         }]
 
