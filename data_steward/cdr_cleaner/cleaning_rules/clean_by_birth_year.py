c6db19e301182fac051545b691bca8b06acf4a4d
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2023-04-19 15:30:58 -0400

    [DC-3128] More clarity in the comment

--- data_steward/cdr_cleaner/cleaning_rules/clean_by_birth_year.py
+++ data_steward/cdr_cleaner/cleaning_rules/clean_by_birth_year.py
@@ -97,7 +97,8 @@ class CleanByBirthYear(BaseCleaningRule):
         NOTE This CR runs on both RDR and UNIONED_EHR. RDR does not have
         AOU_DEATH, and UNIONED_EHR does not have SURVEY_CONDUCT. That is why
         the default affected_tables is AOU_REQUIRED. This setup_rule() will
-        add any tables with person_id that are missing in AOU_REQUIRED.        
+        add any tables with person_id that are missing in AOU_REQUIRED to
+        affected_tables.
         """
         columns_query = LIST_PERSON_ID_TABLES.render(project_id=self.project_id,
                                                      dataset_id=self.dataset_id)
