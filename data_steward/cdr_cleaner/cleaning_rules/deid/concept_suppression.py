6bd6767949484e77f8d5967ec2c3076b21be1925
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2023-04-25 08:57:55 -0400

    [DC-3224] Correct logic

--- data_steward/cdr_cleaner/cleaning_rules/deid/concept_suppression.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/concept_suppression.py
@@ -155,6 +155,7 @@ class AbstractBqLookupTableConceptSuppression(AbstractConceptSuppression):
                  affected_datasets,
                  affected_tables,
                  concept_suppression_lookup_table,
+                 exclude_source_concept_id=False,
                  table_namer=None):
         """
         Initialize the class with proper info.
@@ -174,6 +175,7 @@ class AbstractBqLookupTableConceptSuppression(AbstractConceptSuppression):
                          table_namer=table_namer)
 
         self._concept_suppression_lookup_table = concept_suppression_lookup_table
+        self.exclude_source_concept_id = exclude_source_concept_id
 
     @property
     def concept_suppression_lookup_table(self):
@@ -204,6 +206,9 @@ class AbstractBqLookupTableConceptSuppression(AbstractConceptSuppression):
         suppression concept table 
 
         :param table_name: 
+        :param exclude_source_concept_id: True if `_source_concept_id` columns must
+            be excluded from the return value. ex) MissingConceptRecordSuppression
+            must exclude source concepts from suppression.
         :return: 
         """
         suppression_record_sandbox_query = self.BQ_LOOKUP_TABLE_SANDBOX_QUERY_TEMPLATE.render(
@@ -211,7 +216,8 @@ class AbstractBqLookupTableConceptSuppression(AbstractConceptSuppression):
             dataset=self.dataset_id,
             sandbox_dataset=self.sandbox_dataset_id,
             domain_table=table_name,
-            concept_fields=get_concept_id_fields(table_name),
+            concept_fields=get_concept_id_fields(
+                table_name, self.exclude_source_concept_id),
             suppression_concept=self.concept_suppression_lookup_table)
 
         return {
