c07fd1f5c854e3489dcf49d2df512f7e81f1dc07
lledezma <luis.ledezma@vumc.org>
2023-05-18 09:36:34 -0500

    [DC-3244] Update comment

--- data_steward/tools/import_rdr_dataset.py
+++ data_steward/tools/import_rdr_dataset.py
@@ -115,7 +115,7 @@ def create_rdr_tables(client, destination_dataset, rdr_source_dataset):
 
             fields_name_str = ',\n'.join(sc_list)
 
-            # copy contents from non-schemaed source to schemaed dest
+            # copy contents from source dataset to destination dataset
             sql = (f'SELECT {fields_name_str} ' f'FROM `{source_table_id}`')
 
             job_config = bigquery.job.QueryJobConfig(
