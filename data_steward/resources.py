d00b5372389cafe4c7375f19275d30eee6cd8fb4
lledezma <luis.ledezma@vumc.org>
2023-05-22 11:00:10 -0500

    [DC-3244] Cleanup

--- data_steward/resources.py
+++ data_steward/resources.py
@@ -371,8 +371,7 @@ def rdr_src_id_schemas():
     :return: result
     """
     result = dict()
-    for dir_path, dirs, files in os.walk(rdr_src_id_fields_path, topdown=True):
-        dirs[:] = [d for d in dirs]
+    for dir_path, _, files in os.walk(rdr_src_id_fields_path, topdown=True):
         for f in files:
             file_path = os.path.join(dir_path, f)
             with open(file_path, 'r', encoding='utf-8') as fp:
