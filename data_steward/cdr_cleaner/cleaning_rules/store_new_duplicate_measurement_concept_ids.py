e0322e9131ec1b5271862a16ac76f5d3d295543d
Krishna Kalluri <krishnasaikalluri@gmail.com>
2023-07-10 17:08:57 -0400

    [DC-2716] Add missing table name

--- data_steward/cdr_cleaner/cleaning_rules/store_new_duplicate_measurement_concept_ids.py
+++ data_steward/cdr_cleaner/cleaning_rules/store_new_duplicate_measurement_concept_ids.py
@@ -50,7 +50,7 @@ CREATE OR REPLACE TABLE
       value_as_concept_id,
       COUNT(DISTINCT measurement_concept_id) AS n_distinct_measurement_concept_ids
     FROM
-      `{{project_id}}.{{dataset_id}}`
+      `{{project_id}}.{{dataset_id}}.measurement`
     WHERE
       value_as_concept_id IS NOT NULL
       AND value_as_concept_id > 0
