996ac4622a53c057aa63d70d8441015004235813
lledezma <luis.ledezma@vumc.org>
2023-07-13 10:43:02 -0500

    [DC-3266] Update src_id values

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/person_id_validator_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/person_id_validator_test.py
@@ -78,14 +78,14 @@ class PersonIdValidationTest(BaseTest.CleaningRulesTestBase):
             INSERT INTO `{{project_id}}.{{dataset_id}}.observation_ext`
                 (observation_id, src_id, survey_version_concept_id)
             VALUES
-                (100, 'PT', null),
-                (101, 'HEALTH', null),
+                (100, 'Portal 1', null),
+                (101, 'Portal 2', null),
                 (102, 'site bar', null),
-                (201, 'HEALTH', null),
-                (202, 'TC', null),
+                (201, 'Portal 3', null),
+                (202, 'Portal 4', null),
                 (203, 'site baz', null),
                 (301, 'site raz', null),
-                (302, 'PT', null)
+                (302, 'Portal 5', null)
             """)
 
         observation_query = observation_tmpl.render(project_id=self.project_id,
