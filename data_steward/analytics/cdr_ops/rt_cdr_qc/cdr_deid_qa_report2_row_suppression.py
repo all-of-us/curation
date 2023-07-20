a0405cf740b67372463b0800c91ac8125b3dd4fd
brendagutman <brenda.gutman@vumc.org>
2023-07-20 13:13:37 -0500

    [DC-3339] Add the wear_consent suppression verification check.

--- data_steward/analytics/cdr_ops/rt_cdr_qc/cdr_deid_qa_report2_row_suppression.py
+++ data_steward/analytics/cdr_ops/rt_cdr_qc/cdr_deid_qa_report2_row_suppression.py
@@ -43,6 +43,26 @@ client = BigQueryClient(project_id, credentials=impersonation_creds)
 # df will have a summary in the end
 df = pd.DataFrame(columns = ['query', 'result']) 
 
+# wear_consent and wear_consent_ptsc question and module concepts that are not associated with an OMOP concept_id. 
+WEAR_SURVEY_CODES = ['havesmartphone',
+                      'wearwatch',
+                      'usetracker',
+                      'wear12months',
+                      'receivesms',
+                      'frequency',
+                      'agreetoshare',
+                      'onlyparticipantinhousehold',
+                      'haveaddress',
+                      'resultsconsent_helpmewithconsent',
+                      'helpmewithconsent_name',
+                      'resultsconsent_wear',
+                      'email_help_consent',
+                      'timeofday',
+                      'wearconsent_signature',
+                      'wearconsent_todaysdate',
+                      'wear_consent',
+                      'wear_consent_ptsc']
+
 # # 1 Verify all fields identified for suppression in the OBSERVATION table have been removed from the table in the deid dataset.
 
 query = JINJA_ENV.from_string("""
@@ -540,6 +560,44 @@ if df1.loc[0].sum()==0:
                 ignore_index = True) 
 else:
  df = df.append({'query' : 'Query13 observation_concept_id suppression in observation', 'result' : 'Failure'},  
+                ignore_index = True) 
+df1
+
+# # 14 Check that wear_consent records are suppressed in the 'observation' and 'survey_conduct' tables in the deid dataset.
+
+# Get counts of wear_consent records
+query = JINJA_ENV.from_string("""
+SELECT
+  'observation' as table,
+  COUNT(*) AS bad_rows
+FROM
+  `{{project_id}}.{{deid_cdr}}.observation` o
+  LEFT JOIN   `{{project_id}}.{{deid_cdr}}.survey_conduct` sc
+  ON sc.survey_conduct_id = o.questionnaire_response_id
+WHERE sc.survey_concept_id IN (2100000011,2100000012) -- captures questions asked in multiple surveys --
+OR LOWER(observation_source_value) IN UNNEST ({{wear_codes}}) -- captures those that might be missing from survey_conduct --
+GROUP BY 1
+
+UNION ALL
+
+SELECT
+  'survey_conduct' as table,
+  COUNT(*) AS bad_rows
+FROM
+  `{{project_id}}.{{deid_cdr}}.survey_conduct` sc
+WHERE sc.survey_concept_id IN (2100000011,2100000012) 
+GROUP BY 1
+
+""")
+q = query.render(project_id=project_id,
+            deid_cdr=deid_cdr,
+            wear_codes=WEAR_SURVEY_CODES)
+df1=execute(client, q) 
+if df1['bad_rows'].sum()==0:
+ df = df.append({'query' : 'Query14 wear_consent records are cleaned as expected.', 'result' : 'PASS'},  
+                ignore_index = True) 
+else:
+ df = df.append({'query' : 'Query13 wear_consent records have not been cleaned as expected.', 'result' : 'Failure'},  
                 ignore_index = True) 
 df1
 
