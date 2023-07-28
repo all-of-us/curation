1187e149b763ae8409ae7e6171e4a5a4e5630a1c
lledezma <luis.ledezma@vumc.org>
2023-07-28 10:40:51 -0500

    [DC-3337] Update setUpClass method

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/deid/fitbit_deid_src_id_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/deid/fitbit_deid_src_id_test.py
@@ -2,7 +2,12 @@
 Original Issues: DC-3337
 """
 
+# Python Imports
+import os
+
 # Project Imports
+from app_identity import PROJECT_ID
+from cdr_cleaner.cleaning_rules.deid.fitbit_deid_src_id import FitbitDeidSrcID
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
 
@@ -15,6 +20,35 @@ class FitbitDeidSrcIDTest(BaseTest.CleaningRulesTestBase):
         print('**************************************************************')
 
         super().initialize_class_vars()
+
+        # Set the test project identifier
+        cls.project_id = os.environ.get(PROJECT_ID)
+
+        # Set the expected test datasets
+        cls.dataset_id = os.environ.get('COMBINED_DATASET_ID')
+        cls.sandbox_id = f'{cls.dataset_id}_sandbox'
+
+        # Instantiate class
+        cls.rule_instance = FitbitDeidSrcID(
+            cls.project_id,
+            cls.dataset_id,
+            cls.sandbox_id,
+        )
+
+        # Generates list of fully qualified table names
+        affected_table_names = cls.rule_instance.affected_tables
+        for table_name in affected_table_names:
+            cls.fq_table_names.append(
+                f'{cls.project_id}.{cls.dataset_id}.{table_name}')
+
+        # Generate sandbox table names
+        sandbox_table_names = cls.rule_instance.get_sandbox_tablenames()
+        for table_name in sandbox_table_names:
+            cls.fq_sandbox_table_names.append(
+                f'{cls.project_id}.{cls.sandbox_id}.{table_name}')
+
+        # call super to set up the client, create datasets
+        cls.up_class = super().setUpClass()
 
     def setUp(self):
         """
