65158cacc4466e5a4b0f1b87bab367ab1b3bf653
brendagutman <77469967+brendagutman@users.noreply.github.com>
2023-08-07 14:24:07 -0500

    [DC-3339] Add wear consent suppression check to RT and CT nbs (#1674)
    * [DC-3339] Add check to RT and CT base and clean nbs
    
    * [DC-3339] Add check to RT and CT base and clean nbs
    
    * [DC-3339] rebase
    
    * [DC-3339] Fix merge conflicts
    
    * [DC-3339] Correct typo

--- data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier_part2.py
+++ data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier_part2.py
@@ -1330,6 +1330,43 @@ else:
 
 
 # +
+# Query 18:  Check that wear_consent records are suppressed in the 'observation' and 'survey_conduct' tables
+# -
+
+query = JINJA_ENV.from_string("""
+SELECT
+  'observation' as table,
+  COUNT(*) AS bad_rows
+FROM
+  `{{project_id}}.{{ct_dataset}}.observation` o
+  LEFT JOIN   `{{project_id}}.{{ct_dataset}}.survey_conduct` sc
+  ON sc.survey_conduct_id = o.questionnaire_response_id
+WHERE sc.survey_concept_id IN (2100000011,2100000012) -- captures questions asked in multiple surveys --
+OR LOWER(observation_source_value) IN UNNEST ({{wear_codes}}) -- captures those that might be missing from survey_conduct --
+GROUP BY 1
+UNION ALL
+SELECT
+  'survey_conduct' as table,
+  COUNT(*) AS bad_rows
+FROM
+  `{{project_id}}.{{ct_dataset}}.survey_conduct` sc
+WHERE sc.survey_concept_id IN (2100000011,2100000012)
+GROUP BY 1
+""")
+q = query.render(project_id=project_id,
+            ct_dataset=ct_dataset,
+            wear_codes=WEAR_SURVEY_CODES)
+df1=execute(client, q)
+if df1['bad_rows'].sum()==0:
+ df = df.append({'query' : 'Query18 wear_consent records are cleaned as expected.', 'result' : 'PASS'},
+                ignore_index = True)
+else:
+ df = df.append({'query' : 'Query18 wear_consent records have not been cleaned as expected.', 'result' : 'Failure'},
+                ignore_index = True)
+df1
+
+
+# +
 def highlight_cells(val):
     color = 'red' if 'Failure' in val else 'white'
     return f'background-color: {color}'
