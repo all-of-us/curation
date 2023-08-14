cd02f4397dab25d5b234d6c89be196e3890c0c80
lledezma <luis.ledezma@vumc.org>
2023-08-14 13:03:42 -0500

    [DC-3267] Update docstring

--- data_steward/cdr_cleaner/manual_cleaning_rules/dc732_suppress.py
+++ data_steward/cdr_cleaner/manual_cleaning_rules/dc732_suppress.py
@@ -236,7 +236,7 @@ def get_rows_to_suppress_df(tables_to_suppress_df, concept_lookup_table):
 
 def ppi_pm(g):
     """
-    True if any rows in the group have Portal in source
+    True if any rows in the group have source Participant Portal: PTSC, Participant Portal: TPC, or Staff Portal: HealthPro
 
     :param g: DataFrameGroupBy instance with a 'source' column
     """
