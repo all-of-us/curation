125234fd323fe6f1663c287d1ea2b98a159d2508
Michael Schmidt <michael.schmidt@vumc.org>
2023-08-18 12:07:15 -0500

    [DC-3333] resolve conflicts

--- data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier_part2.py
+++ data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier_part2.py
@@ -76,11 +76,7 @@ pd.options.display.max_rows = 120
 df = pd.DataFrame(columns=['query', 'result'])
 
 # wear_consent and wear_consent_ptsc question and module concepts where not in multiple surveys.
-<<<<<<< HEAD
 # The concepts found in multiple surveys are: 'resultsconsent_helpmewithconsent' and 'helpmewithconsent_name'
-=======
-# The concepts found in multiple surveys are: 'resultsconsent_helpmewithconsent' and 'helpmewithconsent_name' 
->>>>>>> e52c803a ([DC-3339] Add wear consent suppression check to RT and CT nbs (#1674))
 WEAR_SURVEY_CODES = ['havesmartphone',
                       'wearwatch',
                       'usetracker',
@@ -1353,19 +1349,19 @@ SELECT
   COUNT(*) AS bad_rows
 FROM
   `{{project_id}}.{{ct_dataset}}.survey_conduct` sc
-WHERE sc.survey_concept_id IN (2100000011,2100000012) 
+WHERE sc.survey_concept_id IN (2100000011,2100000012)
 GROUP BY 1
 """)
 q = query.render(project_id=project_id,
             ct_dataset=ct_dataset,
             wear_codes=WEAR_SURVEY_CODES)
-df1=execute(client, q) 
+df1=execute(client, q)
 if df1['bad_rows'].sum()==0:
- df = df.append({'query' : 'Query18 wear_consent records are cleaned as expected.', 'result' : 'PASS'},  
-                ignore_index = True) 
+ df = df.append({'query' : 'Query18 wear_consent records are cleaned as expected.', 'result' : 'PASS'},
+                ignore_index = True)
 else:
  df = df.append({'query' : 'Query18 wear_consent records have not been cleaned as expected.', 'result' : 'Failure'},
-                ignore_index = True) 
+                ignore_index = True)
 df1
 
 
