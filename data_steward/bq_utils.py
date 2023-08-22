6117a2155695c17f4c9255f3dc1c35faf3102170
lledezma <luis.ledezma@vumc.org>
2023-08-22 12:38:13 -0500

    [DC-3396] Update warning

--- data_steward/bq_utils.py
+++ data_steward/bq_utils.py
@@ -50,7 +50,7 @@ def get_output_project_id():
     return os.environ.get('OUTPUT_PROJECT_ID')
 
 
-@deprecated(reason='Use resources.BIGQUERY_DATASET_ID instead')
+@deprecated(reason='Use common.BIGQUERY_DATASET_ID instead')
 def get_dataset_id():
     return os.environ.get('BIGQUERY_DATASET_ID')
 
