87371d1884e724ec1728548f8ca44a785576d7ce
Michael Schmidt <michael.schmidt@vumc.org>
2023-08-23 15:48:58 -0500

    [DC-3333] fix naming convention

--- tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/ppi_branching_test.py
+++ tests/unit_tests/data_steward/cdr_cleaner/cleaning_rules/ppi_branching_test.py
@@ -55,9 +55,9 @@ def _get_create_or_replace_table_ddl(project,
             upper_field_type)
         if not standard_sql_type_code:
             raise ValueError(f'{field_type} is not a valid field type')
-        standard_sql_type = bigquery.StandardSqlDataTypes(
+        standard_sql_type = bigquery.StandardSqlDataType(
             standard_sql_type_code)
-        return standard_sql_type.name
+        return standard_sql_type.type_kind.name
 
     def _to_sql_field(field):
         return bigquery.SchemaField(field.name,
