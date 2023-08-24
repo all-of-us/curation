b330f802ba526e3cd1688b406d244c32578de362
brendagutman <77469967+brendagutman@users.noreply.github.com>
2023-08-24 12:28:56 -0500

    [DC-2364] Generate the wear_study table (#1693)
    * [DC-2364] Create CR and add wear_study to common
    
    * [DC-2364] Create the integration test
    
    * [DC-2364] Update test data
    
    * [DC-2364] Update the creation query
    
    * [DC-2364] Update formatting
    
    * [DC-2364] Add cleaning rule to cleaning classes
    
    * [DC-2364] Linting

---
+++ data_steward/cdr_cleaner/cleaning_rules/generate_wear_study_table.py
@@ -0,0 +1,158 @@
+"""
+Generate the wear_study table from records in observation.
+
+Original Issues: DC-2364
+"""
+
+# Python imports
+import logging
+
+# Project imports
+from cdr_cleaner.cleaning_rules.base_cleaning_rule import BaseCleaningRule
+from constants.cdr_cleaner import clean_cdr as cdr_consts
+from common import JINJA_ENV
+from utils import pipeline_logging
+
+LOGGER = logging.getLogger(__name__)
+
+JIRA_ISSUES = ['DC2364']
+
+CREATION_QUERY = JINJA_ENV.from_string("""
+CREATE OR REPLACE TABLE `{{project_id}}.{{dataset_id}}.wear_study` AS
+SELECT
+  DISTINCT person_id,
+  'Yes' AS resultsconsent_wear,
+  MIN(CASE WHEN value_source_concept_id = 2100000009 THEN observation_date END) AS wear_consent_start_date,
+  MIN(CASE WHEN value_source_concept_id = 2100000008 THEN observation_date END) AS wear_consent_end_date
+FROM (  
+  SELECT *
+  FROM `{{project_id}}.{{dataset_id}}.observation`
+  WHERE person_id IN ( -- the first recorded consent was positive --
+    SELECT DISTINCT person_id,
+    FROM `{{project_id}}.{{dataset_id}}.observation`
+    WHERE observation_source_concept_id = 2100000010
+    GROUP BY person_id
+    HAVING MIN(CASE WHEN value_source_concept_id = 2100000009 THEN observation_date END) = MIN(observation_date)
+    ))
+GROUP BY person_id
+""")
+
+
+class GenerateWearStudyTable(BaseCleaningRule):
+
+    def __init__(self,
+                 project_id,
+                 dataset_id,
+                 sandbox_dataset_id,
+                 table_namer=None):
+        """
+        Initialize the class with proper information.
+
+        Set the issue numbers, description and affected datasets. As other tickets may affect
+        this SQL, append them to the list of Jira Issues.
+        DO NOT REMOVE ORIGINAL JIRA ISSUE NUMBERS!
+        """
+        desc = "The purpose of this cleaning rule is to create the wear_study table from observation records. " \
+                "mapping lookup table."
+        super().__init__(issue_numbers=JIRA_ISSUES,
+                         description=desc,
+                         affected_datasets=[
+                             cdr_consts.CONTROLLED_TIER_DEID,
+                             cdr_consts.REGISTERED_TIER_DEID
+                         ],
+                         affected_tables=[],
+                         project_id=project_id,
+                         dataset_id=dataset_id,
+                         sandbox_dataset_id=sandbox_dataset_id,
+                         table_namer=table_namer)
+
+    def get_query_specs(self):
+        """
+        Interface to return a list of query dictionaries.
+
+        :returns:  a list of query dictionaries.  Each dictionary specifies
+            the query to execute and how to execute.  The dictionaries are
+            stored in list order and returned in list order to maintain
+            an ordering.
+        """
+        query_list = []
+
+        table_creation = {
+            cdr_consts.QUERY:
+                CREATION_QUERY.render(project_id=self.project_id,
+                                      dataset_id=self.dataset_id)
+        }
+        query_list.append(table_creation)
+
+        return query_list
+
+    def validate_rule(self, client):
+        """
+        Validates the cleaning rule which deletes or updates the data from the tables
+
+        Method to run validation on cleaning rules that will be updating the values.
+        For example:
+        if your class updates all the datetime fields you should be implementing the
+        validation that checks if the date time values that needs to be updated no
+        longer exists in the table.
+
+        if your class deletes a subset of rows in the tables you should be implementing
+        the validation that checks if the count of final final row counts + deleted rows
+        should equals to initial row counts of the affected tables.
+
+        Raises RunTimeError if the validation fails.
+        """
+
+        raise NotImplementedError("Please fix me.")
+
+    def setup_rule(self, client):
+        """
+        Load required resources prior to executing cleaning rule queries.
+
+        Method to run data upload options before executing the first cleaning
+        rule of a class.  For example, if your class requires loading a static
+        table, that load operation should be defined here.  It SHOULD NOT BE
+        defined as part of get_query_specs().
+        """
+        pass
+
+    def setup_validation(self, client):
+        """
+        Run required steps for validation setup
+
+        Method to run to setup validation on cleaning rules that will be updating or deleting the values.
+        For example:
+        if your class updates all the datetime fields you should be implementing the
+        logic to get the initial list of values which adhere to a condition we are looking for.
+
+        if your class deletes a subset of rows in the tables you should be implementing
+        the logic to get the row counts of the tables prior to applying cleaning rule
+
+        """
+        pass
+
+    def get_sandbox_tablenames(self):
+        pass
+
+
+if __name__ == '__main__':
+    import cdr_cleaner.args_parser as parser
+    import cdr_cleaner.clean_cdr_engine as clean_engine
+
+    ARGS = parser.parse_args()
+    pipeline_logging.configure(level=logging.DEBUG, add_console_handler=True)
+
+    if ARGS.list_queries:
+        clean_engine.add_console_logging()
+        query_list = clean_engine.get_query_list(ARGS.project_id,
+                                                 ARGS.dataset_id,
+                                                 ARGS.sandbox_dataset_id,
+                                                 [(GenerateWearStudyTable,)])
+
+        for query in query_list:
+            LOGGER.info(query)
+    else:
+        clean_engine.add_console_logging(ARGS.console_log)
+        clean_engine.clean_dataset(ARGS.project_id, ARGS.dataset_id,
+                                   ARGS.sandbox_dataset_id,
+                                   [(GenerateWearStudyTable,)])
---
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/generate_wear_study_table_test.py
@@ -0,0 +1,127 @@
+"""
+integration test for generate_wear_study_table
+"""
+
+# Python imports
+import os
+
+# Third party imports
+from dateutil import parser
+
+# Project imports
+from common import WEAR_STUDY, OBSERVATION
+from app_identity import PROJECT_ID
+from cdr_cleaner.cleaning_rules.generate_wear_study_table import GenerateWearStudyTable
+from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
+
+
+class GenerateWearStudyTableTest(BaseTest.CleaningRulesTestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+        super().initialize_class_vars()
+
+        # set the test project identifier
+        project_id = os.environ.get(PROJECT_ID)
+        cls.project_id = project_id
+
+        # set the expected test datasets
+        dataset_id = os.environ.get('COMBINED_DATASET_ID')
+        cls.dataset_id = dataset_id
+        sandbox_id = dataset_id + '_sandbox'
+        cls.sandbox_id = sandbox_id
+
+        cls.rule_instance = GenerateWearStudyTable(cls.project_id,
+                                                   cls.dataset_id,
+                                                   cls.sandbox_id)
+
+        cls.affected_tables = [OBSERVATION, WEAR_STUDY]
+
+        # Generates list of fully qualified table names and their corresponding sandbox table names
+        cls.fq_table_names = [
+            f"{cls.project_id}.{cls.dataset_id}.{table}"
+            for table in cls.affected_tables
+        ]
+
+        cls.fq_sandbox_table_names = []
+
+        # call super to set up the client, create datasets
+        cls.up_class = super().setUpClass()
+
+    def setUp(self):
+        # create tables
+        super().setUp()
+
+        self.date_one = parser.parse('2021-01-01').date()
+        self.date_two = parser.parse('2022-01-01').date()
+        self.date_three = parser.parse('2023-01-01').date()
+
+        observation_query = self.jinja_env.from_string("""
+              INSERT INTO `{{project_id}}.{{dataset_id}}.observation` (
+                  observation_id,
+                  person_id,
+                  observation_concept_id,
+                  observation_source_concept_id,
+                  value_source_concept_id,
+                  observation_date,
+                  observation_type_concept_id
+                  )
+                VALUES
+                  -- one yes record --
+                  (1,1,1,2100000010,2100000009,date('2021-01-01'),1),
+                  -- two yes records --
+                  (1,2,1,2100000010,2100000009,date('2021-01-01'),1),
+                  (1,2,1,2100000010,2100000009,date('2022-01-01'),1),
+                  -- one no record --
+                  (1,3,1,2100000010,2100000008,date('2021-01-01'),1),
+                  -- two no records then a yes --
+                  (1,4,1,2100000010,2100000008,date('2021-01-01'),1),
+                  (1,4,1,2100000010,2100000008,date('2022-01-01'),1),
+                  (1,4,1,2100000010,2100000009,date('2023-01-01'),1),
+                  -- two yes records then a no --
+                  (1,5,1,2100000010,2100000009,date('2021-01-01'),1),
+                  (1,5,1,2100000010,2100000009,date('2022-01-01'),1),
+                  (1,5,1,2100000010,2100000008,date('2023-01-01'),1),
+                  -- yes then no then yes --
+                  (1,6,1,2100000010,2100000009,date('2021-01-01'),1),
+                  (1,6,1,2100000010,2100000008,date('2022-01-01'),1),
+                  (1,6,1,2100000010,2100000009,date('2023-01-01'),1)
+
+            """).render(project_id=self.project_id, dataset_id=self.dataset_id)
+
+        # load the test data
+        self.load_test_data([observation_query])
+
+    def test_queries(self):
+        """
+        Validates pre-conditions, test execution and post conditions based on the tables_and_counts variable.
+
+        """
+        tables_and_counts = [{
+            'name':
+                WEAR_STUDY,
+            'fq_table_name':
+                self.fq_table_names[1],
+            'fields': [
+                'person_id', 'resultsconsent_wear', 'wear_consent_start_date',
+                'wear_consent_end_date'
+            ],
+            'loaded_ids': [],
+            'sandboxed_ids': [],
+            'cleaned_values': [
+                # one yes record.
+                (1, 'Yes', self.date_one, None),
+                # two yes records. earliest recorded
+                (2, 'Yes', self.date_one, None),
+                # two yes records and one no. earliest dates recorded
+                (5, 'Yes', self.date_one, self.date_three),
+                # yes, then no, then yes. earliest dates recorded
+                (6, 'Yes', self.date_one, self.date_two)
+            ]
+        }]
+
+        self.default_test(tables_and_counts)
