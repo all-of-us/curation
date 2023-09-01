0a75978e50dbd1746d82439a06170a5583af16f4
brendagutman <77469967+brendagutman@users.noreply.github.com>
2023-09-01 15:22:06 -0500

    [DC-3400] Update rdr export qc notebooks 'no concept_id' checks (#1710)
    * [DC-3400] Rebase
    
    * [DC-3400] Update a check description
    
    * [DC-3400] Enable the display
    
    * [DC-3400] Remove test code
    
    * [DC-3400] Remove changes from unrelated commits

--- data_steward/analytics/cdr_ops/raw_rdr_export_qc.py
+++ data_steward/analytics/cdr_ops/raw_rdr_export_qc.py
@@ -30,6 +30,7 @@ from utils import auth
 from gcloud.bq import BigQueryClient
 from analytics.cdr_ops.notebook_utils import execute, IMPERSONATION_SCOPES, render_message
 from resources import old_map_short_codes_path
+from IPython.display import display, HTML
 
 impersonation_creds = auth.get_impersonation_credentials(
     run_as, target_scopes=IMPERSONATION_SCOPES)
@@ -160,20 +161,24 @@ WHERE prev_code.value IS NULL OR curr_code.value IS NULL
 query = tpl.render(new_rdr=new_rdr, old_rdr=old_rdr, project_id=project_id)
 execute(client, query)
 
-# # Question codes should have mapped `concept_id`s
+# # Questions lacking concept_ids to be dropped
 # Question codes in `observation_source_value` should be associated with the concept identified by
 # `observation_source_concept_id` and mapped to a standard concept identified by `observation_concept_id`.
-# The table below lists codes having rows where either field is null or zero and the number of rows where this occurs.
+# The table below lists codes having rows where both fields are null or zero and the number of rows where this occurs.
 # This may be associated with an issue in the PPI vocabulary or in the RDR ETL process.
 #
-# Snap codes are not modeled in the vocabulary but may be used in the RDR export.
+# **If the check is failing.** The concepts in the results dataframe will need to be reviewed manually. These concepts will have thier rows deleted in later pipeline stages. Confirm the expectation for these concepts. The results of this check should not be refered to the rdr team.
+#
+# More information on the codes that are expected not to map:
+# * Snap codes are not modeled in the vocabulary but may be used in the RDR export.
 # They are excluded here by filtering out snap codes in the Public PPI Codebook
 # which were loaded into `curation_sandbox.snap_codes`.
 #
-# Long codes are not OMOP concepts and are not expected to have concept_ids. This issue is accounted for in the RDR cleaning class `SetConceptIdsForSurveyQuestionsAnswers`.
+# * Long codes are not OMOP concepts and are not expected to have concept_ids. This issue is accounted for in the RDR cleaning class `SetConceptIdsForSurveyQuestionsAnswers`.
 #
-# Wear codes. Most concept codes in the wear survey modules do not have an OMOP concept_id. This is expected as these records will not be included in the CDR.
+# * Wear codes. Most concept codes in the wear survey modules do not have an OMOP concept_id. This is expected as these records will not be included in the CDR.
 
+# +
 tpl = JINJA_ENV.from_string("""
 SELECT
   observation_source_value
@@ -183,48 +188,138 @@ SELECT
  ,COUNTIF(observation_concept_id=0)              AS concept_id_zero
 FROM `{{project_id}}.{{new_rdr}}.observation`
 WHERE observation_source_value IS NOT NULL
-AND observation_source_value != ''
-AND observation_source_value NOT IN (SELECT concept_code FROM `{{project_id}}.curation_sandbox.snap_codes`)
-AND LOWER(observation_source_value) NOT IN UNNEST ({{wear_codes}})
-AND LOWER(observation_source_value) NOT IN UNNEST ({{long_codes}})
+  AND observation_source_value != ''
+  AND observation_source_value NOT IN (SELECT concept_code FROM `{{project_id}}.curation_sandbox.snap_codes`)
+  AND LOWER(observation_source_value) NOT IN UNNEST ({{wear_codes}})
+  AND LOWER(observation_source_value) NOT IN UNNEST ({{long_codes}})
+  AND NOT REGEXP_CONTAINS(observation_source_value,'(?i)consent|sitepairing|pii|michigan|feedback|nonotsure|schedul|address|screen2|get_help|stopoptions|sensitivetype2|withdrawal|etm|other_|stateofcare|organization|reviewagain|results_decision|saliva_whole_blood_transfusion')
 GROUP BY 1
-HAVING source_concept_id_null + source_concept_id_zero + concept_id_null + concept_id_zero > 0
+HAVING source_concept_id_null + source_concept_id_zero  > 0
+  AND  concept_id_null + concept_id_zero > 0
+  AND MAX(observation_date) > '2019-01-01'
 ORDER BY 2 DESC, 3 DESC, 4 DESC, 5 DESC
 """)
-query = tpl.render(new_rdr=new_rdr,
-                   project_id=project_id,
-                   long_codes=LONG_CODES,
-                   wear_codes=WEAR_SURVEY_CODES)
-execute(client, query)
+query = tpl.render(new_rdr=new_rdr, project_id=project_id,long_codes=LONG_CODES, wear_codes=WEAR_SURVEY_CODES)
+df = execute(client, query)
 
-# # Answer codes should have mapped `concept_id`s
-# Answer codes in value_source_value should be associated with the concept identified by value_source_concept_id
-# and mapped to a standard concept identified by value_as_concept_id. The table below lists codes having rows
-# where either field is null or zero and the number of rows where this occurs.
-# This may be associated with an issue in the PPI vocabulary or in the RDR ETL process.
+success_msg = 'Questions expected to have concept_ids have concept_ids'
+failure_msg = 'These question codes did not map to concept_ids. See description.'
+
+render_message(df,
+               success_msg,
+               failure_msg)
+# -
+
+# # Check the ETL mapped `concept_id`s to question codes
+# If most concepts are mapped, this check passes. If only some concepts are not mapping properly these are most likely known vocabulary issues.
 #
-# Note: Snap codes are not modeled in the vocabulary but may be used in the RDR export.
-# They are excluded here by filtering out snap codes in the Public PPI Codebook
-# which were loaded into `curation_sandbox.snap_codes`.
+# **If the check fails.** Investigate. If none, or only a few, of the codes are being mapped notify rdr.
 #
 
+# +
 tpl = JINJA_ENV.from_string("""
+WITH cte AS (
 SELECT
-  value_source_value
- ,COUNTIF(value_source_concept_id IS NULL) AS source_concept_id_null
- ,COUNTIF(value_source_concept_id=0)       AS source_concept_id_zero
- ,COUNTIF(value_as_concept_id IS NULL)     AS concept_id_null
- ,COUNTIF(value_as_concept_id=0)           AS concept_id_zero
-FROM `{{project_id}}.{{new_rdr}}.observation`
-WHERE value_source_value IS NOT NULL
-AND value_source_value != ''
-AND value_source_value NOT IN (SELECT concept_code FROM `{{project_id}}.curation_sandbox.snap_codes`)
+  'questions' as field
+  ,observation_source_value as code
+  ,COUNTIF(observation_source_concept_id IS NULL OR observation_source_concept_id=0 OR observation_concept_id IS NULL OR observation_concept_id=0) AS n_not_mapped_by_etl 
+  ,COUNTIF(observation_source_concept_id IS NOT NULL AND observation_source_concept_id != 0 AND observation_concept_id IS NOT NULL AND observation_concept_id != 0) AS n_mapped_by_etl 
+  FROM `{{project_id}}.{{new_rdr}}.observation`
+LEFT JOIN (SELECT concept_id_1 FROM `{{project_id}}.{{new_rdr}}.concept_relationship` WHERE relationship_id = 'Maps to') cr1
+ON observation_source_concept_id = cr1.concept_id_1
+WHERE cr1.concept_id_1 IS NOT NULL 
+GROUP BY 2
+
+)
+SELECT 
+  field,
+  COUNTIF(n_not_mapped_by_etl != 0) AS count_not_mapped,
+  COUNTIF(n_mapped_by_etl != 0) AS count_mapped
+FROM cte
 GROUP BY 1
-HAVING source_concept_id_null + source_concept_id_zero + concept_id_null + concept_id_zero > 0
-ORDER BY 2 DESC, 3 DESC, 4 DESC, 5 DESC
+
+
 """)
-query = tpl.render(new_rdr=new_rdr, project_id=project_id)
-execute(client, query)
+query = tpl.render(new_rdr=new_rdr, project_id=project_id, long_codes=LONG_CODES)
+df = execute(client, query)
+
+if sum(df['count_not_mapped']) > .33 * sum(df['count_mapped']):
+    display(df,
+        HTML(f'''
+                <h3>
+                    Check Status: <span style="color:red">FAILURE</span>
+                </h3>
+                <p>
+                    Concept_ids are not being mapped. See description.
+                </p>
+            '''))
+else:
+    display(df,
+        HTML(f'''
+                <h3>
+                    Check Status: <span style="color:green">PASS</span>
+                </h3>
+                <p>
+                    Concept_ids have been mapped.
+                </p>
+                
+            '''))
+# -
+
+# # Check the ETL mapped `concept_id`s to answer codes
+# If most concepts are mapped, this check passes. If only some concepts are not mapping properly these are most likely known vocabulary issues.
+#
+# **If the check fails.** Investigate. If none, or only a few, of the codes are being mapped notify rdr.
+
+# +
+tpl = JINJA_ENV.from_string("""
+WITH cte AS (
+SELECT
+  'answers' as field
+  ,value_source_value as code
+  ,COUNTIF(value_source_value IS NOT NULL AND value_source_value != '' AND value_source_concept_id IS NULL OR value_source_concept_id=0 OR value_as_concept_id IS NULL OR value_as_concept_id=0) AS n_not_mapped_by_etl 
+  ,COUNTIF(value_source_value IS NOT NULL AND value_source_value != '' AND value_source_concept_id IS NOT NULL AND value_source_concept_id != 0 AND value_as_concept_id IS NOT NULL AND value_as_concept_id != 0) AS n_mapped_by_etl 
+FROM `{{project_id}}.{{new_rdr}}.observation`
+LEFT JOIN (SELECT distinct concept_id_1 FROM `{{project_id}}.{{new_rdr}}.concept_relationship` WHERE relationship_id IN ('Maps to','Mapped from','Maps to value')) cr2
+ON value_source_concept_id = cr2.concept_id_1
+WHERE cr2.concept_id_1 IS NOT NULL 
+GROUP BY 2
+)
+SELECT distinct field,
+countif(n_not_mapped_by_etl != 0) as count_not_mapped,
+countif(n_mapped_by_etl != 0) as count_mapped
+FROM cte
+GROUP BY 1
+
+
+""")
+query = tpl.render(new_rdr=new_rdr, project_id=project_id, long_codes=LONG_CODES)
+df = execute(client, query)
+
+if sum(df['count_not_mapped']) > .33 * sum(df['count_mapped']):
+    display(df,
+        HTML(f'''
+                <h3>
+                    Check Status: <span style="color:red">FAILURE</span>
+                </h3>
+                <p>
+                    Concept_ids are not being mapped. See description.
+                </p>
+            '''))
+else:
+    display(df,
+        HTML(f'''
+                <h3>
+                    Check Status: <span style="color:green">PASS</span>
+                </h3>
+                <p>
+                    Concept_ids have been mapped.
+                </p>
+                
+            '''))
+
+
+# -
 
 # # Dates are equal in observation_date and observation_datetime
 # Any mismatches are listed below.
