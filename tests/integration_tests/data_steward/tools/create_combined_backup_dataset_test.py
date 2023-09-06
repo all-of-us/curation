d20a620b92faed77119032d8cd953ccbecccba38
lledezma <luis.ledezma@vumc.org>
2023-09-06 10:46:38 -0500

    [DC-3431] Refactoring

--- tests/integration_tests/data_steward/tools/create_combined_backup_dataset_test.py
+++ tests/integration_tests/data_steward/tools/create_combined_backup_dataset_test.py
@@ -317,7 +317,7 @@ class CreateCombinedBackupDatasetAllDeathTest(BaseTest.BigQueryTestBase):
         cls.project_id = os.environ.get(PROJECT_ID)
         cls.dataset_id = os.environ.get('COMBINED_DATASET_ID')
         cls.sandbox_id = BIGQUERY_DATASET_ID
-        cls.rdr_id = os.environ.get('RDR_DATASET_ID')
+        cls.rdr_id = RDR_DATASET_ID
         cls.unioned_id = os.environ.get('UNIONED_DATASET_ID')
 
         cls.fq_table_names = [
