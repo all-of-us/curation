a55d68e01f018075ee03c49efd485d89ce1aba33
lledezma <luis.ledezma@vumc.org>
2023-09-18 14:33:50 -0500

    [DC-3442] Add docstring

--- data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list.py
+++ data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list.py
@@ -3,6 +3,9 @@ from cdr_cleaner.cleaning_rules.sandbox_and_remove_pids import SandboxAndRemoveP
 
 
 class SandboxAndRemovePidsList(SandboxAndRemovePids):
+    """
+    Removes all participant data using a list of participants.
+    """
 
     def __init__(self):
         """
