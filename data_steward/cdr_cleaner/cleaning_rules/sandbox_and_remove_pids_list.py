1814991c3af7bf3dfc15b853f7c24aa9bad24dcf
lledezma <luis.ledezma@vumc.org>
2023-09-18 14:30:38 -0500

    [DC-3442] Add SandboxAndRemovePidsList CR

---
+++ data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list.py
@@ -0,0 +1,7 @@
+class SandboxAndRemovePidsList(SandboxAndRemovePids):
+
+    def __init__(self):
+        """
+        Initialize the class with proper information.
+        """
+        super().__init__()
