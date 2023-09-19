fbac4ec5a63aed9faa38637b863cde68693ae6f4
hiro-mishima <90213198+hiro-mishima@users.noreply.github.com>
2023-09-19 17:59:20 -0400

    [DC-3457] Update comment

--- data_steward/cdr_cleaner/cleaning_rules/convert_pre_post_coordinated_concepts.py
+++ data_steward/cdr_cleaner/cleaning_rules/convert_pre_post_coordinated_concepts.py
@@ -143,8 +143,8 @@ INSERT_MAPPING_QUERY = JINJA_ENV.from_string("""
 INSERT INTO `{{project}}.{{dataset}}._mapping_observation`
 (observation_id, src_id)
 SELECT 
-    -- ROW_NUMBER() here can be 1 - 4. So, the newly generated IDs will be --
-    -- in the range of 100,000,000,000 - 499,999,999,999. --
+    -- New observation_ids are assigned in the same way as INSERT_QUERY --
+    -- so IDs align between observation and its mapping table --
     ROW_NUMBER() OVER(
         PARTITION BY o.observation_id
         ORDER BY
