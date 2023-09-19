59292cfae49c9c7dfb3cd9ca3277334d01ca1daf
lledezma <luis.ledezma@vumc.org>
2023-09-19 15:36:08 -0500

    [DC-3442] Add setUpClass class method

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list_test.py
@@ -1,4 +1,13 @@
 # Project Imports
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
+
 class SandboxAndRemovePidsListTest(BaseTest.CleaningRulesTestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('**************************************************************')
+        print(cls.__name__)
+        print('**************************************************************')
+
+        super().initialize_class_vars()
