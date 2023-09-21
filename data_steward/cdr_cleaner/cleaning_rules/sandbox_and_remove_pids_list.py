f816981c7fad56a5cd704379e030eb8e885df36c
lledezma <luis.ledezma@vumc.org>
2023-09-21 11:29:47 -0500

    [DC-3442] Import AOU_DEATH and CDM_TABLES

--- data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list.py
+++ data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list.py
@@ -2,7 +2,7 @@
 import logging
 
 # Project imports
-from cdr_cleaner.cleaning_rules.sandbox_and_remove_pids import SandboxAndRemovePids, PERSON_TABLE_QUERY
+from cdr_cleaner.cleaning_rules.sandbox_and_remove_pids import SandboxAndRemovePids, PERSON_TABLE_QUERY, AOU_DEATH, CDM_TABLES
 from constants.cdr_cleaner import clean_cdr as cdr_consts
 from gcloud.bq import BigQueryClient
 
