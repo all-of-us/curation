0c273cd5c624d7b576b77674d2019c77e310cc61
lledezma <luis.ledezma@vumc.org>
2023-09-22 09:45:23 -0500

    [DC-3442] Import RDR_DATASET_ID

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list_test.py
@@ -9,7 +9,7 @@ from google.cloud.bigquery import Table
 
 # Project Imports
 from app_identity import PROJECT_ID
-from common import JINJA_ENV, COMBINED_DATASET_ID
+from common import JINJA_ENV, COMBINED_DATASET_ID, RDR_DATASET_ID
 from cdr_cleaner.cleaning_rules.sandbox_and_remove_pids_list import SandboxAndRemovePidsList
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
@@ -167,6 +167,7 @@ class SandboxAndRemovePidsListTest(BaseTest.CleaningRulesTestBase):
 
         # Set the expected test datasets
         cls.dataset_id = COMBINED_DATASET_ID
+        cls.rdr_dataset_id = RDR_DATASET_ID
         cls.sandbox_id = f'{cls.dataset_id}_sandbox'
 
         # Instantiate class
@@ -174,7 +175,7 @@ class SandboxAndRemovePidsListTest(BaseTest.CleaningRulesTestBase):
             project_id=cls.project_id,
             dataset_id=cls.dataset_id,
             sandbox_dataset_id=cls.sandbox_id,
-            rdr_dataset_id='rdr_dataset_id',
+            rdr_dataset_id=RDR_DATASET_ID,
             lookup_table='lookup_table')
 
         # Generates list of fully qualified table names
