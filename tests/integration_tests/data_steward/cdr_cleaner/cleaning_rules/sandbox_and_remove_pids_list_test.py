be42189375d209a2a27e52523f77b4605fb1ee59
lledezma <luis.ledezma@vumc.org>
2023-09-25 10:44:06 -0500

    [DC-3442] Import OBSERVATION, MEASUREMENT, AOU_DEATH consts

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/sandbox_and_remove_pids_list_test.py
@@ -9,8 +9,8 @@ from google.cloud.bigquery import Table
 
 # Project Imports
 from app_identity import PROJECT_ID
-from common import JINJA_ENV, COMBINED_DATASET_ID, RDR_DATASET_ID
-from cdr_cleaner.cleaning_rules.sandbox_and_remove_pids_list import SandboxAndRemovePidsList
+from common import JINJA_ENV, COMBINED_DATASET_ID, RDR_DATASET_ID, OBSERVATION, MEASUREMENT
+from cdr_cleaner.cleaning_rules.sandbox_and_remove_pids_list import SandboxAndRemovePidsList, AOU_DEATH
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
 OBSERVATION_TABLE_TEMPLATE = JINJA_ENV.from_string("""
