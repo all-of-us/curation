49a99e4f13cb26cdb15f8c3c0e3dbcb7c25bac8a
Michael Schmidt <michael.schmidt@vumc.org>
2023-10-02 09:13:56 -0500

    [DC-3434] revert to pass to raise

--- data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_without_consent.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_ehr_data_without_consent.py
@@ -224,13 +224,13 @@ class RemoveEhrDataWithoutConsent(BaseCleaningRule):
         """
         Run required steps for validation setup
         """
-        pass
+        raise NotImplementedError("Please fix me.")
 
     def validate_rule(self, client):
         """
         Validates the cleaning rule which deletes or updates the data from the tables
         """
-        pass
+        raise NotImplementedError("Please fix me.")
 
     def get_sandbox_tablenames(self):
         """
