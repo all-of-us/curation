0dc146bfae76a419dc994d044a98b57c077f18a9
Krishna Kalluri <krishnasaikalluri@gmail.com>
2023-11-09 15:24:28 -0500

    [DC-3563] refactor dataset_id to old_dataset to avoid confusion

--- data_steward/retraction/yob_retraction.py
+++ data_steward/retraction/yob_retraction.py
@@ -17,7 +17,7 @@
 # Parameters
 run_as = ""
 project_id = ""
-dataset_id = ""
+old_dataset_id = ""
 release_tag = ""
 data_stage = ""
 
@@ -47,8 +47,8 @@ query = JINJA_ENV.from_string("""
 WITH rows_having_brith_date as (
 
 SELECT observation_id
-  FROM `{{project_id}}.{{dataset_id}}.observation` ob
-JOIN  `{{project_id}}.{{dataset_id}}.person` p USING (person_id)
+  FROM `{{project_id}}.{{old_dataset_id}}.observation` ob
+JOIN  `{{project_id}}.{{old_dataset_id}}.person` p USING (person_id)
  WHERE (observation_concept_id NOT IN (4013886, 4135376, 4271761) OR observation_concept_id IS NULL)
   AND ABS(EXTRACT(YEAR FROM observation_date)- p.year_of_birth) < 2
   )
@@ -62,10 +62,10 @@ CASE WHEN
   THEN 1 ELSE 0
 END
  AS Failure_no_birth_date
-FROM `{{project_id}}.{{dataset_id}}.observation` ob
+FROM `{{project_id}}.{{old_dataset_id}}.observation` ob
 WHERE  observation_id IN (SELECT observation_id FROM rows_having_brith_date)
 """)
-q = query.render(project_id=project_id, dataset_id=dataset_id)
+q = query.render(project_id=project_id, old_dataset_id=old_dataset_id)
 execute(client, q)
 
 # ## Creating new datasets to apply retraction on
@@ -74,9 +74,9 @@ dataset_definition = {
     'clean': {
         'name': f'C{release_tag}_{data_stage}',
         'desc':
-            f"Hot fix applied to {dataset_id}."
+            f"Hot fix applied to {old_dataset_id}."
             f"--"
-            f"{dataset_id}'s description for reference -> Certain records removed from {dataset_id} based on the retraction rule",
+            f"{old_dataset_id}'s description for reference -> Certain records removed from {old_dataset_id} based on the retraction rule",
         'labels': {
             "owner": "curation",
             "phase": "clean",
@@ -110,7 +110,7 @@ for dataset_type in ['clean', 'sandbox']:
 
 # ## Copy tables from existing dataset to hotfix dataset
 
-client.copy_dataset(f'{client.project}.{dataset_id}',
+client.copy_dataset(f'{client.project}.{old_dataset_id}',
                     f"{client.project}.{dataset_definition['clean']['name']}")
 
 # ## Applying cleaning rule to retract records from the dataset
@@ -127,5 +127,5 @@ clean_cdr.main(args=all_cleaning_args)
 # ## Running validation check on new dataset
 
 q = query.render(project_id=project_id,
-                 dataset_id=dataset_definition['clean']['name'])
+                 old_dataset_id=dataset_definition['clean']['name'])
 execute(client, q)
