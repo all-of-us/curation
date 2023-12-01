722dbbc6168038a9f247e426083ca91861df0282
Hiro Mishima <90213198+hiro-mishima@users.noreply.github.com>
2023-12-01 09:35:58 -0500

    [DC-3561] Add `EnsureDateDatetimeConsistency` after `TemporalConsistency` (#1811)
    * [DC-3561] Add todo
    
    * [DC-3561] Update CR
    
    * [DC-3561] Update depends_on and comment

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -242,6 +242,7 @@ COMBINED_CLEANING_CLASSES = [
     (DrugRefillsDaysSupply,),
     (PopulateRouteIds,),
     (TemporalConsistency,),
+    (EnsureDateDatetimeConsistency,),
     (drop_duplicate_states.get_drop_duplicate_states_queries,),
     # TODO : Make null_invalid_foreign_keys able to run on de_identified dataset
     (
