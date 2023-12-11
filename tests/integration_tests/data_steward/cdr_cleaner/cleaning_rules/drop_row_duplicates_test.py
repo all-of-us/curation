2e08517853e172b9dcc041f73ec89f888664e64c
brendagutman <brenda.gutman@vumc.org>
2023-12-11 09:54:44 -0600

    [DC-3630] Update CR imports

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/drop_row_duplicates_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/drop_row_duplicates_test.py
@@ -13,9 +13,6 @@ from app_identity import PROJECT_ID
 from cdr_cleaner.cleaning_rules.drop_row_duplicates import DropRowDuplicates
 from tests.integration_tests.data_steward.cdr_cleaner.cleaning_rules.bigquery_tests_base import BaseTest
 
-# Third party imports
-
-
 class DropRowDuplicatesTest(BaseTest.CleaningRulesTestBase):
 
     @classmethod
