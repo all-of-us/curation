fe957dad5d96b5b62f9f276ce60748b4149d5d65
Mark Velez <mav7014@nyp.org>
2024-01-30 13:16:16 -0500

    Remove CT rules unrelated to NPH  * QRIDtoRID  * TruncateEraTables  * NullPersonBirthdate  * TableSuppression  * ControlledCopeSurveySuppression  * DropOrphanedSurveyConductIds  * DropOrphanedPIDS  * GenerateWearStudyTable  * DropViaSurveyConduct  * RemoveExtraTables  * CalculatePrimaryDeathRecord  * CleanMappingExtTables

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -355,11 +355,7 @@ REGISTERED_TIER_FITBIT_CLEANING_CLASSES = [
 ]
 
 CONTROLLED_TIER_DEID_CLEANING_CLASSES = [
-    (RtCtPIDtoRID,),
-    (QRIDtoRID,),  # Should run before any row suppression rules
-    (TruncateEraTables,),
-    (NullPersonBirthdate,),
-    (TableSuppression,),
+    # (RtCtPIDtoRID,),
     (ControlledTierReplacedConceptSuppression,),
     (GeneralizeZipCodes,),  # Should run after any data remapping rules
     # (RaceEthnicityRecordSuppression,),  # Should run after any data remapping rules
@@ -370,7 +366,6 @@ CONTROLLED_TIER_DEID_CLEANING_CLASSES = [
     (GeoLocationConceptSuppression,),
     (BirthInformationSuppression,),
     (YearOfBirthRecordsSuppression,),
-    (ControlledCopeSurveySuppression,),
     (IDFieldSuppression,),  # Should run after any data remapping
     (CancerConceptSuppression,),  # Should run after any data remapping rules
     (SectionParticipationConceptSuppression,),
@@ -381,15 +376,7 @@ CONTROLLED_TIER_DEID_CLEANING_CLASSES = [
         StringFieldsSuppression,),
     (AggregateZipCodes,),
     (DeidentifyAIANZip3Values,),
-    (FreeTextSurveyResponseSuppression,),
-    (DropOrphanedSurveyConductIds,),
-    (DropOrphanedPIDS,),
-    (GenerateWearStudyTable,),
-    (DropViaSurveyConduct,),  # should run after wear study table creation
-    (RemoveExtraTables,),  # Should be last cleaning rule to be run
-    (CalculatePrimaryDeathRecord,),
-    (CleanMappingExtTables,),  # should be one of the last cleaning rules run
-]
+    (FreeTextSurveyResponseSuppression,)]
 
 CONTROLLED_TIER_DEID_BASE_CLEANING_CLASSES = [
     (FillSourceValueTextFields,),
