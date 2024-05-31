02cfdf4f56c7049e4d3446c21a2cc315abd01c80
Mark Velez <mav7014@nyp.org>
2024-05-31 11:16:34 -0400

    Removing CTAdditionalPrivacyConceptSuppression and CTObservationPrivacySuppression
    CTAdditionalPrivacyConceptSuppression does not apply to observation table and are not relevant to NPH
    CTObservationPrivacySuppression is based on non PPI/NPH concepts

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -19,8 +19,6 @@ from cdr_cleaner.cleaning_rules.clean_by_birth_year import CleanByBirthYear
 from cdr_cleaner.cleaning_rules.convert_pre_post_coordinated_concepts import ConvertPrePostCoordinatedConcepts
 from cdr_cleaner.cleaning_rules.create_aian_lookup import CreateAIANLookup
 from cdr_cleaner.cleaning_rules.create_expected_ct_list import StoreExpectedCTList
-from cdr_cleaner.cleaning_rules.deid.ct_additional_privacy_suppression import CTAdditionalPrivacyConceptSuppression
-from cdr_cleaner.cleaning_rules.deid.ct_observation_privacy_suppression import CTObservationPrivacySuppression
 from cdr_cleaner.cleaning_rules.deid.ct_nph_observation_privacy_suppression import CTNPHObservationPrivacySuppression
 from cdr_cleaner.cleaning_rules.deid.rt_additional_privacy_suppression import RTAdditionalPrivacyConceptSuppression
 from cdr_cleaner.cleaning_rules.deid.rt_observation_privacy_suppression import RTObservationPrivacySuppression
@@ -374,8 +372,6 @@ CONTROLLED_TIER_DEID_CLEANING_CLASSES = [
     (IDFieldSuppression,),  # Should run after any data remapping
     (CancerConceptSuppression,),  # Should run after any data remapping rules
     (SectionParticipationConceptSuppression,),
-    (CTAdditionalPrivacyConceptSuppression,),
-    (CTObservationPrivacySuppression,),
     (CTNPHObservationPrivacySuppression,),  # Applies only to NPH data
     (
         StringFieldsSuppression,),
