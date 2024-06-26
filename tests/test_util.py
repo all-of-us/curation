1eebcdc8e524dbf512f49a1a55e6b5337286a7bd
Nishanth Pavinkurve <npp267@nyu.edu>
2024-06-26 19:54:32 -0400

    [DC-3780] Update split parquet files

--- tests/test_util.py
+++ tests/test_util.py
@@ -52,8 +52,10 @@ FIVE_PERSONS_PII_NAME_CSV = os.path.join(FIVE_PERSONS_PATH, 'pii_name.csv')
 FIVE_PERSONS_PARTICIPANT_MATCH_CSV = os.path.join(FIVE_PERSONS_PATH,
                                                   'participant_match.csv')
 FIVE_PERSONS_NOTE_JSONL = os.path.join(FIVE_PERSONS_PATH, 'note.jsonl')
-FIVE_PERSONS_SPECIMEN_PARQUET = os.path.join(FIVE_PERSONS_PATH,
-                                             'specimen.parquet')
+FIVE_PERSONS_SPECIMEN_PARQUET_1 = os.path.join(FIVE_PERSONS_PATH,
+                                               'specimen-part-001.parquet')
+FIVE_PERSONS_SPECIMEN_PARQUET_2 = os.path.join(FIVE_PERSONS_PATH,
+                                               'specimen-part-002.parquet')
 FIVE_PERSONS_PII_PHONE_NUMBER_PARQUET = os.path.join(
     FIVE_PERSONS_PATH, 'pii_phone_number.parquet')
 FIVE_PERSONS_FILES = [
@@ -62,7 +64,8 @@ FIVE_PERSONS_FILES = [
     FIVE_PERSONS_PROCEDURE_OCCURRENCE_CSV, FIVE_PERSONS_DRUG_EXPOSURE_CSV,
     FIVE_PERSONS_MEASUREMENT_CSV, FIVE_PERSONS_PII_NAME_CSV,
     FIVE_PERSONS_PARTICIPANT_MATCH_CSV, FIVE_PERSONS_NOTE_JSONL,
-    FIVE_PERSONS_SPECIMEN_PARQUET, FIVE_PERSONS_PII_PHONE_NUMBER_PARQUET
+    FIVE_PERSONS_SPECIMEN_PARQUET_1, FIVE_PERSONS_SPECIMEN_PARQUET_2,
+    FIVE_PERSONS_PII_PHONE_NUMBER_PARQUET
 ]
 
 FIVE_PERSONS_SUCCESS_RESULT_CSV = os.path.join(
