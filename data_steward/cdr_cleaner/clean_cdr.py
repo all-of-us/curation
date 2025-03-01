3b44aa18a1035b7c582df2fd527efc35528c4b48
Nishanth Pavinkurve <npp267@nyu.edu>
2025-02-28 19:10:02 -0500

    [DC-3866] Add CR to list

--- data_steward/cdr_cleaner/clean_cdr.py
+++ data_steward/cdr_cleaner/clean_cdr.py
@@ -30,6 +30,7 @@ from cdr_cleaner.cleaning_rules.drop_extreme_measurements import DropExtremeMeas
 from cdr_cleaner.cleaning_rules.drop_multiple_measurements import DropMultipleMeasurements
 from cdr_cleaner.cleaning_rules.drop_participants_without_any_basics import DropParticipantsWithoutAnyBasics
 from cdr_cleaner.cleaning_rules.clean_survey_conduct_recurring_surveys import CleanSurveyConductRecurringSurveys
+from cdr_cleaner.cleaning_rules.suppress_note_fields import SuppressNoteFields
 from cdr_cleaner.cleaning_rules.update_survey_source_concept_id import UpdateSurveySourceConceptId
 from cdr_cleaner.cleaning_rules.drop_unverified_survey_data import DropUnverifiedSurveyData
 from cdr_cleaner.cleaning_rules.drug_refills_days_supply import DrugRefillsDaysSupply
@@ -175,6 +176,7 @@ UNIONED_EHR_CLEANING_CLASSES = [
     (RemoveRecordsWithWrongDate,),
     (RemoveInvalidProcedureSourceRecords,),
     (CalculatePrimaryDeathRecord,),
+    (SuppressNoteFields,),
     (CleanMappingExtTables,),  # should be one of the last cleaning rules run
 ]
 
