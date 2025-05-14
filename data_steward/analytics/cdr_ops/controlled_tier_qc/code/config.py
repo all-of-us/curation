a802faf6f12aad0704fcd59b750aa9fe94a3816f
Nishanth Pavinkurve <npp267@nyu.edu>
2025-05-14 17:45:09 -0400

    [DC-3920] Update path [ci skip]

--- data_steward/analytics/cdr_ops/controlled_tier_qc/code/config.py
+++ data_steward/analytics/cdr_ops/controlled_tier_qc/code/config.py
@@ -4,6 +4,8 @@ current_path = Path(__file__)
 code_path = current_path.parent
 controlled_tier_qc_path = code_path.parent
 
+CSV_FOLDER = controlled_tier_qc_path / 'csv'
+SQL_FOLDER = controlled_tier_qc_path / 'sql'
 CHECK_LIST_CSV_FILE = "Controlled_Tier_Check_Description.csv"
 CONCEPT_CSV_FILE = "Controlled_Tier_Concept_Level.csv"
 FIELD_CSV_FILE = "Controlled_Tier_Field_Level.csv"
