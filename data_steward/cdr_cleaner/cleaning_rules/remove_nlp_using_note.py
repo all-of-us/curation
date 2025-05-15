34e93ea116b3426a6bda8f463abdf98e6aa0b737
Nishanth Pavinkurve <npp267@nyu.edu>
2025-05-15 16:30:24 -0400

    [DC-3867] Update yapf

--- data_steward/cdr_cleaner/cleaning_rules/remove_nlp_using_note.py
+++ data_steward/cdr_cleaner/cleaning_rules/remove_nlp_using_note.py
@@ -60,7 +60,15 @@ class RemoveNoteUsingNLP(BaseCleaningRule):
 
         super().__init__(issue_numbers=JIRA_ISSUE_NUMBERS,
                          description=desc,
-                         affected_datasets=[cdr_consts.COMBINED, cdr_consts.REGISTERED_TIER_DEID],
+                         affected_datasets=[
+                             cdr_consts.COMBINED,
+                             cdr_consts.REGISTERED_TIER_DEID,
+                             cdr_consts.REGISTERED_TIER_DEID_BASE,
+                             cdr_consts.REGISTERED_TIER_DEID_CLEAN,
+                             cdr_consts.CONTROLLED_TIER_DEID,
+                             cdr_consts.CONTROLLED_TIER_DEID_BASE,
+                             cdr_consts.CONTROLLED_TIER_DEID_CLEAN
+                         ],
                          affected_tables=[NOTE, NOTE_NLP],
                          project_id=project_id,
                          dataset_id=dataset_id,
