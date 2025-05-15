78398353fdccdc0e20a79d1f6fe30885ef296b41
Nishanth Pavinkurve <np2689@cumc.columbia.edu>
2025-05-15 18:00:01 -0400

    [DC-3924] Update string field suppression for note_nlp (#1891)
    * [DC-3924] Update string field exceptions
    
    * [DC-3924] Update yapf

--- data_steward/cdr_cleaner/cleaning_rules/deid/string_fields_suppression.py
+++ data_steward/cdr_cleaner/cleaning_rules/deid/string_fields_suppression.py
@@ -5,7 +5,7 @@ from google.api_core.exceptions import GoogleAPIError
 from google.cloud.exceptions import GoogleCloudError, NotFound
 
 import resources
-from common import AOU_DEATH, CDM_TABLES, OBSERVATION, JINJA_ENV
+from common import AOU_DEATH, CDM_TABLES, OBSERVATION, JINJA_ENV, NOTE_NLP
 from constants import bq_utils as bq_consts
 import constants.cdr_cleaner.clean_cdr as cdr_consts
 from cdr_cleaner.cleaning_rules.cancer_concept_suppression import CancerConceptSuppression
@@ -69,6 +69,9 @@ WHERE
     {%- if loop.nextitem is defined %} OR {% endif -%}
 {% endfor %}
 """)
+
+NLP_SYSTEM = 'nlp_system'
+TERM_EXISTS = 'term_exists'
 
 
 class SuppressionException(NamedTuple):
@@ -210,7 +213,22 @@ class StringFieldsSuppression(BaseCleaningRule):
                 sandbox_table=self.sandbox_table_for(OBSERVATION),
                 field_name=OBSERVATION_SOURCE_CONCEPT_ID,
                 field_value=APPROXIMATE_DATE_OF_SYMPTOMS,
-                restore_fields=[VALUE_AS_STRING])
+                restore_fields=[VALUE_AS_STRING]),
+            SuppressionException(domain_table=NOTE_NLP,
+                                 sandbox_table=self.sandbox_table_for(NOTE_NLP),
+                                 field_name=NLP_SYSTEM,
+                                 field_value="'CLAMP 1.7.6'",
+                                 restore_fields=[NLP_SYSTEM]),
+            SuppressionException(domain_table=NOTE_NLP,
+                                 sandbox_table=self.sandbox_table_for(NOTE_NLP),
+                                 field_name=TERM_EXISTS,
+                                 field_value="'true'",
+                                 restore_fields=[TERM_EXISTS]),
+            SuppressionException(domain_table=NOTE_NLP,
+                                 sandbox_table=self.sandbox_table_for(NOTE_NLP),
+                                 field_name=TERM_EXISTS,
+                                 field_value="'false'",
+                                 restore_fields=[TERM_EXISTS])
         ]
 
     def get_query_specs(self, *args, **keyword_args) -> query_spec_list:
