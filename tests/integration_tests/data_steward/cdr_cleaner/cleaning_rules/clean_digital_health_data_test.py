c62e24dc4403c433c8ee9fe2bc12456eb208279d
Nishanth Pavinkurve <npp267@nyu.edu>
2026-01-22 21:37:39 -0500

    [DC-3987] Fix fq

--- tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/clean_digital_health_data_test.py
+++ tests/integration_tests/data_steward/cdr_cleaner/cleaning_rules/clean_digital_health_data_test.py
@@ -52,8 +52,8 @@ class CleanDigitalHealthDataTest(BaseTest.CleaningRulesTestBase):
         cls.fq_sandbox_table_names = [
             f'{project_id}.{sandbox_id}.{table}' for table in sandbox_tables
         ] + [
-            cls.rule_instance.sandbox_table_for(
-                clean_dhd.DIGITAL_HEALTH_SHARING_STATUS)
+            f'{project_id}.{sandbox_id}.{cls.rule_instance.sandbox_table_for(
+                clean_dhd.DIGITAL_HEALTH_SHARING_STATUS)}'
         ]
 
         cls.fq_digital_health_table = f'{cls.project_id}.{cls.dataset_id}.{clean_dhd.DIGITAL_HEALTH_SHARING_STATUS}'
